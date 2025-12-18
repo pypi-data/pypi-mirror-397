f95e584578363d371fc244a44aa44ff5ca09fd5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 17:08:33 +0000

    Add package keplemon/2.0.2/keplemon-2.0.2-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/keplemon/keplemon-2.0.2-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/keplemon/saal/__init__.py
@@ -0,0 +1,10 @@
+from keplemon._keplemon.saal import (  # type: ignore
+    SensorInterface,
+    SAALSensor,
+    ObsInterface,
+    SAALObservation,
+    MainInterface,
+    TLEInterface,
+)
+
+__all__ = ["SensorInterface", "SAALSensor", "ObsInterface", "SAALObservation", "MainInterface", "TLEInterface"]
---
+++ packages/keplemon/keplemon-2.0.2-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/keplemon/saal/sgp4_prop_interface.py
@@ -0,0 +1,7 @@
+from keplemon._keplemon.saal.sgp4_prop_interface import (  # type: ignore
+    reepoch_tle,
+)
+
+__all__ = [
+    "reepoch_tle",
+]
