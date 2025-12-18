1a07b36ba2f5df828d6cf062e99ba73f048e6f33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 15:07:09 +0000

    Add package networkdiagram/1.2/networkdiagram-1.2.tar.gz

---
+++ packages/networkdiagram/networkdiagram-1.2.tar.gz/networkdiagram-1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='networkdiagram',
+    version='1.2',
+    description='A Python library for creating, calculating, and visualizing CPM/PERT Network Diagrams.',
+    author='Kathan Majithia',
+    author_email='kathanmajithia@gmail.com',
+    packages=find_packages(),
+    license='MIT',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "networkx",
+        "matplotlib"
+        ],
+)
