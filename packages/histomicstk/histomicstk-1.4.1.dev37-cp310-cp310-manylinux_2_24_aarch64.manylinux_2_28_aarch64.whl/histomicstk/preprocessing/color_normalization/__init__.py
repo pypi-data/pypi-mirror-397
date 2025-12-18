231bb71799da84370efd273ed811382edabe8a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 05:05:47 +0000

    Add package histomicstk/1.4.1.dev37/histomicstk-1.4.1.dev37-cp310-cp310-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/histomicstk/histomicstk-1.4.1.dev37-cp310-cp310-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl/histomicstk/preprocessing/color_normalization/__init__.py
@@ -0,0 +1,20 @@
+"""Functions to correct non-uniform staining issues."""
+
+# make functions available at the package level using shadow imports
+# since we mostly have one function per file
+from .background_intensity import background_intensity
+from .deconvolution_based_normalization import \
+    deconvolution_based_normalization
+from .reinhard import reinhard
+from .reinhard_stats import reinhard_stats, reinhard_stats_rgb
+
+# list out things that are available for public use
+__all__ = (
+
+    # functions and classes of this package
+    'background_intensity',
+    'reinhard',
+    'reinhard_stats',
+    'reinhard_stats_rgb',
+    'deconvolution_based_normalization',
+)
