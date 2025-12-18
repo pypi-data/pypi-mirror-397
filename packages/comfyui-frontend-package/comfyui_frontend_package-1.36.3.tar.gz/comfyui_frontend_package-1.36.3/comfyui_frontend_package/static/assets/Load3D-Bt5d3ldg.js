d46f36b4d67cb2dc60c8e6993bcbf70866e26292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 18:05:59 +0000

    Add package comfyui-frontend-package/1.36.3/comfyui_frontend_package-1.36.3.tar.gz

---
+++ packages/comfyui-frontend-package/comfyui_frontend_package-1.36.3.tar.gz/comfyui_frontend_package-1.36.3/comfyui_frontend_package/static/assets/Load3D-Bt5d3ldg.js
@@ -0,0 +1,2 @@
+import{_ as o}from"./Load3D.vue_vue_type_script_setup_true_lang-DA_7LzJL.js";import"./vendor-other-wAcxE2NF.js";import"./vendor-primevue-Co2cDqb_.js";import"./index-nlhHbsOC.js";import"./vendor-vue-eyHytYfh.js";import"./vendor-xterm-CWYFmgbN.js";import"./vendor-three-b7sMVVQK.js";import"./vendor-tiptap-DhuDS-V5.js";export{o as default};
+//# sourceMappingURL=Load3D-Bt5d3ldg.js.map
