7d4fb18a349d86267b6778888d8cbaea541ec826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 15:05:59 +0000

    Add package dsp-tools/18.4.0.post10/dsp_tools-18.4.0.post10.tar.gz

---
+++ packages/dsp-tools/dsp_tools-18.4.0.post10.tar.gz/dsp_tools-18.4.0.post10/src/dsp_tools/commands/create/communicate_problems.py
@@ -0,0 +1,24 @@
+from loguru import logger
+
+from dsp_tools.commands.create.models.create_problems import CollectedProblems
+from dsp_tools.commands.create.models.create_problems import CreateProblem
+from dsp_tools.setup.ansi_colors import BOLD_RED
+from dsp_tools.setup.ansi_colors import RED
+from dsp_tools.setup.ansi_colors import RESET_TO_DEFAULT
+
+
+def print_all_problem_collections(problems: list[CollectedProblems]) -> None:
+    for prob in problems:
+        print_problem_collection(prob)
+
+
+def print_problem_collection(problem_collection: CollectedProblems) -> None:
+    individual_problems = _create_individual_problem_strings(problem_collection.problems)
+    logger.error(problem_collection.header + individual_problems)
+    print(BOLD_RED, problem_collection.header, RESET_TO_DEFAULT)
+    print(RED, individual_problems, RESET_TO_DEFAULT)
+
+
+def _create_individual_problem_strings(problems: list[CreateProblem]) -> str:
+    str_list = [f"{p.problematic_object}: {p.problem!s}" for p in problems]
+    return "   - " + "\n    - ".join(str_list)
