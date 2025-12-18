5faab9cda8ad2404f83401ece6d682cae8f350e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 15:05:46 +0000

    Add package bella-companion/0.1.10/bella_companion-0.1.10.tar.gz

---
+++ packages/bella-companion/bella_companion-0.1.10.tar.gz/bella_companion-0.1.10/src/bella_companion/backend/type_hints.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+Array = np.typing.NDArray[np.float64]
+Weights = list[Array]
---
+++ packages/bella-companion/bella_companion-0.1.10.tar.gz/bella_companion-0.1.10/src/bella_companion/backend/utils/__init__.py
@@ -0,0 +1,40 @@
+from bella_companion.backend.utils.beast import (
+    ESS_POSTFIX,
+    LOWER_POSTFIX,
+    MEDIAN_POSTFIX,
+    UPPER_POSTFIX,
+    read_log_file,
+    read_weights,
+    read_weights_dir,
+    summarize_log,
+    summarize_logs_dir,
+)
+from bella_companion.backend.utils.metrics import (
+    avg_CI_width_from_summaries,
+    coverage_from_summaries,
+    mae_distribution_from_summaries,
+    mae_from_summaries,
+    mean_ess_per_hour_from_summaries,
+)
+from bella_companion.backend.utils.misc import normalize
+from bella_companion.backend.utils.slurm import get_job_metadata, submit_job
+
+__all__ = [
+    "ESS_POSTFIX",
+    "LOWER_POSTFIX",
+    "MEDIAN_POSTFIX",
+    "UPPER_POSTFIX",
+    "read_log_file",
+    "read_weights",
+    "read_weights_dir",
+    "summarize_log",
+    "summarize_logs_dir",
+    "avg_CI_width_from_summaries",
+    "coverage_from_summaries",
+    "mae_distribution_from_summaries",
+    "mae_from_summaries",
+    "mean_ess_per_hour_from_summaries",
+    "normalize",
+    "get_job_metadata",
+    "submit_job",
+]
