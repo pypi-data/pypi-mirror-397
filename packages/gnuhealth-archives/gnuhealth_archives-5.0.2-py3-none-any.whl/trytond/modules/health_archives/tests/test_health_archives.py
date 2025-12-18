506b5e06d09af6b30751082f5023a5a3212288bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:30:56 +0000

    Add package gnuhealth-archives/5.0.2/gnuhealth_archives-5.0.2-py3-none-any.whl

---
+++ packages/gnuhealth-archives/gnuhealth_archives-5.0.2-py3-none-any.whl/trytond/modules/health_archives/tests/test_health_archives.py
@@ -0,0 +1,33 @@
+# SPDX-FileCopyrightText: 2008-2025 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2025 GNU Solidario <health@gnusolidario.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+#########################################################################
+#   Hospital Management Information System (HMIS) component of the      #
+#                       GNU Health project                              #
+#                   https://www.gnuhealth.org                           #
+#########################################################################
+#                       HEALTH ARCHIVES package                         #
+#                test_health_archives.py unittest file                  #
+#########################################################################
+import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class HealthArchivesTestCase(ModuleTestCase):
+    '''
+    Test Health Archives module.
+    '''
+    module = 'health_archives'
+
+
+del ModuleTestCase
+
+
+def suite():
+    suite = trytond.tests.test_tryton.suite()
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        HealthArchivesTestCase))
+    return suite
