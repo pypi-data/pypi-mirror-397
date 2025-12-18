6d99bada589abd8862fce23d2a5bb9823a5af97c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:31:08 +0000

    Add package gnuhealth-reporting/5.0.2/gnuhealth_reporting-5.0.2-py3-none-any.whl

---
+++ packages/gnuhealth-reporting/gnuhealth_reporting-5.0.2-py3-none-any.whl/trytond/modules/health_reporting/report/__init__.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2008-2025 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2025 GNU Solidario <health@gnusolidario.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# -*- coding: utf-8 -*-
+
+from . import summary_report
+from . import epidemics_report
+
+
+__all__ = ['summary_report', 'epidemics_report']
---
+++ packages/gnuhealth-reporting/gnuhealth_reporting-5.0.2-py3-none-any.whl/trytond/modules/health_reporting/wizard/__init__.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2008-2025 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2014 Sebastian Marro <smarro@thymbra.com>
+# SPDX-FileCopyrightText: 2011-2025 GNU Solidario <health@gnusolidario.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from . import wizard_top_diseases
+from . import wizard_evaluations
+from . import wizard_summary_report
+from . import wizard_epidemics_report
+
+
+__all__ = ['wizard_top_diseases', 'wizard_evaluations',
+           'wizard_summary_report', 'wizard_epidemics_report']
