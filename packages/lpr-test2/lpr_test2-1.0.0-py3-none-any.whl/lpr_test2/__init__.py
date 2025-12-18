b087fb0fb1812ab6b777d14bfb4b22ac68963490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 08:06:48 +0000

    Add package lpr-test2/1.0.0/lpr_test2-1.0.0-py3-none-any.whl

---
+++ packages/lpr-test2/lpr_test2-1.0.0-py3-none-any.whl/lpr_test2/__init__.py
@@ -0,0 +1,3 @@
+from .test import test as lpr_test2
+
+name = "test"
---
+++ packages/lpr-test2/lpr_test2-1.0.0-py3-none-any.whl/lpr_test2/test.py
@@ -0,0 +1,9 @@
+import lpr_test1
+
+def test():
+    lpr_test1.lpr_test1()
+    print("This is a test function in pkg2/test/test.py")
+
+
+if __name__ == "__main__":
+    test()
