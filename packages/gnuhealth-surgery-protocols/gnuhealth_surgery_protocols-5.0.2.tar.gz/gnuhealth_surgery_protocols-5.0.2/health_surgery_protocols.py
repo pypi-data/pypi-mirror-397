2777b89ceac3339d729d622a8dd7342dcdc168f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 04:12:18 +0000

    Add package gnuhealth-surgery-protocols/5.0.2/gnuhealth_surgery_protocols-5.0.2.tar.gz

---
+++ packages/gnuhealth-surgery-protocols/gnuhealth_surgery_protocols-5.0.2.tar.gz/gnuhealth_surgery_protocols-5.0.2/health_surgery_protocols.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2011-2025 GNU Solidario <health@gnusolidario.org>
+# SPDX-FileCopyrightText: 2023 ITARA <info@itaraong.com>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#########################################################################
+#   Hospital Management Information System (HMIS) component of the      #
+#                       GNU Health project                              #
+#                   https://www.gnuhealth.org                           #
+#########################################################################
+#                  HEALTH  SURGERY PROTOCOLS PACKAGE                    #
+#              health_surgery_protocols.py: main module                 #
+#########################################################################
