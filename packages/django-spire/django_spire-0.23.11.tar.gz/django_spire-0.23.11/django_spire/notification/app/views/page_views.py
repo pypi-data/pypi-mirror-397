59465c927f82babf8a43c02deadc8df53c3db067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 08:06:38 +0000

    Add package django-spire/0.23.11/django_spire-0.23.11.tar.gz

---
+++ packages/django-spire/django_spire-0.23.11.tar.gz/django_spire-0.23.11/django_spire/notification/app/views/page_views.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from django.contrib.auth.decorators import login_required
+
+from django_spire.contrib.generic_views import portal_views
+
+from django_spire.notification.app.models import AppNotification
+
+if TYPE_CHECKING:
+    from django.core.handlers.wsgi import WSGIRequest
+    from django.template.response import TemplateResponse
+
+
+@login_required()
+def app_notification_list_view(request: WSGIRequest) -> TemplateResponse:
+    app_notification_list = (
+        AppNotification.objects.active()
+        .is_sent()
+        .annotate_is_viewed_by_user(request.user)
+        .select_related('notification')
+        .distinct()
+        .ordered_by_priority_and_sent_datetime()
+    )
+
+    return portal_views.list_view(
+        request,
+        context_data={'notification_list': app_notification_list},
+        model=AppNotification,
+        template='django_spire/notification/app/page/list_page.html'
+    )
