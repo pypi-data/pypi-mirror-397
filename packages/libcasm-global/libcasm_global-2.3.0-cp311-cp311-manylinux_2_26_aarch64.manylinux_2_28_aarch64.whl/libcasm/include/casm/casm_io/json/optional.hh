2f16cb8ab3168447d6980951e05448da3f1ddd46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 06:05:17 +0000

    Add package libcasm-global/2.3.0/libcasm_global-2.3.0-cp311-cp311-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/libcasm-global/libcasm_global-2.3.0-cp311-cp311-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl/libcasm/include/casm/casm_io/json/optional.hh
@@ -0,0 +1,52 @@
+#ifndef CASM_json_optional
+#define CASM_json_optional
+
+#include <optional>
+
+namespace CASM {
+
+template <typename T>
+struct jsonConstructor;
+class jsonParser;
+
+template <typename T, typename... Args>
+jsonParser &to_json(std::optional<T> const &value, jsonParser &json,
+                    Args &&...args) {
+  if (value.has_value()) {
+    to_json(value.value(), json, std::forward<Args>(args)...);
+  } else {
+    json.put_null();
+  }
+  return json;
+}
+
+template <typename T, typename... Args>
+void from_json(std::optional<T> &value, jsonParser const &json,
+               Args &&...args) {
+  if (json.is_null()) {
+    value.reset();
+  } else {
+    value = jsonConstructor<T>::from_json(json, std::forward<Args>(args)...);
+  }
+}
+
+/// Helper struct for constructing std::optional<T> objects
+template <typename T>
+struct jsonConstructor<std::optional<T>> {
+  /// \brief Default from_json is equivalent to \code
+  /// CASM::from_json<ReturnType>(json) \endcode
+  template <typename... Args>
+  static std::optional<T> from_json(jsonParser const &json, Args &&...args) {
+    if (json.is_null()) {
+      return std::nullopt;
+    } else {
+      std::optional<T> value =
+          jsonConstructor<T>::from_json(json, std::forward<Args>(args)...);
+      return value;
+    }
+  }
+};
+
+}  // namespace CASM
+
+#endif
