587af1f15d5f7bfd39b6805606001d56a261a10c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 06:05:22 +0000

    Add package plone.meta/2.3.1/plone_meta-2.3.1-py3-none-any.whl

---
+++ packages/plone.meta/plone_meta-2.3.1-py3-none-any.whl/plone/meta/shared/path.py
@@ -0,0 +1,39 @@
+import argparse
+import contextlib
+import os
+import pathlib
+
+
+@contextlib.contextmanager
+def change_dir(path):
+    """Change current working directory temporarily to `path`.
+
+    Yields the current working directory before the change.
+    """
+    cwd = os.getcwd()
+    try:
+        os.chdir(path)
+        yield cwd
+    finally:
+        os.chdir(cwd)
+
+
+def path_factory(parameter_name, *, has_extension=None, is_dir=False):
+    """Return factory creating pathlib.Path object if requirements are matched.
+
+    The factory raises an exception otherwise.
+    """
+
+    def factory(str):
+        path = pathlib.Path(str)
+        if not path.exists():
+            raise argparse.ArgumentTypeError(f"{str!r} does not exist!")
+        if has_extension is not None and path.suffix != has_extension:
+            raise argparse.ArgumentTypeError(
+                f"The required extension is {has_extension!r}" f" not {path.suffix!r}."
+            )
+        if is_dir and not path.is_dir():
+            raise argparse.ArgumentTypeError("has to point to a directory!")
+        return path
+
+    return factory
