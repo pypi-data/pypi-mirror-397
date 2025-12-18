0098cf0413dbff240f117a1cfd2d0b0edf498a8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 07:07:28 +0000

    Add package kumoai/2.14.0.dev202512151731/kumoai-2.14.0.dev202512151731-cp311-cp311-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/kumoai/kumoai-2.14.0.dev202512151731-cp311-cp311-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/kumoai/experimental/rfm/base/__init__.py
@@ -0,0 +1,25 @@
+from kumoapi.common import StrEnum
+
+
+class DataBackend(StrEnum):
+    LOCAL = 'local'
+    SQLITE = 'sqlite'
+    SNOWFLAKE = 'snowflake'
+
+
+from .source import SourceColumn, SourceForeignKey  # noqa: E402
+from .column import Column  # noqa: E402
+from .table import Table  # noqa: E402
+from .sampler import SamplerOutput, Sampler  # noqa: E402
+from .sql_sampler import SQLSampler  # noqa: E402
+
+__all__ = [
+    'DataBackend',
+    'SourceColumn',
+    'SourceForeignKey',
+    'Column',
+    'Table',
+    'SamplerOutput',
+    'Sampler',
+    'SQLSampler',
+]
