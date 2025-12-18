d24d9a19130b6d9612c2a08c5b913092edcf1692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 08:07:17 +0000

    Add package tatools01/3.0.6/tatools01-3.0.6-py3-none-any.whl

---
+++ packages/tatools01/tatools01-3.0.6-py3-none-any.whl/tatools01/__init__.py
@@ -0,0 +1,33 @@
+"""
+
+tatools01
+
+
+An python parametters library.
+"""
+
+
+__version__ = "3.0.6" # Nhớ update cả Readme.md
+
+
+__author__ = "Nguyễn Tuấn Anh - nt.anh.fai@gmail.com"
+
+__credits__ = "MIT License"
+__console__ = "tact"
+import os
+
+import sys
+
+import argparse
+
+import json
+ 
+   
+
+from . import ParamsBase
+
+from tatools01.Thoi_gian.taTimers import MultiTimer
+
+ 
+    
+
