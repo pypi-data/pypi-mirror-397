277a05ae0e244b144fecbb6f870290c6d9db116c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 12:05:45 +0000

    Add package aspire-bilby/0.1.0a3/aspire_bilby-0.1.0a3-py3-none-any.whl

---
+++ packages/aspire-bilby/aspire_bilby-0.1.0a3-py3-none-any.whl/aspire_bilby/__init__.py
@@ -0,0 +1,9 @@
+"""aspire-bilby: A Bilby wrapper for aspire."""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "unknown"
