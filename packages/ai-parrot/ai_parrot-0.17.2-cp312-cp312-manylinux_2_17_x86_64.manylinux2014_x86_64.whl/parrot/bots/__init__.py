2feabd50312e3f66555486480df564db52bb84a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 18:05:29 +0000

    Add package ai-parrot/0.17.2/ai_parrot-0.17.2-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ai-parrot/ai_parrot-0.17.2-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/parrot/bots/__init__.py
@@ -0,0 +1,12 @@
+
+from .abstract import AbstractBot
+from .basic import BasicBot
+from .chatbot import Chatbot
+from .agent import Agent, BasicAgent
+__all__ = (
+    "AbstractBot",
+    "BasicBot",
+    "Agent",
+    "BasicAgent",
+    "Chatbot",
+)
