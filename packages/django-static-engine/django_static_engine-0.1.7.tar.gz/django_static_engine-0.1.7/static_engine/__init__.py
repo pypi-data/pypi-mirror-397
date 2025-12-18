21739f440e94c9ae53e386db5634e296de2b59e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:29:48 +0000

    Add package django-static-engine/0.1.7/django_static_engine-0.1.7.tar.gz

---
+++ packages/django-static-engine/django_static_engine-0.1.7.tar.gz/django_static_engine-0.1.7/static_engine/__init__.py
@@ -0,0 +1,4 @@
+from django.conf import settings
+
+STATICSITE_USE_DIR_TEMPLATE = getattr(settings, 'STATICSITE_USE_DIR_TEMPLATE', False)
+STATICSITE_DIR = getattr(settings, 'STATICSITE_DIR', settings.BASE_DIR / 'src')
