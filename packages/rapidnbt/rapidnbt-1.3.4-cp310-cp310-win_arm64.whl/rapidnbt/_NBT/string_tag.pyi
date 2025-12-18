e9f5fe611934a74f469636d1c6c56a7c1c059c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:31:11 +0000

    Add package rapidnbt/1.3.4/rapidnbt-1.3.4-cp310-cp310-win_arm64.whl

---
+++ packages/rapidnbt/rapidnbt-1.3.4-cp310-cp310-win_arm64.whl/rapidnbt/_NBT/string_tag.pyi
@@ -0,0 +1,126 @@
+# Copyright © 2025 GlacieTeam.All rights reserved.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy
+# of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from collections.abc import Buffer
+from typing import overload
+from rapidnbt._NBT.tag import Tag
+from rapidnbt._NBT.tag_type import TagType
+
+class StringTag(Tag):
+    """
+    A tag contains a string
+    """
+
+    def __eq__(self, other: StringTag) -> bool:
+        """
+        Equality operator (==), case-sensitive comparison
+        """
+
+    def __getitem__(self, index: int) -> str:
+        """
+        Get character at specified position
+        """
+
+    def __hash__(self) -> int:
+        """
+        Compute hash value for Python hashing operations
+        """
+
+    @overload
+    def __init__(self) -> None:
+        """
+        Construct an empty StringTag
+        """
+
+    @overload
+    def __init__(self, str: str) -> None:
+        """
+        Construct from a Python string
+        """
+
+    @overload
+    def __init__(self, str: Buffer) -> None:
+        """
+        Construct from a Python bytes / bytearray
+        """
+
+    def __len__(self) -> int:
+        """
+        Get the length of the string in bytes
+        """
+
+    def __repr__(self) -> str:
+        """
+        Official representation with quoted content
+        """
+
+    def __setitem__(self, index: int, character: str) -> None:
+        """
+        Set character at specified position
+        """
+
+    def __str__(self) -> str:
+        """
+        String representation (SNBT minimized format)
+        """
+
+    def copy(self) -> Tag:
+        """
+        Create a deep copy of this tag
+        """
+
+    def equals(self, other: Tag) -> bool:
+        """
+        Check if this tag equals another tag (same content and type)
+        """
+
+    def get_type(self) -> TagType:
+        """
+        Get the NBT type ID (String)
+        """
+
+    def hash(self) -> int:
+        """
+        Compute hash value of this tag (based on string content)
+        """
+
+    def load(self, stream: ...) -> None:
+        """
+        Load tag value from a binary stream (UTF-8)
+        """
+
+    def size(self) -> int:
+        """
+        Get the length of the string in bytes
+        """
+
+    def write(self, stream: ...) -> None:
+        """
+        Write tag to a binary stream (UTF-8 encoded)
+        """
+
+    def get(self) -> str:
+        """
+        Get the string content of this tag
+        """
+
+    def set(self, value: str) -> None:
+        """
+        Set the string content of this tag
+        """
+
+    @property
+    def value(self) -> bytes:
+        """
+        Access the original bytes of this tag
+        """
+
+    @value.setter
+    def value(self, value: Buffer) -> None:
+        """
+        Access the original bytes of this tag
+        """
