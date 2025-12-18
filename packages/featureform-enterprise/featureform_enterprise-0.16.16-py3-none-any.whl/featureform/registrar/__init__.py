95134548dd0dcb8577b266e572229512dc9197e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 15:06:46 +0000

    Add package featureform-enterprise/0.16.16/featureform_enterprise-0.16.16-py3-none-any.whl

---
+++ packages/featureform-enterprise/featureform_enterprise-0.16.16-py3-none-any.whl/featureform/registrar/__init__.py
@@ -0,0 +1,82 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+"""
+Registrar module for Featureform.
+
+This module contains registrar classes that wrap registered resources and
+provide registration functionality.
+"""
+
+from .column_mapping import ColumnMapping
+from .column_resources import (
+    ColumnResource,
+    EmbeddingColumnResource,
+    FeatureColumnResource,
+    LabelColumnResource,
+)
+from .entity_decorator import entity
+from .feature_api import (
+    AggregateBuiltFeatures,
+    AggregateConfig,
+    AttributeBuiltFeature,
+    BackfillConfig,
+    BuiltFeatures,
+    Feature,
+    FeatureBuilder,
+    FeatureSourceType,
+    FeatureType,
+    SelectConfig,
+)
+from .registrar import ONE_DAY_TARGET_LAG, Incremental, Registrar
+from .registrars import (
+    ColumnSourceRegistrar,
+    EntityRegistrar,
+    ModelRegistrar,
+    ResourceRegistrar,
+    SourceRegistrar,
+    UserRegistrar,
+)
+from .transformation_decorators import (
+    DFTransformationDecorator,
+    SQLTransformationDecorator,
+    SubscriptableTransformation,
+)
+from .variants import Variants
+
+__all__ = [
+    "Registrar",
+    "Incremental",
+    "ONE_DAY_TARGET_LAG",
+    "EntityRegistrar",
+    "UserRegistrar",
+    "SourceRegistrar",
+    "ColumnSourceRegistrar",
+    "ResourceRegistrar",
+    "ModelRegistrar",
+    "ColumnMapping",
+    "Variants",
+    "SubscriptableTransformation",
+    "SQLTransformationDecorator",
+    "DFTransformationDecorator",
+    "ColumnResource",
+    "FeatureColumnResource",
+    "LabelColumnResource",
+    "EmbeddingColumnResource",
+    "entity",
+    # Feature API v2
+    "Feature",
+    "FeatureBuilder",
+    "FeatureSourceType",
+    "FeatureType",
+    "SelectConfig",
+    "AggregateConfig",
+    "BackfillConfig",
+    "BuiltFeatures",
+    "AggregateBuiltFeatures",
+    "AttributeBuiltFeature",
+]
+
+# Create global registrar instance
+global_registrar = Registrar()
