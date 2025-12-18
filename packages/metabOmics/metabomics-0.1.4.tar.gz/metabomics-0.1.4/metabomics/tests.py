216dc41bbfa7b16c2194795d8d01b2e0b10f4be7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 06:04:51 +0000

    Add package metabOmics/0.1.4/metabomics-0.1.4.tar.gz

---
+++ packages/metabOmics/metabomics-0.1.4.tar.gz/metabomics-0.1.4/metabomics/tests.py
@@ -0,0 +1,9 @@
+import unittest
+
+from metabolitics.analysis.tests import *
+from metabolitics.extensions.tests import *
+from metabolitics.preprocessing.tests import *
+from metabolitics.utils.tests import *
+
+if __name__ == '__main__':
+    unittest.main()
