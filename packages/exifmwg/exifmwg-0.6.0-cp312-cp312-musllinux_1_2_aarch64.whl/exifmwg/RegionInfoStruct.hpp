8657b0bcc8c694a0d47ac06ba16d47396b783dfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 06:04:57 +0000

    Add package exifmwg/0.6.0/exifmwg-0.6.0-cp312-cp312-musllinux_1_2_aarch64.whl

---
+++ packages/exifmwg/exifmwg-0.6.0-cp312-cp312-musllinux_1_2_aarch64.whl/exifmwg/RegionInfoStruct.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <optional>
+#include <string>
+
+#include <exiv2/exiv2.hpp>
+
+#include "DimensionsStruct.hpp"
+#include "PythonBindable.hpp"
+#include "XmpAreaStruct.hpp"
+#include "XmpSerializable.hpp"
+
+class RegionInfoStruct {
+public:
+  class RegionStruct {
+  public:
+    XmpAreaStruct Area;
+    std::string Name;
+    std::string Type;
+    std::optional<std::string> Description;
+
+    RegionStruct(XmpAreaStruct area, std::string name, std::string type, std::optional<std::string> description);
+
+    // XMP serialization
+    static RegionStruct fromXmp(const Exiv2::XmpData& xmpData, const std::string& baseKey);
+    void toXmp(Exiv2::XmpData& xmpData, const std::string& itemPath) const;
+
+    // Python bindable
+    std::string to_string() const;
+
+    friend bool operator==(const RegionStruct& lhs, const RegionStruct& rhs) = default;
+  };
+
+  DimensionsStruct AppliedToDimensions;
+  std::vector<RegionStruct> RegionList;
+
+  RegionInfoStruct(DimensionsStruct appliedToDimensions, const std::vector<RegionStruct>& regionList);
+
+  // XMP serialization
+  static RegionInfoStruct fromXmp(const Exiv2::XmpData& xmpData);
+  void toXmp(Exiv2::XmpData& xmpData) const;
+
+  // Python bindable
+  std::string to_string() const;
+
+  friend bool operator==(const RegionInfoStruct& lhs, const RegionInfoStruct& rhs) = default;
+};
+
+static_assert(std::copy_constructible<RegionInfoStruct::RegionStruct>);
+static_assert(std::equality_comparable<RegionInfoStruct::RegionStruct>);
+static_assert(XmpSerializableWithKey<RegionInfoStruct::RegionStruct>);
+
+static_assert(std::copy_constructible<RegionInfoStruct>);
+static_assert(std::equality_comparable<RegionInfoStruct>);
+static_assert(XmpSerializable<RegionInfoStruct>);
