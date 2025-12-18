68f2c1643beaba8ea6bac4e5d8e2655a20f79c2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 09:07:35 +0000

    Add package teradataml/20.0.0.9/teradataml-20.0.0.9-py3-none-any.whl

---
+++ packages/teradataml/teradataml-20.0.0.9-py3-none-any.whl/teradataml/_version.py
@@ -0,0 +1,11 @@
+# ##################################################################
+# 
+# Copyright 2025 Teradata. All rights reserved.
+# TERADATA CONFIDENTIAL AND TRADE SECRET
+# 
+# Primary Owner: Pankaj Purandare (PankajVinod.Purandare@teradata.com)
+# Secondary Owner: 
+# 
+# ################################################################## 
+
+version = "20.00.00.09"
