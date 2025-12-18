c34b80d8a7526ad57cf09e0189a91e28c7fb6a49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 14:07:30 +0000

    Add package npap/0.0.1/npap-0.0.1-py3-none-any.whl

---
+++ packages/npap/npap-0.0.1-py3-none-any.whl/npap/__init__.py
@@ -0,0 +1,7 @@
+"""
+Network Partitioning & Aggregation Package (NPAP)
+
+A Python library for partitioning and aggregation of spatial network graph-based data with focus on electrical
+power systems. The system operates on NetworkX graphs and implements a strategy pattern throughout, enabling
+extensibility for new partitioning algorithms, aggregation methods, and physical constraint handling.
+"""
