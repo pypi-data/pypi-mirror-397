9aed95661438eb9ddd2ca6255f4da3767496d7ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:31:04 +0000

    Add package gnuhealth-ntd/5.0.1/gnuhealth_ntd-5.0.1-py3-none-any.whl

---
+++ packages/gnuhealth-ntd/gnuhealth_ntd-5.0.1-py3-none-any.whl/trytond/modules/health_ntd/health_ntd.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# SPDX-FileCopyrightText: 2008-2025 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2025 GNU Solidario <health@gnusolidario.org>
+
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+#########################################################################
+#   Hospital Management Information System (HMIS) component of the      #
+#                       GNU Health project                              #
+#                   https://www.gnuhealth.org                           #
+#########################################################################
+#                     HEALTH NTD package                                #
+#                health_ntd.py: main module                             #
+#########################################################################
