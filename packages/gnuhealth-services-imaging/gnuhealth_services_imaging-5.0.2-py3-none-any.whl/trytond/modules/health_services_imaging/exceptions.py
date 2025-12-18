9d6e2154a0ccaa1d5290dc41aec1d22157f09f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:31:08 +0000

    Add package gnuhealth-services-imaging/5.0.2/gnuhealth_services_imaging-5.0.2-py3-none-any.whl

---
+++ packages/gnuhealth-services-imaging/gnuhealth_services_imaging-5.0.2-py3-none-any.whl/trytond/modules/health_services_imaging/exceptions.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2008-2025 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2025 GNU Solidario <health@gnusolidario.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# This file is part of GNU Health.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+from trytond.exceptions import UserError
+
+
+class NoServiceAssociated(UserError):
+    pass
+
+
+class ServiceHasBeenUpdated(UserError):
+    pass
