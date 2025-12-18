b6843d6afab8b58ce0a0b3eaa80e6b4d7299f65a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 13:17:42 +0000

    Add package pySigma-backend-quickwit/0.1.4/pysigma_backend_quickwit-0.1.4-py3-none-any.whl

---
+++ packages/pySigma-backend-quickwit/pysigma_backend_quickwit-0.1.4-py3-none-any.whl/sigma/backends/quickwit/__init__.py
@@ -0,0 +1,5 @@
+from .quickwit import QuickwitBackend
+
+backends = {
+    "quickwit": QuickwitBackend,
+}
---
+++ packages/pySigma-backend-quickwit/pysigma_backend_quickwit-0.1.4-py3-none-any.whl/sigma/pipelines/quickwit/__init__.py
@@ -0,0 +1,5 @@
+from .quickwit import quickwit_windows_pipeline
+
+pipelines = {
+    "quickwit_windows_pipeline": quickwit_windows_pipeline,
+}
