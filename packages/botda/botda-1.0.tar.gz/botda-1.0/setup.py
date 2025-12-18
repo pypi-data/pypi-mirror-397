8bbabacbbb0fe6b1f9e03c004b5742b79056000e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 15:06:44 +0000

    Add package botda/1.0/botda-1.0.tar.gz

---
+++ packages/botda/botda-1.0.tar.gz/botda-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='botda',
+    version='1.0',
+    packages = ['botda', 'botda_admin'],
+    install_requires=['netifaces'],
+    python_requires=">=3.3",
+    author="David_Yudin",
+    author_email="dyudin1204@gmail.com"
+)
