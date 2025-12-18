3574b46c4a93f217287f7cc2c67eac3f7d055ac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 14:06:49 +0000

    Add package LatentSemanticAnalyzer/0.2.1/latentsemanticanalyzer-0.2.1-py3-none-any.whl

---
+++ packages/LatentSemanticAnalyzer/latentsemanticanalyzer-0.2.1-py3-none-any.whl/LatentSemanticAnalyzer/__init__.py
@@ -0,0 +1,5 @@
+"""Latent Semantic Analysis (LSA) package for creating LSA objects and computational workflows with them."""
+from LatentSemanticAnalyzer.LatentSemanticAnalyzer import LatentSemanticAnalyzer
+from LatentSemanticAnalyzer.DocumentTermMatrixConstruction import to_bag_of_words
+from LatentSemanticAnalyzer.DocumentTermMatrixConstruction import document_term_matrix
+from LatentSemanticAnalyzer.DataLoaders import load_abstracts_data_frame
