45cb17be3a6e3c40477b3e84439832ede795f149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 13:18:21 +0000

    Add package cyreal/0.1.5/cyreal-0.1.5-py3-none-any.whl

---
+++ packages/cyreal/cyreal-0.1.5-py3-none-any.whl/cyreal/datasets/__init__.py
@@ -0,0 +1,26 @@
+"""Dataset helpers bundled with cyreal."""
+from __future__ import annotations
+
+from .cifar10 import CIFAR10Dataset
+from .cifar100 import CIFAR100Dataset
+from .daily_min_temperatures import DailyMinTemperaturesDataset
+from .emnist import EMNISTDataset, EMNIST_URLS
+from .fashion_mnist import FashionMNISTDataset, FASHION_MNIST_URLS
+from .kmnist import KMNISTDataset, KMNIST_URLS
+from .mnist import MNISTDataset, MNIST_URLS
+from .sunspots import SunspotsDataset
+
+__all__ = [
+    "CIFAR10Dataset",
+    "CIFAR100Dataset",
+    "DailyMinTemperaturesDataset",
+    "EMNISTDataset",
+    "EMNIST_URLS",
+    "FashionMNISTDataset",
+    "FASHION_MNIST_URLS",
+    "KMNISTDataset",
+    "KMNIST_URLS",
+    "MNISTDataset",
+    "MNIST_URLS",
+    "SunspotsDataset",
+]
