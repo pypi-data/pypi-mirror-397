2fba6beb2db7df742e8ac16590e07eb33f788bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 12:05:36 +0000

    Add package sqltypes/0.1.7/sqltypes-0.1.7-py3-none-any.whl

---
+++ packages/sqltypes/sqltypes-0.1.7-py3-none-any.whl/sqltypes/__init__.pyi
@@ -0,0 +1,9 @@
+from .meta import CustomTypeMeta
+from .strings import SpaceDelimitedList, CustomStringMeta, ValidatedStr
+from .jsons import ValidatedJSON
+
+__all__ = [
+  'CustomTypeMeta',
+  'CustomStringMeta', 'SpaceDelimitedList',
+  'ValidatedJSON', 'ValidatedStr',
+]
