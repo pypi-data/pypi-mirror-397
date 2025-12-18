a7c850e8c843dc4bfe6c323872194a98be1e8e39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 19:07:04 +0000

    Add package yolov5-face/0.0.1/yolov5_face-0.0.1.tar.gz

---
+++ packages/yolov5-face/yolov5_face-0.0.1.tar.gz/yolov5_face-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="yolov5-face",
+    version="0.0.1-alpha.1",
+    description="Real-time face detection using YOLOv5 - Forked Version",
+    license="GPL-3.0",
+    packages=find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
