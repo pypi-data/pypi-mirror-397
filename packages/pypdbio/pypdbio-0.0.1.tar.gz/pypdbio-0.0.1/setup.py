83d20349ff936c7c31fa15e86ab0fcd62493ac29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 07:07:22 +0000

    Add package pypdbio/0.0.1/pypdbio-0.0.1.tar.gz

---
+++ packages/pypdbio/pypdbio-0.0.1.tar.gz/pypdbio-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pypdbio',
+    version='0.0.1',
+    py_modules=['pypdbio'],
+    requires=['requests'],
+    install_requires=['requests'],
+    url='',
+    license='',
+    author='Xiaoyang Liu',
+    author_email='',
+    description='A package for read/write pdb file'
+)
