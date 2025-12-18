b5b690c8916709a9638bdcd63f9753f4342a815f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 16:06:31 +0000

    Add package prometheus-pve-exporter/3.7.0/prometheus_pve_exporter-3.7.0-py3-none-any.whl

---
+++ packages/prometheus-pve-exporter/prometheus_pve_exporter-3.7.0-py3-none-any.whl/pve_exporter/cli.py
@@ -0,0 +1,119 @@
+"""
+Proxmox VE exporter for the Prometheus monitoring system.
+"""
+
+from argparse import ArgumentParser, BooleanOptionalAction
+import os
+import pathlib
+import yaml
+from pve_exporter import scrape_metrics
+from pve_exporter.http import start_http_server
+from pve_exporter.config import config_from_yaml
+from pve_exporter.config import config_from_env
+from pve_exporter.collector import CollectorsOptions
+
+
+def main():
+    """
+    Main entry point.
+    """
+
+    parser = ArgumentParser()
+    clusterflags = parser.add_argument_group('cluster collectors', description=(
+        'cluster collectors are run if the url parameter cluster=1 is set and '
+        'skipped if the url parameter cluster=0 is set on a scrape url.'
+    ))
+    clusterflags.add_argument('--collector.status', dest='collector_status',
+                              action=BooleanOptionalAction, default=True,
+                              help='Exposes Node/VM/CT-Status')
+    clusterflags.add_argument('--collector.version', dest='collector_version',
+                              action=BooleanOptionalAction, default=True,
+                              help='Exposes PVE version info')
+    clusterflags.add_argument('--collector.node', dest='collector_node',
+                              action=BooleanOptionalAction, default=True,
+                              help='Exposes PVE node info')
+    clusterflags.add_argument('--collector.cluster', dest='collector_cluster',
+                              action=BooleanOptionalAction, default=True,
+                              help='Exposes PVE cluster info')
+    clusterflags.add_argument('--collector.resources', dest='collector_resources',
+                              action=BooleanOptionalAction, default=True,
+                              help='Exposes PVE resources info')
+    clusterflags.add_argument('--collector.backup-info', dest='collector_backup_info',
+                              action=BooleanOptionalAction, default=True,
+                              help=('Exposes information about guests which are not '
+                                    'covered by any backup job'))
+
+    nodeflags = parser.add_argument_group('node collectors', description=(
+        'node collectors are run if the url parameter node=1 is set and '
+        'skipped if the url parameter node=0 is set on a scrape url.'
+    ))
+    nodeflags.add_argument('--collector.config', dest='collector_config',
+                           action=BooleanOptionalAction, default=True,
+                           help='Exposes PVE onboot status')
+
+    nodeflags.add_argument('--collector.replication', dest='collector_replication',
+                           action=BooleanOptionalAction, default=True,
+                           help='Exposes PVE replication info')
+    nodeflags.add_argument('--collector.subscription', dest='collector_subscription',
+                              action=BooleanOptionalAction, default=True,
+                              help='Exposes PVE subscription info')
+
+    scrapeflags = parser.add_argument_group('scrape collectors', description=(
+        'metrics concerning the operation of the Prometheus PVE exporter itself.'
+    ))
+    scrapeflags.add_argument('--collector.pve-api-metrics', dest='api_metrics_enabled',
+                              action=BooleanOptionalAction, default=False,
+                              help='Exposes duration of PVE API calls')
+    scrapeflags.add_argument('--collector.target-metrics', dest='target_metrics_enabled',
+                              action=BooleanOptionalAction, default=False,
+                              help='Exposes duration of scrapes by target')
+
+    parser.add_argument('--config.file', type=pathlib.Path,
+                        dest="config_file", default='/etc/prometheus/pve.yml',
+                        help='Path to config file (/etc/prometheus/pve.yml)')
+
+    parser.add_argument('--web.listen-address',
+                        dest="web_listen_address", default='[::]:9221',
+                        help=(
+                            'Address on which to expose metrics and web server. '
+                            '([::]:9221)'
+                        ))
+    parser.add_argument('--server.keyfile', dest='server_keyfile',
+                        help='SSL key for server')
+    parser.add_argument('--server.certfile', dest='server_certfile',
+                        help='SSL certificate for server')
+
+    params = parser.parse_args()
+
+    collectors = CollectorsOptions(
+        status=params.collector_status,
+        version=params.collector_version,
+        subscription=params.collector_subscription,
+        node=params.collector_node,
+        cluster=params.collector_cluster,
+        resources=params.collector_resources,
+        backup_info=params.collector_backup_info,
+        config=params.collector_config,
+        replication=params.collector_replication
+    )
+    scrape_metrics.API_METRICS_ENABLED = params.api_metrics_enabled
+    scrape_metrics.TARGET_METRICS_ENABLED = params.target_metrics_enabled
+
+    # Load configuration.
+    if 'PVE_USER' in os.environ:
+        config = config_from_env(os.environ)
+    else:
+        with open(params.config_file, encoding='utf-8') as handle:
+            config = config_from_yaml(yaml.safe_load(handle))
+
+    gunicorn_options = {
+        'bind': f'{params.web_listen_address}',
+        'threads': 2,
+        'keyfile': params.server_keyfile,
+        'certfile': params.server_certfile,
+    }
+
+    if config.valid:
+        start_http_server(config, gunicorn_options, collectors)
+    else:
+        parser.error(str(config))
---
+++ packages/prometheus-pve-exporter/prometheus_pve_exporter-3.7.0-py3-none-any.whl/pve_exporter/http.py
@@ -0,0 +1,169 @@
+"""
+HTTP API for Proxmox VE prometheus collector.
+"""
+
+import logging
+import time
+from functools import partial
+
+import gunicorn.app.base
+from prometheus_client import CONTENT_TYPE_LATEST, Summary, Counter, generate_latest
+from werkzeug.routing import Map, Rule
+from werkzeug.wrappers import Request, Response
+from werkzeug.exceptions import InternalServerError
+from pve_exporter.collector import collect_pve
+
+
+class PveExporterApplication:
+    """
+    Proxmox VE prometheus collector HTTP handler.
+    """
+
+    def __init__(self, config, collectors, logger):
+        self._config = config
+        self._collectors = collectors
+        self._log = logger
+
+        self._duration = Summary(
+            'pve_collection_duration_seconds',
+            'Duration of collections by the PVE exporter',
+            ['module'],
+        )
+        self._errors = Counter(
+            'pve_request_errors_total',
+            'Errors in requests to PVE exporter',
+            ['module'],
+        )
+
+        # Initialize metrics.
+        for module in config.keys():
+            # pylint: disable=no-member
+            self._errors.labels(module)
+            # pylint: disable=no-member
+            self._duration.labels(module)
+
+
+    def on_pve(self, module='default', target='localhost', cluster='1', node='1'):
+        """
+        Request handler for /pve route
+        """
+
+        if module in self._config:
+            start = time.time()
+            output = collect_pve(
+                self._config[module],
+                target,
+                cluster.lower() not in ['false', '0', ''],
+                node.lower() not in ['false', '0', ''],
+                self._collectors
+            )
+            response = Response(output)
+            response.headers['content-type'] = CONTENT_TYPE_LATEST
+            self._duration.labels(module).observe(time.time() - start)
+        else:
+            response = Response("Module '{module}' not found in config")
+            response.status_code = 400
+
+        return response
+
+    def on_metrics(self):
+        """
+        Request handler for /metrics route
+        """
+
+        response = Response(generate_latest())
+        response.headers['content-type'] = CONTENT_TYPE_LATEST
+
+        return response
+
+    def on_index(self):
+        """
+        Request handler for index route (/).
+        """
+
+        response = Response(
+            """<html>
+            <head><title>Proxmox VE Exporter</title></head>
+            <body>
+            <h1>Proxmox VE Exporter</h1>
+            <p>Visit <code>/pve?target=1.2.3.4</code> to use.</p>
+            </body>
+            </html>"""
+        )
+        response.headers['content-type'] = 'text/html'
+
+        return response
+
+    def view(self, endpoint, values, args):
+        """
+        Werkzeug views mapping method.
+        """
+
+        allowed_args = {
+            'pve': ['module', 'target', 'cluster', 'node']
+        }
+
+        view_registry = {
+            'index': self.on_index,
+            'metrics': self.on_metrics,
+            'pve': self.on_pve,
+        }
+
+        params = dict(values)
+        if endpoint in allowed_args:
+            params.update({key: args[key] for key in allowed_args[endpoint] if key in args})
+
+        try:
+            return view_registry[endpoint](**params)
+        except Exception as error:  # pylint: disable=broad-except
+            self._log.exception("Exception thrown while rendering view")
+            self._errors.labels(args.get('module', 'default')).inc()
+            raise InternalServerError from error
+
+    @Request.application
+    def __call__(self, request):
+        url_map = Map([
+            Rule('/', endpoint='index'),
+            Rule('/metrics', endpoint='metrics'),
+            Rule('/pve', endpoint='pve'),
+        ])
+
+        urls = url_map.bind_to_environ(request.environ)
+        view_func = partial(self.view, args=request.args)
+        return urls.dispatch(view_func, catch_http_exceptions=True)
+
+
+class StandaloneGunicornApplication(gunicorn.app.base.BaseApplication):
+    """
+    Copy-paste from https://docs.gunicorn.org/en/stable/custom.html
+    """
+
+    # 'init' and 'load' methods are implemented by WSGIApplication.
+    # pylint: disable=abstract-method
+
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        config = {key: value for key, value in self.options.items()
+                  if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
+
+
+def start_http_server(config, gunicorn_options, collectors):
+    """
+    Start a HTTP API server for Proxmox VE prometheus collector.
+    """
+
+    # If running under gunicorn, reuse the gunicorn.error logger for the
+    # exporter application.
+    # https://trstringer.com/logging-flask-gunicorn-the-manageable-way/
+    logger = logging.getLogger('gunicorn.error')
+    app = PveExporterApplication(config, collectors, logger)
+    StandaloneGunicornApplication(app, gunicorn_options).run()
