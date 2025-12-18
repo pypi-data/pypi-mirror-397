71e5bf55546bb558c4ffff6c596d40ed10aa0981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 19:07:03 +0000

    Add package forecasting-tools/0.2.79/forecasting_tools-0.2.79-py3-none-any.whl

---
+++ packages/forecasting-tools/forecasting_tools-0.2.79-py3-none-any.whl/forecasting_tools/forecast_bots/template_bot.py
@@ -0,0 +1,14 @@
+from forecasting_tools.forecast_bots.official_bots.fall_research_only_bot import (
+    FallResearchOnlyBot2025,
+)
+from forecasting_tools.forecast_bots.official_bots.fall_template_bot import (
+    FallTemplateBot2025,
+)
+
+
+class TemplateBot(FallTemplateBot2025):
+    pass
+
+
+class ResearchOnlyBot(FallResearchOnlyBot2025):
+    pass
