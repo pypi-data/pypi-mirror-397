3e7ff2496fbc78cf38d008d4735e8297605f2f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:31:03 +0000

    Add package gnuhealth-mdg6/5.0.2/gnuhealth_mdg6-5.0.2.tar.gz

---
+++ packages/gnuhealth-mdg6/gnuhealth_mdg6-5.0.2.tar.gz/gnuhealth_mdg6-5.0.2/tests/test_health_mdg6.py
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: 2008-2025 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2025 GNU Solidario <health@gnusolidario.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class HealthMDG6TestCase(ModuleTestCase):
+    '''
+    Test Health MDG6 module.
+    '''
+    module = 'health_mdg6'
+
+
+del ModuleTestCase
+
+
+def suite():
+    suite = trytond.tests.test_tryton.suite()
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        HealthMDG6TestCase))
+    return suite
