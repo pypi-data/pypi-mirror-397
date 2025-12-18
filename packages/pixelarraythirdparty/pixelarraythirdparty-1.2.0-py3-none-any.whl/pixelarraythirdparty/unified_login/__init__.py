a70f8e15fbbab00b149b7e76674e8e3868222ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 20:06:03 +0000

    Add package pixelarraythirdparty/1.2.0/pixelarraythirdparty-1.2.0-py3-none-any.whl

---
+++ packages/pixelarraythirdparty/pixelarraythirdparty-1.2.0-py3-none-any.whl/pixelarraythirdparty/unified_login/__init__.py
@@ -0,0 +1,20 @@
+from .unified_login import (
+    GoogleLogin,
+    WechatLogin,
+    GitHubLogin,
+    DouyinLogin,
+    GitLabLogin,
+    SMSLogin,
+    EmailLogin,
+)
+
+__all__ = [
+    "GoogleLogin",
+    "WechatLogin",
+    "GitHubLogin",
+    "DouyinLogin",
+    "GitLabLogin",
+    "SMSLogin",
+    "EmailLogin",
+]
+
