1cde3cbc10b89efb9f310ef00d0ba9ee55a032e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 06:04:53 +0000

    Add package civic-line-cli/0.8/civic_line_cli-0.8.tar.gz

---
+++ packages/civic-line-cli/civic_line_cli-0.8.tar.gz/civic_line_cli-0.8/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages 
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="civic_line_cli",
+    version="0.8",  
+    author="Hemit Vimal Patel",
+    author_email="hemit@nextvoters.com",
+    description="Dead simple email sending for developers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Important!
+    packages=find_packages(),
+    install_requires=[
+        "openai",
+        "psycopg2-binary",
+        "requests",
+        "beautifulsoup4",  
+        "python-docx"
+    ],
+    entry_points={
+        "console_scripts": [
+            "civicline = civic_line_cli:cli"
+        ]
+    }
+)
