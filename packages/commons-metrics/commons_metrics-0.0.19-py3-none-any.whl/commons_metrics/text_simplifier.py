63cf6dee07f94d6ce70448f30a30ed0f976d35b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 18:05:26 +0000

    Add package commons-metrics/0.0.19/commons_metrics-0.0.19-py3-none-any.whl

---
+++ packages/commons-metrics/commons_metrics-0.0.19-py3-none-any.whl/commons_metrics/text_simplifier.py
@@ -0,0 +1,48 @@
+import html
+import json
+import re
+
+
+def compact_data(data: dict) -> str:
+    """
+    Removes keys with empty values (None, "", [], {}) from a dictionary
+    and returns a compact JSON string without unnecessary spaces.
+
+    Args:
+        data(dict): Original dictionary.
+    Returns:
+        str: Compact JSON representation of the filtered dictionary.
+    """
+    data = {k: v for k, v in data.items() if v not in (None, "", [], {})}
+    return json.dumps(data, separators=(",", ":"))
+
+def clear_text(text: str) -> str:
+    """
+    Cleans text by removing HTML tags, Markdown links,
+    decoding HTML characters, removing escaped quotes, and
+    normalizing spaces.
+
+    Args:
+        text (str): Original text.
+    Returns:
+        str: Cleaned and simplified text.
+    """
+    if not text:
+        return ""
+
+    # Remove HTML tags
+    text = re.sub(r'<[^>]+>', '', text)
+
+    # Replace Markdown links [Text](URLxt
+    text = re.sub(r'\[([^\]]+)\]\([^)]+\)', r'\1', text)
+
+    # Decode HTML characters (&nbsp;, &lt;, etc.)
+    text = html.unescape(text)
+
+    # Remove escaped quotes
+    text = text.replace('\\"', '"').replace("\\'", "'")
+
+    # Remove line breaks, tabs, and multiple spaces
+    text = re.sub(r'\s+', ' ', text).strip()
+
+    return text
