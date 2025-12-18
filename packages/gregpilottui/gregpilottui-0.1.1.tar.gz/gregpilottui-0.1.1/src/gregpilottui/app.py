8531481fe82e2caf6ddfd05d3f1bd768a2c56939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 19:06:42 +0000

    Add package gregpilottui/0.1.1/gregpilottui-0.1.1.tar.gz

---
+++ packages/gregpilottui/gregpilottui-0.1.1.tar.gz/gregpilottui-0.1.1/src/gregpilottui/app.py
@@ -0,0 +1,59 @@
+from os import makedirs, path
+from typing import Iterable
+from textual import work
+from textual.app import App, ComposeResult, SystemCommand
+from textual.screen import Screen
+from textual.widgets import Footer, Header, TabbedContent, TabPane, Button
+from .invtable import ItemTable, FluidTable, EssentiaTable
+from platformdirs import user_config_dir
+from configparser import ConfigParser
+from .initpopup import InitialConfigScreen
+
+class PrimaryScreen(Screen):
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
+        
+        with TabbedContent():
+            with TabPane("Overview", id="overview"):
+                yield Button.success()
+            with TabPane("Items", id="items"):
+                yield ItemTable()
+            with TabPane("Fluids", id="fluids"):
+                yield FluidTable()
+            with TabPane("Essentia", id="essentia"):
+                yield EssentiaTable()
+
+class GregPilotTUI(App):
+    CSS_PATH = "gp.tcss"
+
+    def __init__(self):
+        super().__init__()
+
+    MODES = {
+        "PrimaryMode": PrimaryScreen
+    }
+
+    @work
+    async def on_mount(self):
+        cfgdir = user_config_dir(appname = "GregPilotTUI")
+        if not path.exists(cfgdir):
+            makedirs(user_config_dir(appname = "GregPilotTUI"))
+            
+        cfgfile = cfgdir + "/" + "config.ini"
+        config = ConfigParser()
+        config.read(cfgfile)
+        
+        if config.sections() == []:
+            baseurl = await self.push_screen_wait(InitialConfigScreen())
+            config["DEFAULT"] = {}
+            config["API"] = {}
+            config["API"]["baseurl"] = baseurl
+            
+            with open(cfgfile, "w") as f:
+                config.write(f)
+        
+        self.switch_mode("PrimaryMode")
+
+    def get_system_commands(self, screen: Screen) -> Iterable[SystemCommand]:
+        yield from super().get_system_commands(screen)
