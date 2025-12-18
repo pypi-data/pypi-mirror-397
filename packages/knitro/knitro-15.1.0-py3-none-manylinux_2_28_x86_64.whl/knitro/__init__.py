5b37720ddc46fe2147b8679dc79ad72863f371c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 21:06:36 +0000

    Add package knitro/15.1.0/knitro-15.1.0-py3-none-manylinux_2_28_x86_64.whl

---
+++ packages/knitro/knitro-15.1.0-py3-none-manylinux_2_28_x86_64.whl/knitro/__init__.py
@@ -0,0 +1,10 @@
+#*******************************************************
+#* Copyright (c) 2025 by Artelys                       *
+#* All Rights Reserved                                 *
+#*******************************************************
+
+__version__ = '15.1.0'
+
+from .constants import *
+from .wrapper import *
+from .knitro import *
