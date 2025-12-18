89c431d49a16938001bbd77164d2759ee0b1885c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 20:05:30 +0000

    Add package appdevcommons/0.1.2/appdevcommons-0.1.2.tar.gz

---
+++ packages/appdevcommons/appdevcommons-0.1.2.tar.gz/appdevcommons-0.1.2/src/appdevcommons/__init__.py
@@ -0,0 +1,10 @@
+"""
+AppDevCommons - A collection of common utilities and functionalities for Python applications.
+"""
+
+__version__ = "0.1.2"
+
+from appdevcommons.kms_encryptor import KMSEncryptor
+from appdevcommons.unique_id import UniqueIdGenerator
+
+__all__ = ["KMSEncryptor", "UniqueIdGenerator"]
