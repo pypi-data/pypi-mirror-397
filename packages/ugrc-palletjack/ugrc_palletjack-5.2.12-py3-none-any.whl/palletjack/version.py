f3d5734898e6579dca40723a860f94b0610d1fd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 16:07:02 +0000

    Add package ugrc-palletjack/5.2.12/ugrc_palletjack-5.2.12-py3-none-any.whl

---
+++ packages/ugrc-palletjack/ugrc_palletjack-5.2.12-py3-none-any.whl/palletjack/version.py
@@ -0,0 +1,6 @@
+"""A single source of truth for the version in a programmatically-accessible variable.
+This file must only include the single line of code below
+"""
+
+#: This will be automatically set by the publish action when it is uploaded to pypi; only change this for local dev
+__version__ = "5.2.12" #: x-release-please-version
