22b52b8cd43f30feb40d82761597ce77299b0a53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 04:12:25 +0000

    Add package trytond-product-attribute/7.8.0/trytond_product_attribute-7.8.0.tar.gz

---
+++ packages/trytond-product-attribute/trytond_product_attribute-7.8.0.tar.gz/trytond_product_attribute-7.8.0/doc/index.rst
@@ -0,0 +1,11 @@
+########################
+Product Attribute Module
+########################
+
+The *Product Attribute Module* adds attributes to product variants.
+
+.. toctree::
+   :maxdepth: 2
+
+   design
+   releases
---
+++ packages/trytond-product-attribute/trytond_product_attribute-7.8.0.tar.gz/trytond_product_attribute-7.8.0/tests/test_module.py
@@ -0,0 +1,13 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class ProductTestCase(ModuleTestCase):
+    'Test Product Attribute module'
+    module = 'product_attribute'
+    extras = ['purchase', 'sale']
+
+
+del ModuleTestCase
