1fc2893a6dd1de25309847476626ba8de7806f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 15:06:41 +0000

    Add package howler-api/3.0.0.dev370/howler_api-3.0.0.dev370.tar.gz

---
+++ packages/howler-api/howler_api-3.0.0.dev370.tar.gz/howler_api-3.0.0.dev370/howler/odm/models/lead.py
@@ -0,0 +1,24 @@
+# mypy: ignore-errors
+from typing import Optional
+
+from howler import odm
+from howler.odm.models.localized_label import LocalizedLabel
+
+
+@odm.model(
+    index=False,
+    store=True,
+    description="The dossier object stores individual tabs/fields for a given alert.",
+)
+class Lead(odm.Model):
+    icon: Optional[str] = odm.Text(
+        description="An optional icon to use in the tab display for this dossier.", optional=True
+    )
+    label: LocalizedLabel = odm.Compound(LocalizedLabel, description="Labels for the lead in the UI.")
+    format: str = odm.Keyword(description="The format of the lead.")
+    content: str = odm.Text(
+        description="The data for the content. Could be a link, raw markdown text, or other valid lead format.",
+    )
+    metadata: Optional[str] = odm.Json(
+        optional=True, description="Metadata associated with this dossier. Use varies based on format."
+    )
