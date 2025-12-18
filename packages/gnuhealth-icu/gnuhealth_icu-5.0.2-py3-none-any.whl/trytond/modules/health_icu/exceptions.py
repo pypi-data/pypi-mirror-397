eb42103ff1dac10f5e46b468d286083dfd62ed7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:30:55 +0000

    Add package gnuhealth-icu/5.0.2/gnuhealth_icu-5.0.2-py3-none-any.whl

---
+++ packages/gnuhealth-icu/gnuhealth_icu-5.0.2-py3-none-any.whl/trytond/modules/health_icu/exceptions.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2008-2025 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2025 GNU Solidario <health@gnusolidario.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+from trytond.model.exceptions import ValidationError
+
+
+class PatientAlreadyInICU(ValidationError):
+    pass
+
+
+class PatientAlreadyOnMV(ValidationError):
+    pass
