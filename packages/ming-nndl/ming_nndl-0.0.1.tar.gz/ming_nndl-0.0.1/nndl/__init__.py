cc2c1dda9469c342cf45b05a1788447bae3e063c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 16:07:57 +0000

    Add package ming-nndl/0.0.1/ming_nndl-0.0.1.tar.gz

---
+++ packages/ming-nndl/ming_nndl-0.0.1.tar.gz/ming_nndl-0.0.1/nndl/__init__.py
@@ -0,0 +1,15 @@
+"""nndl - Neural Network and Deep Learning Library
+
+基于 PaddlePaddle 的深度学习基础库，包含数据集、优化器、运行器、指标等组件。
+"""
+
+from nndl.dataset import *
+from nndl.runner import *
+from nndl.optimizer import *
+from nndl.metric import *
+from nndl.op import *
+from nndl.activation import *
+from nndl.tools import *
+
+__version__ = "0.0.1"
+__author__ = "Ming"
