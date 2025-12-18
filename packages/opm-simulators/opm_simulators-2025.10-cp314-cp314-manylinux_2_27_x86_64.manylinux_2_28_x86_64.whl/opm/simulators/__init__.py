379a31d841c2707e0fd1f99309bf19a6a4d4863d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 12:05:29 +0000

    Add package opm-simulators/2025.10/opm_simulators-2025.10-cp314-cp314-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/opm-simulators/opm_simulators-2025.10-cp314-cp314-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/opm/simulators/__init__.py
@@ -0,0 +1,14 @@
+# Instead of having the pybind11 extension module, e.g.
+#   simulators.cpython-310-x86_64-linux-gnu.so  located
+#   directly in the opm directory, we create a package (sub
+#   directory) with the same name and place it there.
+#   In this way we can do (if needed in the future)
+#
+#  from opm.simulators import BlackOilSimulator, FoamSimulator, PurePythonUtils, ...
+#
+#  where FoamSimulator and PurePythonUtils does not currently exists,
+#  but could be possible future extensions..
+#
+from .BlackOil import BlackOilSimulator
+from .GasWater import GasWaterSimulator
+from .OnePhase import OnePhaseSimulator
