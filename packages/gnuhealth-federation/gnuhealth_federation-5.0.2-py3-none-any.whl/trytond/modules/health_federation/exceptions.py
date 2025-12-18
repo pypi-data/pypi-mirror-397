8b8d3d5759d6d488cb9a3063faea1b661ae4c689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:30:50 +0000

    Add package gnuhealth-federation/5.0.2/gnuhealth_federation-5.0.2-py3-none-any.whl

---
+++ packages/gnuhealth-federation/gnuhealth_federation-5.0.2-py3-none-any.whl/trytond/modules/health_federation/exceptions.py
@@ -0,0 +1,26 @@
+# SPDX-FileCopyrightText: 2008-2025 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2025 GNU Solidario <health@gnusolidario.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from trytond.exceptions import UserError
+
+
+class NeedLoginCredentials(UserError):
+    pass
+
+
+class ServerAuthenticationError(UserError):
+    pass
+
+
+class ThalamusConnectionError(UserError):
+    pass
+
+
+class ThalamusConnectionOK(UserError):
+    pass
+
+
+class NoInstitution(UserError):
+    pass
