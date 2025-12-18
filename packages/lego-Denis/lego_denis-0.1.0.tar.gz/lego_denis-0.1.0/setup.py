d9a3a2dff54a0ffc049d6742ec05df9d43a4737c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 05:07:19 +0000

    Add package lego-Denis/0.1.0/lego_denis-0.1.0.tar.gz

---
+++ packages/lego-Denis/lego_denis-0.1.0.tar.gz/lego_denis-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lego_Denis',        # название вашей библиотеки
+    version='0.1.0',          # версия
+    packages=find_packages(),
+    author='Denis',
+    author_email='denisfomenko098@gmail.com',
+    description='Моя первая библиотека',
+    url='https://github.com/Fifaro/lego_Denis',  # если есть
+)
