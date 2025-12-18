e0557dbf298e36443347f8f4600c1ea706f77e55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 09:07:11 +0000

    Add package MetaRpcMT4/0.0.78/metarpcmt4-0.0.78.tar.gz

---
+++ packages/MetaRpcMT4/metarpcmt4-0.0.78.tar.gz/metarpcmt4-0.0.78/tests/test.py
@@ -0,0 +1,27 @@
+﻿import asyncio
+from MetaRpcMT4.mt4_account import MT4Account
+
+
+async def test_account_summary():
+    user = 910798516
+    password = "bzen2yz"
+    host = "18.163.180.152"
+    port = 443
+    server_name = "VTMarkets-Demo"
+
+    account = MT4Account(user=user, password=password)
+    #await account.connect_by_host_port(host=host, port=port, base_chart_symbol="EURUSD")
+    await account.connect_by_server_name(server_name=server_name, base_chart_symbol="EURUSD")
+
+    summary = await account.account_summary()
+    print("✅ Account summary:")
+    print(summary)
+
+    some  = await account.quote("sadfsf");
+
+    async for tick in account.on_symbol_tick(["EURUSD", "GBPUSD", "BTCUSD"]):
+        print(tick)
+
+
+if __name__ == "__main__":
+    asyncio.run(test_account_summary())
