bd7aa361a22096773dfcac2d2190ef1f3de0864b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:30:33 +0000

    Add package btx-lib-mail/1.0.3/btx_lib_mail-1.0.3-py3-none-any.whl

---
+++ packages/btx-lib-mail/btx_lib_mail-1.0.3-py3-none-any.whl/btx_lib_mail/__init__.py
@@ -0,0 +1,24 @@
+"""Public package surface exposing greeting, failure, and metadata hooks."""
+
+from __future__ import annotations
+
+from .behaviors import (
+    CANONICAL_GREETING,
+    emit_greeting,
+    noop_main,
+    raise_intentional_failure,
+)
+from .lib_mail import ConfMail, conf, logger, send
+from .__init__conf__ import print_info
+
+__all__ = [
+    "CANONICAL_GREETING",
+    "emit_greeting",
+    "noop_main",
+    "print_info",
+    "raise_intentional_failure",
+    "ConfMail",
+    "conf",
+    "send",
+    "logger",
+]
