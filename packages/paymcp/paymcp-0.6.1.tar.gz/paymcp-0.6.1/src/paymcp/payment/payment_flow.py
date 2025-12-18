b95b33c8828071b0af11691fac57c6b09ea8adeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 07:06:55 +0000

    Add package paymcp/0.6.1/paymcp-0.6.1.tar.gz

---
+++ packages/paymcp/paymcp-0.6.1.tar.gz/paymcp-0.6.1/src/paymcp/payment/payment_flow.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class Mode(str, Enum):
+    TWO_STEP = "two_step"
+    PROGRESS = "progress"
+    ELICITATION = "elicitation"
+    OOB = "oob"
+    DYNAMIC_TOOLS = "dynamic_tools" 
+    RESUBMIT = "resubmit"
+
+PaymentFlow = Mode # Alias for backward compatibility; PaymentFlow will be deprecated in future versions
