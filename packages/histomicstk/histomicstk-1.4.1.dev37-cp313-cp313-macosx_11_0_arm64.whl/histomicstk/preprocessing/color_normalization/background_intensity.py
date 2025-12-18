44a73c8fc23ee102a85baa670df1095c1a4dda2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 05:05:52 +0000

    Add package histomicstk/1.4.1.dev37/histomicstk-1.4.1.dev37-cp313-cp313-macosx_11_0_arm64.whl

---
+++ packages/histomicstk/histomicstk-1.4.1.dev37-cp313-cp313-macosx_11_0_arm64.whl/histomicstk/preprocessing/color_normalization/background_intensity.py
@@ -0,0 +1,29 @@
+import numpy
+
+from histomicstk.utils import sample_pixels
+
+
+def background_intensity(*args, **kwargs):
+    """Sample the background of the slide identified by slide_path to
+    compute the background intensities in the RGB channels.  Arguments
+    are as in histomicstk.utils.sample_pixels, with background forced
+    to True.
+
+    Notes
+    -----
+    The `magnification` parameter defaults to 1.25x, instead of the
+    native scan magnification as in sample_pixels.
+
+    """
+    if 'background' in kwargs:
+        msg = '"background" argument may not be specified'
+        raise ValueError(msg)
+
+    kwargs['background'] = True
+
+    if 'magnification' not in kwargs:
+        kwargs['magnification'] = 1.25
+
+    sample = sample_pixels(*args, **kwargs)
+
+    return numpy.median(sample, axis=0)
