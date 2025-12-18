dcccfc75d5adc02aaa2e281ab16482703a0bcc5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 13:17:47 +0000

    Add package kaq-quant-common/0.1.82/kaq_quant_common-0.1.82-py3-none-any.whl

---
+++ packages/kaq-quant-common/kaq_quant_common-0.1.82-py3-none-any.whl/kaq_quant_common/api/rest/instruction/models/account.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+
+from . import InstructionRequestBase, InstructionResponseBase
+
+
+# 资产信息
+class AssetsInfo(BaseModel):
+    # 币种
+    coin: str
+    # 余额
+    balance: float
+
+
+# ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ 查询合约账户余额
+class ContractBalanceRequest(InstructionRequestBase):
+    # 币种
+    coin: str
+
+
+class ContractBalanceResponse(InstructionResponseBase):
+    #
+    assets: list[AssetsInfo]
