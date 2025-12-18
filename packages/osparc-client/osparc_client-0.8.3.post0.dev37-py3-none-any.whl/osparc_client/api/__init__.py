e4e4c574692cde17a755a6d0a4368ae87255039b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 13:18:07 +0000

    Add package osparc-client/0.8.3.post0.dev37/osparc_client-0.8.3.post0.dev37-py3-none-any.whl

---
+++ packages/osparc-client/osparc_client-0.8.3.post0.dev37-py3-none-any.whl/osparc_client/api/__init__.py
@@ -0,0 +1,17 @@
+# flake8: noqa
+
+# import apis into api package
+from osparc_client.api.credits_api import CreditsApi
+from osparc_client.api.files_api import FilesApi
+from osparc_client.api.function_job_collections_api import FunctionJobCollectionsApi
+from osparc_client.api.function_jobs_api import FunctionJobsApi
+from osparc_client.api.functions_api import FunctionsApi
+from osparc_client.api.licensed_items_api import LicensedItemsApi
+from osparc_client.api.meta_api import MetaApi
+from osparc_client.api.programs_api import ProgramsApi
+from osparc_client.api.solvers_api import SolversApi
+from osparc_client.api.studies_api import StudiesApi
+from osparc_client.api.tasks_api import TasksApi
+from osparc_client.api.users_api import UsersApi
+from osparc_client.api.wallets_api import WalletsApi
+
