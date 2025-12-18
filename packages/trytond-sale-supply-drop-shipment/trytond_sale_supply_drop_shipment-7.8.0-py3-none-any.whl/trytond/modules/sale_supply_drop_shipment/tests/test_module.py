c23de76b7a38fdd89dfb25084fa1539c238be5c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 04:12:21 +0000

    Add package trytond-sale-supply-drop-shipment/7.8.0/trytond_sale_supply_drop_shipment-7.8.0-py3-none-any.whl

---
+++ packages/trytond-sale-supply-drop-shipment/trytond_sale_supply_drop_shipment-7.8.0-py3-none-any.whl/trytond/modules/sale_supply_drop_shipment/tests/test_module.py
@@ -0,0 +1,18 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+from trytond.modules.company.tests import (
+    CompanyTestMixin, PartyCompanyCheckEraseMixin)
+from trytond.modules.party.tests import PartyCheckReplaceMixin
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class SaleSupplyDropShipmentTestCase(
+        PartyCompanyCheckEraseMixin, PartyCheckReplaceMixin, CompanyTestMixin,
+        ModuleTestCase):
+    'Test SaleSupplyDropShipment module'
+    module = 'sale_supply_drop_shipment'
+    extras = ['sale_amendment', 'stock_split']
+
+
+del ModuleTestCase
