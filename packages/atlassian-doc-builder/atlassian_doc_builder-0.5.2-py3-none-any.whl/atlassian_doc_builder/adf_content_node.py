89ba15b133a597c125af9388642a0ba0df6de878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 16:07:40 +0000

    Add package atlassian-doc-builder/0.5.2/atlassian_doc_builder-0.5.2-py3-none-any.whl

---
+++ packages/atlassian-doc-builder/atlassian_doc_builder-0.5.2-py3-none-any.whl/atlassian_doc_builder/adf_content_node.py
@@ -0,0 +1,158 @@
+import jsonschema
+
+from .adf_object import adf_schema, ADFObject
+
+
+class ADFContentObject(ADFObject):
+    def __getitem__(self, idx):
+        if type(idx) is int:
+            return self.local_info['content'][idx]
+        levels, cur_obj = list(idx), self.local_info['content']
+        while levels:
+            cur_idx = levels.pop(0)
+            cur_obj = cur_obj[cur_idx]
+            if not levels:
+                return cur_obj
+            cur_obj = cur_obj.local_info['content']
+
+    def __len__(self):
+        return len(self.local_info['content'])
+
+    def __iter__(self):
+        return self.local_info['content'].__iter__()
+
+
+ADFParagraph, ADFBlockquote, ADFBulletList, ADFOrderList, ADFListItem = tuple(
+    ADFContentObject.node_class_factory(node_type)
+    for node_type in (
+        'paragraph', 'blockquote',
+        'bulletList', 'orderedList', 'listItem',
+    )
+)
+
+
+class ADFDoc(ADFContentObject.node_class_factory('doc')):
+    def __init__(self, chain_mode=True, **kwargs):
+        super(ADFDoc, self).__init__(chain_mode=chain_mode, **kwargs)
+        self.local_info['version'] = 1
+
+    def validate(self, schema_url=None):
+        """
+        Validate the output object with the ADF Schema. Raise an Exception when validation fails.
+        :param schema_url: Specify the Schema URL used for validation. Passing None to use the default, latest Schema.
+        :return: Rendered result
+        """
+        render_result = self.render()
+        jsonschema.validate(render_result, adf_schema(schema_url))
+        return render_result
+
+
+class ADFHeading(ADFContentObject.node_class_factory('heading')):
+    def __init__(self, level=None, chain_mode=True, **kwargs):
+        new_kwargs = {k: v for k, v in kwargs.items() if k != 'attrs'}
+        super(ADFHeading, self).__init__(chain_mode=chain_mode, **new_kwargs)
+        self.level = level if level is not None else \
+            kwargs.get('attrs', {}).get('level', 1)
+
+    @property
+    def level(self):
+        return self.local_info['attrs']['level']
+
+    @level.setter
+    def level(self, value):
+        self.assign_info('attrs', level=value)
+
+
+class ADFCodeBlock(ADFContentObject.node_class_factory('codeBlock')):
+    def __init__(self, language=None, chain_mode=True, **kwargs):
+        new_kwargs = {k: v for k, v in kwargs.items() if k != 'attrs'}
+        super(ADFCodeBlock, self).__init__(chain_mode=chain_mode, **new_kwargs)
+        self.language = language if language is not None else \
+            kwargs.get('attrs', {}).get('language', '')
+
+    @property
+    def language(self):
+        return self.local_info['attrs'].get('language')
+
+    @language.setter
+    def language(self, value):
+        self.assign_info('attrs', language=value)
+
+
+class ADFPanel(ADFContentObject.node_class_factory('panel')):
+    def __init__(self, panel_type=None, chain_mode=True, **kwargs):
+        new_kwargs = {k: v for k, v in kwargs.items() if k != 'attrs'}
+        new_attrs = {k: v for k, v in kwargs.get('attrs', {}).items() if k != 'panelType'}
+        super(ADFPanel, self).__init__(chain_mode=chain_mode, attrs=new_attrs, **new_kwargs)
+        self.panel_type = panel_type if panel_type is not None else \
+            kwargs.get('attrs', {}).get('panelType', 'info')
+
+    @property
+    def panel_type(self):
+        return self.local_info['attrs'].get('panelType')
+
+    @panel_type.setter
+    def panel_type(self, value):
+        self.assign_info('attrs', panelType=value)
+
+
+class ADFExpand(ADFContentObject.node_class_factory('expand')):
+    def __init__(self, title=None, chain_mode=True, **kwargs):
+        new_kwargs = {k: v for k, v in kwargs.items() if k != 'attrs'}
+        super(ADFExpand, self).__init__(chain_mode=chain_mode, **new_kwargs)
+        self.title = title if title is not None else \
+            kwargs.get('attrs', {}).get('title', '')
+
+    @property
+    def title(self):
+        return self.local_info['attrs'].get('title')
+
+    @title.setter
+    def title(self, value):
+        self.assign_info('attrs', title=value)
+
+
+class ADFTaskList(ADFContentObject.node_class_factory('taskList')):
+    def __init__(self, local_id=None, chain_mode=True, **kwargs):
+        new_kwargs = {k: v for k, v in kwargs.items() if k != 'attrs'}
+        super(ADFTaskList, self).__init__(chain_mode=chain_mode, **new_kwargs)
+        self.local_id = local_id if local_id is not None else \
+            kwargs.get('attrs', {}).get('localId', '')
+
+    @property
+    def local_id(self):
+        return self.local_info['attrs'].get('localId')
+
+    @local_id.setter
+    def local_id(self, value):
+        self.assign_info('attrs', localId=value)
+
+
+class ADFTaskItem(ADFContentObject.node_class_factory('taskItem')):
+    def __init__(self, state=None, local_id=None, chain_mode=True, **kwargs):
+        new_kwargs = {k: v for k, v in kwargs.items() if k != 'attrs'}
+        super(ADFTaskItem, self).__init__(chain_mode=chain_mode, **new_kwargs)
+        self.state = state if state is not None else \
+            kwargs.get('attrs', {}).get('state', '')
+        self.local_id = local_id if local_id is not None else \
+            kwargs.get('attrs', {}).get('localId', '')
+
+    @property
+    def state(self):
+        return self.local_info['attrs'].get('state')
+
+    @state.setter
+    def state(self, value):
+        self.assign_info('attrs', state=value)
+
+    @property
+    def local_id(self):
+        return self.local_info['attrs'].get('localId')
+
+    @local_id.setter
+    def local_id(self, value):
+        self.assign_info('attrs', localId=value)
+
+
+ADFDecisionList = ADFTaskList.node_class_factory('decisionList')
+ADFDecisionItem = ADFTaskItem.node_class_factory('decisionItem')
---
+++ packages/atlassian-doc-builder/atlassian_doc_builder-0.5.2-py3-none-any.whl/atlassian_doc_builder/adf_simple.py
@@ -0,0 +1,90 @@
+from datetime import datetime
+
+from .adf_object import ADFObject
+
+# Node Type with no inputs
+ADFHardBreak, ADFRule = tuple(
+    ADFObject.node_class_factory(node_type)
+    for node_type in ('hardBreak', 'rule',)
+)
+
+
+class ADFText(ADFObject.node_class_factory('text')):
+    def __init__(self, text=None, chain_mode=True, **kwargs):
+        if not text and 'text' not in kwargs:
+            raise ValueError('Text cannot be empty.')
+        super(ADFText, self).__init__(text=text, chain_mode=chain_mode, **kwargs)
+
+    @property
+    def text(self):
+        return self.local_info['text']
+
+    @text.setter
+    def text(self, value):
+        self.assign_info('text', value)
+
+
+class ADFDate(ADFObject.node_class_factory('date')):
+    def __init__(self, timestamp=None, chain_mode=True, **kwargs):
+        new_kwargs = {k: v for k, v in kwargs.items() if k != 'attrs'}
+        super(ADFDate, self).__init__(chain_mode=chain_mode, **new_kwargs)
+        self.timestamp = int(timestamp) if timestamp is not None else \
+            int(kwargs.get('attrs', {}).get('timestamp', (datetime.now().timestamp())))
+
+    @property
+    def timestamp(self):
+        return int(self.local_info['attrs'].get('timestamp')) // 1000
+
+    @timestamp.setter
+    def timestamp(self, value):
+        if isinstance(value, str):
+            if len(value) != 13:  # len(1676596381123), Looks like in ms format
+                value = f"{value}000"[:13]  # Multiply by 1000 to turn this in ms.
+        else:
+            value = int(value)
+            if value <= 9999999999:  # Looks like in sec format, which have 10 digits or less
+                value *= 1000
+            value = str(value)
+        self.assign_info('attrs', timestamp=value)
+
+
+class ADFPlaceholder(ADFObject.node_class_factory('placeholder')):
+    def __init__(self, text=None, chain_mode=True, **kwargs):
+        new_kwargs = {k: v for k, v in kwargs.items() if k != 'attrs'}
+        super(ADFPlaceholder, self).__init__(chain_mode=chain_mode, **new_kwargs)
+        self.text = text if text is not None else \
+            kwargs.get('attrs', {}).get('text', '')
+
+    @property
+    def text(self):
+        return self.local_info['attrs'].get('text')
+
+    @text.setter
+    def text(self, value):
+        self.assign_info('attrs', text=value)
+
+
+class ADFStatus(ADFObject.node_class_factory('status')):
+    def __init__(self, text=None, color=None, chain_mode=True, **kwargs):
+        new_kwargs = {k: v for k, v in kwargs.items() if k != 'attrs'}
+        super(ADFStatus, self).__init__(chain_mode=chain_mode, **new_kwargs)
+        self.text = text if text is not None else \
+            kwargs.get('attrs', {}).get('text', '')
+        self.color = color if color is not None else \
+            kwargs.get('attrs', {}).get('color', 'neutral')
+
+    @property
+    def text(self):
+        return self.local_info['attrs'].get('text')
+
+    @text.setter
+    def text(self, value):
+        self.assign_info('attrs', text=value)
+
+    @property
+    def color(self):
+        return self.local_info['attrs'].get('color')
+
+    @color.setter
+    def color(self, value):
+        self.assign_info('attrs', color=value)
