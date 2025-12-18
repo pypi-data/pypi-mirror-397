c0a6fdda3c477801a8999dc6a6e20806e06a4b84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 05:06:19 +0000

    Add package gnome-speech2text-service/1.0.7/gnome_speech2text_service-1.0.7-py3-none-any.whl

---
+++ packages/gnome-speech2text-service/gnome_speech2text_service-1.0.7-py3-none-any.whl/gnome_speech2text_service/__init__.py
@@ -0,0 +1,12 @@
+"""GNOME Speech2Text Service
+
+A D-Bus service that provides speech-to-text functionality for the GNOME Shell extension.
+"""
+
+__version__ = "1.0.7"
+__author__ = "Kaveh Tehrani"
+__email__ = "codemonkey13x@gmail.com"
+
+from .service import Speech2TextService
+
+__all__ = ["Speech2TextService"]
