22eab2ecf21fc707390f6541b5b5609c30cdec55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 15:05:20 +0000

    Add package wds-client/0.38.37/wds_client-0.38.37-py3-none-any.whl

---
+++ packages/wds-client/wds_client-0.38.37-py3-none-any.whl/wds_client/api/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+
+# import apis into api package
+from wds_client.api.capabilities_api import CapabilitiesApi
+from wds_client.api.collection_api import CollectionApi
+from wds_client.api.general_wds_information_api import GeneralWDSInformationApi
+from wds_client.api.import_api import ImportApi
+from wds_client.api.job_api import JobApi
+from wds_client.api.record_api import RecordApi
+from wds_client.api.records_api import RecordsApi
+from wds_client.api.schema_api import SchemaApi
+from wds_client.api.workspace_api import WorkspaceApi
+
