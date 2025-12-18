ee593046b67c52a2863c48430c9ef3239f3bb963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 21:06:25 +0000

    Add package blobtoolkit/4.5.0/blobtoolkit-4.5.0-py313-none-manylinux2014_x86_64.whl

---
+++ packages/blobtoolkit/blobtoolkit-4.5.0-py313-none-manylinux2014_x86_64.whl/blobtoolkit-pipeline/src/lib/extract_busco_genes.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+"""
+Extract BUSCO gene sequences and set headers.
+
+Usage: blobtoolkit-pipeline extract-busco-genes --busco PATH... --out FASTA
+
+Options:
+    --busco PATH         BUSCO full summary tsv file or busco_sequences directory.
+    --out FASTA          output FASTA filename.
+"""
+
+import codecs
+import glob
+import logging
+import re
+import tarfile
+from pathlib import Path
+
+from docopt import DocoptExit
+from docopt import docopt
+
+logger_config = {
+    "level": logging.INFO,
+    "format": "%(asctime)s [%(levelname)s] line %(lineno)d %(message)s",
+    "filemode": "w",
+}
+logging.basicConfig(**logger_config)
+logger = logging.getLogger()
+
+
+# def parse_args():
+#     """Parse snakemake args if available."""
+#     args = {}
+#     try:
+#         args["--busco"] = snakemake.input.busco
+#         args["--out"] = snakemake.output.fasta
+#     except NameError as err:
+#         pass
+#     for key, value in args.items:
+#         sys.argv.append(key)
+#         sys.argv.append(value)
+
+
+def parse_busco_file(fh, ofh, status, busco_id):
+    """Parse a busco sequences file."""
+    header_pattern = re.compile(r">\S+:\d+-\d+")
+    busco_id_pattern = re.compile(r"\d+at\d+")
+    for line in fh:
+        if line.startswith(">"):
+            header = line.strip().split(" ")[0]
+            title = ""
+            # remove trailing "|-" or "|+" from header if present
+            header = re.sub(r"\|[\-+]+$", "", header)
+            # header busco_id may not match the busco_id in the filename
+            # so cannot use if header.startswith(f">{busco_id}"):
+            if busco_id_pattern.match(header[1:]):
+                seq_id = header.split("|")[-1]
+                title = f">{seq_id}={busco_id}={status}"
+            elif header_pattern.match(header):
+                title = f"{header}={busco_id}={status}"
+            else:
+                parts = header.split(" # ")
+                title = f'{re.sub("_[^_]+$", "", parts[0])}:{parts[1]}-{parts[2]}={busco_id}={status}'
+            ofh.write("%s\n" % title)
+        else:
+            ofh.write(line)
+
+
+def main(rename=None):
+    """Entry point."""
+    docs = __doc__
+    if rename is not None:
+        docs = docs.replace("blobtoolkit-pipeline", rename)
+    try:
+        args = docopt(docs)
+    except DocoptExit as e:
+        raise DocoptExit from e
+    file_pattern = re.compile(r"busco_sequences\/(\w+?)_\w+\/(\d+at\d+)\.faa")
+    try:
+        with open(args["--out"], "w") as ofh:
+            for busco_file in args["--busco"]:
+                if busco_file.endswith("busco_sequences"):
+                    # plain directory
+                    parse_busco_directory(file_pattern, ofh, busco_file)
+                else:
+                    # tarred archive
+                    utf8reader = codecs.getreader("utf-8")
+                    busco_dir = Path(busco_file).absolute().parent
+                    busco_seqs = f"{busco_dir}/busco_sequences.tar.gz"
+                    if not Path(busco_seqs).is_file():
+                        continue
+                    parse_tarred_busco_directory(
+                        file_pattern, ofh, utf8reader, busco_seqs
+                    )
+
+    except Exception as err:
+        logger.error(err)
+        exit(1)
+
+
+def parse_tarred_busco_directory(file_pattern, ofh, utf8reader, busco_seqs):
+    """Parse a tarred busco directory."""
+    tar = tarfile.open(busco_seqs)
+    for tarinfo in tar.getmembers():
+        if tarinfo.name.endswith(".faa"):
+            match = file_pattern.match(tarinfo.name)
+            status, busco_id = match.groups()
+            with utf8reader(
+                tar.extractfile(
+                    tarinfo,
+                )
+            ) as fh:
+                parse_busco_file(fh, ofh, status, busco_id)
+
+
+def parse_busco_directory(file_pattern, ofh, busco_file):
+    """Parse a plain busco_directory."""
+    files = glob.glob(f"{Path(busco_file).resolve()}/*/*.faa")
+    for file in files:
+        match = file_pattern.search(file)
+        status, busco_id = match.groups()
+        with open(file, "r") as fh:
+            parse_busco_file(fh, ofh, status, busco_id)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/blobtoolkit/blobtoolkit-4.5.0-py313-none-manylinux2014_x86_64.whl/blobtools/blobtools.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+
+"""
+BlobTools2 - assembly exploration, QC and filtering.
+
+usage: blobtools [<command>] [<args>...] [-h|--help] [--version]
+
+commands:
+    add             add data to a BlobDir
+    create          create a new BlobDir
+    filter          filter a BlobDir
+    host            host interactive view of all BlobDirs in a directory
+    replace         call blobtools add with --replace flag
+    remove          remove one or more fields from a BlobDir
+    validate        validate a BlobDir
+    view            generate plots using BlobToolKit Viewer
+    -h, --help      show this
+    -v, --version   show version number
+See 'blobtools <command> --help' for more information on a specific command.
+
+examples:
+    # 1. Create a new BlobDir from a FASTA file:
+    blobtools create --fasta examples/assembly.fasta BlobDir
+
+    # 2. Create a new BlobDir from a BlobDB:
+    blobtools create --blobdb examples/blobDB.json BlobDir
+
+    # 3. Add Coverage data from a BAM file:
+    blobtools add --cov examples/assembly.reads.bam BlobDir
+
+    # 4. Assign taxonomy from BLAST hits:
+    blobtools add add --hits examples/blast.out --taxdump ../taxdump BlobDir
+
+    # 5. Add BUSCO results:
+    blobtools add --busco examples/busco.tsv BlobDir
+
+    # 6. Host an interactive viewer:
+    blobtools host BlobDir
+
+    # 7. Filter a BlobDir:
+    blobtools filter --param length--Min=5000 --output BlobDir_len_gt_5000 BlobDir
+"""
+
+
+import os
+import sys
+
+from docopt import DocoptExit
+from docopt import docopt
+from pkg_resources import working_set
+from tolkein import tolog
+
+from .lib.version import __version__
+
+LOGGER = tolog.logger(__name__)
+
+
+def suggest_option(command):
+    options = {
+        "host": ["host"],
+    }
+    options_list = options.get(command, [])
+    options_list.append("full")
+    LOGGER.error(
+        f"The `blobtools {command}` command is not available, to enable this option use{' one of' if len(options_list) > 1 else ''}:"
+    )
+    for option in options_list:
+        print(
+            f"                                - pip install blobtoolkit[{option}]",
+            file=sys.stderr,
+        )
+
+
+def cli():
+    """Entry point."""
+    args = {}
+    if len(sys.argv) > 2:
+        try:
+            args = docopt(__doc__, help=False, version=__version__)
+        except DocoptExit:
+            args = {
+                "<command>": " ".join(sys.argv[1:2]),
+            }
+    elif len(sys.argv) > 1:
+        try:
+            args = docopt(__doc__, help=False, version=__version__)
+        except DocoptExit:
+            args = {"<command>": sys.argv[1]}
+    else:
+        print(__doc__)
+    if "<command>" in args and args["<command>"]:
+        args.update({"<tool>": os.path.basename(sys.argv[0])})
+        # load <command> from entry_points
+        command_index = sys.argv.index(args["<command>"])
+        if args["<command>"] in ["create", "replace"]:
+            if len(sys.argv) > command_index + 1:
+                if args["<command>"] == "replace" and "--replace" not in sys.argv:
+                    sys.argv.insert(command_index + 1, "--replace")
+                args["<command>"] = "add"
+            sys.argv[command_index] = "add"
+        for entry_point in working_set.iter_entry_points("%s.subcmd" % args["<tool>"]):
+            if entry_point.name == args["<command>"]:
+                try:
+                    subcommand = entry_point.load()
+                    sys.exit(subcommand())
+                except ModuleNotFoundError:
+                    suggest_option(args["<command>"])
+                    exit(1)
+        LOGGER.error(
+            "'%s %s' is not a valid command", args["<tool>"], args["<command>"]
+        )
+        sys.exit(1)
+    elif "--help" in args and args["--help"]:
+        print(__doc__)
+        exit(0)
+    elif "--version" in args and args["--version"]:
+        print(__version__)
+        exit(0)
+    raise DocoptExit
