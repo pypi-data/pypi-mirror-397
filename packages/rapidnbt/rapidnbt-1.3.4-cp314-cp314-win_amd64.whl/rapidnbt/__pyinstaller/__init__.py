56d0f12cf9c8bca1ce85c88e7c472f7eea3cf768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:31:11 +0000

    Add package rapidnbt/1.3.4/rapidnbt-1.3.4-cp314-cp314-win_amd64.whl

---
+++ packages/rapidnbt/rapidnbt-1.3.4-cp314-cp314-win_amd64.whl/rapidnbt/__pyinstaller/__init__.py
@@ -0,0 +1,10 @@
+# Copyright © 2025 GlacieTeam.All rights reserved.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy
+# of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# SPDX-License-Identifier: MPL-2.0
+
+
+def get_hook_dirs() -> list[str]:
+    return __path__
