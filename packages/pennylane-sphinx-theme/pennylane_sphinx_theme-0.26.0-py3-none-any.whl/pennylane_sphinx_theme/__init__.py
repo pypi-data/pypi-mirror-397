7c8ba9c32d84667a5b738aaf852edfb5688c9cd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 17:08:06 +0000

    Add package pennylane-sphinx-theme/0.26.0/pennylane_sphinx_theme-0.26.0-py3-none-any.whl

---
+++ packages/pennylane-sphinx-theme/pennylane_sphinx_theme-0.26.0-py3-none-any.whl/pennylane_sphinx_theme/__init__.py
@@ -0,0 +1,31 @@
+"""
+This module registers the PennyLane Sphinx Theme. For more information, see
+https://www.sphinx-doc.org/en/master/development/theming.html
+"""
+
+from pathlib import Path
+
+from xanadu_sphinx_theme import templates_dir
+
+from ._version import __version__
+from .footer import FOOTER
+from .navbar import NAVBAR_LEFT, NAVBAR_RIGHT
+
+
+def setup(app):
+    """See https://www.sphinx-doc.org/en/master/extdev/appapi.html."""
+    cwd = Path(__file__).resolve().parent
+    app.add_html_theme("pennylane", str(cwd))
+
+    # set default left navbar links
+    if "navbar_left_links" not in app.config["html_theme_options"]:
+        app.config["html_theme_options"]["navbar_left_links"] = NAVBAR_LEFT
+
+    # set default right navbar links
+    if "navbar_right_links" not in app.config["html_theme_options"]:
+        app.config["html_theme_options"]["navbar_right_links"] = NAVBAR_RIGHT
+
+    # set default footer sections
+    for section in ["about", "policies", "links", "social_icons", "taglines"]:
+        if f"footer_{section}" not in app.config["html_theme_options"]:
+            app.config["html_theme_options"][f"footer_{section}"] = FOOTER[f"footer_{section}"]
