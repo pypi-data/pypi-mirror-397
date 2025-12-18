b68d0582ba5217b360a3ddffc337d4bfea0bf66a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 06:05:03 +0000

    Add package thousandeyes-sdk-endpoint-agents/2.22.0/thousandeyes_sdk_endpoint_agents-2.22.0.tar.gz

---
+++ packages/thousandeyes-sdk-endpoint-agents/thousandeyes_sdk_endpoint_agents-2.22.0.tar.gz/thousandeyes_sdk_endpoint_agents-2.22.0/src/thousandeyes_sdk/endpoint_agents/api/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+
+# import apis into api package
+from thousandeyes_sdk.endpoint_agents.api.endpoint_agents_api import EndpointAgentsApi
+from thousandeyes_sdk.endpoint_agents.api.endpoint_agents_transfer_api import EndpointAgentsTransferApi
+
