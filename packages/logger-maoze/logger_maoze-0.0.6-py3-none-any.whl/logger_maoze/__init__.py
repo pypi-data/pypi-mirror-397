de1fd2c7644090692f3d4b500d3d07560e72f293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 15:05:41 +0000

    Add package logger-maoze/0.0.6/logger_maoze-0.0.6-py3-none-any.whl

---
+++ packages/logger-maoze/logger_maoze-0.0.6-py3-none-any.whl/logger_maoze/__init__.py
@@ -0,0 +1,3 @@
+from .logger import term_cols, anilog, loadingbar, iterable_loadingbar, colorize, log, casclog, casciterlog, generator_loadingbar, centerize, update_terminal_size, centerlog, gridlog
+
+__all__ = ["term_cols", "anilog", "loadingbar", "iterable_loadingbar", "colorize", "log", "centerize", "casclog", "casciterlog", "generator_loadingbar", "update_terminal_size", "centerlog", "gridlog"]
