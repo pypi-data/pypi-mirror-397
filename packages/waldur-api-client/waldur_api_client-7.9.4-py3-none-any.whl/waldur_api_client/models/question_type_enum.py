969a3cc00449c063ab8271c81a33370c73ddb441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 09:06:56 +0000

    Add package waldur-api-client/7.9.4/waldur_api_client-7.9.4-py3-none-any.whl

---
+++ packages/waldur-api-client/waldur_api_client-7.9.4-py3-none-any.whl/waldur_api_client/models/question_type_enum.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class QuestionTypeEnum(str, Enum):
+    BOOLEAN = "boolean"
+    DATE = "date"
+    FILE = "file"
+    MULTIPLE_FILES = "multiple_files"
+    MULTI_SELECT = "multi_select"
+    NUMBER = "number"
+    SINGLE_SELECT = "single_select"
+    TEXT_AREA = "text_area"
+    TEXT_INPUT = "text_input"
+
+    def __str__(self) -> str:
+        return str(self.value)
