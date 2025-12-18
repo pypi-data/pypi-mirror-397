a2a69e877e64951893a8d22354497acaa54f0460
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 09:06:41 +0000

    Add package luxai-magpie/0.6.0/luxai_magpie-0.6.0.tar.gz

---
+++ packages/luxai-magpie/luxai_magpie-0.6.0.tar.gz/luxai_magpie-0.6.0/examples/zmq_server_node.py
@@ -0,0 +1,35 @@
+import os, sys
+import time
+import argparse
+
+
+from luxai.magpie.nodes import ServerNode
+from luxai.magpie.transport import ZMQRpcResponder
+from luxai.magpie.utils import Logger
+
+
+
+def on_request(req: object):
+    Logger.info(f"on_request: {req}")
+    return {"echo": req}
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--address", 
+                        help="ZeroMQ server socket endpoint (e.g. tcp://*:5555)",
+                        default="tcp://*:5555",                        
+                        type=str)
+
+
+    args = parser.parse_args()    
+    server = ServerNode(name="MyServerNode",
+                        responder=ZMQRpcResponder(args.address),
+                        handler=on_request)
+
+    try:
+        while True:
+            time.sleep(10)
+    except KeyboardInterrupt:
+        Logger.info("Stopping server...")
+        # optionally server.terminate()
