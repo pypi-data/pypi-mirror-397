684f558722a200ad4d06eb251bbcfce92ced607d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 15:07:02 +0000

    Add package noisemonitor/1.0.1/noisemonitor-1.0.1.tar.gz

---
+++ packages/noisemonitor/noisemonitor-1.0.1.tar.gz/noisemonitor-1.0.1/src/noisemonitor/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__version__ = "1.0.1"
+
+from . import (profile, summary)
+from .util import (core, display, filter)
+from .util.load import load
+
+# Optional weather module (requires extra dependencies)
+try:
+    from . import weather
+except ImportError:
+    # Weather module dependencies not available - will be handled
+    # by weather.__init__.py
+    from . import weather
