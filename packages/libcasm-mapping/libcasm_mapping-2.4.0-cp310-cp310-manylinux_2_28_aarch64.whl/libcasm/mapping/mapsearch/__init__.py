8d4dd5613f929b2a2ce3e935a94674edd0471e7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 08:05:03 +0000

    Add package libcasm-mapping/2.4.0/libcasm_mapping-2.4.0-cp310-cp310-manylinux_2_28_aarch64.whl

---
+++ packages/libcasm-mapping/libcasm_mapping-2.4.0-cp310-cp310-manylinux_2_28_aarch64.whl/libcasm/mapping/mapsearch/__init__.py
@@ -0,0 +1,18 @@
+"""Construct custom mapping search algorithms"""
+
+from ._mapping_mapsearch import (
+    AtomMappingSearchData,
+    IsotropicAtomCost,
+    LatticeMappingSearchData,
+    MappingNode,
+    MappingSearch,
+    PrimSearchData,
+    QueueConstraints,
+    StructureSearchData,
+    SymmetryBreakingAtomCost,
+    WeightedTotalCost,
+    make_atom_to_site_cost,
+    make_atom_to_site_cost_future,
+    make_superstructure_data,
+    make_trial_translations,
+)
