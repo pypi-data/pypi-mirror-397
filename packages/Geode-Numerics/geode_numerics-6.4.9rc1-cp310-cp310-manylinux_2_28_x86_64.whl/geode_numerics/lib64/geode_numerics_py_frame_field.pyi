4070d6bd2ef096bfd0ce92d3ea681daf61d70639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 14:06:51 +0000

    Add package Geode-Numerics/6.4.9rc1/geode_numerics-6.4.9rc1-cp310-cp310-manylinux_2_28_x86_64.whl

---
+++ packages/Geode-Numerics/geode_numerics-6.4.9rc1-cp310-cp310-manylinux_2_28_x86_64.whl/geode_numerics/lib64/geode_numerics_py_frame_field.pyi
@@ -0,0 +1,9 @@
+"""
+Geode-Numerics Python binding for frame field
+"""
+from __future__ import annotations
+__all__: list[str] = ['NumericsFrameFieldLibrary']
+class NumericsFrameFieldLibrary:
+    @staticmethod
+    def initialize() -> None:
+        ...
---
+++ packages/Geode-Numerics/geode_numerics-6.4.9rc1-cp310-cp310-manylinux_2_28_x86_64.whl/geode_numerics/scalar_function.pyi
@@ -0,0 +1,12 @@
+from __future__ import annotations
+from geode_numerics.lib64.geode_numerics_py_scalar_function import ComputationGrid2D
+from geode_numerics.lib64.geode_numerics_py_scalar_function import ComputationGrid3D
+from geode_numerics.lib64.geode_numerics_py_scalar_function import DataConstraintsManager2D
+from geode_numerics.lib64.geode_numerics_py_scalar_function import DataConstraintsManager3D
+from geode_numerics.lib64.geode_numerics_py_scalar_function import DataPointsManager2D
+from geode_numerics.lib64.geode_numerics_py_scalar_function import DataPointsManager3D
+from geode_numerics.lib64.geode_numerics_py_scalar_function import FDMCurvatureMinimization2D
+from geode_numerics.lib64.geode_numerics_py_scalar_function import FDMCurvatureMinimization3D
+from geode_numerics.lib64.geode_numerics_py_scalar_function import NumericsScalarFunctionLibrary
+import opengeode as opengeode
+__all__: list[str] = ['ComputationGrid2D', 'ComputationGrid3D', 'DataConstraintsManager2D', 'DataConstraintsManager3D', 'DataPointsManager2D', 'DataPointsManager3D', 'FDMCurvatureMinimization2D', 'FDMCurvatureMinimization3D', 'NumericsScalarFunctionLibrary', 'opengeode']
