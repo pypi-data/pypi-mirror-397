a623e76844ac2469360dbfaa3fc8dac5f5ceea9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 07:06:51 +0000

    Add package omdev/0.0.0.dev489/omdev-0.0.0.dev489-py3-none-any.whl

---
+++ packages/omdev/omdev-0.0.0.dev489-py3-none-any.whl/omdev/tui/rich/__init__.py
@@ -0,0 +1,37 @@
+# ruff: noqa: F401
+# flake8: noqa: F401
+from omlish import lang as _lang
+
+
+with _lang.auto_proxy_init(globals()):
+    ##
+
+    from rich import align  # noqa
+    from rich import console  # noqa
+    from rich import live  # noqa
+    from rich import markdown  # noqa
+    from rich import repr  # noqa
+    from rich import text  # noqa
+    from rich.align import Align  # noqa
+    from rich.console import Console  # noqa
+    from rich.console import Group  # noqa
+    from rich.live import Live  # noqa
+    from rich.markdown import Markdown  # noqa
+    from rich.text import Text  # noqa
+
+    ##
+
+    from .console2 import (  # noqa
+        console_render,
+    )
+
+    from .markdown2 import (  # noqa
+        configure_markdown_parser,
+        markdown_from_tokens,
+        flatten_tokens_filter,
+        flatten_tokens,
+
+        MarkdownLiveStream,
+        NaiveMarkdownLiveStream,
+        IncrementalMarkdownLiveStream,
+    )
