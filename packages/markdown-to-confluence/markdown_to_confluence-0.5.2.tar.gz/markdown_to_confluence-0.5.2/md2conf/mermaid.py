68bbb793985acc6bc1760b1390910a267a842469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 18:05:45 +0000

    Add package markdown-to-confluence/0.5.2/markdown_to_confluence-0.5.2.tar.gz

---
+++ packages/markdown-to-confluence/markdown_to_confluence-0.5.2.tar.gz/markdown_to_confluence-0.5.2/md2conf/mermaid.py
@@ -0,0 +1,101 @@
+"""
+Publish Markdown files to Confluence wiki.
+
+Copyright 2022-2025, Levente Hunyadi
+
+:see: https://github.com/hunyadi/md2conf
+"""
+
+import logging
+import os
+import os.path
+import shutil
+import subprocess
+from dataclasses import dataclass
+from typing import Literal
+
+LOGGER = logging.getLogger(__name__)
+
+
+@dataclass
+class MermaidConfigProperties:
+    """
+    Configuration options for rendering Mermaid diagrams.
+
+    :param scale: Scaling factor for the rendered diagram.
+    """
+
+    scale: float | None = None
+
+
+def is_docker() -> bool:
+    "True if the application is running in a Docker container."
+
+    return os.environ.get("CHROME_BIN") == "/usr/bin/chromium-browser" and os.environ.get("PUPPETEER_SKIP_DOWNLOAD") == "true"
+
+
+def get_mmdc() -> str:
+    "Path to the Mermaid diagram converter."
+
+    if is_docker():
+        full_path = "/home/md2conf/node_modules/.bin/mmdc"
+        if os.path.exists(full_path):
+            return full_path
+        else:
+            return "mmdc"
+    elif os.name == "nt":
+        return "mmdc.cmd"
+    else:
+        return "mmdc"
+
+
+def has_mmdc() -> bool:
+    "True if Mermaid diagram converter is available on the OS."
+
+    executable = get_mmdc()
+    return shutil.which(executable) is not None
+
+
+def render_diagram(source: str, output_format: Literal["png", "svg"] = "png", config: MermaidConfigProperties | None = None) -> bytes:
+    "Generates a PNG or SVG image from a Mermaid diagram source."
+
+    if config is None:
+        config = MermaidConfigProperties()
+
+    cmd = [
+        get_mmdc(),
+        "--input",
+        "-",
+        "--output",
+        "-",
+        "--outputFormat",
+        output_format,
+        "--backgroundColor",
+        "transparent",
+        "--scale",
+        str(config.scale or 2),
+    ]
+    root = os.path.dirname(__file__)
+    if is_docker():
+        cmd.extend(["-p", os.path.join(root, "puppeteer-config.json")])
+    LOGGER.debug("Executing: %s", " ".join(cmd))
+
+    proc = subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        stdin=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=False,
+    )
+    stdout, stderr = proc.communicate(input=source.encode("utf-8"))
+    if proc.returncode:
+        messages = [f"failed to convert Mermaid diagram; exit code: {proc.returncode}"]
+        console_output = stdout.decode("utf-8")
+        if console_output:
+            messages.append(f"output:\n{console_output}")
+        console_error = stderr.decode("utf-8")
+        if console_error:
+            messages.append(f"error:\n{console_error}")
+        raise RuntimeError("\n".join(messages))
+
+    return stdout
---
+++ packages/markdown-to-confluence/markdown_to_confluence-0.5.2.tar.gz/markdown_to_confluence-0.5.2/tests/test_scanner.py
@@ -0,0 +1,96 @@
+"""
+Publish Markdown files to Confluence wiki.
+
+Copyright 2022-2025, Levente Hunyadi
+
+:see: https://github.com/hunyadi/md2conf
+"""
+
+import logging
+import unittest
+from pathlib import Path
+
+from cattrs import BaseValidationError
+
+from md2conf.extra import override
+from md2conf.scanner import MermaidScanner, Scanner
+from tests.utility import TypedTestCase
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(funcName)s [%(lineno)d] - %(message)s",
+)
+
+mermaid_front_matter = """---
+title: Tiny flow diagram
+config:
+  scale: 1
+---
+flowchart LR
+    A[Component A] --> B[Component B]
+    B --> C[Component C]
+"""
+mermaid_no_front_matter = """flowchart LR
+    A[Component A] --> B[Component B]
+    B --> C[Component C]
+"""
+mermaid_malformed_front_matter = """---
+title: Tiny flow diagram
+config:
+  scale: 1.2.5
+---
+flowchart LR
+    A[Component A] --> B[Component B]
+    B --> C[Component C]
+"""
+
+
+class TestScanner(TypedTestCase):
+    sample_dir: Path
+
+    @override
+    def setUp(self) -> None:
+        self.maxDiff = 1024
+
+        test_dir = Path(__file__).parent
+        parent_dir = test_dir.parent
+
+        self.sample_dir = parent_dir / "sample"
+
+    def test_tag(self) -> None:
+        document = Scanner().read(self.sample_dir / "index.md")
+        self.assertIsNotNone(document.page_id)
+        self.assertIsNone(document.space_key)
+        self.assertIsNone(document.title)
+
+    def test_json_frontmatter(self) -> None:
+        document = Scanner().read(self.sample_dir / "parent" / "index.md")
+        self.assertEqual(document.page_id, "1966122")
+        self.assertEqual(document.space_key, "~hunyadi")
+        self.assertEqual(document.title, "🏠 Markdown parent page")
+
+    def test_yaml_frontmatter(self) -> None:
+        document = Scanner().read(self.sample_dir / "sibling.md")
+        self.assertIsNotNone(document.page_id)
+        self.assertIsNone(document.space_key)
+        self.assertEqual(document.generated_by, "This page has been generated with md2conf.")
+        self.assertEqual(document.title, "Markdown example document")
+        self.assertEqual(document.tags, ["markdown", "confluence", "md", "wiki"])
+
+    def test_mermaid_frontmatter(self) -> None:
+        properties = MermaidScanner().read(mermaid_front_matter)
+        if properties.config is None:
+            self.fail()
+        self.assertEqual(properties.config.scale, 1)
+
+    def test_mermaid_no_frontmatter(self) -> None:
+        properties = MermaidScanner().read(mermaid_no_front_matter)
+        self.assertIsNone(properties.config)
+
+    def test_mermaid_malformed_frontmatter(self) -> None:
+        with self.assertRaises(BaseValidationError):
+            MermaidScanner().read(mermaid_malformed_front_matter)
+
+
+if __name__ == "__main__":
+    unittest.main()
