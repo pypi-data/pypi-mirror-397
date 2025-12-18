7af47ab6a28be23cc36781d124c90a70baf853c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 13:18:01 +0000

    Add package django-esi/8.2.2/django_esi-8.2.2.tar.gz

---
+++ packages/django-esi/django_esi-8.2.2.tar.gz/django_esi-8.2.2/esi/__init__.py
@@ -0,0 +1,7 @@
+"""Django app for accessing the EVE Swagger Interface (ESI)."""
+
+__version__ = "8.2.2"
+__title__ = 'Django-ESI'
+__url__ = 'https://gitlab.com/allianceauth/django-esi'
+__build_date__ = "2025-12-16"
+__esi_compatibility_date__ = "2025-12-16"
