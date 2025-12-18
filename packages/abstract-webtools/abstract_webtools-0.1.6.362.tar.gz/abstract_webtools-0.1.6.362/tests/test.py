60db5c83d38e4692e201ca52bbe1a6ec47f51fbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 17:08:05 +0000

    Add package abstract-webtools/0.1.6.362/abstract_webtools-0.1.6.362.tar.gz

---
+++ packages/abstract-webtools/abstract_webtools-0.1.6.362.tar.gz/abstract_webtools-0.1.6.362/tests/test.py
@@ -0,0 +1,6 @@
+from imports import *
+from imports.src.abstract_webtools.managers.videoDownloader import *
+
+url = "https://www.facebook.com/share/v/1A5JMu3YiW/"
+input(downloadvideo(url).get('id'))
+
