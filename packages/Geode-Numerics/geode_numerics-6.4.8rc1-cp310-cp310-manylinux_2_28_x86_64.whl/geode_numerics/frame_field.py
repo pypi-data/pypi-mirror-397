665c58ace54ff0a9efea3cde0cfbbcd190f2fdad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:29:22 +0000

    Add package Geode-Numerics/6.4.8rc1/geode_numerics-6.4.8rc1-cp310-cp310-manylinux_2_28_x86_64.whl

---
+++ packages/Geode-Numerics/geode_numerics-6.4.8rc1-cp310-cp310-manylinux_2_28_x86_64.whl/geode_numerics/frame_field.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2019 - 2025 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+import geode_common
+
+from .lib64.geode_numerics_py_frame_field import *
+
+NumericsFrameFieldLibrary.initialize()
---
+++ packages/Geode-Numerics/geode_numerics-6.4.8rc1-cp310-cp310-manylinux_2_28_x86_64.whl/geode_numerics/surface_numerics.pyi
@@ -0,0 +1,6 @@
+from __future__ import annotations
+from geode_numerics.lib64.geode_numerics_py_surface import NumericsSurfaceLibrary
+from geode_numerics.lib64.geode_numerics_py_surface import compute_LSCM_parameterization
+from geode_numerics.lib64.geode_numerics_py_surface import convert_surface3d_into_2d
+import opengeode as opengeode
+__all__: list[str] = ['NumericsSurfaceLibrary', 'compute_LSCM_parameterization', 'convert_surface3d_into_2d', 'opengeode']
