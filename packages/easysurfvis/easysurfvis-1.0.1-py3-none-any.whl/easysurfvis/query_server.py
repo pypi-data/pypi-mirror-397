25246b4699ccecb7cdf9c37d6f8aa19273328204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 20:06:10 +0000

    Add package easysurfvis/1.0.1/easysurfvis-1.0.1-py3-none-any.whl

---
+++ packages/easysurfvis/easysurfvis-1.0.1-py3-none-any.whl/easysurfvis/query_server.py
@@ -0,0 +1,47 @@
+
+# Common Libraries
+import os
+import sys
+from pathlib import Path
+from flask_cors import CORS
+from flask import Flask, request, jsonify
+
+# Custom Libraries
+if os.getenv("easysurfvis_isRunSource"):
+    sys.path.append(os.getenv("easysurfvis_source_home"))
+    from cores.afni_extension import whereami
+else:
+    from easysurfvis.cores.afni_extension import whereami
+
+# 
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/click", methods=["POST"])
+def click_callback():
+    data = request.get_json()  # JS에서 보낸 JSON 받기
+
+    hemi = data.get("hemi")
+    idx = data.get("idx")
+    mni = data.get("mni")
+
+    atlas_data = whereami(x = mni[0], y = mni[1], z = mni[2])
+    return jsonify({
+        "status": "ok",
+        "message": "Click received",
+        "hemi": hemi,
+        "idx": idx,
+        "mni": mni,
+        "atlas_data" : atlas_data.to_dict(orient="records")
+    })
+
+if __name__ == "__main__":
+    port = 5000
+    if len(sys.argv) > 1:
+        try:
+            port = int(sys.argv[1])
+        except ValueError:
+            print("⚠️ Port must be an integer. Using default port 5000.")
+
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
