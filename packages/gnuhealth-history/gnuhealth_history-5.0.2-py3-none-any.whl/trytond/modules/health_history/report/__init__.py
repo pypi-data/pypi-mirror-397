6a60ee2cc0f1dffb8527aae9597eec767a51945b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:30:53 +0000

    Add package gnuhealth-history/5.0.2/gnuhealth_history-5.0.2-py3-none-any.whl

---
+++ packages/gnuhealth-history/gnuhealth_history-5.0.2-py3-none-any.whl/trytond/modules/health_history/report/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2008-2025 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2025 GNU Solidario <health@gnusolidario.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from . import patient_evaluation_report
+
+
+__all__ = ['patient_evaluation_report']
