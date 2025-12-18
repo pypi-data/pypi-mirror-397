2e0a0aef3d787e7d7f5ba27c7c6b0f75aa205985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:29:22 +0000

    Add package Geode-Numerics/6.4.8rc1/geode_numerics-6.4.8rc1-cp311-cp311-manylinux_2_28_x86_64.whl

---
+++ packages/Geode-Numerics/geode_numerics-6.4.8rc1-cp311-cp311-manylinux_2_28_x86_64.whl/geode_numerics/frame_field.pyi
@@ -0,0 +1,5 @@
+from __future__ import annotations
+import geode_common as geode_common
+from geode_numerics.lib64.geode_numerics_py_frame_field import NumericsFrameFieldLibrary
+import opengeode as opengeode
+__all__: list[str] = ['NumericsFrameFieldLibrary', 'geode_common', 'opengeode']
