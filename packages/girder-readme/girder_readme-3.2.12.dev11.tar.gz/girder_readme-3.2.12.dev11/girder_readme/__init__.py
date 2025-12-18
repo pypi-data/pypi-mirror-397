44f225512c8828993ce2bffda5517a9a4d795817
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 14:07:43 +0000

    Add package girder-readme/3.2.12.dev11/girder_readme-3.2.12.dev11.tar.gz

---
+++ packages/girder-readme/girder_readme-3.2.12.dev11.tar.gz/girder_readme-3.2.12.dev11/girder_readme/__init__.py
@@ -0,0 +1,10 @@
+from girder.plugin import GirderPlugin
+from .rest import _getFolderReadme
+
+
+class ReadmePlugin(GirderPlugin):
+    DISPLAY_NAME = 'README'
+    CLIENT_SOURCE_PATH = 'web_client'
+
+    def load(self, info):
+        info['apiRoot'].folder.route('GET', (':id', 'readme'), _getFolderReadme)
