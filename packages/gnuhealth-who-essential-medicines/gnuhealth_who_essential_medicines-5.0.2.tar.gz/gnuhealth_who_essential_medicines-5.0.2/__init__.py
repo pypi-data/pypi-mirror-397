4d42dbb1067756a6d85710b5224acdc4f35b7b63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 04:12:18 +0000

    Add package gnuhealth-who-essential-medicines/5.0.2/gnuhealth_who_essential_medicines-5.0.2.tar.gz

---
+++ packages/gnuhealth-who-essential-medicines/gnuhealth_who_essential_medicines-5.0.2.tar.gz/gnuhealth_who_essential_medicines-5.0.2/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# SPDX-FileCopyrightText: 2008-2025 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2025 GNU Solidario <health@gnusolidario.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+#########################################################################
+#   Hospital Management Information System (HMIS) component of the      #
+#                       GNU Health project                              #
+#                   https://www.gnuhealth.org                           #
+#########################################################################
+#                HEALTH WHO_ESSENTIAL_MEDICINES package                 #
+#               __init__.py: Package declaration file                   #
+#########################################################################
+
+from . import health_who_essential_medicines
+
+
+__all__ = ['health_who_essential_medicines']
