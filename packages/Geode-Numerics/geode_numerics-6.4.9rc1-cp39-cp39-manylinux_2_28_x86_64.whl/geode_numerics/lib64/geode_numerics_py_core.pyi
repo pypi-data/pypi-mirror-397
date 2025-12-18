e39876d32e79491dc069dd84bb72113a7071acd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 14:06:50 +0000

    Add package Geode-Numerics/6.4.9rc1/geode_numerics-6.4.9rc1-cp39-cp39-manylinux_2_28_x86_64.whl

---
+++ packages/Geode-Numerics/geode_numerics-6.4.9rc1-cp39-cp39-manylinux_2_28_x86_64.whl/geode_numerics/lib64/geode_numerics_py_core.pyi
@@ -0,0 +1,9 @@
+"""
+Geode-Numerics Python binding for core
+"""
+from __future__ import annotations
+__all__: list[str] = ['NumericsCoreLibrary']
+class NumericsCoreLibrary:
+    @staticmethod
+    def initialize() -> None:
+        ...
