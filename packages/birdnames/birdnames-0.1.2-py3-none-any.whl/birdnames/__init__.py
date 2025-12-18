6bc579ff9fe0bb70ca54c7947e801c32e2f95e00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 15:05:42 +0000

    Add package birdnames/0.1.2/birdnames-0.1.2-py3-none-any.whl

---
+++ packages/birdnames/birdnames-0.1.2-py3-none-any.whl/birdnames/__init__.py
@@ -0,0 +1,23 @@
+"""
+BirdNames - A Python package for managing avian taxonomy.
+
+Provides functionality for mapping between scientific and common names,
+and 4 and 6 letter species codes, with integration across several
+taxonomic authorities.
+"""
+
+from .converter import Converter, determine_name_type, alpha, scientific, common, ebird
+from .utils import TAXONOMIES, list_taxonomies
+
+
+__version__ = "0.1.1"
+__all__ = [
+    "Converter",
+    "determine_name_type",
+    "alpha",
+    "scientific",
+    "common",
+    "ebird",
+    "TAXONOMIES",
+    "list_taxonomies",
+]
