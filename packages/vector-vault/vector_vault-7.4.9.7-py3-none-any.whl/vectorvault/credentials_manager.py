10480352aa642ab44689972fd2084a2a8c325c03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:31:10 +0000

    Add package vector-vault/7.4.9.7/vector_vault-7.4.9.7-py3-none-any.whl

---
+++ packages/vector-vault/vector_vault-7.4.9.7-py3-none-any.whl/vectorvault/credentials_manager.py
@@ -0,0 +1,64 @@
+# VECTOR VAULT CONFIDENTIAL
+# __________________
+# 
+#  All Rights Reserved.
+# 
+# NOTICE:  All information contained herein is, and remains
+# the property of Vector Vault and its suppliers,
+# if any.  The intellectual and technical concepts contained
+# herein are proprietary to Vector Vault
+# and its suppliers and may be covered by U.S. and Foreign Patents,
+# patents in process, and are protected by trade secret or copyright law.
+# Dissemination of this information or reproduction of this material
+# is strictly forbidden unless prior written permission is obtained
+# from Vector Vault. See license for consent.
+
+from google.auth.credentials import Credentials
+import requests
+from threading import RLock 
+from datetime import datetime
+
+class CredentialsManager(Credentials):
+    def __init__(self, user, api):
+        self.user = user
+        self.api = api
+        self.token = None
+        self.expiry = None
+        self._universe_domain = "googleapis.com"
+        self._lock = RLock()
+        self.refresh(requests.Request())
+
+    def apply(self, headers, token=None):
+        with self._lock:
+            headers["Authorization"] = f"Bearer {self.token}"
+
+    @property
+    def valid(self):
+        with self._lock:
+            if self.expiry is None:
+                return False
+            return datetime.utcnow() < self.expiry
+
+    def refresh(self, request):
+        with self._lock:
+            if self.expiry is not None and datetime.utcnow() < self.expiry:
+                return
+            data = {
+                "user_id": self.user,
+                "api_key": self.api,
+            }
+            response = requests.post('https://credentials.vectorvault.io/access', json=data)
+            response.raise_for_status()  
+            response_data = response.json()
+            self.token = response_data['access_token']
+            self.expiry = datetime.fromisoformat(response_data['expiry'])
+
+    def before_request(self, request, method, url, headers):
+        self.refresh(request)
+        self.apply(headers)
+
+    def __getstate__(self):
+        return self.__dict__.copy()
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
