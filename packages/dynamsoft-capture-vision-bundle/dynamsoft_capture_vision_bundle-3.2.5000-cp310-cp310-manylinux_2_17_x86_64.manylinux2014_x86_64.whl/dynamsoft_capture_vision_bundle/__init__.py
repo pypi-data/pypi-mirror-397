c351dcc7ffbc2719cabe3ddb035ec68f55c18fdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 08:06:58 +0000

    Add package dynamsoft-capture-vision-bundle/3.2.5000/dynamsoft_capture_vision_bundle-3.2.5000-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/dynamsoft-capture-vision-bundle/dynamsoft_capture_vision_bundle-3.2.5000-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/dynamsoft_capture_vision_bundle/__init__.py
@@ -0,0 +1,13 @@
+from .core import *
+from .cvr import *
+from .dbr import *
+from .dcp import *
+from .dcpd import *
+from .ddn import *
+from .dip import *
+from .dlr import *
+from .dnn import *
+from .license import *
+from .utility import *
+
+__version__ = "3.2.5000"
