7013ae9e26d39e10d5f4d58b0d4b3788e1877a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 20:06:05 +0000

    Add package OpenGeode-Geosciences/9.5.6/opengeode_geosciences-9.5.6-cp310-cp310-win_amd64.whl

---
+++ packages/OpenGeode-Geosciences/opengeode_geosciences-9.5.6-cp310-cp310-win_amd64.whl/opengeode_geosciences/__init__.py
@@ -0,0 +1,7 @@
+## Copyright (c) 2019 - 2025 Geode-solutions
+
+import os, pathlib
+os.add_dll_directory(pathlib.Path(__file__).parent.resolve().joinpath('bin'))
+
+from .explicit import *
+from .implicit import *
