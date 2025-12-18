3a2cec0c7954031cc3ce78f608e56c2f17a37e42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 07:07:07 +0000

    Add package linkml-browser/0.1.0/linkml_browser-0.1.0-py3-none-any.whl

---
+++ packages/linkml-browser/linkml_browser-0.1.0-py3-none-any.whl/linkml_browser/__init__.py
@@ -0,0 +1,4 @@
+from .core import BrowserGenerator, load_json_data, load_schema, save_schema
+from .main import app, main
+
+__all__ = ["app", "main", "BrowserGenerator", "load_json_data", "load_schema", "save_schema"]
