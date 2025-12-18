2529f3aec9803e963e07e60c12ec9ff630530055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 17:08:14 +0000

    Add package rapidnbt/1.3.5/rapidnbt-1.3.5-cp314-cp314-win_amd64.whl

---
+++ packages/rapidnbt/rapidnbt-1.3.5-cp314-cp314-win_amd64.whl/rapidnbt/_NBT/nbt_compression_type.pyi
@@ -0,0 +1,17 @@
+# Copyright © 2025 GlacieTeam.All rights reserved.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy
+# of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from enum import Enum
+
+class NbtCompressionType(Enum):
+    """
+    Enumeration of compression types for NBT serialization
+    """
+
+    NONE = 0
+    GZIP = 1
+    ZLIB = 2
