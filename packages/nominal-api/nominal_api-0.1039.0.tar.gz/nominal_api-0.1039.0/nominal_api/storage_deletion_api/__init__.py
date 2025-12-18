a82ef8f949d53986a2ff06188f3befadd85faefb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 19:06:33 +0000

    Add package nominal-api/0.1039.0/nominal_api-0.1039.0.tar.gz

---
+++ packages/nominal-api/nominal_api-0.1039.0.tar.gz/nominal_api-0.1039.0/nominal_api/storage_deletion_api/__init__.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+from .._impl import (
+    storage_deletion_api_DeleteDataRequest as DeleteDataRequest,
+    storage_deletion_api_InternalNominalStorageDataDeletionService as InternalNominalStorageDataDeletionService,
+    storage_deletion_api_TimeRange as TimeRange,
+)
+
+__all__ = [
+    'DeleteDataRequest',
+    'TimeRange',
+    'InternalNominalStorageDataDeletionService',
+]
+
