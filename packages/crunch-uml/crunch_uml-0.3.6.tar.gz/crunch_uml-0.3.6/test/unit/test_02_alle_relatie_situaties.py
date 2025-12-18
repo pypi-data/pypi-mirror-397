e57cdbb3bff0360d45476de270f20213c948e546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 13:18:00 +0000

    Add package crunch-uml/0.3.6/crunch_uml-0.3.6.tar.gz

---
+++ packages/crunch-uml/crunch_uml-0.3.6.tar.gz/crunch_uml-0.3.6/test/unit/test_02_alle_relatie_situaties.py
@@ -0,0 +1,25 @@
+import crunch_uml.schema as sch
+from crunch_uml import cli, const, db
+
+
+def test_relations():
+    test_args = [
+        "import",
+        "-f",
+        "./test/data/AlleRelatieSitiuaties.xml",
+        "-t",
+        "xmi",
+        "-db_create",
+    ]
+    cli.main(test_args)
+
+    database = db.Database(const.DATABASE_URL, db_create=False)
+    schema = sch.Schema(database)
+
+    assert schema.count_package() == 1
+    assert schema.count_enumeratie() == 0
+    assert schema.count_class() == 14
+    assert schema.count_attribute() == 4
+    assert schema.count_enumeratieliteral() == 0
+    assert schema.count_association() == 5
+    assert schema.count_generalizations() == 0
