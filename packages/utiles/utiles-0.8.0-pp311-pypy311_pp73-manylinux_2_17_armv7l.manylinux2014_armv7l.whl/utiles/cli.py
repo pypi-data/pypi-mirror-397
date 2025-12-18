34c7bad0b9782844d2fb43a6096229172b75972c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 08:05:44 +0000

    Add package utiles/0.8.0/utiles-0.8.0-pp311-pypy311_pp73-manylinux_2_17_armv7l.manylinux2014_armv7l.whl

---
+++ packages/utiles/utiles-0.8.0-pp311-pypy311_pp73-manylinux_2_17_armv7l.manylinux2014_armv7l.whl/utiles/cli.py
@@ -0,0 +1,29 @@
+"""Utiles cli"""
+
+from __future__ import annotations
+
+import logging
+import sys
+
+from utiles import ut_cli
+
+
+def cli(
+    args: list[str | int | float | bool | None] | None = None,
+) -> None:
+    _args = args or sys.argv[1:]
+
+    # if first arg is "utiles" then remove it
+    if _args and (_args[0] == "utiles" or _args[0] == "ut"):
+        _args = _args[1:]
+
+    try:
+        ut_cli(["ut", *map(str, _args)])
+    except Exception as e:
+        logger = logging.getLogger(__name__)
+        logger.error(e)
+        raise e from e
+
+
+if __name__ == "__main__":
+    cli()
