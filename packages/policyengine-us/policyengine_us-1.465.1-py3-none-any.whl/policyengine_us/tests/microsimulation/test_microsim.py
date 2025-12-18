7aff053255c3e259852e6ec0ae79a30f2d912411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 20:05:28 +0000

    Add package policyengine-us/1.465.1/policyengine_us-1.465.1-py3-none-any.whl

---
+++ packages/policyengine-us/policyengine_us-1.465.1-py3-none-any.whl/policyengine_us/tests/microsimulation/test_microsim.py
@@ -0,0 +1,35 @@
+import pytest
+
+DATASETS = [
+    "hf://policyengine/policyengine-us-data/cps_2023.h5",
+    "hf://policyengine/policyengine-us-data/enhanced_cps_2024.h5",
+]
+
+YEARS = list(range(2024, 2026))
+
+
+@pytest.mark.parametrize("dataset", DATASETS)
+@pytest.mark.parametrize("year", YEARS)
+def test_microsim_runs(dataset: str, year: int):
+    import numpy as np
+    from policyengine_us import Microsimulation
+
+    sim = Microsimulation(dataset=dataset)
+    sim.subsample(1_000)
+    hnet = sim.calc("household_net_income", period=year)
+    assert not hnet.isna().any(), "Some households have NaN net income."
+    # Deciles are 1-10, with -1 for negative income.
+    DECILES = [
+        "household_income_decile",
+        "spm_unit_income_decile",
+        "income_decile",
+    ]
+    for decile_var in DECILES:
+        decile = sim.calc(decile_var)
+        assert np.all(decile >= -1) and np.all(
+            decile <= 10
+        ), f"{decile_var} out of bounds."
+
+    # Check that the microsim calculates important variables as nonzero in current year.
+    for var in ["employment_income", "self_employment_income"]:
+        assert sim.calc(var, period=2024).sum() > 0, f"{var} is zero in 2024."
