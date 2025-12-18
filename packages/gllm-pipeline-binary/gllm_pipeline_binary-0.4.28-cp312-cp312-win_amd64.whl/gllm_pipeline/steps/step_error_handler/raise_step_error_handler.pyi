34f7f12973c181a0a0a7415c42232024b3f342e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 21:06:32 +0000

    Add package gllm-pipeline-binary/0.4.28/gllm_pipeline_binary-0.4.28-cp312-cp312-win_amd64.whl

---
+++ packages/gllm-pipeline-binary/gllm_pipeline_binary-0.4.28-cp312-cp312-win_amd64.whl/gllm_pipeline/steps/step_error_handler/raise_step_error_handler.pyi
@@ -0,0 +1,9 @@
+from gllm_pipeline.steps.step_error_handler.step_error_handler import BaseStepErrorHandler as BaseStepErrorHandler
+from gllm_pipeline.utils.error_handling import ErrorContext as ErrorContext
+from langgraph.runtime import Runtime as Runtime
+from pydantic import BaseModel as BaseModel
+
+class RaiseStepErrorHandler(BaseStepErrorHandler):
+    """Strategy that raises exceptions with enhanced context."""
+    def __init__(self) -> None:
+        """Initialize the raise error handler."""
