645c567ccd01e1a3b3af29fa5c71904bc8185160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 20:06:13 +0000

    Add package Geode-Conversion/6.5.9/geode_conversion-6.5.9-cp312-cp312-manylinux_2_28_x86_64.whl

---
+++ packages/Geode-Conversion/geode_conversion-6.5.9-cp312-cp312-manylinux_2_28_x86_64.whl/geode_conversion/model_conversion.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2019 - 2025 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+import geode_common
+
+from .lib64.geode_conversion_py_model import *
+
+ConversionModelLibrary.initialize()
