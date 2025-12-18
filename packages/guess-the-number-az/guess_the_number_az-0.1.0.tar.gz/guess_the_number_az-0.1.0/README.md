2b097c09b8ea0bc78883e272ccbf592edae0a665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 16:07:22 +0000

    Add package guess-the-number-az/0.1.0/guess_the_number_az-0.1.0.tar.gz

---
+++ packages/guess-the-number-az/guess_the_number_az-0.1.0.tar.gz/guess_the_number_az-0.1.0/README.md
@@ -0,0 +1,10 @@
+# 🔢 Guess the Number (Ədədi Tap) AZ
+
+Azərbaycan dilində sadə, lakin cəlbedici, terminal əsaslı "Ədədi Tap" oyunu.
+
+## 🚀 Quraşdırma
+
+PyPI-dən quraşdırın:
+
+```bash
+pip install guess-the-number-az
---
+++ packages/guess-the-number-az/guess_the_number_az-0.1.0.tar.gz/guess_the_number_az-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='guess-the-number-az',
+    version='0.1.0',
+    packages=find_packages(),
+    description='Azerbaycan dilində "Ədədi Tap" mini oyunu.',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Sizin-Github-Adınız/guess-the-number-az', # GitHub linkinizi qeyd edin
+    author='Sizin Adınız',
+    author_email='sizin_emailiniz@example.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Games/Entertainment',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    keywords='game, number, terminal, python, azerbaijan',
+    python_requires='>=3.6',
+)
