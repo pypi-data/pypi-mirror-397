a23a82af805c4a70d4384d7a66a35d627e18d196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 07:08:11 +0000

    Add package aionetifaces/0.0.2/aionetifaces-0.0.2.tar.gz

---
+++ packages/aionetifaces/aionetifaces-0.0.2.tar.gz/aionetifaces-0.0.2/aionetifaces/init.py
@@ -0,0 +1,6 @@
+raise ImportError(
+    "The package 'aionetifaces' is a placeholder.\n"
+    "The real package is named 'aionetiface'.\n"
+    "Install it with:\n"
+    "  pip install aionetiface\n"
+)
