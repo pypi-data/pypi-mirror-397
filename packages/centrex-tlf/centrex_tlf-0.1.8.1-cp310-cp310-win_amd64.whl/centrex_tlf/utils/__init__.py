a99c1519ebf41204fea7154c3526a4113efd0efa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 08:06:29 +0000

    Add package centrex-tlf/0.1.8.1/centrex_tlf-0.1.8.1-cp310-cp310-win_amd64.whl

---
+++ packages/centrex-tlf/centrex_tlf-0.1.8.1-cp310-cp310-win_amd64.whl/centrex_tlf/utils/__init__.py
@@ -0,0 +1,14 @@
+"""Utility functions for TlF molecular spectroscopy calculations.
+
+This module provides utilities for:
+- Doppler shifts and velocity-to-detuning conversions
+- Multipass beam geometry and intensity profiles
+- Thermal and uniform population distributions
+- Rabi frequency and beam power calculations
+"""
+from . import detuning, multipass, population, rabi
+
+__all__ = detuning.__all__
+__all__ += multipass.__all__
+__all__ += population.__all__
+__all__ += rabi.__all__
