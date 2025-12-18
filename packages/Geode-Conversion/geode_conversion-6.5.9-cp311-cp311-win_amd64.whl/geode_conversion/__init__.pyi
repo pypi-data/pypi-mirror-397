2b4846e2a75fb4f593b440b08ba0c6ddff8f84d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 20:06:11 +0000

    Add package Geode-Conversion/6.5.9/geode_conversion-6.5.9-cp311-cp311-win_amd64.whl

---
+++ packages/Geode-Conversion/geode_conversion-6.5.9-cp311-cp311-win_amd64.whl/geode_conversion/__init__.pyi
@@ -0,0 +1,16 @@
+from __future__ import annotations
+import geode_common as geode_common
+from geode_conversion.bin.geode_conversion_py_model import ConversionModelLibrary
+from geode_conversion.bin.geode_conversion_py_model import add_brep_sharp_features
+from geode_conversion.bin.geode_conversion_py_model import add_section_sharp_features
+from geode_conversion.bin.geode_conversion_py_model import convert_meshes_into_brep
+from geode_conversion.bin.geode_conversion_py_model import convert_meshes_into_section
+from geode_conversion.bin.geode_conversion_py_model import convert_solid_elements_into_brep
+from geode_conversion.bin.geode_conversion_py_model import convert_surface_elements_into_section
+from geode_conversion.bin.geode_conversion_py_model import convert_surface_into_section_from_attribute
+import opengeode as opengeode
+import os as os
+import pathlib as pathlib
+from . import bin
+from . import model_conversion
+__all__: list[str] = ['ConversionModelLibrary', 'add_brep_sharp_features', 'add_section_sharp_features', 'bin', 'convert_meshes_into_brep', 'convert_meshes_into_section', 'convert_solid_elements_into_brep', 'convert_surface_elements_into_section', 'convert_surface_into_section_from_attribute', 'geode_common', 'model_conversion', 'opengeode', 'os', 'pathlib']
