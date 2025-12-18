71a957b6795ae9d7f848f8935074f43dc65c3e79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 08:07:07 +0000

    Add package media-downloader/2.1.20/media_downloader-2.1.20-py3-none-any.whl

---
+++ packages/media-downloader/media_downloader-2.1.20-py3-none-any.whl/media_downloader/__init__.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import importlib
+import inspect
+
+# List of modules to import from
+MODULES = [
+    "media_downloader.media_downloader",
+    "media_downloader.media_downloader_mcp",
+    "media_downloader.media_downloader_a2a",
+]
+
+# Initialize __all__ to expose all public classes and functions
+__all__ = []
+
+# Dynamically import all classes and functions from the specified modules
+for module_name in MODULES:
+    module = importlib.import_module(module_name)
+    for name, obj in inspect.getmembers(module):
+        # Include only classes and functions, excluding private (starting with '_')
+        if (inspect.isclass(obj) or inspect.isfunction(obj)) and not name.startswith(
+            "_"
+        ):
+            globals()[name] = obj
+            __all__.append(name)
+
+"""
+media-downloader
+
+Download videos and audio from the internet!
+"""
