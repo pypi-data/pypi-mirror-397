0c921ded084e662007eb5e2c2310a084f1ba7cf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 12:05:18 +0000

    Add package pyprofqueue/0.4.1a12/pyprofqueue-0.4.1a12.tar.gz

---
+++ packages/pyprofqueue/pyprofqueue-0.4.1a12.tar.gz/pyprofqueue-0.4.1a12/pyprofqueue/batch_systems/slurm.py
@@ -0,0 +1,38 @@
+parameters = {
+    'queue_name': 'Slurm',
+    'Option_Flag': '#SBATCH',
+    'submission_command': 'sbatch',
+    'environment_variable':
+        {
+            'job_array_index': '${SLURM_ARRAY_TASK_ID}',
+            'job_id': '${SLURM_JOB_ID}',
+            'job_name': '${SLURM_JOB_NAME}',
+            'node_list': '${SLURM_JOB_NODELIST}',
+            'submit_directory': '${SLURM_SUBMIT_DIR}',
+            'submit_host': '${SLURM_SUBMIT_HOST}'
+        },
+    'option_environment_variable': {'${SLURM_JOB_NAME}': '%x', '${SLURM_JOB_ID}': '%j', 'CPU_PER_NODE': '${SLURM_JOB_CPUS_PER_NODE}'},
+    'options':
+        {
+            'account': ['A', 'account'],
+            'work_dir': ['D', 'chdir'],
+            'cores': ['c', 'cpus-per-task'],        # CPU per Task
+            'error_file': ['e'],
+            'job_dependency': ['d', 'dependency'],
+            'GPUs': ['G', 'gpus'],
+            'Generic_resource_list': ['gres'],
+            'GPUS_perN': ['gpus-per-node'],
+            'job_name': ['J', 'job-name'],
+            'event_notification': ['mail-type'],
+            'event_email': ['mail-user'],
+            'memory': ['mem'],
+            'mpc': ['mem-per-cpu'],                 # Memory per CPU
+            'nodes': ['N', 'nodes'],
+            'n_tasks': ['n', 'ntasks'],
+            'tpn': ['ntasks-per-node'],             # Tasks per Node
+            'output_file': ['o', 'out'],
+            'partition': ['p', 'partition'],
+            'time': ['t', 'time'],
+            'user': ['uid']
+        }
+}
