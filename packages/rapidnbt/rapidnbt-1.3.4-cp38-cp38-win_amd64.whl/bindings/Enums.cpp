997233c0d2430b834204c4386be664adc47afaa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:30:36 +0000

    Add package rapidnbt/1.3.4/rapidnbt-1.3.4-cp38-cp38-win_amd64.whl

---
+++ packages/rapidnbt/rapidnbt-1.3.4-cp38-cp38-win_amd64.whl/bindings/Enums.cpp
@@ -0,0 +1,129 @@
+// Copyright © 2025 GlacieTeam.All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+// distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// SPDX-License-Identifier: MPL-2.0
+
+#include "NativeModule.hpp"
+
+namespace py = pybind11;
+
+namespace rapidnbt {
+
+void bindEnums(py::module& m) {
+    {
+        auto sm = m.def_submodule("tag_type");
+
+        py::native_enum<nbt::Tag::Type>(sm, "TagType", "enum.Enum", "The tag type enum")
+            .value("End", nbt::Tag::Type::End)
+            .value("Byte", nbt::Tag::Type::Byte)
+            .value("Short", nbt::Tag::Type::Short)
+            .value("Int", nbt::Tag::Type::Int)
+            .value("Long", nbt::Tag::Type::Long)
+            .value("Float", nbt::Tag::Type::Float)
+            .value("Double", nbt::Tag::Type::Double)
+            .value("ByteArray", nbt::Tag::Type::ByteArray)
+            .value("String", nbt::Tag::Type::String)
+            .value("List", nbt::Tag::Type::List)
+            .value("Compound", nbt::Tag::Type::Compound)
+            .value("IntArray", nbt::Tag::Type::IntArray)
+            .value("LongArray", nbt::Tag::Type::LongArray)
+            .export_values()
+            .finalize();
+    }
+    {
+        auto sm = m.def_submodule(
+            "snbt_format",
+            R"(The SNBT format enum
+            You can use | operation to combime flags
+            Example:
+                format = SnbtFormat.Classic | SnbtFormat.ForceUppercase)"
+        );
+
+        py::native_enum<nbt::SnbtFormat>(sm, "SnbtFormat", "enum.IntFlag")
+            .value("Minimize", nbt::SnbtFormat::Minimize)
+            .value("CompoundLineFeed", nbt::SnbtFormat::CompoundLineFeed)
+            .value("ListArrayLineFeed", nbt::SnbtFormat::ListArrayLineFeed)
+            .value("BinaryArrayLineFeed", nbt::SnbtFormat::BinaryArrayLineFeed)
+            .value("ForceLineFeedIgnoreIndent", nbt::SnbtFormat::ForceLineFeedIgnoreIndent)
+            .value("ForceAscii", nbt::SnbtFormat::ForceAscii)
+            .value("ForceKeyQuote", nbt::SnbtFormat::ForceKeyQuote)
+            .value("ForceValueQuote", nbt::SnbtFormat::ForceValueQuote)
+            .value("ForceUppercase", nbt::SnbtFormat::ForceUppercase)
+            .value("MarkIntTag", nbt::SnbtFormat::MarkIntTag)
+            .value("MarkDoubleTag", nbt::SnbtFormat::MarkDoubleTag)
+            .value("CommentMarks", nbt::SnbtFormat::CommentMarks)
+            .value("MarkSigned", nbt::SnbtFormat::MarkSigned)
+            .value("ArrayLineFeed", nbt::SnbtFormat::ArrayLineFeed)
+            .value("AlwaysLineFeed", nbt::SnbtFormat::AlwaysLineFeed)
+            .value("MarkAllTags", nbt::SnbtFormat::MarkAllTags)
+            .value("PrettyFilePrint", nbt::SnbtFormat::PrettyFilePrint)
+            .value("ForceQuote", nbt::SnbtFormat::ForceQuote)
+            .value("Classic", nbt::SnbtFormat::Classic)
+            .value("Jsonify", nbt::SnbtFormat::Jsonify)
+            .value("Default", nbt::SnbtFormat::Default)
+            .export_values()
+            .finalize();
+
+        py::native_enum<nbt::SnbtNumberFormat>(sm, "SnbtNumberFormat", "enum.Enum")
+            .value("Decimal", nbt::SnbtNumberFormat::Decimal)
+            .value("LowerHexadecimal", nbt::SnbtNumberFormat::LowerHexadecimal)
+            .value("UpperHexadecimal", nbt::SnbtNumberFormat::UpperHexadecimal)
+            .value("Binary", nbt::SnbtNumberFormat::Binary)
+            .export_values()
+            .finalize();
+    }
+    {
+        auto sm = m.def_submodule("nbt_file_format");
+
+        py::native_enum<nbt::NbtFileFormat>(sm, "NbtFileFormat", "enum.Enum", "Enumeration of NBT binary file formats")
+            .value("LITTLE_ENDIAN", nbt::NbtFileFormat::LittleEndian)
+            .value("LITTLE_ENDIAN_WITH_HEADER", nbt::NbtFileFormat::LittleEndianWithHeader)
+            .value("BIG_ENDIAN", nbt::NbtFileFormat::BigEndian)
+            .value("BIG_ENDIAN_WITH_HEADER", nbt::NbtFileFormat::BigEndianWithHeader)
+            .value("BEDROCK_NETWORK", nbt::NbtFileFormat::BedrockNetwork)
+            .export_values()
+            .finalize();
+    }
+    {
+        auto sm = m.def_submodule("nbt_compression_type");
+
+        py::native_enum<nbt::NbtCompressionType>(
+            sm,
+            "NbtCompressionType",
+            "enum.Enum",
+            "Enumeration of compression types for NBT serialization"
+        )
+            .value("NONE", nbt::NbtCompressionType::None)
+            .value("GZIP", nbt::NbtCompressionType::Gzip)
+            .value("ZLIB", nbt::NbtCompressionType::Zlib)
+            .export_values()
+            .finalize();
+    }
+    {
+        auto sm = m.def_submodule("nbt_compression_level");
+
+        py::native_enum<nbt::NbtCompressionLevel>(
+            sm,
+            "NbtCompressionLevel",
+            "enum.IntEnum",
+            "Enumeration of compression levels"
+        )
+            .value("DEFAULT", nbt::NbtCompressionLevel::Default)
+            .value("NO_COMPRESSION", nbt::NbtCompressionLevel::NoCompression)
+            .value("BEST_SPEED", nbt::NbtCompressionLevel::BestSpeed)
+            .value("LOW", nbt::NbtCompressionLevel::Low)
+            .value("MEDIUM_LOW", nbt::NbtCompressionLevel::MediumLow)
+            .value("MEDIUM", nbt::NbtCompressionLevel::Medium)
+            .value("MEDIUM_HIGH", nbt::NbtCompressionLevel::MediumHigh)
+            .value("HIGH", nbt::NbtCompressionLevel::High)
+            .value("VERY_HIGH", nbt::NbtCompressionLevel::VeryHigh)
+            .value("ULTRA", nbt::NbtCompressionLevel::Ultra)
+            .value("BEST_COMPRESSION", nbt::NbtCompressionLevel::BestCompression)
+            .export_values()
+            .finalize();
+    }
+}
+
+} // namespace rapidnbt
---
+++ packages/rapidnbt/rapidnbt-1.3.4-cp38-cp38-win_amd64.whl/bindings/ListTag.cpp
@@ -0,0 +1,226 @@
+// Copyright © 2025 GlacieTeam.All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+// distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// SPDX-License-Identifier: MPL-2.0
+
+#include "NativeModule.hpp"
+
+namespace rapidnbt {
+
+void bindListTag(py::module& m) {
+    auto sm = m.def_submodule("list_tag", "A tag contains a tag list");
+
+    py::class_<nbt::ListTag, nbt::Tag>(sm, "ListTag")
+        .def(py::init<>(), "Construct an empty ListTag")
+        .def(
+            py::init([](std::vector<py::object> elements) {
+                auto result = std::make_unique<nbt::ListTag>();
+                for (auto& element : elements) { result->push_back(makeNativeTag(static_cast<py::object&>(element))); }
+                result->checkAndFixElements();
+                return result;
+            }),
+            py::arg("elements"),
+            "Construct from a list of Tag elements (e.g., [IntTag(1), StringTag('test')])"
+        )
+
+        .def("get_type", &nbt::ListTag::getType, "Get the NBT type ID (List)")
+        .def(
+            "equals",
+            &nbt::ListTag::equals,
+            py::arg("other"),
+            "Check if this tag equals another tag (same elements in same order)"
+        )
+        .def("copy", &nbt::ListTag::copy, "Create a deep copy of this tag")
+
+        .def(
+            "write",
+            [](nbt::ListTag& self, bstream::BinaryStream& stream) { self.write(stream); },
+            py::arg("stream"),
+            "Write list to a binary stream"
+        )
+        .def(
+            "load",
+            [](nbt::ListTag& self, bstream::ReadOnlyBinaryStream& stream) { self.load(stream); },
+            py::arg("stream"),
+            "Load list from a binary stream"
+        )
+
+        .def(
+            "append",
+            [](nbt::ListTag& self, py::object const& element, bool checkType) {
+                auto tag = makeNativeTag(element);
+                if (checkType) {
+                    auto type = self.getElementType();
+                    if (type == tag->getType() || type == nbt::Tag::Type::End) {
+                        self.push_back(std::move(tag));
+                    } else {
+                        throw py::value_error(
+                            std::format(
+                                "New tag type must be same as the original element type in the ListTag[{1}], "
+                                "received type: {0}, expect types can be converted to {1}Tag",
+                                py_type_name(element),
+                                magic_enum::enum_name(type)
+                            )
+                        );
+                    }
+                } else {
+                    self.push_back(std::move(tag));
+                }
+            },
+            py::arg("element"),
+            py::arg("check_type") = true,
+            "Append a Tag element if self is ListTag"
+            "Throw TypeError if wrong type and check_type is True"
+            ""
+            "Args:"
+            "    value (Any): value append to ListTag"
+            "    check_type (bool): check value type is same as the type that ListTag holds"
+        )
+
+        .def(
+            "__getitem__",
+            [](nbt::ListTag& self, size_t index) -> nbt::CompoundTagVariant& {
+                if (index >= self.size()) { throw py::index_error("Index out of range"); }
+                return self[index];
+            },
+            py::return_value_policy::reference_internal,
+            py::arg("index"),
+            "Get element at specified index"
+        )
+        .def(
+            "__setitem__",
+            [](nbt::ListTag& self, size_t index, py::object const& element) {
+                if (index >= self.size()) { throw py::index_error("Index out of range"); }
+                self[index] = *makeNativeTag(element);
+            },
+            py::arg("index"),
+            py::arg("element"),
+            "Set element at specified index"
+        )
+
+        .def("size", &nbt::ListTag::size, "Get number of elements in the list")
+        .def("empty", &nbt::ListTag::empty, "Check if the list is empty")
+        .def(
+            "get_element_type",
+            [](nbt::ListTag const& self) { return self.getElementType(); },
+            "Get the type of elements in this list (returns nbt.Type enum)"
+        )
+        .def("reserve", &nbt::ListTag::reserve, py::arg("size"), "Preallocate memory for future additions")
+        .def(
+            "pop",
+            [](nbt::ListTag& self, size_t index) { return self.remove(index); },
+            py::arg("index"),
+            "Remove element at specified index"
+        )
+        .def(
+            "pop",
+            [](nbt::ListTag& self, size_t start, size_t end) { return self.remove(start, end); },
+            py::arg("start_index"),
+            py::arg("end_index"),
+            "Remove elements in the range [start_index, end_index)"
+        )
+        .def("clear", &nbt::ListTag::clear, "Remove all elements from the list")
+        .def(
+            "merge",
+            &nbt::ListTag::merge,
+            py::arg("other"),
+            "Merge another ListTag into this one (appends all elements)"
+        )
+
+        .def(
+            "insert",
+            [](nbt::ListTag& self, size_t index, py::object const& element, bool checkType) {
+                if (index > self.size()) { throw py::index_error("Index out of range"); }
+                auto it = self.begin();
+                std::advance(it, index);
+                auto tag = makeNativeTag(element);
+                if (checkType) {
+                    auto type = self.getElementType();
+                    if (type == tag->getType() || type == nbt::Tag::Type::End) {
+                        self.storage().insert(it, std::move(tag));
+                    } else {
+                        throw py::value_error(
+                            std::format(
+                                "New tag type must be same as the original element type in the ListTag[{1}], "
+                                "received type: {0}, expect types can be converted to {1}Tag",
+                                py_type_name(element),
+                                magic_enum::enum_name(type)
+                            )
+                        );
+                    }
+                } else {
+                    self.storage().insert(it, std::move(tag));
+                }
+            },
+            py::arg("index"),
+            py::arg("element"),
+            py::arg("check_type") = true,
+            "Insert element at specified position"
+            "Throw TypeError if wrong type and check_type is True"
+            ""
+            "Args:"
+            "    value (Any): value append to ListTag"
+            "    check_type (bool): check value type is same as the type that ListTag holds"
+        )
+        .def(
+            "check_and_fix_list_elements",
+            &nbt::ListTag::checkAndFixElements,
+            "Check the whether elements in this ListTag is the same, and fix it."
+        )
+        .def(
+            "to_list",
+            [](nbt::ListTag& self) -> py::list {
+                py::list result;
+                for (auto& tag : self) { result.append(py::cast(tag)); }
+                return result;
+            }
+        )
+
+        .def_property(
+            "value",
+            [](nbt::ListTag& self) -> py::list {
+                py::list result;
+                for (auto& tag : self) { result.append(py::cast(tag)); }
+                return result;
+            },
+            [](nbt::ListTag& self, py::list const& value) {
+                self.clear();
+                for (auto const& element : value) {
+                    self.push_back(makeNativeTag(static_cast<py::object const&>(element)));
+                    self.checkAndFixElements();
+                }
+            },
+            "Access the list value of this tag"
+        )
+
+        .def("__len__", &nbt::ListTag::size, "Get number of elements in the list")
+        .def(
+            "__iter__",
+            [](nbt::ListTag& self) { return py::make_iterator(self.begin(), self.end()); },
+            py::keep_alive<0, 1>(),
+            "Iterate over elements in the list"
+        )
+        .def("__eq__", &nbt::ListTag::equals, py::arg("other"), "Equality operator (==)")
+        .def("__hash__", &nbt::ListTag::hash, "Compute hash value for Python hashing operations")
+        .def(
+            "__str__",
+            [](nbt::ListTag const& self) { return self.toSnbt(nbt::SnbtFormat::Minimize); },
+            "String representation (SNBT minimized format)"
+        )
+        .def(
+            "__repr__",
+            [](nbt::ListTag const& self) {
+                return std::format(
+                    "<rapidnbt.ListTag(size={0}) object at 0x{1:0{2}X}>",
+                    self.size(),
+                    reinterpret_cast<uintptr_t>(&self),
+                    ADDRESS_LENGTH
+                );
+            },
+            "Official string representation"
+        );
+}
+
+} // namespace rapidnbt
