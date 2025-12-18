54138d11c203ead0699e1d2827a49f104140f516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:30:25 +0000

    Add package eagle-tools/0.6.0/eagle_tools-0.6.0.tar.gz

---
+++ packages/eagle-tools/eagle_tools-0.6.0.tar.gz/eagle_tools-0.6.0/src/eagle/tools/utils.py
@@ -0,0 +1,24 @@
+import os
+import logging
+import yaml
+
+logger = logging.getLogger("eagle.tools")
+
+def open_yaml_config(config_filename: str):
+    with open(config_filename, "r") as f:
+        config = yaml.safe_load(f)
+
+    # expand any environment variables
+    for key, val in config.items():
+        if "path" in key:
+            if isinstance(val, str):
+                config[key] = os.path.expandvars(val)
+            else:
+                logger.warning(f"Not expanding environment variables in {key} in config, since it could be many different types")
+
+    # if output_path is not created, make it here
+    if "output_path" in config and not os.path.isdir(config["output_path"]):
+        logger.info(f"Creating output_path: {config['output_path']}")
+        os.makedirs(config["output_path"])
+
+    return config
