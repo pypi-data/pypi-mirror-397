1f57ee101334c82d14d340c0f202493ae8a712b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 17:08:31 +0000

    Add package abstract-hugpy/0.0.0.288/abstract_hugpy-0.0.0.288.tar.gz

---
+++ packages/abstract-hugpy/abstract_hugpy-0.0.0.288.tar.gz/abstract_hugpy-0.0.0.288/src/abstract_hugpy/imports/src/imports/video_imports.py
@@ -0,0 +1,7 @@
+from m3u8 import M3U8  # Install: pip install m3u8
+from yt_dlp import YoutubeDL
+from moviepy.editor import *
+import multiprocessing as mproc
+import moviepy.editor as mp
+from yt_dlp.postprocessor.ffmpeg import FFmpegFixupPostProcessor
+import m3u8_To_MP4,yt_dlp
