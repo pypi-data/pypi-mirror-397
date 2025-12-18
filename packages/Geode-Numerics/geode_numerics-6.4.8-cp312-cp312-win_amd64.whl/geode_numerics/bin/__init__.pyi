9909a58f9ff953e15edc8c5fa04191544c45be83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 07:08:12 +0000

    Add package Geode-Numerics/6.4.8/geode_numerics-6.4.8-cp312-cp312-win_amd64.whl

---
+++ packages/Geode-Numerics/geode_numerics-6.4.8-cp312-cp312-win_amd64.whl/geode_numerics/bin/__init__.pyi
@@ -0,0 +1,6 @@
+from __future__ import annotations
+from . import geode_numerics_py_core
+from . import geode_numerics_py_frame_field
+from . import geode_numerics_py_scalar_function
+from . import geode_numerics_py_surface
+__all__: list[str] = ['geode_numerics_py_core', 'geode_numerics_py_frame_field', 'geode_numerics_py_scalar_function', 'geode_numerics_py_surface']
