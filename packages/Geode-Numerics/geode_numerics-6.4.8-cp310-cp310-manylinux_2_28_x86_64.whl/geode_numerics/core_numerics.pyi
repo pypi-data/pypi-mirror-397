00d0f8caf75da79483f5632a1c3651370a1b1a4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 07:08:07 +0000

    Add package Geode-Numerics/6.4.8/geode_numerics-6.4.8-cp310-cp310-manylinux_2_28_x86_64.whl

---
+++ packages/Geode-Numerics/geode_numerics-6.4.8-cp310-cp310-manylinux_2_28_x86_64.whl/geode_numerics/core_numerics.pyi
@@ -0,0 +1,4 @@
+from __future__ import annotations
+from geode_numerics.lib64.geode_numerics_py_core import NumericsCoreLibrary
+import opengeode as opengeode
+__all__: list[str] = ['NumericsCoreLibrary', 'opengeode']
