8fbb0f0d5a71967ec8d786ffddf8cab541abeaec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:30:51 +0000

    Add package gnuhealth-genetics/5.0.2/gnuhealth_genetics-5.0.2-py3-none-any.whl

---
+++ packages/gnuhealth-genetics/gnuhealth_genetics-5.0.2-py3-none-any.whl/trytond/modules/health_genetics/__init__.py
@@ -0,0 +1,26 @@
+# SPDX-FileCopyrightText: 2008-2025 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2025 GNU Solidario <health@gnusolidario.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#########################################################################
+#   Hospital Management Information System (HMIS) component of the      #
+#                       GNU Health project                              #
+#                   https://www.gnuhealth.org                           #
+#########################################################################
+#                     HEALTH GENETICS package                           #
+#              __init__.py: Package declaration file                    #
+#########################################################################
+
+from trytond.pool import Pool
+from . import health_genetics
+
+
+def register():
+    Pool.register(
+        health_genetics.Gene,
+        health_genetics.ProteinDisease,
+        health_genetics.GeneVariant,
+        health_genetics.GeneVariantPhenotype,
+        health_genetics.PatientGeneticRisk,
+        health_genetics.GnuHealthPatient,
+        module='health_genetics', type_='model')
