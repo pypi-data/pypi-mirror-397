cfab7cf3b6db944327f9ab3efb628c4e3c8eee0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:30:26 +0000

    Add package heros-boss/0.6.0/heros_boss-0.6.0.tar.gz

---
+++ packages/heros-boss/heros_boss-0.6.0.tar.gz/heros_boss-0.6.0/src/boss/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.6.0"
+
+from .boss import BOSS
+
+__all__ = ["BOSS"]
---
+++ packages/heros-boss/heros_boss-0.6.0.tar.gz/heros_boss-0.6.0/src/boss/dummies.py
@@ -0,0 +1,38 @@
+import random
+import time
+
+
+class Dummy:
+    testme: int = 0
+
+    def __init__(self):
+        self.val = 3
+        print("call me maybe")
+
+    def read_temp(self, min: int, max: int) -> float:
+        result = random.randint(min, max)
+        print(f"returning result {result}")
+        print(f"btw, foovar is {self.foovar}")
+        return result
+
+    def hello(self) -> str:
+        self.testme += 1
+        return "world"
+
+    def _secret_hello(self) -> str:
+        self.testme -= 1
+        return "(secret) world"
+
+    def blocking_call(self, block_time: int):
+        print(f"I will start sleeping for {block_time} s now...")
+        time.sleep(block_time)
+        print("Done sleeping!")
+
+
+class PolledDatasourceDummy(Dummy):
+    foovar: str = ""
+    testme: int = 0
+
+    def _observable_data(self):
+        print("new data got called")
+        return self.testme
