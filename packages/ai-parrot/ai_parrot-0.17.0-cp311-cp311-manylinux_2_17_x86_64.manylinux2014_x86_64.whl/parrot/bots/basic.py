806d5fbe62762b1fbf4766adc4e485306459184f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:30:49 +0000

    Add package ai-parrot/0.17.0/ai_parrot-0.17.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ai-parrot/ai_parrot-0.17.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/parrot/bots/basic.py
@@ -0,0 +1,9 @@
+from .abstract import AbstractBot
+
+class BasicBot(AbstractBot):
+    """Represents an BasicBot in Navigator.
+
+        Each BasicBot has a name, a role, a goal, a backstory,
+        and an optional language model (llm).
+    """
+    pass
