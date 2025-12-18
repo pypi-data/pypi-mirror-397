c4e21768035a888714228dcce389c80e58504753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 14:07:42 +0000

    Add package VIStk/0.3.12.11/vistk-0.3.12.11.tar.gz

---
+++ packages/VIStk/vistk-0.3.12.11.tar.gz/vistk-0.3.12.11/VIStk/Objects/_Root.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from VIStk.Objects._WindowGeometry import WindowGeometry
+from VIStk.Objects._Window import Window
+
+class Root(Tk, Window):
+    """A wrapper for the Tk class with VIS attributes"""
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.WindowGeometry = WindowGeometry(self)
