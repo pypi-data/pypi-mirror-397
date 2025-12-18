43cf039e80c5a2ed2c8f1931727681a897a30ed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 08:05:09 +0000

    Add package gecatsim/1.6.6/gecatsim-1.6.6.tar.gz

---
+++ packages/gecatsim/gecatsim-1.6.6.tar.gz/gecatsim-1.6.6/setup.py
@@ -0,0 +1,36 @@
+# Copyright 2020, General Electric Company. All rights reserved. See https://github.com/xcist/main/blob/master/license/LICENSE
+
+# To install XCIST-CatSim, open Python console, run: pip install [folder name]
+# e.g., you can navigate to this folder, run: pip install .
+
+from setuptools import setup
+
+setup(name='gecatsim',
+      version='1.6.6',
+      description='Simulation toolkit for X-ray based cancer imaging',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/xcist/documentation/wiki',
+      author='Mingye Wu, Paul FitzGerald, James Jobin, Jiayong Zhang, Nandan Reddy, Anupama Debnath, Brion Sarachan, Bruno De Man',
+      author_email='Mingye.Wu@gehealthcare.com',
+      license='BSD 3-Clause License',
+      install_requires=['numpy', 'scipy', 'matplotlib', 'tqdm'],
+      packages=['gecatsim', 'gecatsim.pyfiles', 'gecatsim.dose', 'gecatsim.reconstruction'],
+      package_dir={'': 'src'},
+      zip_safe=False,
+      package_data={
+          'gecatsim':[r'bowtie/*.*', 
+                    r'cfg/*.*', 
+                    r'dose/*.*', r'dose/data/*.*', r'dose/examples/*.*', r'dose/lib/*.*', r'dose/pyfiles/*.*', r'dose/src/*.*', 
+                    r'examples/*.*', r'examples/cfg/*.*', r'examples/vct_examples/*.*',
+                    r'focal_spot/*.*',
+                    r'lib/*.*',
+                    r'material/*', r'material/edlp/*/*.dat',
+                    r'phantom/*.*', r'phantom/CatSimLogo_1024/*.*', r'phantom/poly_bin/poly*',
+                    r'pyfiles/FlatPanel/*.*',
+                    r'pyfiles/*.*',
+                    r'reconstruction/*.*', r'reconstruction/lib/*.*', r'reconstruction/pyfiles/*.*', r'reconstruction/src/*.*',
+                    r'response_matrix/*.*',
+                    r'scatter/*.*',
+                    r'spectrum/*.*']},
+      include_package_data=True)
