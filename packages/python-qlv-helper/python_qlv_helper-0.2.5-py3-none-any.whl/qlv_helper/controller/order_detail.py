1db4a751973ccc45f22f27f4eef40b3ee2c3a885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 09:07:35 +0000

    Add package python-qlv-helper/0.2.5/python_qlv_helper-0.2.5-py3-none-any.whl

---
+++ packages/python-qlv-helper/python_qlv_helper-0.2.5-py3-none-any.whl/qlv_helper/controller/order_detail.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+# ---------------------------------------------------------------------------------------------------------
+# ProjectName:  qlv-helper
+# FileName:     order_detail.py
+# Description:  订单详情页面控制器
+# Author:       ASUS
+# CreateDate:   2025/11/29
+# Copyright ©2011-2025. Hunan xxxxxxx Company limited. All rights reserved.
+# ---------------------------------------------------------------------------------------------------------
+"""
+import aiohttp
+from typing import Dict, Any, Optional
+from qlv_helper.http.order_page import parser_order_info, get_order_page_html, parser_order_flight_table
+
+
+async def get_order_info_with_http(
+        order_id: int, domain: str, protocol: str = "http", retry: int = 1, timeout: int = 5, enable_log: bool = True,
+        cookie_jar: Optional[aiohttp.CookieJar] = None, playwright_state: Dict[str, Any] = None
+) -> Dict[str, Any]:
+    response = await get_order_page_html(
+        order_id=order_id, domain=domain, protocol=protocol, retry=retry, timeout=timeout, enable_log=enable_log,
+        cookie_jar=cookie_jar, playwright_state=playwright_state
+    )
+    if response.get("code") != 200:
+        return response
+
+    html = response.get("data")
+    order_info = parser_order_info(html=html)
+    flight_info = parser_order_flight_table(html=html)
+    if flight_info:
+        order_info["flights"] = flight_info
+        order_info["peoples"] = flight_info
+    response["data"] = order_info
+    return response
