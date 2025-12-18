9d8b13946e4ed77f866deb75cbeed6861a96dcd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 17:07:49 +0000

    Add package matiopy/1.0.2/matiopy-1.0.2-py3-none-any.whl

---
+++ packages/matiopy/matiopy-1.0.2-py3-none-any.whl/matiopy/__init__.py
@@ -0,0 +1,5 @@
+from src.matiopy.transform2d.transform2d import Transform2D as transform2d
+from src.matiopy.vector2d.vector2d import Vector2D as vector2d
+
+__version__ = "1.0.2"
+__all__ = ['transform2d', 'vector2d']
