6004f3d0b3e513d9f0a6d4425e85e5fd226bd783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 16:06:32 +0000

    Add package musicdl/2.7.3/musicdl-2.7.3.tar.gz

---
+++ packages/musicdl/musicdl-2.7.3.tar.gz/musicdl-2.7.3/musicdl/modules/sources/__init__.py
@@ -0,0 +1,41 @@
+'''initialize'''
+from .qq import QQMusicClient
+from .mitu import MituMusicClient
+from .joox import JooxMusicClient
+from .base import BaseMusicClient
+from .kuwo import KuwoMusicClient
+from .migu import MiguMusicClient
+from .tidal import TIDALMusicClient
+from .lizhi import LizhiMusicClient
+from .apple import AppleMusicClient
+from .kugou import KugouMusicClient
+from .fangpi import FangpiMusicClient
+from .buguyy import BuguyyMusicClient
+from ..utils import BaseModuleBuilder
+from .netease import NeteaseMusicClient
+from .youtube import YouTubeMusicClient
+from .gequbao import GequbaoMusicClient
+from .jamendo import JamendoMusicClient
+from ..common import GDStudioMusicClient
+from .mp3juice import MP3JuiceMusicClient
+from .fivesing import FiveSingMusicClient
+from .qianqian import QianqianMusicClient
+from .ximalaya import XimalayaMusicClient
+from .bilibili import BilibiliMusicClient
+from .yinyuedao import YinyuedaoMusicClient
+
+
+'''MusicClientBuilder'''
+class MusicClientBuilder(BaseModuleBuilder):
+    REGISTERED_MODULES = {
+        'QQMusicClient': QQMusicClient, 'MituMusicClient': MituMusicClient, 'BuguyyMusicClient': BuguyyMusicClient, 'GequbaoMusicClient': GequbaoMusicClient,
+        'MP3JuiceMusicClient': MP3JuiceMusicClient, 'YinyuedaoMusicClient': YinyuedaoMusicClient, 'LizhiMusicClient': LizhiMusicClient, 'XimalayaMusicClient': XimalayaMusicClient,
+        'JooxMusicClient': JooxMusicClient, 'KuwoMusicClient': KuwoMusicClient, 'KugouMusicClient': KugouMusicClient, 'FiveSingMusicClient': FiveSingMusicClient,
+        'QianqianMusicClient': QianqianMusicClient, 'MiguMusicClient': MiguMusicClient, 'NeteaseMusicClient': NeteaseMusicClient, 'YouTubeMusicClient': YouTubeMusicClient,
+        'TIDALMusicClient': TIDALMusicClient, 'AppleMusicClient': AppleMusicClient, 'FangpiMusicClient': FangpiMusicClient, 'GDStudioMusicClient': GDStudioMusicClient,
+        'JamendoMusicClient': JamendoMusicClient, 'BilibiliMusicClient': BilibiliMusicClient,
+    }
+
+
+'''BuildMusicClient'''
+BuildMusicClient = MusicClientBuilder().build
