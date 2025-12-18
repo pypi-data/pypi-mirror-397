b892c07d9957443309c10c43168903a5e28dab83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:31:03 +0000

    Add package gnuhealth-inpatient-calendar/5.0.2/gnuhealth_inpatient_calendar-5.0.2-py3-none-any.whl

---
+++ packages/gnuhealth-inpatient-calendar/gnuhealth_inpatient_calendar-5.0.2-py3-none-any.whl/trytond/modules/health_inpatient_calendar/tests/test_health_inpatient_calendar.py
@@ -0,0 +1,33 @@
+# SPDX-FileCopyrightText: 2008-2025 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2025 GNU Solidario <health@gnusolidario.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+#########################################################################
+#   Hospital Management Information System (HMIS) component of the      #
+#                       GNU Health project                              #
+#                   https://www.gnuhealth.org                           #
+#########################################################################
+#                HEALTH INPATIENT CALENDAR package                      #
+#         test_health_inpatient_calendar.py main test module            #
+#########################################################################
+import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class HealthInpatientCalendarTestCase(ModuleTestCase):
+    '''
+    Test Health Inpatient Calendar module.
+    '''
+    module = 'health_inpatient_calendar'
+
+
+del ModuleTestCase
+
+
+def suite():
+    suite = trytond.tests.test_tryton.suite()
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        HealthInpatientCalendarTestCase))
+    return suite
