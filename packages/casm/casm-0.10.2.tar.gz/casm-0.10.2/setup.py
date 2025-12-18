438cb78a99cbdee8755807708f05e375b2e3d38b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 13:17:38 +0000

    Add package casm/0.10.2/casm-0.10.2.tar.gz

---
+++ packages/casm/casm-0.10.2.tar.gz/casm-0.10.2/setup.py
@@ -0,0 +1,56 @@
+# coding=utf-8
+
+from os.path import dirname
+from os.path import join
+from urllib.parse import urljoin
+
+from setuptools import find_packages
+from setuptools import setup
+from setuptools.command.install import install
+
+from casm.attribute import __author__
+from casm.attribute import __author_email__
+from casm.attribute import __description__
+from casm.attribute import __project__
+from casm.attribute import __urlhome__
+from casm.attribute import __version__
+
+__urlcode__ = __urlhome__
+__urldocs__ = __urlhome__
+__urlbugs__ = urljoin(__urlhome__, "issues")
+
+
+def all_requirements():
+    def read_requirements(path: str):
+        with open(path, "r", encoding="utf-8") as rhdl:
+            return rhdl.read().splitlines()
+
+    path: str = join(dirname(__file__), "requirements.txt")
+    requirements = read_requirements(path)
+    return requirements
+
+
+class CustomInstallCommand(install):
+    """Customized setuptools install command"""
+
+    def run(self):
+        install.run(self)  # Run the standard installation
+        # Execute your custom code after installation
+
+
+setup(
+    name=__project__,
+    version=__version__,
+    description=__description__,
+    url=__urlhome__,
+    author=__author__,
+    author_email=__author_email__,
+    project_urls={"Source Code": __urlcode__,
+                  "Bug Tracker": __urlbugs__,
+                  "Documentation": __urldocs__},
+    packages=find_packages(include=["casm*"], exclude=["casm.unittest"]),
+    install_requires=all_requirements(),
+    cmdclass={
+        "install": CustomInstallCommand,
+    }
+)
