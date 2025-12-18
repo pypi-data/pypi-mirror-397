9170f42b319cf98d66c69fac378185c4941c9870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:31:00 +0000

    Add package adaptive-cards-py/0.4.0/adaptive_cards_py-0.4.0-py3-none-any.whl

---
+++ packages/adaptive-cards-py/adaptive_cards_py-0.4.0-py3-none-any.whl/adaptive_cards/__init__.py
@@ -0,0 +1,11 @@
+"""
+This package provides small components for building adaptive cards compliant to
+the current interface definite with Python.
+
+[Schema Explorer](https://adaptivecards.io/explorer/)
+
+This `__init__.py` file exposes key components of the package for easier access:
+
+This module also initializes the package and ensures that any necessary
+configuration or setup is performed.
+"""
