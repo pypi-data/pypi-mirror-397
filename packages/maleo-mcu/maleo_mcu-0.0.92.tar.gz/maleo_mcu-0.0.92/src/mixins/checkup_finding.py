5c2cb05983e8b23cac3dd83aec146730810c35d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 09:07:00 +0000

    Add package maleo-mcu/0.0.92/maleo_mcu-0.0.92.tar.gz

---
+++ packages/maleo-mcu/maleo_mcu-0.0.92.tar.gz/maleo_mcu-0.0.92/src/mixins/checkup_finding.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel, Field
+from typing import Annotated, Generic
+from nexo.types.string import OptStrT
+
+
+class Recommendation(BaseModel, Generic[OptStrT]):
+    recommendation: Annotated[
+        OptStrT, Field(..., description="Checkup finding's recommendation")
+    ]
---
+++ packages/maleo-mcu/maleo_mcu-0.0.92.tar.gz/maleo_mcu-0.0.92/src/mixins/common.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel, Field
+from typing import Annotated, Generic
+from nexo.types.integer import OptIntT, OptListOfIntsT
+from nexo.types.misc import (
+    OptIntOrUUIDT,
+    OptListOfIntsOrUUIDsT,
+)
+
+
+class IncludeURL(BaseModel):
+    include_url: Annotated[bool, Field(False, description="Whether to include URL")] = (
+        False
+    )
+
+
+class CheckupId(BaseModel, Generic[OptIntT]):
+    checkup_id: Annotated[OptIntT, Field(..., description="Checkup's Id")]
+
+
+class CheckupIds(BaseModel, Generic[OptListOfIntsT]):
+    checkup_ids: Annotated[OptListOfIntsT, Field(..., description="Checkup's Ids")]
+
+
+class ClientId(BaseModel, Generic[OptIntOrUUIDT]):
+    client_id: Annotated[OptIntOrUUIDT, Field(..., description="Client's Id")]
+
+
+class ClientIds(BaseModel, Generic[OptListOfIntsOrUUIDsT]):
+    client_ids: Annotated[OptListOfIntsOrUUIDsT, Field(..., description="Client's Ids")]
+
+
+class ParameterIds(BaseModel, Generic[OptListOfIntsOrUUIDsT]):
+    parameter_ids: Annotated[
+        OptListOfIntsOrUUIDsT, Field(..., description="Parameter's Ids")
+    ]
