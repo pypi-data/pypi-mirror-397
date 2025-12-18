0c7b8e66fa04da9059b7224c8f57ccd628909b34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:31:08 +0000

    Add package gnuhealth-reporting/5.0.2/gnuhealth_reporting-5.0.2.tar.gz

---
+++ packages/gnuhealth-reporting/gnuhealth_reporting-5.0.2.tar.gz/gnuhealth_reporting-5.0.2/tests/test_health_reporting.py
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: 2008-2025 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2025 GNU Solidario <health@gnusolidario.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class HealthReportingTestCase(ModuleTestCase):
+    '''
+    Test Health Reporting module.
+    '''
+    module = 'health_reporting'
+
+
+del ModuleTestCase
+
+
+def suite():
+    suite = trytond.tests.test_tryton.suite()
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        HealthReportingTestCase))
+    return suite
