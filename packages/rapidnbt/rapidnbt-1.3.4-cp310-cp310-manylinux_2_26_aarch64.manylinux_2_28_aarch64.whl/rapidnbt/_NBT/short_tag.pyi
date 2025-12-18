3d5459b0d0ccb3d2ffce365e3c3efaa2ada1d2a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:31:11 +0000

    Add package rapidnbt/1.3.4/rapidnbt-1.3.4-cp310-cp310-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/rapidnbt/rapidnbt-1.3.4-cp310-cp310-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl/rapidnbt/_NBT/short_tag.pyi
@@ -0,0 +1,114 @@
+# Copyright © 2025 GlacieTeam.All rights reserved.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy
+# of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from typing import overload
+from rapidnbt._NBT.tag import Tag
+from rapidnbt._NBT.tag_type import TagType
+
+class ShortTag(Tag):
+    """
+    A tag contains a short
+    """
+
+    def __eq__(self, other: Tag) -> bool:
+        """
+        Equality operator (==)
+        """
+
+    def __hash__(self) -> int:
+        """
+        Compute hash value for Python hashing operations
+        """
+
+    @overload
+    def __init__(self) -> None:
+        """
+        Construct an ShortTag with default value (0)
+        """
+
+    @overload
+    def __init__(self, value: int) -> None:
+        """
+        Construct an ShortTag from an integer value
+        """
+
+    def __int__(self) -> int:
+        """
+        Convert to Python int
+        """
+
+    def __pos__(self) -> ShortTag:
+        """
+        Unary plus operator (+)
+        """
+
+    def __repr__(self) -> str:
+        """
+        Official string representation
+        """
+
+    def __str__(self) -> str:
+        """
+        String representation (SNBT minimized format)
+        """
+
+    def assign(self, value: int) -> ShortTag:
+        """
+        Assign a new integer value to this tag
+        """
+
+    def copy(self) -> Tag:
+        """
+        Create a deep copy of this tag
+        """
+
+    def equals(self, other: Tag) -> bool:
+        """
+        Check if this tag equals another tag
+        """
+
+    def get_signed(self) -> int:
+        """
+        Get the integer value as a signed value
+        """
+
+    def get_type(self) -> TagType:
+        """
+        Get the NBT type ID (Short)
+        """
+
+    def get_unsigned(self) -> int:
+        """
+        Get the integer value as an unsigned value
+        """
+
+    def hash(self) -> int:
+        """
+        Compute hash value of this tag
+        """
+
+    def load(self, stream: ...) -> None:
+        """
+        Load tag value from a binary stream
+        """
+
+    def write(self, stream: ...) -> None:
+        """
+        Write tag to a binary stream
+        """
+
+    @property
+    def value(self) -> int:
+        """
+        Access the integer value of this tag
+        """
+
+    @value.setter
+    def value(self, value: int) -> None:
+        """
+        Access the integer value of this tag
+        """
