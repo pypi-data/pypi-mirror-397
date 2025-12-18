639eedfa6e34b42c252f1e901dc725627e5c7df8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 15:05:47 +0000

    Add package llmskit/0.0.3/llmskit-0.0.3.tar.gz

---
+++ packages/llmskit/llmskit-0.0.3.tar.gz/llmskit-0.0.3/requires.py
@@ -0,0 +1,9 @@
+# -*- coding: UTF-8 -*-
+# @Time : 2023/9/27 17:02 
+# @Author : 刘洪波
+install_requires = [
+    'httpx>=0.27.0',
+    'loguru>=0.7.2',
+    'openai>=1.100.1',
+    'tenacity>=8.2.3',
+]
