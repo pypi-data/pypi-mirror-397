3dcc922a9f64e5c369a1c3a4fe14450950e9c58e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 14:06:36 +0000

    Add package OpenGeode-Geosciences/9.5.6rc1/opengeode_geosciences-9.5.6rc1-cp311-cp311-manylinux_2_28_x86_64.whl

---
+++ packages/OpenGeode-Geosciences/opengeode_geosciences-9.5.6rc1-cp311-cp311-manylinux_2_28_x86_64.whl/opengeode_geosciences/lib64/__init__.pyi
@@ -0,0 +1,4 @@
+from __future__ import annotations
+from . import opengeode_geosciences_py_explicit
+from . import opengeode_geosciences_py_implicit
+__all__: list[str] = ['opengeode_geosciences_py_explicit', 'opengeode_geosciences_py_implicit']
