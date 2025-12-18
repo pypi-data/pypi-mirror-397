078e606cae661013c52e592e047a4b78b8482c15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 08:05:02 +0000

    Add package ipmaths/0.1.1/ipmaths-0.1.1-py3-none-any.whl

---
+++ packages/ipmaths/ipmaths-0.1.1-py3-none-any.whl/ipmaths/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+""""""
+# SPDX-FileCopyrightText: 2025 Ben Bonacci <ben at benbonaccci dot com>
+# SPDX-License-Identifier: GPL-3.0-only
+""""""
+from .ipmaths import calc_ip4_hosts,calc_subnet_mask,check_mask_format,is_ip4_valid,is_mask_valid,crvt_mask_binary,crvt_mask_decimal
