142f65b3df2b21d469439fea102ab007dac7b6c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 17:08:25 +0000

    Add package ElecSolver/1.0.5/elecsolver-1.0.5-py3-none-any.whl

---
+++ packages/ElecSolver/elecsolver-1.0.5-py3-none-any.whl/ElecSolver/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "1.0.5"
+
+from .FrequencySystemBuilder import FrequencySystemBuilder
+from .TemporalSystemBuilder import TemporalSystemBuilder
+from .netlist_parser import NetlistParser
