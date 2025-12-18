3d6e2ed758e3fae2115c23ee4ea7e83517185456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:30:57 +0000

    Add package gnuhealth-dentistry/5.0.4/gnuhealth_dentistry-5.0.4.tar.gz

---
+++ packages/gnuhealth-dentistry/gnuhealth_dentistry-5.0.4.tar.gz/gnuhealth_dentistry-5.0.4/tests/__init__.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2008-2025 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2025 GNU Solidario <health@gnusolidario.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+try:
+    from trytond.modules.health_dentistry.tests.test_health_dentistry \
+        import suite
+except ImportError:
+    from .test_health_dentistry import suite
+
+__all__ = ['suite']
