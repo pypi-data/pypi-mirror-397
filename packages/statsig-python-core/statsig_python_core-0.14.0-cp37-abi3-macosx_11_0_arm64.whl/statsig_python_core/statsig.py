c74e258a6c5897cd9bc4bd2ff94f4373c5546585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 07:07:12 +0000

    Add package statsig-python-core/0.14.0/statsig_python_core-0.14.0-cp37-abi3-macosx_11_0_arm64.whl

---
+++ packages/statsig-python-core/statsig_python_core-0.14.0-cp37-abi3-macosx_11_0_arm64.whl/statsig_python_core/statsig.py
@@ -0,0 +1,77 @@
+import os
+from statsig_python_core import (
+    StatsigBasePy,
+    StatsigOptions,
+    notify_python_fork,
+    notify_python_shutdown,
+)
+from typing import Optional
+from .error_boundary import ErrorBoundary
+import atexit
+
+
+def handle_atexit():
+    notify_python_shutdown()
+
+
+atexit.register(handle_atexit)
+
+
+def handle_fork():
+    notify_python_fork()
+
+
+if hasattr(os, "register_at_fork"):
+    os.register_at_fork(
+        before=handle_fork,
+    )
+
+
+class Statsig(StatsigBasePy):
+    _statsig_shared_instance = None
+
+    def __new__(cls, sdk_key: str, options: Optional[StatsigOptions] = None):
+        instance = super().__new__(cls, sdk_key, options)
+        ErrorBoundary.wrap(instance)
+        return instance
+
+    # ----------------------------
+    #       Shared Instance
+    # ----------------------------
+
+    @classmethod
+    def shared(cls) -> StatsigBasePy:
+        if not Statsig.has_shared_instance() or cls._statsig_shared_instance is None:
+            return create_statsig_error_instance(
+                "Statsig.shared() called, but no instance has been set with Statsig.new_shared(...)"
+            )
+
+        return cls._statsig_shared_instance
+
+    @classmethod
+    def new_shared(
+        cls, sdk_key: str, options: Optional[StatsigOptions] = None
+    ) -> StatsigBasePy:
+        if Statsig.has_shared_instance():
+            return create_statsig_error_instance(
+                "Statsig shared instance already exists. Call Statsig.remove_shared() before creating a new instance."
+            )
+
+        cls._statsig_shared_instance = super().__new__(cls, sdk_key, options)
+        return cls._statsig_shared_instance
+
+    @classmethod
+    def remove_shared(cls) -> None:
+        cls._statsig_shared_instance = None
+
+    @classmethod
+    def has_shared_instance(cls) -> bool:
+        return (
+            hasattr(cls, "_statsig_shared_instance")
+            and cls._statsig_shared_instance is not None
+        )
+
+
+def create_statsig_error_instance(message: str) -> StatsigBasePy:
+    print("Error: ", message)
+    return StatsigBasePy.__new__(StatsigBasePy, "__STATSIG_ERROR_SDK_KEY__", None)
