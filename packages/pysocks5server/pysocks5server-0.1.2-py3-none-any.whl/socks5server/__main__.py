1d87f88241ef5cd6363335490b7a32e03a7fdd52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 14:06:36 +0000

    Add package pysocks5server/0.1.2/pysocks5server-0.1.2-py3-none-any.whl

---
+++ packages/pysocks5server/pysocks5server-0.1.2-py3-none-any.whl/socks5server/__main__.py
@@ -0,0 +1,50 @@
+from argparse import ArgumentParser, Namespace
+from asyncio import new_event_loop
+
+from socks5server import SocksServer, PasswordAuthentication, Socks5Client
+from socks5server.enums import AuthMethod
+
+parser = ArgumentParser()
+parser.add_argument("--host", type=str, default="0.0.0.0", required=False)
+parser.add_argument("--port", type=int, default=1080, required=False)
+parser.add_argument("--no-auth", action="store_true", default=False, required=False,
+                    help="Allow connections without authentication.")
+parser.add_argument("--users", nargs="*", default=[], required=False,
+                    help="List of users. Example: \"--users user1:password1 user2:password2\"")
+
+
+class Args(Namespace):
+    host: str
+    port: int
+    no_auth: bool
+    users: list[str]
+
+
+args = parser.parse_args(namespace=Args())
+server = SocksServer(args.host, args.port, args.no_auth)
+
+if args.users:
+    users = {login: password for user in args.users for login, password in [user.split(":")]}
+    auth = PasswordAuthentication(users)
+    server.register_authentication(AuthMethod.PASSWORD, auth)
+    print("Registered users: " + ", ".join(users.keys()))
+
+
+@server.on_client_connected
+async def on_client_connected(client: Socks5Client):
+    print(f"Client connected: {client}!")
+
+
+@server.on_dest_connected
+async def on_dest_connected(client: Socks5Client):
+    print(f"We connected to destination (for client {client})!")
+
+
+@server.on_client_disconnected
+async def on_client_disconnected(client: Socks5Client):
+    print(f"Client disconnected: {client}!")
+
+
+if __name__ == "__main__":
+    print("Server running")
+    new_event_loop().run_until_complete(server.serve())
