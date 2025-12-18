a2dbb514a61f3f38e56cdf5eb8ab276ad5dbe2d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 03:30:54 +0000

    Add package gnuhealth-icd10pcs/5.0.1/gnuhealth_icd10pcs-5.0.1.tar.gz

---
+++ packages/gnuhealth-icd10pcs/gnuhealth_icd10pcs-5.0.1.tar.gz/gnuhealth_icd10pcs-5.0.1/health_icd10pcs.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2008-2025 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2025 GNU Solidario <health@gnusolidario.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+#########################################################################
+#   Hospital Management Information System (HMIS) component of the      #
+#                       GNU Health project                              #
+#                   https://www.gnuhealth.org                           #
+#########################################################################
+#                       HEALTH ICD10PCS package                         #
+#                  health_icd10pcs.py: main module                      #
+#########################################################################
