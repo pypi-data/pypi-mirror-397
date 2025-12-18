781cf44b537112e7df440267a699997ec19502ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 21:06:22 +0000

    Add package gllm-pipeline-binary/0.4.28/gllm_pipeline_binary-0.4.28-cp312-cp312-manylinux_2_31_x86_64.whl

---
+++ packages/gllm-pipeline-binary/gllm_pipeline_binary-0.4.28-cp312-cp312-manylinux_2_31_x86_64.whl/gllm_pipeline/steps/step_error_handler/keep_step_error_handler.pyi
@@ -0,0 +1,9 @@
+from gllm_pipeline.steps.step_error_handler.step_error_handler import BaseStepErrorHandler as BaseStepErrorHandler
+from gllm_pipeline.utils.error_handling import ErrorContext as ErrorContext
+from langgraph.runtime import Runtime as Runtime
+from pydantic import BaseModel as BaseModel
+
+class KeepStepErrorHandler(BaseStepErrorHandler):
+    """Strategy that preserves the current state on error."""
+    def __init__(self) -> None:
+        """Initialize the keep error handler."""
