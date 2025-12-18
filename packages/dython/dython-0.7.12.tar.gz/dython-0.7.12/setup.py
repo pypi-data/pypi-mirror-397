0135774066f4ed88b5ad588d3df3384a853fc5e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 17:08:12 +0000

    Add package dython/0.7.12/dython-0.7.12.tar.gz

---
+++ packages/dython/dython-0.7.12.tar.gz/dython-0.7.12/setup.py
@@ -0,0 +1,47 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent.resolve()
+
+PACKAGE_NAME = "dython"
+AUTHOR = "Shaked Zychlinski"
+AUTHOR_EMAIL = "shakedzy@gmail.com"
+URL = "http://shakedzy.xyz/dython"
+DOWNLOAD_URL = "https://pypi.org/project/dython/"
+
+LICENSE = "MIT"
+VERSION = (HERE / "VERSION").read_text(encoding="utf8").strip()
+DESCRIPTION = "A set of data tools in Python"
+LONG_DESCRIPTION = (HERE / "README.md").read_text(encoding="utf8")
+LONG_DESC_TYPE = "text/markdown"
+
+requirements = (HERE / "requirements.txt").read_text(encoding="utf8")
+INSTALL_REQUIRES = [s.strip() for s in requirements.split("\n")]
+
+dev_requirements = (HERE / "dev_requirements.txt").read_text(encoding="utf8")
+EXTRAS_REQUIRE = {"dev": [s.strip() for s in dev_requirements.split("\n")]}
+
+min_minor = 10
+max_minor = 14
+CLASSIFIERS = [
+    f"Programming Language :: Python :: 3.{str(v)}" for v in range(min_minor, max_minor+1)
+]
+PYTHON_REQUIRES = f">=3.{min_minor}"
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESC_TYPE,
+    author=AUTHOR,
+    license=LICENSE,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    download_url=DOWNLOAD_URL,
+    python_requires=PYTHON_REQUIRES,
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRE,
+    packages=find_packages(),
+    classifiers=CLASSIFIERS,
+)
