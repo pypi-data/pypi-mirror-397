6107fb67cb216e4bf9bc1d78d408944d6851597b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 17:08:21 +0000

    Add package keplemon/2.0.2/keplemon-2.0.2-cp311-cp311-win_amd64.whl

---
+++ packages/keplemon/keplemon-2.0.2-cp311-cp311-win_amd64.whl/keplemon/enums.py
@@ -0,0 +1,17 @@
+from keplemon._keplemon.enums import (  # type: ignore
+    SAALKeyMode,
+    TimeSystem,
+    Classification,
+    KeplerianType,
+    ReferenceFrame,
+    CovarianceType,
+)
+
+__all__ = [
+    "SAALKeyMode",
+    "TimeSystem",
+    "Classification",
+    "KeplerianType",
+    "ReferenceFrame",
+    "CovarianceType",
+]
---
+++ packages/keplemon/keplemon-2.0.2-cp311-cp311-win_amd64.whl/keplemon/propagation.py
@@ -0,0 +1,7 @@
+from keplemon._keplemon.propagation import (  # type: ignore
+    ForceProperties,
+    b_star_to_drag_coefficient,
+    drag_coefficient_to_b_star,
+)
+
+__all__ = ["ForceProperties", "b_star_to_drag_coefficient", "drag_coefficient_to_b_star"]
