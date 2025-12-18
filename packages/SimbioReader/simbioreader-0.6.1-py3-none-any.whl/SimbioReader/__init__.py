bd297bd2b71d68dc5a515371d3be384b4c2154eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 14:07:34 +0000

    Add package SimbioReader/0.6.1/simbioreader-0.6.1-py3-none-any.whl

---
+++ packages/SimbioReader/simbioreader-0.6.1-py3-none-any.whl/SimbioReader/__init__.py
@@ -0,0 +1,2 @@
+from SimbioReader.sr import SimbioReader, DataStructure, Data, Detector, HK
+from SimbioReader.sr import __version__
---
+++ packages/SimbioReader/simbioreader-0.6.1-py3-none-any.whl/SimbioReader/constants.py
@@ -0,0 +1,30 @@
+class MSG:
+    ERROR = "[red][ERROR][/red] "
+    CRITICAL = "[red][CRITICAL][/red] "
+    INFO = "[green][INFO][/green] "
+    DEBUG = "[blue][DEBUG][/blue] "
+    WARNING = "[yellow][WARNING][/yellow] "
+    TODO = "[yellow][TODO][/yellow] "
+
+
+class FMODE:
+    READ = 'r'
+    READ_BINARY = 'rb'
+    WRITE = 'w'
+    WRITE_BINARY = 'wb'
+    APPEND = 'a'
+
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+progEpilog = "- For any information or suggestion please contact " \
+    "[bold magenta]Romolo.Politi@inaf.it[/bold magenta]"
+
+VERSION = (0,6,1,'f',1)
+
+
+datamodel='1.22.0.0'
+
+data_types = {
+    "UnsignedLSB2": {'envi':2,'bits':16},
+    "IEEE754LSBSingle":{'envi':4,'bits':32},
+}
