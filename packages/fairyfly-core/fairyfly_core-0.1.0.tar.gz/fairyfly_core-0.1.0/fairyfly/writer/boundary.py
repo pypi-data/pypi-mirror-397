e16b699e34456a50c2672f6b43fefaeb0aad71b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 07:07:45 +0000

    Add package fairyfly-core/0.1.0/fairyfly_core-0.1.0.tar.gz

---
+++ packages/fairyfly-core/fairyfly_core-0.1.0.tar.gz/fairyfly_core-0.1.0/fairyfly/writer/boundary.py
@@ -0,0 +1,6 @@
+"""Boundary Writer.
+
+Note to developers:
+Use this module to extend fairyfly's Boundary writer for new extensions.
+(eg. adding `thmz` to this module adds the method `Boundary.to.thmz`)
+"""
