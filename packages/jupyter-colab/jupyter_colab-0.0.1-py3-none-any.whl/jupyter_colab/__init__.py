fb9932db99e227b006187d7fb321a732a8d0c895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 09:06:49 +0000

    Add package jupyter-colab/0.0.1/jupyter_colab-0.0.1-py3-none-any.whl

---
+++ packages/jupyter-colab/jupyter_colab-0.0.1-py3-none-any.whl/jupyter_colab/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2025-2026 Datalayer, Inc.
+#
+# BSD 3-Clause License
+
+"""Jupyter Colab."""
+
+__all__ = []
---
+++ packages/jupyter-colab/jupyter_colab-0.0.1-py3-none-any.whl/jupyter_colab/__version__.py
@@ -0,0 +1,6 @@
+#
+# BSD 3-Clause License
+
+"""Jupyter Colab."""
+
+__version__ = "0.0.1"
