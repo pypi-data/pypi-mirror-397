0cd39c32da8b5a1b2a90dc2851dff626c57bc2d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 15:06:57 +0000

    Add package rhiza/0.4.0/rhiza-0.4.0-py3-none-any.whl

---
+++ packages/rhiza/rhiza-0.4.0-py3-none-any.whl/rhiza/__init__.py
@@ -0,0 +1,5 @@
+"""Utility tools and command-line helpers for the Rhiza project.
+
+This package groups small, user-facing utilities that can be invoked from
+the command line or other automation scripts.
+"""
