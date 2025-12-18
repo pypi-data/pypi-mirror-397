7d4da6a90e181324d768ea96cab9e91b61d2e7ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 09:07:25 +0000

    Add package telesignenterprise/3.0.1/telesignenterprise-3.0.1.tar.gz

---
+++ packages/telesignenterprise/telesignenterprise-3.0.1.tar.gz/telesignenterprise-3.0.1/telesignenterprise/__init__.py
@@ -0,0 +1,10 @@
+from pkg_resources import get_distribution
+
+__version__ = get_distribution("telesignenterprise").version
+__author__ = "TeleSign"
+__copyright__ = "Copyright 2017, TeleSign Corp."
+__credits__ = ["TeleSign"]
+__license__ = "MIT"
+__maintainer__ = "TeleSign Corp."
+__email__ = "support@telesign.com"
+__status__ = "Production"
