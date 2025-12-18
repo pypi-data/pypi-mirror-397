b212d5fc78b01722348d999467f86f8b9e15333b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 04:12:20 +0000

    Add package raylib/5.6.0.0.dev2/raylib-5.6.0.0.dev2.tar.gz

---
+++ packages/raylib/raylib-5.6.0.0.dev2.tar.gz/raylib-5.6.0.0.dev2/setup.py
@@ -0,0 +1,54 @@
+import pathlib
+from setuptools import setup
+from setuptools.dist import Distribution
+import os
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+VERSION = (HERE / "version.py").read_text().split()[-1].strip("\"'")
+
+RAYLIB_PLATFORM = os.getenv("RAYLIB_PLATFORM", "Desktop")
+if RAYLIB_PLATFORM == "SDL":
+    NAME = "_sdl"
+elif RAYLIB_PLATFORM == "SDL_SOFT":
+    NAME = "_software"
+elif RAYLIB_PLATFORM == "DRM":
+    NAME = "_drm"
+else:
+    NAME = ""
+
+class BinaryDistribution(Distribution):
+    """Distribution which always forces a binary package with platform name"""
+    def has_ext_modules(foo):
+        return True
+
+# should be name="raylib"+NAME but then Github doesn't track dependants
+setup(
+    name="raylib"+NAME,
+    version=VERSION,
+    description="Python CFFI bindings for Raylib",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/electronstudio/raylib-python-cffi",
+    author="Electron Studio",
+    author_email="github@electronstudio.co.uk",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.14",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.7"
+    ],
+    packages=["raylib", "pyray"],
+    include_package_data=True,
+    install_requires=["cffi>=1.15.1"],
+    distclass=BinaryDistribution,
+    cffi_modules=["raylib/build.py:ffibuilder"]
+)
