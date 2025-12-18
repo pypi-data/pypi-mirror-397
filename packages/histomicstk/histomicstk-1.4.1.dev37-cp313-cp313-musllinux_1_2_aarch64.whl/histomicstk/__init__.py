7d5e9da431602e3ce6d8a1ee58e823125bb54d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 05:05:53 +0000

    Add package histomicstk/1.4.1.dev37/histomicstk-1.4.1.dev37-cp313-cp313-musllinux_1_2_aarch64.whl

---
+++ packages/histomicstk/histomicstk-1.4.1.dev37-cp313-cp313-musllinux_1_2_aarch64.whl/histomicstk/__init__.py
@@ -0,0 +1,32 @@
+# import sub-packages to support nested calls
+from importlib.metadata import PackageNotFoundError
+from importlib.metadata import version as _importlib_version
+
+from . import segmentation  # must be imported before features
+from . import utils  # must be imported before other packages
+from . import (annotations_and_masks, features, filters, preprocessing,
+               saliency, workflows)
+
+try:
+    from ._version import __version__
+except ImportError:
+    try:
+        __version__ = _importlib_version(__name__)
+    except PackageNotFoundError:
+        # package is not installed
+        pass
+
+
+# list out things that are available for public use
+__all__ = (
+
+    # sub-packages
+    'features',
+    'filters',
+    'preprocessing',
+    'segmentation',
+    'utils',
+    'annotations_and_masks',
+    'saliency',
+    'workflows',
+)
