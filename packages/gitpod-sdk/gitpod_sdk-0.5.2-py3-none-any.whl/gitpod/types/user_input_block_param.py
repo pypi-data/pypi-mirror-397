7609ca9b02262f07a9cedb572129fe3c5953cb2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 21:06:42 +0000

    Add package gitpod-sdk/0.5.2/gitpod_sdk-0.5.2-py3-none-any.whl

---
+++ packages/gitpod-sdk/gitpod_sdk-0.5.2-py3-none-any.whl/gitpod/types/user_input_block_param.py
@@ -0,0 +1,24 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Union
+from datetime import datetime
+from typing_extensions import Required, Annotated, TypedDict
+
+from .._utils import PropertyInfo
+
+__all__ = ["UserInputBlockParam", "Text"]
+
+
+class Text(TypedDict, total=False):
+    content: str
+
+
+class UserInputBlockParam(TypedDict, total=False):
+    text: Required[Text]
+
+    id: str
+
+    created_at: Annotated[Union[str, datetime], PropertyInfo(alias="createdAt", format="iso8601")]
+    """Timestamp when this block was created. Used for debugging and support bundles."""
