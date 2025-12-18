5295e4c3f065c75f305b339f41c149440b18e4d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-18 08:05:17 +0000

    Add package turbodiffusion/1.0.0/turbodiffusion-1.0.0.tar.gz

---
+++ packages/turbodiffusion/turbodiffusion-1.0.0.tar.gz/turbodiffusion-1.0.0/setup.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+ops_dir = Path(__file__).parent / "turbodiffusion" / "ops"
+cutlass_dir = ops_dir / "cutlass"
+
+nvcc_flags = [
+    "-O3",
+    "-std=c++17",
+    "-U__CUDA_NO_HALF_OPERATORS__",
+    "-U__CUDA_NO_HALF_CONVERSIONS__",
+    "-U__CUDA_NO_BFLOAT16_OPERATORS__",
+    "-U__CUDA_NO_BFLOAT16_CONVERSIONS__",
+    "-U__CUDA_NO_BFLOAT162_OPERATORS__",
+    "-U__CUDA_NO_BFLOAT162_CONVERSIONS__",
+    "--expt-relaxed-constexpr",
+    "--expt-extended-lambda",
+    "--use_fast_math",
+    "--ptxas-options=--verbose,--warn-on-local-memory-usage",
+    "-lineinfo",
+    "-DCUTLASS_DEBUG_TRACE_LEVEL=0",
+    "-DNDEBUG",
+    "-Xcompiler",
+    "-fPIC"
+]
+
+cc_flag = [
+    "-gencode", "arch=compute_120a,code=sm_120a", 
+    "-gencode", "arch=compute_90,code=sm_90",
+    "-gencode", "arch=compute_89,code=sm_89",
+    "-gencode", "arch=compute_80,code=sm_80"
+]
+
+ext_modules = [
+    CUDAExtension(
+        name="turbo_diffusion_ops",
+        sources=[
+            "turbodiffusion/ops/bindings.cpp",
+            "turbodiffusion/ops/quant/quant.cu", 
+            "turbodiffusion/ops/norm/rmsnorm.cu",
+            "turbodiffusion/ops/norm/layernorm.cu",
+            "turbodiffusion/ops/gemm/gemm.cu"
+        ],
+        extra_compile_args={
+            "cxx": ["-O3", "-std=c++17"],
+            "nvcc": nvcc_flags + ["-DEXECMODE=0"] + cc_flag + ["--threads", "4"],
+        },
+        include_dirs=[
+            cutlass_dir / "include",
+            cutlass_dir / "tools" / "util" / "include",
+            ops_dir 
+        ],
+        libraries=["cuda"],
+    )
+]
+
+setup(
+    packages=find_packages(
+        exclude=("build", "csrc", "include", "tests", "dist", "docs", "benchmarks")
+    ),
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": BuildExtension},
+)
