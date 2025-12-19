fa89fe141eb8906dd65c1d49ecaadeb44e7d7699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 03:33:28 +0000

    Add package exonware-xwformats/0.0.1.7/exonware_xwformats-0.0.1.7.tar.gz

---
+++ packages/exonware-xwformats/exonware_xwformats-0.0.1.7.tar.gz/exonware_xwformats-0.0.1.7/src/exonware/xwformats/formats/text/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#exonware/xwformats/src/exonware/xwformats/formats/text/__init__.py
+"""
+Enterprise text serialization formats.
+
+Company: eXonware.com
+Author: Eng. Muhammad AlShehri
+Email: connect@exonware.com
+Version: 0.0.1.7
+Generation Date: 02-Nov-2025
+
+Note: Currently empty - reserved for future XML enterprise features.
+      Basic XML support is available in xwsystem core.
+"""
+
+__all__ = []
+
+
