ca7749c5a74ed07cae6763a8d8a7a235e6b24ad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 15:06:12 +0000

    Add package jxc-python/0.1.9/jxc_python-0.1.9.tar.gz

---
+++ packages/jxc-python/jxc_python-0.1.9.tar.gz/jxc_python-0.1.9/libxc/xc_version.h
@@ -0,0 +1,13 @@
+#ifndef _XC_VERSION_H
+#define _XC_VERSION_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+#define XC_VERSION "7.0.0"
+#define XC_MAJOR_VERSION 7
+#define XC_MINOR_VERSION 0
+#define XC_MICRO_VERSION 0
+#ifdef __cplusplus
+}
+#endif
+#endif
