949d30afafc3822545af66d498e9c82f457a0d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 05:08:03 +0000

    Add package pypdfcodebook/0.5.2/pypdfcodebook-0.5.2-py3-none-any.whl

---
+++ packages/pypdfcodebook/pypdfcodebook-0.5.2-py3-none-any.whl/pypdfcodebook/__init__.py
@@ -0,0 +1,18 @@
+"""
+******************************************************************************
+* Project: pypdfcodebook                                                     *
+* License: BSD 3-Clause License                                              *
+* Author/Maintainer: Nathanael Rosenheim                                     *
+* Copyright (c) 2025 Nathanael Rosenheim                                     *
+******************************************************************************
+"""
+
+__version__ = "0.5.2"
+
+# For all other functions, import directly from submodules, e.g.:
+# from pypdfcodebook.simple import add_one
+# from pypdfcodebook.pdfcb_03a_figures import income_distribution
+
+def main():
+    """Entry point for the application script"""
+    print("Call your main application code here")
---
+++ packages/pypdfcodebook/pypdfcodebook-0.5.2-py3-none-any.whl/pypdfcodebook/simple.py
@@ -0,0 +1,11 @@
+"""
+******************************************************************************
+* Project: pypdfcodebook                                                     *
+* License: BSD 3-Clause License                                              *
+* Author/Maintainer: Nathanael Rosenheim                                     *
+* Copyright (c) 2025 Nathanael Rosenheim                                     *
+******************************************************************************
+"""
+
+def add_one(number):
+    return number + 1
