f109f2dc83647f2a0caa1d6f68b0df1def2ea7cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 20:07:09 +0000

    Add package bluer-agent/5.8.1/bluer_agent-5.8.1.tar.gz

---
+++ packages/bluer-agent/bluer_agent-5.8.1.tar.gz/bluer_agent-5.8.1/bluer_agent/help/functions.py
@@ -0,0 +1,7 @@
+from bluer_agent.help.chat import help_functions as help_chat
+from bluer_agent.help.transcription import help_functions as help_transcription
+
+help_functions = {
+    "chat": help_chat,
+    "transcription": help_transcription,
+}
