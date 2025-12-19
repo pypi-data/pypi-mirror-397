bbbd18aff37cf3b0d9485f3e1454169fd3e86b97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 18:07:52 +0000

    Add package whereto/0.5.0/whereto-0.5.0-py3-none-any.whl

---
+++ packages/whereto/whereto-0.5.0-py3-none-any.whl/whereto/rules.py
@@ -0,0 +1,136 @@
+# Copyright 2010-2011 OpenStack Foundation
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import logging
+import pcre2
+import re
+
+
+LOG = logging.getLogger()
+
+
+class Rule:
+    "Base class for rules."
+
+    def __init__(self, linenum, *params):
+        self.linenum = linenum
+        self._params = params
+        if len(params) == 4:
+            # redirect code pattern target
+            self.code = params[1]
+            self.pattern = params[2]
+            self.target = params[3]
+        elif len(params) == 3:
+            if params[1] == '410':
+                # The page has been deleted and is not coming back.
+                self.code = params[1]
+                self.pattern = params[2]
+                self.target = None
+            else:
+                # redirect pattern target
+                # (code is implied)
+                self.code = '301'
+                self.pattern = params[1]
+                self.target = params[2]
+        else:
+            raise ValueError('Could not understand rule {}'.format(params))
+
+    def __str__(self):
+        return '[{}] {}'.format(
+            self.linenum,
+            ' '.join(p for p in self._params if p),
+        )
+
+    def match(self, path):
+        raise NotImplementedError('Base class does not implement match()')
+
+
+class Redirect(Rule):
+    "A Redirect rule."
+
+    def match(self, path):
+        if path == self.pattern:
+            return (self.code, self.target)
+        return None
+
+
+class RedirectMatch(Rule):
+    "A RedirectMatch rule with a regular expression."
+
+    _group_subst = re.compile(r'(?<!\\)\$([0-9])')
+
+    def __init__(self, linenum, *params):
+        super().__init__(linenum, *params)
+        self.regex = pcre2.compile(self.pattern)
+        if self.target:
+            self.target_repl = self._get_target_repl()
+        else:
+            self.target_repl = None
+
+    def _get_target_repl(self):
+        return self.target
+
+    def match(self, path):
+        m = self.regex.search(path)
+        if m:
+            if self.target_repl:
+                return (self.code, self.regex.sub(self.target_repl, path))
+            else:
+                # A rule that doesn't have a response target, like 410.
+                return (self.code, self.target_repl)
+        return None
+
+
+class RuleSet:
+    "An ordered collection of rules."
+
+    _factories = {
+        'redirect': Redirect,
+        'redirectmatch': RedirectMatch,
+    }
+
+    def __init__(self):
+        self._rules = []
+        self._by_num = {}
+
+    def add(self, linenum, *params):
+        rule_type = params[0].lower()
+        rule = self._factories[rule_type](linenum, *params)
+        self._rules.append(rule)
+        self._by_num[linenum] = rule
+
+    def __getitem__(self, index):
+        return self._by_num[index]
+
+    def __iter__(self):
+        return iter(self._rules)
+
+    @property
+    def all_ids(self):
+        return list(self._by_num.keys())
+
+    def match(self, path):
+        for rule in self:
+            try:
+                m = rule.match(path)
+            except Exception as e:
+                LOG.warning('Failed to evaluate {} against {}: {}'.format(
+                    rule, path, e))
+            else:
+                if m is not None:
+                    LOG.debug(
+                        'Matched "{}" for path "{}" producing {}'.format(
+                            rule, path, m))
+                    return (rule.linenum,) + m
---
+++ packages/whereto/whereto-0.5.0-py3-none-any.whl/whereto/tests/test_rules.py
@@ -0,0 +1,264 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from whereto import rules
+from whereto.tests import base
+
+
+class TestRedirect(base.TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.rule = rules.Redirect(
+            1,
+            'redirect', '301', '/path', '/new/path',
+        )
+
+    def test_match(self):
+        self.assertEqual(
+            ('301', '/new/path'),
+            self.rule.match('/path'),
+        )
+
+    def test_no_match(self):
+        self.assertIsNone(
+            self.rule.match('/different/path'),
+        )
+
+    def test_implied_code(self):
+        rule = rules.Redirect(
+            1,
+            'redirect', '/the/path', '/new/path',
+        )
+        self.assertEqual(
+            '301',
+            rule.code,
+        )
+
+    def test_str(self):
+        self.assertEqual(
+            '[1] redirect 301 /path /new/path',
+            str(self.rule),
+        )
+
+    def test_too_few_args(self):
+        self.assertRaises(
+            ValueError,
+            rules.Redirect,
+            (1, 'redirect', '/the/path'),
+        )
+
+    def test_too_many_args(self):
+        self.assertRaises(
+            ValueError,
+            rules.Redirect,
+            (1, 'redirect', '301', '/the/path', '/new/path', 'extra-value'),
+        )
+
+    def test_410(self):
+        rule = rules.Redirect(
+            1,
+            'redirect', '410', '/the/path', None,
+        )
+        self.assertEqual(
+            '410',
+            rule.code,
+        )
+        self.assertIsNone(
+            rule.target,
+        )
+        self.assertEqual(
+            ('410', None),
+            rule.match('/the/path'),
+        )
+
+
+class TestRedirectMatch(base.TestCase):
+
+    def test_match(self):
+        rule = rules.RedirectMatch(
+            1,
+            'redirectmatch', '301', '^/user/.*$', '/pike/user/',
+        )
+        self.assertEqual(
+            ('301', '/pike/user/'),
+            rule.match('/user/'),
+        )
+
+    def test_match_pcre_syntax(self):
+        rule = rules.RedirectMatch(
+            1,
+            'redirectmatch', '301', '^/((?i)pike)/user/.*$', '/pike/user/',
+        )
+        self.assertIsNone(
+            rule.match('/Pike/USER/')
+        )
+
+    def test_match_with_group(self):
+        rule = rules.RedirectMatch(
+            1,
+            'redirectmatch', '301', '^/user/(.*)$', '/pike/user/$1',
+        )
+        self.assertEqual(
+            ('301', '/pike/user/foo'),
+            rule.match('/user/foo'),
+        )
+
+    def test_match_with_group_braces(self):
+        rule = rules.RedirectMatch(
+            1,
+            'redirectmatch', '301', '^/user/(.*)$', '/pike/user/{1}/$1',
+        )
+        self.assertEqual(
+            ('301', '/pike/user/{1}/foo'),
+            rule.match('/user/foo'),
+        )
+
+    def test_match_with_no_group_dollar_escape(self):
+        rule = rules.RedirectMatch(
+            1,
+            'redirectmatch', '301', '^/user/(.*)$', '/pike/user/\\$a',
+        )
+        self.assertEqual(
+            ('301', '/pike/user/$a'),
+            rule.match('/user/foo'),
+        )
+
+    def test_match_with_group_escape(self):
+        rule = rules.RedirectMatch(
+            1,
+            'redirectmatch', '301', '^/user/(.*)$', '/pike/user/\\$1',
+        )
+        self.assertEqual(
+            ('301', '/pike/user/$1'),
+            rule.match('/user/foo'),
+        )
+
+    def test_no_match(self):
+        rule = rules.RedirectMatch(
+            1,
+            'redirectmatch', '301', '^/user/.*$', '/pike/user/',
+        )
+        self.assertIsNone(
+            rule.match('/different/path'),
+        )
+
+    def test_implied_code(self):
+        rule = rules.RedirectMatch(
+            1,
+            'redirectmatch', '^/user/.*$', '/pike/user/',
+        )
+        self.assertEqual(
+            '301',
+            rule.code,
+        )
+
+    def test_str(self):
+        rule = rules.RedirectMatch(
+            1,
+            'redirectmatch', '301', '^/user/.*$', '/pike/user/',
+        )
+        self.assertEqual(
+            '[1] redirectmatch 301 ^/user/.*$ /pike/user/',
+            str(rule),
+        )
+
+    def test_too_few_args(self):
+        self.assertRaises(
+            ValueError,
+            rules.RedirectMatch,
+            (1, 'redirectmatch', '/the/path'),
+        )
+
+    def test_too_many_args(self):
+        self.assertRaises(
+            ValueError,
+            rules.RedirectMatch,
+            (1, 'redirectmatch', '301', '/the/path', '/new/path',
+             'extra-value'),
+        )
+
+    def test_410(self):
+        rule = rules.RedirectMatch(
+            1,
+            'redirect', '410', '/the/path', None,
+        )
+        self.assertEqual(
+            '410',
+            rule.code,
+        )
+        self.assertIsNone(
+            rule.target,
+        )
+        self.assertEqual(
+            ('410', None),
+            rule.match('/the/path'),
+        )
+
+
+class TestRuleSet(base.TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.ruleset = rules.RuleSet()
+
+    def test_add_redirect(self):
+        self.ruleset.add(
+            1,
+            'redirect', '301', '/path', '/new/path',
+        )
+        self.assertEqual(1, len(self.ruleset._rules))
+        self.assertIsInstance(self.ruleset._rules[0], rules.Redirect)
+
+    def test_add_redirectmatch(self):
+        self.ruleset.add(
+            1,
+            'redirectmatch', '301', '/path', '/new/path',
+        )
+        self.assertEqual(1, len(self.ruleset._rules))
+        self.assertIsInstance(self.ruleset._rules[0], rules.RedirectMatch)
+
+    def test_all_ids(self):
+        self.ruleset.add(
+            1,
+            'redirect', '301', '/path', '/new/path',
+        )
+        self.assertEqual([1], self.ruleset.all_ids)
+        self.ruleset.add(
+            2,
+            'redirect', '301', '/path', '/other/path',
+        )
+        self.assertEqual([1, 2], self.ruleset.all_ids)
+
+    def test_match_one(self):
+        self.ruleset.add(
+            1,
+            'redirect', '301', '/path', '/new/path',
+        )
+        self.assertEqual(
+            (1, '301', '/new/path'),
+            self.ruleset.match('/path'),
+        )
+
+    def test_match_multiple(self):
+        self.ruleset.add(
+            1,
+            'redirect', '301', '/path', '/new/path',
+        )
+        self.ruleset.add(
+            2,
+            'redirect', '301', '/path', '/other/path',
+        )
+        self.assertEqual(
+            (1, '301', '/new/path'),
+            self.ruleset.match('/path'),
+        )
