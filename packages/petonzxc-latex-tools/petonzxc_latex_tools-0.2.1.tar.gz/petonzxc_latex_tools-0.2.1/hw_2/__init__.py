763b44003a3c75c30c5340ed3565b0f714b4e86f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 04:16:11 +0000

    Add package petonzxc-latex-tools/0.2.1/petonzxc_latex_tools-0.2.1.tar.gz

---
+++ packages/petonzxc-latex-tools/petonzxc_latex_tools-0.2.1.tar.gz/petonzxc_latex_tools-0.2.1/hw_2/__init__.py
@@ -0,0 +1,4 @@
+from .latex_generator import generate_table_latex, generate_image_latex
+
+__version__ = "0.1.0"
+__all__ = ["generate_table_latex", "generate_image_latex"]
---
+++ packages/petonzxc-latex-tools/petonzxc_latex_tools-0.2.1.tar.gz/petonzxc_latex_tools-0.2.1/latex_tools/__init__.py
@@ -0,0 +1,4 @@
+from .latex_generator import generate_table_latex, generate_image_latex
+
+__version__ = "0.2.1"
+__all__ = ["generate_table_latex", "generate_image_latex"]
