59d35409ef015eb9ab3a6479e7a9000426958c7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 03:32:49 +0000

    Add package DeltaAgent/0.0.7/deltaagent-0.0.7.tar.gz

---
+++ packages/DeltaAgent/deltaagent-0.0.7.tar.gz/deltaagent-0.0.7/DeltaAgent/__init__.py
@@ -0,0 +1,25 @@
+
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+from .fetch_data import fetch_data
+
+from pandas.core.base import PandasObject
+
+# add the fetch_data method to the pandas DataFrame
+PandasObject.fetch_data = fetch_data
+
+class DeltaAgent:
+
+    def __init__(self, account_name=None, account_key=None, container_name=None, table_path=None):
+
+        self.account_name = account_name
+        self.account_key = account_key
+        self.container_name = container_name
+        self.table_path = table_path
+    
+    from .get_azure_service_client import get_azure_service_client
+    from .validate_attrs import validate_attrs
+    from .parse_log_as_df import parse_log_as_df
+    
