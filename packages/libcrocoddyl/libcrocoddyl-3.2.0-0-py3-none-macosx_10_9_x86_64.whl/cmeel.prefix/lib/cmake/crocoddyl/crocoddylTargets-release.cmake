fecef145283a3778db902476d9ceb6dae28d753d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 18:07:40 +0000

    Add package libcrocoddyl/3.2.0/libcrocoddyl-3.2.0-0-py3-none-macosx_10_9_x86_64.whl

---
+++ packages/libcrocoddyl/libcrocoddyl-3.2.0-0-py3-none-macosx_10_9_x86_64.whl/cmeel.prefix/lib/cmake/crocoddyl/crocoddylTargets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "crocoddyl::crocoddyl" for configuration "Release"
+set_property(TARGET crocoddyl::crocoddyl APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(crocoddyl::crocoddyl PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libcrocoddyl.dylib"
+  IMPORTED_SONAME_RELEASE "@rpath/libcrocoddyl.dylib"
+  )
+
+list(APPEND _cmake_import_check_targets crocoddyl::crocoddyl )
+list(APPEND _cmake_import_check_files_for_crocoddyl::crocoddyl "${_IMPORT_PREFIX}/lib/libcrocoddyl.dylib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
