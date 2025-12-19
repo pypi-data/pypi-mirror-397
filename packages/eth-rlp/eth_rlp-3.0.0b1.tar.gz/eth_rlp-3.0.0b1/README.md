fd488191286488ebbea970fe9975bb152d604f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 04:16:34 +0000

    Add package eth-rlp/3.0.0b1/eth_rlp-3.0.0b1.tar.gz

---
+++ packages/eth-rlp/eth_rlp-3.0.0b1.tar.gz/eth_rlp-3.0.0b1/README.md
@@ -0,0 +1,19 @@
+# eth-rlp
+
+[![Join the conversation on Discord](https://img.shields.io/discord/809793915578089484?color=blue&label=chat&logo=discord&logoColor=white)](https://discord.gg/GHryRvPB84)
+[![Build Status](https://circleci.com/gh/ethereum/eth-rlp.svg?style=shield)](https://circleci.com/gh/ethereum/eth-rlp)
+[![PyPI version](https://badge.fury.io/py/eth-rlp.svg)](https://badge.fury.io/py/eth-rlp)
+[![Python versions](https://img.shields.io/pypi/pyversions/eth-rlp.svg)](https://pypi.python.org/pypi/eth-rlp)
+[![Docs build](https://readthedocs.org/projects/eth-rlp/badge/?version=latest)](https://eth-rlp.readthedocs.io/en/latest/?badge=latest)
+
+RLP definitions for common Ethereum objects in Python
+
+Read the [documentation](https://eth-rlp.readthedocs.io/).
+
+View the [change log](https://eth-rlp.readthedocs.io/en/latest/release_notes.html).
+
+## Installation
+
+```sh
+python -m pip install eth-rlp
+```
---
+++ packages/eth-rlp/eth_rlp-3.0.0b1.tar.gz/eth_rlp-3.0.0b1/setup.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+extras_require = {
+    "dev": [
+        "build>=0.9.0",
+        "bump_my_version>=0.19.0",
+        "ipython",
+        "mypy==1.18.2",
+        "pre-commit>=3.4.0",
+        "tox>=4.0.0",
+        "twine",
+        "wheel>=0.38.1",
+    ],
+    "docs": [
+        "sphinx>=6.0.0",
+        "sphinx-autobuild>=2021.3.14",
+        "sphinx_rtd_theme>=1.0.0",
+        "towncrier>=24,<25",
+    ],
+    "test": [
+        "eth-hash[pycryptodome]",
+        "pytest>=7.0.0",
+        "pytest-xdist>=2.4.0",
+    ],
+}
+
+extras_require["dev"] = (
+    extras_require["dev"] + extras_require["docs"] + extras_require["test"]
+)
+
+
+with open("./README.md") as readme:
+    long_description = readme.read()
+
+
+setup(
+    name="eth-rlp",
+    # *IMPORTANT*: Don't manually change the version here. See Contributing docs for the release process.
+    version="3.0.0-beta.1",
+    description="""eth-rlp: RLP definitions for common Ethereum objects in Python""",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="The Ethereum Foundation",
+    author_email="snakecharmers@ethereum.org",
+    url="https://github.com/ethereum/eth-rlp",
+    include_package_data=True,
+    install_requires=[
+        "eth-utils>=2.0.0",
+        "hexbytes>=1.2.0",
+        "rlp>=1.0.0",
+        "typing_extensions>=4.0.1; python_version <= '3.10'",
+    ],
+    python_requires=">=3.10, <4",
+    extras_require=extras_require,
+    py_modules=["eth_rlp"],
+    zip_safe=False,
+    keywords="ethereum",
+    packages=find_packages(exclude=["scripts", "scripts.*", "tests", "tests.*"]),
+    package_data={"eth_rlp": ["py.typed"]},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
+    ],
+)
