465ab1cbb9410734b946d271239b627b9a85a1da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 22:07:40 +0000

    Add package streamz/0.6.5/streamz-0.6.5-py2.py3-none-any.whl

---
+++ packages/streamz/streamz-0.6.5-py2.py3-none-any.whl/streamz/plugins.py
@@ -0,0 +1,31 @@
+import warnings
+
+import importlib.metadata
+
+
+def try_register(cls, entry_point, *modifier):
+    try:
+        cls.register_plugin_entry_point(entry_point, *modifier)
+    except ValueError:
+        warnings.warn(
+            f"Can't add {entry_point.name} from {entry_point.module_name}: "
+            "name collision with existing stream node."
+        )
+
+
+def get_entry_point(eps, group):
+    if hasattr(eps, "select"):  # Python 3.10+ / importlib_metadata >= 3.9.0
+        return eps.select(group=group)
+    else:
+        return eps.get(group, [])
+
+
+def load_plugins(cls):
+    eps = importlib.metadata.entry_points()
+
+    for entry_point in get_entry_point(eps, "streamz.sources"):
+        try_register(cls, entry_point, staticmethod)
+    for entry_point in get_entry_point(eps, "streamz.nodes"):
+        try_register(cls, entry_point)
+    for entry_point in get_entry_point(eps, "streamz.sinks"):
+        try_register(cls, entry_point)
