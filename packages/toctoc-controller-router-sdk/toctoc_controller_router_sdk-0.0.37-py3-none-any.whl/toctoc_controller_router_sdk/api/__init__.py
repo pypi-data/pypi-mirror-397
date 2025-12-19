22ee0ee7b836cbd89f4aa21a16afbc5dfec538f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 04:15:40 +0000

    Add package toctoc-controller-router-sdk/0.0.37/toctoc_controller_router_sdk-0.0.37-py3-none-any.whl

---
+++ packages/toctoc-controller-router-sdk/toctoc_controller_router_sdk-0.0.37-py3-none-any.whl/toctoc_controller_router_sdk/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from toctoc_controller_router_sdk.api.appraisal_api import AppraisalApi
+from toctoc_controller_router_sdk.api.environment_api import EnvironmentApi
+from toctoc_controller_router_sdk.api.highlight_api import HighlightApi
+from toctoc_controller_router_sdk.api.properties_api import PropertiesApi
+
