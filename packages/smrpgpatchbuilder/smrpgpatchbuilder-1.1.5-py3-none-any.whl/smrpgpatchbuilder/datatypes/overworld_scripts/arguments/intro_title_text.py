d07f43404ddc0b320f1a8c459bdbb98752473693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 14:05:08 +0000

    Add package smrpgpatchbuilder/1.1.5/smrpgpatchbuilder-1.1.5-py3-none-any.whl

---
+++ packages/smrpgpatchbuilder/smrpgpatchbuilder-1.1.5-py3-none-any.whl/smrpgpatchbuilder/datatypes/overworld_scripts/arguments/intro_title_text.py
@@ -0,0 +1,10 @@
+"""Int subclass instances representing predefined texts that are displayed in the game's intro."""
+
+from .types.intro_title_text import IntroTitleText
+
+SUPER_MARIO = IntroTitleText(0)
+PRINCESS_TOADSTOOL = IntroTitleText(1)
+KING_BOWSER = IntroTitleText(2)
+MALLOW = IntroTitleText(3)
+GENO = IntroTitleText(4)
+IN = IntroTitleText(5)
