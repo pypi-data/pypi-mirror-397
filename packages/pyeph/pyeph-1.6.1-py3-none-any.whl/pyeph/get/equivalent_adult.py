c32121350de47344b66b4dfcce25a38a3609d77a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 03:32:37 +0000

    Add package pyeph/1.6.1/pyeph-1.6.1-py3-none-any.whl

---
+++ packages/pyeph/pyeph-1.6.1-py3-none-any.whl/pyeph/get/equivalent_adult.py
@@ -0,0 +1,9 @@
+from ._base_getter import Getter
+
+class EquivalentAdult(Getter):
+	"""
+		Obtencion del df de adulto equivalente
+	"""
+
+	folder = "adulto_equivalente"
+	filename = "adulto_equivalente.zip"
