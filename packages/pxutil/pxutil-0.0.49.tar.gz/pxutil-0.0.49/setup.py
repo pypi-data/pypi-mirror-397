40b845ce53e3ea104429e77291739f5c2eb8024e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 15:05:44 +0000

    Add package pxutil/0.0.49/pxutil-0.0.49.tar.gz

---
+++ packages/pxutil/pxutil-0.0.49.tar.gz/pxutil-0.0.49/setup.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup, Extension
+from Cython.Build import cythonize
+
+_version = "0.0.49"
+
+with open("README.md") as readme:
+    long_description = readme.read()
+
+# extensions = [
+#     Extension("*", ["*/*.pyx"],
+#         # include_dirs=[...],
+#         # libraries=[...],
+#         # library_dirs=[...]
+#         ),
+# ]
+
+setup(
+    name="pxutil",
+    version=_version,
+    description="Some handy Python tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # default is text/x-rst
+    author="Peter Jiping Xie",
+    author_email="peter.jp.xie@gmail.com",
+    url="https://github.com/peterjpxie/pxutil",
+    license="MIT",
+    packages=find_packages(),
+    python_requires=">=3.10",
+    install_requires=[
+        "requests>=2.26.0",
+        "cython>=3.0.0",
+        "pathspec>=0.12.1",
+        "tiktoken",
+    ],
+    # # optional dependency example:
+    # # pip install pxutil[test] to install optional test dependencies
+    # extras_require={
+    #     "test": [
+    #         "pytest>=6.0.0",
+    #         "pytest-cov>=2.10.0",
+    #     ],
+    # },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
+        "Topic :: Software Development :: Libraries",
+    ],
+    # build cython modules
+    ext_modules=cythonize("*/*_cy.py"),  # pure python with cython type annotation
+    # ext_modules=cythonize("*/*.pyx"), # language_level="3"
+    # ext_modules=cythonize(extensions),
+    zip_safe=False,
+    # shared lib from Cython - No need! setup.py build and include them in the wheel automatically.
+    # package_data={'': ['pxutil/*.so', 'pxutil/*.dll', 'pxutil/*.pyd']},
+    # cli
+    entry_points={
+        "console_scripts": [
+            "px.loop=pxutil.cli:loop_main",
+            "px.chat=pxutil.cli:chat_main",
+            "chat=pxutil.cli:chat_main",
+            "px.runc=pxutil.cli:runc_main",
+            "px.ls.mod=pxutil.cli:ls_mod_main",
+            "px.onefile=pxutil.cli:onefile_main",
+            "px.token.counter=pxutil.cli:token_counter_main",
+        ]
+    },
+)
