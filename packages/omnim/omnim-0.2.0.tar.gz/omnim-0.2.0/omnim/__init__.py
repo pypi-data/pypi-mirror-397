f0543ed698dce61bee68052e0c7ebca4ab48ae91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 19:06:40 +0000

    Add package omnim/0.2.0/omnim-0.2.0.tar.gz

---
+++ packages/omnim/omnim-0.2.0.tar.gz/omnim-0.2.0/omnim/__init__.py
@@ -0,0 +1,15 @@
+from . import delegate
+from . import reactive
+from . import nullable
+from . import readonly
+from . import linq
+from . import task
+
+__all__ = [
+    "delegate",
+    "reactive",
+    "nullable",
+    "readonly",
+    "linq",
+    "task",
+]
