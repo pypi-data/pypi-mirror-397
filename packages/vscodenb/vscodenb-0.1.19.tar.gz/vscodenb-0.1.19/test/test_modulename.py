95f9181ec55f8b6f2687b5f08f6ec869cf75f683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 15:06:29 +0000

    Add package vscodenb/0.1.19/vscodenb-0.1.19.tar.gz

---
+++ packages/vscodenb/vscodenb-0.1.19.tar.gz/vscodenb-0.1.19/test/test_modulename.py
@@ -0,0 +1,13 @@
+import unittest
+
+from vscodenb.modulename import functionname
+
+class TestModulename(unittest.TestCase):
+
+    def test_functionname(self):
+        self.assertEqual(
+            functionname(
+                1, 1
+                ),
+                2
+            )
