fd6a8e9e17a9a8ed9695d683b92e1199922a16b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 19:08:19 +0000

    Add package ngr/0.0.1/ngr-0.0.1-py3-none-any.whl

---
+++ packages/ngr/ngr-0.0.1-py3-none-any.whl/ngr/__init__.py
@@ -0,0 +1,9 @@
+"""Python interface for the NGR (Next Generation Reporting) R package.
+
+This initial version only reserves the `ngr` name on PyPI and prepares the
+package structure. Functionality will be implemented progressively to mirror
+NGR's R API.
+"""
+
+__all__ = []
+__version__ = "0.0.1"
