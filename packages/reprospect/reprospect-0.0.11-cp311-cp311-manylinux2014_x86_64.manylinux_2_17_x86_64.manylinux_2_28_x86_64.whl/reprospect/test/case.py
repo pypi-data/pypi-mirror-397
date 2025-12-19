a57d54ece3f48c3e71390f3324eff9d427de88c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 17:07:33 +0000

    Add package reprospect/0.0.11/reprospect-0.0.11-cp311-cp311-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/reprospect/reprospect-0.0.11-cp311-cp311-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/reprospect/test/case.py
@@ -0,0 +1,43 @@
+import abc
+import pathlib
+
+from reprospect.tools.architecture import NVIDIAArch
+from reprospect.tools.binaries     import CuppFilt, LlvmCppFilt
+from reprospect.test.cmake         import CMakeMixin
+
+class TestCase(abc.ABC):
+    """
+    Test case.
+    """
+    @property
+    @abc.abstractmethod
+    def cwd(self) -> pathlib.Path:
+        """
+        Working directory.
+        """
+
+    @property
+    @abc.abstractmethod
+    def arch(self) -> NVIDIAArch:
+        """
+        NVIDIA architecture.
+        """
+
+    @property
+    @abc.abstractmethod
+    def executable(self) -> pathlib.Path:
+        """
+        Executable.
+        """
+
+    @property
+    @abc.abstractmethod
+    def demangler(self) -> type[CuppFilt | LlvmCppFilt]:
+        """
+        Demangler.
+        """
+
+class CMakeAwareTestCase(CMakeMixin, TestCase):
+    """
+    Test case with CMake integration.
+    """
