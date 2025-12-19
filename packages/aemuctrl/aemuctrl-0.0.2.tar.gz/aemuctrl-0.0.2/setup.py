32a0064ba4c74a4b92967d79def616252753244c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 22:07:25 +0000

    Add package aemuctrl/0.0.2/aemuctrl-0.0.2.tar.gz

---
+++ packages/aemuctrl/aemuctrl-0.0.2.tar.gz/aemuctrl-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aemuctrl",
+    version="0.0.2",
+    description="A lightweight Android Emulator Control library using ADB and PyAutoGUI",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Your Name", 
+    url="https://github.com/Solee-in/aemuctrl", 
+    license="MIT",
+    packages=find_packages(),
+    py_modules=["aemuctrl"], 
+    install_requires=[
+        "pyautogui", 
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires='>=3.6',
+)
