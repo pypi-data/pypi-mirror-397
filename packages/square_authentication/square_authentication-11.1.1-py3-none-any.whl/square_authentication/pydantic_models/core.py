054b7e4e3af7feac68aed59ba14df566a5c9f82f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 23:07:04 +0000

    Add package square_authentication/11.1.1/square_authentication-11.1.1-py3-none-any.whl

---
+++ packages/square_authentication/square_authentication-11.1.1-py3-none-any.whl/square_authentication/pydantic_models/core.py
@@ -0,0 +1,69 @@
+from enum import Enum
+from typing import Optional, List
+
+from pydantic import BaseModel
+from square_database_structure.square.authentication.enums import RecoveryMethodEnum
+
+
+class RegisterUsernameV0(BaseModel):
+    username: str
+    password: str
+    app_id: Optional[int] = None
+
+
+class RegisterLoginGoogleV0(BaseModel):
+    google_id: str
+    app_id: Optional[int] = None
+    assign_app_id_if_missing: bool = False
+
+
+class LoginUsernameV0(BaseModel):
+    username: str
+    password: str
+    app_id: int
+    assign_app_id_if_missing: bool = False
+
+
+class DeleteUserV0(BaseModel):
+    password: str
+
+
+class UpdatePasswordV0(BaseModel):
+    old_password: str
+    new_password: str
+    logout_other_sessions: bool = False
+    preserve_session_refresh_token: Optional[str] = None
+
+
+class TokenType(Enum):
+    access_token = "access_token"
+    refresh_token = "refresh_token"
+
+
+class LogoutAppsV0(BaseModel):
+    app_ids: List[int]
+
+
+class ResetPasswordAndLoginUsingBackupCodeV0(BaseModel):
+    backup_code: str
+    username: str
+    new_password: str
+    app_id: int
+    logout_other_sessions: bool = False
+
+
+class SendResetPasswordEmailV0(BaseModel):
+    username: str
+
+
+class ResetPasswordAndLoginUsingResetEmailCodeV0(BaseModel):
+    reset_email_code: str
+    username: str
+    new_password: str
+    app_id: int
+    logout_other_sessions: bool = False
+
+
+class UpdateUserRecoveryMethodsV0(BaseModel):
+    recovery_methods_to_add: List[RecoveryMethodEnum] = None
+    recovery_methods_to_remove: List[RecoveryMethodEnum] = None
