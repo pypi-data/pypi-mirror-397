0e4be903e20cb5d4d998ad17bd76704c3f80a6fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 14:05:15 +0000

    Add package halib/0.2.2/halib-0.2.2.tar.gz

---
+++ packages/halib/halib-0.2.2.tar.gz/halib-0.2.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="halib",
+    version="0.2.2",
+    author="Hoang Van Ha",
+    author_email="hoangvanhauit@gmail.com",
+    description="Small library for common tasks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+    install_requires=required,
+)
