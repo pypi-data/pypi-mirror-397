b0f41821484c1e7faf33076c8f72a6556178269d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 17:07:44 +0000

    Add package dodopayments/1.67.2/dodopayments-1.67.2-py3-none-any.whl

---
+++ packages/dodopayments/dodopayments-1.67.2-py3-none-any.whl/dodopayments/types/products/__init__.py
@@ -0,0 +1,6 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .image_update_params import ImageUpdateParams as ImageUpdateParams
+from .image_update_response import ImageUpdateResponse as ImageUpdateResponse
