f2277dcb1d9c6d03397983c0b176b9982f5c2860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 14:05:21 +0000

    Add package utah-housing-stat386/0.3.4/utah_housing_stat386-0.3.4-py3-none-any.whl

---
+++ packages/utah-housing-stat386/utah_housing_stat386-0.3.4-py3-none-any.whl/utah_housing_stat386/__init__.py
@@ -0,0 +1,31 @@
+from utah_housing_stat386.core import get_data
+from utah_housing_stat386.cleaning import (
+    get_cleaned_data,
+    clean_housing_data,
+    clean_price,
+    clean_numeric_field,
+    clean_year_built,
+    clean_lot_size,
+    clean_garage,
+    remove_duplicates,
+    remove_invalid_entries
+)
+from utah_housing_stat386.demo import load_demo_data, demo_cleaning, run_demo
+
+__version__ = "0.3.2"
+
+__all__ = [
+    "get_data",
+    "get_cleaned_data",
+    "clean_housing_data",
+    "clean_price",
+    "clean_numeric_field",
+    "clean_year_built",
+    "clean_lot_size",
+    "clean_garage",
+    "remove_duplicates",
+    "remove_invalid_entries",
+    "load_demo_data",
+    "demo_cleaning",
+    "run_demo"
+]
