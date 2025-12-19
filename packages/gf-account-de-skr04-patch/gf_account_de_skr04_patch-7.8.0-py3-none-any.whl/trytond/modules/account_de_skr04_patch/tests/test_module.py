2d3fd76153f97a75e44da6931ee5061d45683629
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 20:07:26 +0000

    Add package gf-account-de-skr04-patch/7.8.0/gf_account_de_skr04_patch-7.8.0-py3-none-any.whl

---
+++ packages/gf-account-de-skr04-patch/gf_account_de_skr04_patch-7.8.0-py3-none-any.whl/trytond/modules/account_de_skr04_patch/tests/test_module.py
@@ -0,0 +1,10 @@
+
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class AccountDeSkr04PatchTestCase(ModuleTestCase):
+    "Test Account De Skr04 Patch module"
+    module = 'account_de_skr04_patch'
+
+
+del ModuleTestCase
