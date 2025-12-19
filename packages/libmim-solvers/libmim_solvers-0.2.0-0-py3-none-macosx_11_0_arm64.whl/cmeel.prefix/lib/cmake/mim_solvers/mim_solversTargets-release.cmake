5a5a8387c27bb1591a9b66fda5a358a30a709770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 20:07:21 +0000

    Add package libmim-solvers/0.2.0/libmim_solvers-0.2.0-0-py3-none-macosx_11_0_arm64.whl

---
+++ packages/libmim-solvers/libmim_solvers-0.2.0-0-py3-none-macosx_11_0_arm64.whl/cmeel.prefix/lib/cmake/mim_solvers/mim_solversTargets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "mim_solvers::mim_solvers" for configuration "Release"
+set_property(TARGET mim_solvers::mim_solvers APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(mim_solvers::mim_solvers PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libmim_solvers.0.2.0.dylib"
+  IMPORTED_SONAME_RELEASE "@rpath/libmim_solvers.0.2.0.dylib"
+  )
+
+list(APPEND _cmake_import_check_targets mim_solvers::mim_solvers )
+list(APPEND _cmake_import_check_files_for_mim_solvers::mim_solvers "${_IMPORT_PREFIX}/lib/libmim_solvers.0.2.0.dylib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
