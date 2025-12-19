d0cd3ef4310997fe3cca39fdf69091e5ee438440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 21:07:24 +0000

    Add package eldar-chainword/1.0.0/eldar_chainword-1.0.0.tar.gz

---
+++ packages/eldar-chainword/eldar_chainword-1.0.0.tar.gz/eldar_chainword-1.0.0/eldar_chainword/words.py
@@ -0,0 +1,7 @@
+# Sadə nümunə sözlər, istədiyin kimi genişləndirə bilərsən
+WORDS = [
+    "baku", "ustan", "naxçıvan", "nəriman", "nar", "rusiya",
+    "azerbaijan", "novruz", "zəfər", "rəvan", "nabat",
+    "computer",  "home",  "glass",  "screen",  "comment"
+]
+
