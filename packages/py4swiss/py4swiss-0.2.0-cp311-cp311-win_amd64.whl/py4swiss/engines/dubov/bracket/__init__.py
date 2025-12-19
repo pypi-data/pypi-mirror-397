73d7f9a18a0ec11509b2749ec4451375cf3c7770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 21:07:06 +0000

    Add package py4swiss/0.2.0/py4swiss-0.2.0-cp311-cp311-win_amd64.whl

---
+++ packages/py4swiss/py4swiss-0.2.0-cp311-cp311-win_amd64.whl/py4swiss/engines/dubov/bracket/__init__.py
@@ -0,0 +1,5 @@
+from py4swiss.engines.dubov.bracket.bracket import Bracket
+from py4swiss.engines.dubov.bracket.bracket_pairer import BracketPairer
+from py4swiss.engines.dubov.bracket.brackets import Brackets
+
+__all__ = ["Bracket", "BracketPairer", "Brackets"]
