c366684f359f57eb2dd66dc94e6e9fcfdb843a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 03:33:14 +0000

    Add package hexbytes/2.0.0b1/hexbytes-2.0.0b1.tar.gz

---
+++ packages/hexbytes/hexbytes-2.0.0b1.tar.gz/hexbytes-2.0.0b1/README.md
@@ -0,0 +1,19 @@
+# HexBytes
+
+[![Join the conversation on Discord](https://img.shields.io/discord/809793915578089484?color=blue&label=chat&logo=discord&logoColor=white)](https://discord.gg/GHryRvPB84)
+[![Build Status](https://circleci.com/gh/ethereum/hexbytes.svg?style=shield)](https://circleci.com/gh/ethereum/hexbytes)
+[![PyPI version](https://badge.fury.io/py/hexbytes.svg)](https://badge.fury.io/py/hexbytes)
+[![Python versions](https://img.shields.io/pypi/pyversions/hexbytes.svg)](https://pypi.python.org/pypi/hexbytes)
+[![Docs build](https://readthedocs.org/projects/hexbytes/badge/?version=latest)](https://hexbytes.readthedocs.io/en/latest/?badge=latest)
+
+Python `bytes` subclass that decodes hex, with a readable console output
+
+Read the [documentation](https://hexbytes.readthedocs.io/).
+
+View the [change log](https://hexbytes.readthedocs.io/en/latest/release_notes.html).
+
+## Installation
+
+```sh
+python -m pip install hexbytes
+```
---
+++ packages/hexbytes/hexbytes-2.0.0b1.tar.gz/hexbytes-2.0.0b1/setup.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+extras_require = {
+    "dev": [
+        "build>=0.9.0",
+        "bump_my_version>=0.19.0",
+        "ipython",
+        "mypy==1.18.2",
+        "pre-commit>=3.4.0",
+        "tox>=4.0.0",
+        "twine",
+        "wheel>=0.38.1",
+    ],
+    "docs": [
+        "sphinx>=6.0.0",
+        "sphinx-autobuild>=2021.3.14",
+        "sphinx_rtd_theme>=1.0.0",
+        "towncrier>=24,<25",
+    ],
+    "test": [
+        "eth_utils>=2.0.0",
+        "hypothesis>=3.44.24",
+        "pytest>=7.0.0",
+        "pytest-xdist>=2.4.0",
+    ],
+}
+
+extras_require["dev"] = (
+    extras_require["dev"] + extras_require["docs"] + extras_require["test"]
+)
+
+
+with open("./README.md") as readme:
+    long_description = readme.read()
+
+
+setup(
+    name="hexbytes",
+    # *IMPORTANT*: Don't manually change the version here. See Contributing docs for the release process.
+    version="2.0.0-beta.1",
+    description="""hexbytes: Python `bytes` subclass that decodes hex, with a readable console output""",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="The Ethereum Foundation",
+    author_email="snakecharmers@ethereum.org",
+    url="https://github.com/ethereum/hexbytes",
+    include_package_data=True,
+    install_requires=[],
+    python_requires=">=3.10, <4",
+    extras_require=extras_require,
+    py_modules=["hexbytes"],
+    zip_safe=False,
+    keywords="ethereum",
+    packages=find_packages(exclude=["scripts", "scripts.*", "tests", "tests.*"]),
+    package_data={"hexbytes": ["py.typed"]},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
+    ],
+)
