56be5bb97f7fad266ba7f1fca75143b106be54f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 16:06:18 +0000

    Add package tobj/0.2.1/tobj-0.2.1.tar.gz

---
+++ packages/tobj/tobj-0.2.1.tar.gz/tobj-0.2.1/tobj/__init__.py
@@ -0,0 +1,20 @@
+"""
+TinyObj v0.2.1,
+a tiny object format for freedom.
+"""
+from tobj.interpreter import Interpreter
+from tobj.parser import Parser
+from tobj.lexer import Lexer
+from tobj.main import dump, dumps, load, loads
+
+__all__ = [
+    "Interpreter",
+    "Parser",
+    "Lexer",
+    
+	"dump", "dumps",
+    "load", "loads",
+]
+
+__version__ = "0.2.1"
+__author__ = "Razka Rizaldi"
