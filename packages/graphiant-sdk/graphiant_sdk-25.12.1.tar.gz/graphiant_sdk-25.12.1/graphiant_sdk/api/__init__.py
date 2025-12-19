041a3b6ea5c7183409a96dd61fcfaada46724d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 03:33:31 +0000

    Add package graphiant-sdk/25.12.1/graphiant_sdk-25.12.1.tar.gz

---
+++ packages/graphiant-sdk/graphiant_sdk-25.12.1.tar.gz/graphiant_sdk-25.12.1/graphiant_sdk/api/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+
+# import apis into api package
+from graphiant_sdk.api.default_api import DefaultApi
+
---
+++ packages/graphiant-sdk/graphiant_sdk-25.12.1.tar.gz/graphiant_sdk-25.12.1/setup.py
@@ -0,0 +1,59 @@
+# coding: utf-8
+"""
+Copyright (c) 2025 Graphiant-Inc
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from setuptools import setup, find_packages  # noqa: H301
+
+NAME = "graphiant_sdk"
+VERSION = "25.12.1"
+PYTHON_REQUIRES = ">= 3.9"
+REQUIRES = [
+    "urllib3 >= 2.1.0, < 3.0.0",
+    "python-dateutil >= 2.8.2",
+    "pydantic >= 2",
+    "typing-extensions >= 4.7.1",
+]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="Python SDK for Graphiant NaaS",
+    author="Graphiant Inc",
+    author_email="support@graphiant.com",
+    url="https://www.graphiant.com",
+    keywords=["SDK", "Graphiant", "NaaS"],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    package_data={"graphiant_sdk": ["py.typed"]},
+    project_urls={
+         "Source Code": "https://github.com/Graphiant-Inc/graphiant-sdk-python"
+    },
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
