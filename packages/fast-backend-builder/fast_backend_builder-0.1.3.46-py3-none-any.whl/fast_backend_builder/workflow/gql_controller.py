c007da8d6db4e6918a58e751a57bcc561f5935c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 16:06:45 +0000

    Add package fast-backend-builder/0.1.3.46/fast_backend_builder-0.1.3.46-py3-none-any.whl

---
+++ packages/fast-backend-builder/fast_backend_builder-0.1.3.46-py3-none-any.whl/fast_backend_builder/workflow/gql_controller.py
@@ -0,0 +1,185 @@
+from typing import Generic, Type
+
+from tortoise.exceptions import DoesNotExist, IntegrityError, ValidationError
+from tortoise.transactions import in_transaction
+
+from fast_backend_builder.auth.auth import Auth
+from fast_backend_builder.common.response.codes import ResponseCode
+from fast_backend_builder.common.response.schemas import ApiResponse
+from fast_backend_builder.common.schemas import ModelType
+from fast_backend_builder.models.workflow import Evaluation, Workflow
+from fast_backend_builder.utils.error_logging import log_exception
+from fast_backend_builder.utils.helpers.log_activity import log_user_activity
+from fast_backend_builder.workflow.exceptions import WorkflowException
+from fast_backend_builder.workflow.request import EvaluationStatus
+from fast_backend_builder.workflow.response import EvaluationStatusResponse
+
+
+# MinIO setup
+class TransitionBaseController(Generic[ModelType]):
+    def __init__(self, model: Type[ModelType]):
+        self.model = model
+
+    async def before_transit(
+            self,
+            evaluation_status: EvaluationStatus,
+            obj: ModelType,
+            connection,
+    ):
+        pass
+
+    async def after_transit(
+            self,
+            obj: ModelType,
+            evaluation: Evaluation,
+            connection,
+    ):
+        pass
+
+    async def transit(self, evaluation_status: EvaluationStatus) -> ApiResponse:
+        """
+        Creates new transition for ModelType.
+        """
+        try:
+            current_user = Auth.user()
+            user_id = current_user.get('user_id')
+            username = current_user.get('username')
+
+            async with in_transaction("default") as connection:
+                obj = await self.model.get(
+                    id=evaluation_status.object_id,
+                    using_db=connection,
+                )
+
+                res = await self.before_transit(
+                    evaluation_status,
+                    obj,
+                    connection,
+                )
+                if isinstance(res, ApiResponse):
+                    return res
+
+                workflow = await Workflow.filter(
+                    code=self.model.__name__,
+                    is_active=True,
+                ).using_db(connection).first()
+
+                if not workflow:
+                    return ApiResponse(
+                        status=False,
+                        code=ResponseCode.BAD_REQUEST,
+                        message=f"Workflow of {self.model.Meta.verbose_name} is not configured properly",
+                        data=False,
+                    )
+
+                evaluation, next_step_code = await workflow.transit(
+                    object_id=evaluation_status.object_id,
+                    next_step=evaluation_status.status,
+                    user_id=user_id,
+                    remark=evaluation_status.remark,
+                    connection=connection,
+                )
+
+                # Keep in-memory and DB state consistent
+                obj.evaluation_status = next_step_code
+                await obj.save(using_db=connection)
+
+                await self.after_transit(obj, evaluation, connection)
+
+                await log_user_activity(
+                    user_id=user_id,
+                    username=username,
+                    entity=Evaluation.Meta.verbose_name,
+                    action='CHANGE',
+                    details=f"{self.model.Meta.verbose_name} transitioned to {next_step_code} successfully",
+                )
+
+                return ApiResponse(
+                    status=True,
+                    code=ResponseCode.SUCCESS,
+                    message=f"{self.model.Meta.verbose_name} transitioned to {evaluation.workflow_step.name} successfully",
+                    data=True,
+                )
+
+        except WorkflowException as ve:
+            log_exception(Exception(ve))
+            return ApiResponse(
+                status=False,
+                code=ResponseCode.BAD_REQUEST,
+                message=f"{str(ve)}",
+                data=False
+            )
+        except DoesNotExist:
+            return ApiResponse(
+                status=False,
+                code=ResponseCode.NO_RECORD_FOUND,
+                message=f"The {self.model.Meta.verbose_name} does not exist",
+                data=False
+            )
+        except ValueError as ve:
+            log_exception(Exception(ve))
+            return ApiResponse(
+                status=False,
+                code=ResponseCode.BAD_REQUEST,
+                message=str(ve),
+                data=False
+            )
+        except IntegrityError as e:
+            log_exception(Exception(e))
+            # Check the exception message to identify a unique constraint violation
+
+            return ApiResponse(
+                status=False,
+                code=ResponseCode.BAD_REQUEST,
+                message=self.parse_integrity_error(e),
+                data=False
+            )
+        except ValidationError as e:
+            log_exception(e)
+            # Check the exception message to identify a unique constraint violation
+            return ApiResponse(
+                status=False,
+                code=ResponseCode.BAD_REQUEST,
+                message=str(e),
+                data=False
+            )
+        except Exception as e:
+            log_exception(e)
+            return ApiResponse(
+                status=False,
+                code=ResponseCode.FAILURE,
+                message=f"Failed to transit {self.model.Meta.verbose_name}. Try again.",
+                data=None
+            )
+
+    async def get_transitions(self, model_id: str) -> ApiResponse:
+        try:
+            evaluation_status = await Evaluation.filter(object_id=model_id, object_name=self.model.__name__).select_related('user','workflow_step')
+            data_list = [EvaluationStatusResponse(
+                id=evaluation.id,
+                object_name=evaluation.object_name,
+                object_id=evaluation.object_id,
+                status=evaluation.workflow_step.code,
+                remark=evaluation.remark,
+                user_id=evaluation.user.id,
+                user_full_name = evaluation.user.get_short_name(),
+                created_at=evaluation.created_at
+
+            )for evaluation in evaluation_status]
+
+
+            return ApiResponse(
+                status=True,
+                code=ResponseCode.SUCCESS,
+                message=f"{self.model.Meta.verbose_name} evaluation statuses fetched successfully",
+                data=data_list,
+
+            )
+        except Exception as e:
+            log_exception(Exception(e))
+            return ApiResponse(
+                status=False,
+                code=ResponseCode.BAD_REQUEST,
+                message=f"Failed to fetch {self.model.Meta.verbose_name} evaluation statuses. Try again",
+                data=None
+            )
