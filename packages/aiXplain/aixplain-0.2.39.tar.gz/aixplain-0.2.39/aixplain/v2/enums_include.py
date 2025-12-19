97d6bfe86e5bfbbeec5769e70d1c24fb1404d650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 21:08:22 +0000

    Add package aiXplain/0.2.39/aixplain-0.2.39.tar.gz

---
+++ packages/aiXplain/aixplain-0.2.39.tar.gz/aixplain-0.2.39/aixplain/v2/enums_include.py
@@ -0,0 +1,44 @@
+# This is an auto generated module. PLEASE DO NOT EDIT
+# This module provides compatibility imports from legacy enums for v2
+
+# Import all enums from legacy system for compatibility
+from aixplain.enums import (
+    AssetStatus,
+    ErrorHandler,
+    FileType,
+    Function,
+    Language,
+    License,
+    OnboardStatus,
+    OwnershipType,
+    Privacy,
+    ResponseStatus,
+    SortBy,
+    SortOrder,
+    StorageType,
+    Supplier,
+    FunctionType,
+    EvolveType,
+    CodeInterpreterModel,
+)
+
+# Re-export for compatibility
+__all__ = [
+    "AssetStatus",
+    "ErrorHandler",
+    "FileType",
+    "Function",
+    "Language",
+    "License",
+    "OnboardStatus",
+    "OwnershipType",
+    "Privacy",
+    "ResponseStatus",
+    "SortBy",
+    "SortOrder",
+    "StorageType",
+    "Supplier",
+    "FunctionType",
+    "EvolveType",
+    "CodeInterpreterModel",
+]
