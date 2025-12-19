ef55aea31a00badcf57c6702db6c4e3a6381ea10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 18:07:43 +0000

    Add package pywis-pubsub/0.11.0/pywis_pubsub-0.11.0.tar.gz

---
+++ packages/pywis-pubsub/pywis_pubsub-0.11.0.tar.gz/pywis_pubsub-0.11.0/setup.py
@@ -0,0 +1,97 @@
+###############################################################################
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+###############################################################################
+
+from pathlib import Path
+import re
+from setuptools import find_packages, setup
+
+
+def read(filename, encoding='utf-8'):
+    """read file contents"""
+
+    fullpath = Path(__file__).resolve().parent / filename
+
+    with fullpath.open() as fh:
+        contents = fh.read().strip()
+    return contents
+
+
+def get_package_version():
+    """get version from top-level package init"""
+    version_file = read('pywis_pubsub/__init__.py')
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
+
+
+LONG_DESCRIPTION = read('README.md')
+
+DESCRIPTION = 'pywis-pubsub provides subscription and download capability of WMO data from WIS2 infrastructure services'  # noqa
+
+MANIFEST = Path('MANIFEST')
+
+if MANIFEST.exists():
+    MANIFEST.unlink()
+
+
+setup(
+    name='pywis-pubsub',
+    version=get_package_version(),
+    description=DESCRIPTION.strip(),
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    license='Apache Software License',
+    platforms='all',
+    keywords=' '.join([
+        'WIS2',
+        'PubSub',
+        'broker',
+        'topic'
+    ]),
+    author='Antje Schremmer',
+    author_email='antje.schremmer@dwd.de',
+    maintainer='Tom Kralidis',
+    maintainer_email='tomkraldis@gmail.com',
+    url='https://github.com/World-Meteorological-Organization/pywis-pubsub',
+    install_requires=read('requirements.txt').splitlines(),
+    extras_require={
+        'backend-s3': read('requirements-s3.txt').splitlines()
+    },
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'pywis-pubsub=pywis_pubsub:cli'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ],
+    test_suite='tests.run_tests'
+)
