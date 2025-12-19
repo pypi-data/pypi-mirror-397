d3c9dd6243be1fd69a0626318b1d380720a3ddd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 17:07:48 +0000

    Add package CTK-Color/0.0.1/ctk_color-0.0.1.tar.gz

---
+++ packages/CTK-Color/ctk_color-0.0.1.tar.gz/ctk_color-0.0.1/tests/test2.py
@@ -0,0 +1,12 @@
+import CTK_Color
+import customtkinter as ctk
+
+win=ctk.CTk()
+
+text=ctk.CTkTextbox(win)
+text.pack()
+CTK_Color(text,"for","bal","red").for_word()
+CTk_Color(text,"print","edit","orange").for_word()
+CTk_Color(text,"while","gang","blue").for_word()
+CTk_Color(text,"boxer","baler","green").for_word()
+win.mainloop()
