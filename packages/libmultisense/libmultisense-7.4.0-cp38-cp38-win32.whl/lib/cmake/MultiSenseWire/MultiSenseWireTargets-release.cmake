897626635c925677570b6e8eeddd68dc9b85bb14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 18:08:06 +0000

    Add package libmultisense/7.4.0/libmultisense-7.4.0-cp38-cp38-win32.whl

---
+++ packages/libmultisense/libmultisense-7.4.0-cp38-cp38-win32.whl/lib/cmake/MultiSenseWire/MultiSenseWireTargets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "MultiSenseWire" for configuration "Release"
+set_property(TARGET MultiSenseWire APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(MultiSenseWire PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/MultiSenseWire.lib"
+  )
+
+list(APPEND _cmake_import_check_targets MultiSenseWire )
+list(APPEND _cmake_import_check_files_for_MultiSenseWire "${_IMPORT_PREFIX}/lib/MultiSenseWire.lib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
