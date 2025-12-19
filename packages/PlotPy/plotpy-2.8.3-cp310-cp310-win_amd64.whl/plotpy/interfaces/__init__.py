23ffba888369ac55878269457f55b2642f728e41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 16:06:59 +0000

    Add package PlotPy/2.8.3/plotpy-2.8.3-cp310-cp310-win_amd64.whl

---
+++ packages/PlotPy/plotpy-2.8.3-cp310-cp310-win_amd64.whl/plotpy/interfaces/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# pylint: disable=unused-import
+# flake8: noqa
+
+from .items import (
+    IBaseImageItem,
+    IBasePlotItem,
+    IColormapImageItemType,
+    ICSImageItemType,
+    ICurveItemType,
+    IDecoratorItemType,
+    IExportROIImageItemType,
+    IHistDataSource,
+    IImageItemType,
+    IItemType,
+    ISerializableType,
+    IShapeItemType,
+    ITrackableItemType,
+    IVoiImageItemType,
+)
+from .panel import IPanel
+from .plotmanager import IPlotManager
---
+++ packages/PlotPy/plotpy-2.8.3-cp310-cp310-win_amd64.whl/plotpy/plot/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# pylint: disable=unused-import
+# flake8: noqa
+
+from .base import BasePlot, BasePlotOptions
+from .manager import PlotManager
+from .plotwidget import (
+    PlotDialog,
+    PlotOptions,
+    PlotWidget,
+    PlotWindow,
+    SubplotWidget,
+    SyncPlotWindow,
+    SyncPlotDialog,
+    set_widget_title_icon,
+)
