941466466ad695e50173bc1d5859429c04fd4a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 03:33:18 +0000

    Add package tuk-config/0.1.0/tuk_config-0.1.0-py3-none-any.whl

---
+++ packages/tuk-config/tuk_config-0.1.0-py3-none-any.whl/tuk/config/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+
+
+__author__ = "Tarcadia"
+__url__ = "https://github.com/Tarcadia/tuk-config"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Tarcadia"]
+__license__ = "MIT"
+__version__ = "0.1.0"
+
