7c72a6bd1f844d4285cd12acf40a02115f778901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 16:06:26 +0000

    Add package coinsignal/1.4.1/coinsignal-1.4.1-cp312-cp312-macosx_10_13_universal2.whl

---
+++ packages/coinsignal/coinsignal-1.4.1-cp312-cp312-macosx_10_13_universal2.whl/coinsignal/sampler.py
@@ -0,0 +1,30 @@
+# --------------------------------------------------------------------------------
+# Copyright (c) 2025 Zehao Yang
+#
+# Author: Zehao Yang
+#
+# This module implements data sampling functions:
+# • Sampling data on time using time intervals
+# • Sampling data on move using smoothed price change
+# • Sampling data on return using weighted random sampling
+# --------------------------------------------------------------------------------
+
+
+import _ext.sampler as _sampler
+
+
+DEFAULT_SAMPLER_PARAMS_DICT = {
+    'sampling_time': 0,
+    'is_time_random': False,
+    'sampling_bp': 0,
+    'rolling_step': 0,
+    'is_sampling_on_ret': False,
+    'fraction': 1,
+    'weight_limit': 1,
+    'is_sign_balanced': False,
+}
+
+
+def sample_data(features_map, sampler_params_dict):
+    features_map = _sampler.sample_data(features_map, sampler_params_dict)
+    return features_map
