860166fe28fb418d1442abc50e8a04646528f890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 14:05:10 +0000

    Add package jaxsso/1.0.1/jaxsso-1.0.1.tar.gz

---
+++ packages/jaxsso/jaxsso-1.0.1.tar.gz/jaxsso-1.0.1/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jaxsso",
+    version="1.0.1",
+    author="Gaoyuan Wu",
+    author_email="gaoyuanw@princeton.edu",
+    description="A framework for structural shape optimization based on automatic differentiation (AD) and the adjoint method, enabled by JAX",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GaoyuanWu/JaxSSO",
+    packages=setuptools.find_packages(include=['JaxSSO']),
+    keywords=["jax", "automatic differentiation", "shape optimization", "finite element analysis", "form finding", "topology optimization", "structural optimization"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    install_requires=[
+        'numpy>=1.22.0',
+        'matplotlib',
+        'jax>=0.4.24',
+        'scipy'
+    ],
+    include_package_data = True,
+    python_requires=">=3.7",
+)
