3eca34a5f70e8b33d0c57859bfd93b0edcab93d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 15:05:43 +0000

    Add package pxutil/0.0.49/pxutil-0.0.49-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/pxutil/pxutil-0.0.49-cp310-cp310-macosx_11_0_arm64.whl/pxutil/__init__.py
@@ -0,0 +1,35 @@
+"""
+pxutil
+~~~~~
+
+Peter Jiping Xie's personal utils.
+
+:copyright: © 2019 by Peter Jiping Xie.
+:license: MIT.
+"""
+
+# import func/classes from .pxutil here so users can them directly,
+# i.e., pxutil.bash() instead of pxutl.pxutil.bash().
+from .pxutil import (
+    bash,
+    bashx,
+    grep,
+    purge,
+    time2seconds,
+    replace_in_file,
+    normal_path,
+    exit_on_exception,
+    register_signal_ctrl_c,
+    post,
+    request,
+    set_work_path,
+    prepend_sys_path,
+    import_any,
+    ChatAPI,
+    list_module_contents,
+    setup_logger,
+    read_dotenv,
+    is_text_file,
+    token_counter,
+)
+from .pxutil_cy import run_loop, fib
