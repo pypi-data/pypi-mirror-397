2dfb891a866e7fe3bf03530997a1a988470297f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 16:06:19 +0000

    Add package pynetping/1.0.1/pynetping-1.0.1.tar.gz

---
+++ packages/pynetping/pynetping-1.0.1.tar.gz/pynetping-1.0.1/PyNetPing/__init__.py
@@ -0,0 +1,9 @@
+# Developer: Sreeraj
+# GitHub: https://github.com/s-r-e-e-r-a-j
+
+from .ping import ping
+from .async_engine import ping_hosts
+from .result import PingResult
+from .output import to_json, to_csv
+
+__all__ = ["ping", "ping_hosts", "PingResult", "to_json", "to_csv"]
---
+++ packages/pynetping/pynetping-1.0.1.tar.gz/pynetping-1.0.1/PyNetPing/async_engine.py
@@ -0,0 +1,23 @@
+# Developer: Sreeraj
+# GitHub: https://github.com/s-r-e-e-r-a-j
+
+import asyncio
+from typing import Iterable
+from .ping import ping
+from .result import PingResult
+
+async def ping_hosts(
+    hosts: Iterable[str],
+    count: int = 4,
+    timeout: float = 1.0,
+    port: int = 80,
+    limit: int = 100
+) -> list[PingResult]:
+
+    sem: asyncio.Semaphore = asyncio.Semaphore(limit)
+
+    async def run(host: str) -> PingResult:
+        async with sem:
+            return await asyncio.to_thread(ping, host, count, timeout, port=port, use_dns=True)
+
+    return await asyncio.gather(*(run(h) for h in hosts))
