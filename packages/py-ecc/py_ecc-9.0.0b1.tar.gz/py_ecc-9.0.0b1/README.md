859028161ddf10b6a68b2fb6ab6f7230116d6f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 04:16:01 +0000

    Add package py-ecc/9.0.0b1/py_ecc-9.0.0b1.tar.gz

---
+++ packages/py-ecc/py_ecc-9.0.0b1.tar.gz/py_ecc-9.0.0b1/README.md
@@ -0,0 +1,20 @@
+# py_ecc
+
+[![Join the conversation on Discord](https://img.shields.io/discord/809793915578089484?color=blue&label=chat&logo=discord&logoColor=white)](https://discord.gg/GHryRvPB84)
+[![Build Status](https://circleci.com/gh/ethereum/py_ecc.svg?style=shield)](https://circleci.com/gh/ethereum/py_ecc)
+[![PyPI version](https://badge.fury.io/py/py-ecc.svg)](https://badge.fury.io/py/py-ecc)
+[![Python versions](https://img.shields.io/pypi/pyversions/py-ecc.svg)](https://pypi.python.org/pypi/py-ecc)
+
+Elliptic curve crypto in python including secp256k1, alt_bn128, and bls12_381.
+
+> **Warning**: This library contains some experimental code and has **NOT** been audited.
+
+Read the [documentation](https://py-ecc.readthedocs.io/).
+
+View the [change log](https://py-ecc.readthedocs.io/en/latest/release_notes.html).
+
+## Installation
+
+```sh
+python -m pip install py_ecc
+```
---
+++ packages/py-ecc/py_ecc-9.0.0b1.tar.gz/py_ecc-9.0.0b1/setup.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+extras_require = {
+    "dev": [
+        "build>=0.9.0",
+        "bump_my_version>=0.19.0",
+        "ipython",
+        "mypy==1.18.2",
+        "pre-commit>=3.4.0",
+        "tox>=4.0.0",
+        "twine",
+        "wheel>=0.38.1",
+    ],
+    "docs": [
+        "sphinx>=6.0.0",
+        "sphinx-autobuild>=2021.3.14",
+        "sphinx_rtd_theme>=1.0.0",
+        "towncrier>=24,<25",
+    ],
+    "test": [
+        "pytest>=7.0.0",
+        "pytest-xdist>=2.4.0",
+    ],
+}
+
+extras_require["dev"] = (
+    extras_require["dev"] + extras_require["docs"] + extras_require["test"]
+)
+
+
+with open("./README.md") as readme:
+    long_description = readme.read()
+
+
+setup(
+    name="py-ecc",
+    # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
+    version="9.0.0-beta.1",
+    description="""py-ecc: Elliptic curve crypto in python including secp256k1, alt_bn128, and bls12_381""",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="The Ethereum Foundation",
+    author_email="snakecharmers@ethereum.org",
+    url="https://github.com/ethereum/py_ecc",
+    include_package_data=True,
+    install_requires=[
+        "eth-typing>=3.0.0",
+        "eth-utils>=2.0.0",
+    ],
+    python_requires=">=3.10, <4",
+    extras_require=extras_require,
+    py_modules=["py_ecc"],
+    license="MIT",
+    zip_safe=False,
+    keywords="ethereum",
+    packages=find_packages(exclude=["scripts", "scripts.*", "tests", "tests.*"]),
+    package_data={"py_ecc": ["py.typed"]},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
+    ],
+)
