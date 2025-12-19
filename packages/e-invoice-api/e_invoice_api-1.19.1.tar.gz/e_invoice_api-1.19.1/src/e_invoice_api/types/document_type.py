33e9db730691e8f9308fa0a7e09adfa6cd13e80e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 14:05:26 +0000

    Add package e-invoice-api/1.19.1/e_invoice_api-1.19.1.tar.gz

---
+++ packages/e-invoice-api/e_invoice_api-1.19.1.tar.gz/e_invoice_api-1.19.1/src/e_invoice_api/types/document_type.py
@@ -0,0 +1,7 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing_extensions import Literal, TypeAlias
+
+__all__ = ["DocumentType"]
+
+DocumentType: TypeAlias = Literal["INVOICE", "CREDIT_NOTE", "DEBIT_NOTE"]
