8046cb6de2c3760dee7bfcc2c9d8e64fb964833a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 04:16:44 +0000

    Add package nimare/0.6.2rc1/nimare-0.6.2rc1.tar.gz

---
+++ packages/nimare/nimare-0.6.2rc1.tar.gz/nimare-0.6.2rc1/nimare/__init__.py
@@ -0,0 +1,45 @@
+"""NiMARE: Neuroimaging Meta-Analysis Research Environment."""
+
+import logging
+import warnings
+
+from ._version import get_versions
+
+logging.basicConfig(level=logging.INFO)
+
+with warnings.catch_warnings(record=True) as w:
+    warnings.simplefilter("ignore")
+    from . import (
+        annotate,
+        base,
+        correct,
+        dataset,
+        decode,
+        io,
+        meta,
+        reports,
+        resources,
+        stats,
+        utils,
+        workflows,
+    )
+
+    __version__ = get_versions()["version"]
+
+    __all__ = [
+        "base",
+        "dataset",
+        "meta",
+        "correct",
+        "annotate",
+        "decode",
+        "resources",
+        "io",
+        "stats",
+        "utils",
+        "reports",
+        "workflows",
+        "__version__",
+    ]
+
+del get_versions
