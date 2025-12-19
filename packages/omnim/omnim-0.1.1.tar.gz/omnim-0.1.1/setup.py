efc5bac042bb25ca660fd315ce7ac54e703d512d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 17:07:46 +0000

    Add package omnim/0.1.1/omnim-0.1.1.tar.gz

---
+++ packages/omnim/omnim-0.1.1.tar.gz/omnim-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="omnim",
+    url="https://github.com/cet-t/omnipy",
+    version="0.1.1",
+    description="useful toolkit",
+    author="cet",
+    packages=["omnim"],
+    include_package_data=True,
+    zip_safe=True,
+)
