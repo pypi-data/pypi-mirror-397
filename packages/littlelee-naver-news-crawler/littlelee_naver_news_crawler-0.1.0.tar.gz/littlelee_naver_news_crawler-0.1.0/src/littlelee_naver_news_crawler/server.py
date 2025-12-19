673f03217b3b5f471d91d61bf72588c0e34daad2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 14:05:34 +0000

    Add package littlelee-naver-news-crawler/0.1.0/littlelee_naver_news_crawler-0.1.0.tar.gz

---
+++ packages/littlelee-naver-news-crawler/littlelee_naver_news_crawler-0.1.0.tar.gz/littlelee_naver_news_crawler-0.1.0/src/littlelee_naver_news_crawler/server.py
@@ -0,0 +1,50 @@
+import requests
+from bs4 import BeautifulSoup
+
+from mcp.server.fastmcp import FastMCP
+
+
+# Initialize FastMCP server
+mcp = FastMCP("naver_news_crawler")
+
+@mcp.tool()
+def get_crawling(keyword: str, numofpages: int) -> list:
+    """
+    Naver News titles are collected and stored in a list based on the number of news article pages related to the keywords requested by the user..
+    
+    Args:
+        keyword (str): Keywords requested by users for Naver News search.
+        numofpages (int): Number of Naver news pages to collect as requested by the user.
+    
+    Returns:
+        list: Collected Naver News Titles.
+    """
+    
+    base_url = 'https://search.naver.com/search.naver?where=news&sm=tab_jum&query=' + keyword + '&start='    
+
+    naver_crawling = []
+    
+    for i in range(1, numofpages * 10, 10) :
+        url = base_url + str(i)
+
+        response = requests.get(url, headers={'User-Agent':'Moailla/5.0'})
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        headlines = soup.find_all('span', {'class' : 'sds-comps-text sds-comps-text-ellipsis sds-comps-text-ellipsis-1 sds-comps-text-type-headline1'})
+
+        for headline in headlines:
+            title = headline.text
+            naver_crawling.append(title)
+
+    return naver_crawling
+
+
+def main() -> None:
+    # Initialize and run the server
+    print("Starting Naver News Crawler server...")
+    mcp.run(transport='stdio')
+    
+
+if __name__ == "__main__":
+   main()  
