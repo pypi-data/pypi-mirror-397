18c76772ec1f260dcfdf12a88a549520bed4e22a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 18:07:16 +0000

    Add package lcdp-deployment-manager/1.2.8rc20337652782/lcdp_deployment_manager-1.2.8rc20337652782-py3-none-any.whl

---
+++ packages/lcdp-deployment-manager/lcdp_deployment_manager-1.2.8rc20337652782-py3-none-any.whl/lcdp_deployment_manager/constant.py
@@ -0,0 +1,35 @@
+# Constant.py
+
+# Color
+BLUE = 'blue'
+GREEN = 'green'
+
+# LISTENER
+HTTPS_TUPLE = ('HTTPS', 443)
+HTTP_TUPLE = ('HTTP', 80)
+
+# Target group
+TARGET_GROUP_COLOR_TAG_NAME = 'Color'
+TARGET_GROUP_TYPE_TAG_NAME = 'Type'
+# Default type means 'api gateway'
+TARGET_GROUP_DEFAULT_TYPE = 'default'
+TARGET_GROUP_MAINTENANCE_TYPE = 'maintenance'
+
+# ECR
+ECR_SERVICE_PREFIX = 'lcdp-'
+
+# ECS
+MINIMUM_HEALTHY_DESIRED_COUNT = 1
+DEFAULT_DESIRED_COUNT = 2
+DEFAULT_MAX_CAPACITY = 4
+HEALTHCHECK_RETRY_LIMIT = 26
+HEALTHCHECK_SLEEPING_TIME = 30
+ECS_SERVICE_NAMESPACE = 'ecs'
+
+# SES
+FROM_MAIL = 'no-reply@lecomptoirdespharmacies.fr'
+DEVELOPERS_MAIL = 'webmaster@lecomptoirdespharmacies.fr'
+DEFAULT_CHARSET = 'UTF-8'
+
+# ASG
+DEFAULT_SCALABLE_DIMENSION = 'ecs:service:DesiredCount'
