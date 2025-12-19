3027103244e9cd9c61825707e4562e7720b66607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 04:16:48 +0000

    Add package assemblyai/0.48.4/assemblyai-0.48.4.tar.gz

---
+++ packages/assemblyai/assemblyai-0.48.4.tar.gz/assemblyai-0.48.4/setup.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+long_description = (Path(__file__).parent / "README.md").read_text()
+
+
+def get_version() -> str:
+    version = {}
+    with open(Path(__file__).parent / "assemblyai" / "__version__.py") as f:
+        exec(f.read(), version)
+    return version["__version__"]
+
+
+setup(
+    name="assemblyai",
+    version=get_version(),
+    description="AssemblyAI Python SDK",
+    author="AssemblyAI",
+    author_email="engineering.sdk@assemblyai.com",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    install_requires=[
+        "httpx>=0.19.0",
+        "pydantic>=2.0; python_version>='3.14'",
+        "pydantic>=1.10.17; python_version<'3.14'",
+        "pydantic-settings>=2.0; python_version>='3.14'",
+        "typing-extensions>=3.7",
+        "websockets>=11.0",
+    ],
+    extras_require={
+        "extras": ["pyaudio>=0.2.13"],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AssemblyAI/assemblyai-python-sdk",
+    license="MIT License",
+    license_files=["LICENSE"],
+    python_requires=">=3.8",
+    project_urls={
+        "Code": "https://github.com/AssemblyAI/assemblyai-python-sdk",
+        "Issues": "https://github.com/AssemblyAI/assemblyai-python-sdk/issues",
+        "Documentation": "https://github.com/AssemblyAI/assemblyai-python-sdk/blob/master/README.md",
+        "API Documentation": "https://www.assemblyai.com/docs/",
+        "Website": "https://assemblyai.com/",
+    },
+)
