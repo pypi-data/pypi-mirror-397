53888d293e1aa3207fbab0db581e695393c51c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 04:15:46 +0000

    Add package invenio-vocabularies/8.3.3/invenio_vocabularies-8.3.3.tar.gz

---
+++ packages/invenio-vocabularies/invenio_vocabularies-8.3.3.tar.gz/invenio_vocabularies-8.3.3/invenio_vocabularies/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020-2025 CERN.
+# Copyright (C) 2024-2025 Graz University of Technology.
+# Copyright (C) 2025 KTH Royal Institute of Technology.
+#
+# Invenio-Vocabularies is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Invenio module for managing vocabularies."""
+
+from .ext import InvenioVocabularies
+
+__version__ = "8.3.3"
+
+__all__ = ("__version__", "InvenioVocabularies")
---
+++ packages/invenio-vocabularies/invenio_vocabularies-8.3.3.tar.gz/invenio_vocabularies-8.3.3/invenio_vocabularies/contrib/funders/schema.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021-2022 CERN.
+# Copyright (C) 2024 California Institute of Technology.
+#
+# Invenio-Vocabularies is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Funders schema."""
+
+from functools import partial
+
+from invenio_i18n import lazy_gettext as _
+from marshmallow import (
+    ValidationError,
+    fields,
+    post_load,
+    pre_dump,
+    validate,
+    validates_schema,
+)
+from marshmallow_utils.fields import IdentifierSet, SanitizedUnicode
+from marshmallow_utils.schemas import IdentifierSchema
+
+from ...services.schema import BaseVocabularySchema, ContribVocabularyRelationSchema
+from .config import funder_schemes
+
+
+class FunderRelationSchema(ContribVocabularyRelationSchema):
+    """Funder schema."""
+
+    ftf_name = "name"
+    parent_field_name = "funder"
+    name = SanitizedUnicode(
+        validate=validate.Length(min=1, error=_("Name cannot be blank."))
+    )
+
+
+class FunderSchema(BaseVocabularySchema):
+    """Service schema for funders."""
+
+    name = SanitizedUnicode(
+        required=True, validate=validate.Length(min=1, error=_("Name cannot be blank."))
+    )
+    country = SanitizedUnicode()
+    country_name = SanitizedUnicode()
+    location_name = SanitizedUnicode()
+    identifiers = IdentifierSet(
+        fields.Nested(
+            partial(
+                IdentifierSchema,
+                allowed_schemes=funder_schemes,
+                identifier_required=False,
+            )
+        )
+    )
+
+    id = SanitizedUnicode(
+        validate=validate.Length(min=1, error=_("PID cannot be blank."))
+    )
+
+    acronym = SanitizedUnicode()
+    aliases = fields.List(SanitizedUnicode())
+    status = SanitizedUnicode()
+    types = fields.List(SanitizedUnicode())
+
+    @validates_schema
+    def validate_id(self, data, **kwargs):
+        """Validates ID."""
+        is_create = "record" not in self.context
+        if is_create and "id" not in data:
+            raise ValidationError(_("Missing PID."), "id")
+        if not is_create:
+            data.pop("id", None)
+
+    @post_load(pass_many=False)
+    def move_id(self, data, **kwargs):
+        """Moves id to pid."""
+        if "id" in data:
+            data["pid"] = data.pop("id")
+        return data
+
+    @pre_dump(pass_many=False)
+    def extract_pid_value(self, data, **kwargs):
+        """Extracts the PID value."""
+        data["id"] = data.pid.pid_value
+        return data
