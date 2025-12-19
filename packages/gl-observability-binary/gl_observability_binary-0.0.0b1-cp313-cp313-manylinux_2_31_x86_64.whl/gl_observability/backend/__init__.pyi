5c6dc797256d0ffb7477c734c2a0e220e2bc97b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 16:06:15 +0000

    Add package gl-observability-binary/0.0.0b1/gl_observability_binary-0.0.0b1-cp313-cp313-manylinux_2_31_x86_64.whl

---
+++ packages/gl-observability-binary/gl_observability_binary-0.0.0b1-cp313-cp313-manylinux_2_31_x86_64.whl/gl_observability/backend/__init__.pyi
@@ -0,0 +1,4 @@
+from gl_observability.backend.opentelemetry.initializer import FastAPIConfig as FastAPIConfig, OpenTelemetryConfig as OpenTelemetryConfig
+from gl_observability.backend.sentry.initializer import SentryConfig as SentryConfig
+
+__all__ = ['OpenTelemetryConfig', 'SentryConfig', 'FastAPIConfig']
