a8fcdb0e48ea6b7721a2578c92b8da8772c21c70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 16:06:30 +0000

    Add package agentbill-py-crewai/7.6.7/agentbill_py_crewai-7.6.7-py3-none-any.whl

---
+++ packages/agentbill-py-crewai/agentbill_py_crewai-7.6.7-py3-none-any.whl/agentbill_crewai/__init__.py
@@ -0,0 +1,9 @@
+"""AgentBill CrewAI Integration
+
+Zero-config crew tracking for CrewAI agents in AgentBill.
+"""
+
+from .tracker import track_crew
+
+__version__ = "7.6.7"
+__all__ = ["track_crew"]
