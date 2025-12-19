5458ab7496341f36e33447ba189e498195a14501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 15:06:20 +0000

    Add package freeai-utils/0.6.3/freeai_utils-0.6.3-py3-none-any.whl

---
+++ packages/freeai-utils/freeai_utils-0.6.3-py3-none-any.whl/freeai_utils/audio_to_text_whisper.py
@@ -0,0 +1,161 @@
+import warnings
+warnings.filterwarnings(
+    "ignore",
+    category=UserWarning,
+    module=r"whisper\.transcribe",
+)
+import whisper #need pip install openai-whisper
+import numpy as np # need pip install numpy
+import torch # need pip install torch
+from typing import Dict, Any, List
+from typing import Optional
+from freeai_utils.log_set_up import setup_logging
+import ffmpeg #need pip install ffmpeg-python
+import imageio_ffmpeg as iioff #need pip install imageio-ffmpeg
+from .utils import enforce_type
+
+# 4 function use: transcribe -> return Dict (return everything and you choose which to get)
+#                 get_lang_detect -> return str (return the language)
+#                 get_transcription -> return str (return the transcription only, for people who doesnt care what language or anything else)
+#                 get_time_transcription -> return a list of list containing 3 item each: start_time, end_time, and the text
+
+class OpenAIWhisper:
+    
+    __slots__ = ("_model", "_initialized", "logger", "_device", "_sample_rate")
+    
+    def __init__(self, model: str = "medium", sample_rate: int = 16000, device : Optional[str]  = None) -> None:
+        #check type
+        enforce_type(sample_rate, int, sample_rate)
+        self.logger = setup_logging(self.__class__.__name__)
+        self.logger.propagate = False 
+        # init not lock
+        super().__setattr__("_initialized", False)
+        
+        # model: Whisper model size ("tiny", "base", "small", "medium", "large")
+        self._sample_rate = sample_rate
+        self._model = None 
+        self._device = None 
+        
+        #init the var to hold device available
+        preferred_devices = []
+        
+        #try input first
+        if device is not None:
+            enforce_type(device, str, "device")
+            preferred_devices.append(device)
+        
+        # try cuda second 
+        if torch.cuda.is_available() and "cuda" not in preferred_devices:
+            preferred_devices.append("cuda")
+
+        # fall back to CPU if not already there
+        if "cpu" not in preferred_devices:
+            preferred_devices.append("cpu")
+
+        last_err = None
+        for dev in preferred_devices:
+            try:
+                self.logger.info(f"Loading '{model}' model on {dev}...")
+                self._model = whisper.load_model(model, device=dev)
+                self._model.eval()
+                self._device = dev
+                self.logger.info(f"Model successfully loaded on {dev}.")
+                break
+            except RuntimeError as e:
+                last_err = e
+                self.logger.error(f"Failed to load on {dev}: {e}")
+            except Exception as e: 
+                last_err = e
+                self.logger.error(f"An unexpected error occurred while loading model on {dev}: {e}")
+        
+        if self._model is None: #check if nothing works, then raise error
+            raise RuntimeError(f"Could not load model on any device. Last error:\n{last_err}")
+        
+        # lock
+        super().__setattr__("_initialized", True)
+        
+    @property
+    def sample_rate(self):
+        return self._sample_rate
+
+    @property
+    def model(self):
+        return self._model
+
+    @property
+    def device(self):
+        return self._device
+
+    def _load_media(self, path: str) -> np.ndarray:
+        #Load ANY audio/video file via the private FFmpeg bundled by imageio-ffmpeg,
+        # get the path to the ffmpeg binary that imageio_ffmpeg downloaded
+        ffmpeg_exe = iioff.get_ffmpeg_exe()
+
+        out, _ = (ffmpeg.input(path).output(
+                'pipe:',
+                format='f32le',       # 32-bit float samples
+                ar=self._sample_rate, 
+                ac=1                  # mono
+                ).run(cmd=ffmpeg_exe, capture_stdout=True, capture_stderr=True)
+            )
+
+        # convert raw bytes to NumPy array
+        audio = np.frombuffer(out, dtype=np.float32).copy()
+        return audio
+
+    def transcribe(self, audio_path: str, fp16: bool = False, **transcribe_kwargs: Any) -> Dict[str, Any]:
+        """
+        Transcribes an audio file from a given file path. 
+        Returns a dictionary with the transcription results, including text, and other data.
+        """
+        # Transcribes audio from a file path.
+        # param audio_path: Path to the audio file.
+        # param fp16: Whether to use fp16 precision (GPU only).
+        # param transcribe_kwargs: Additional arguments dokter for whisper.model.transcribe().
+        # return: A dictionary containing the transcription results.
+        
+        enforce_type(audio_path, str, "audio_path")
+        enforce_type(fp16, bool, "fp16")
+        
+        try:
+            audio_data = self._load_media(audio_path)
+                
+            # get the np.ndarray to the transcribe
+            self.logger.info(f"Transcribing audio from {audio_path} on {self._device}...")
+            result = self._model.transcribe(audio_data, fp16=fp16 if self._device == "cuda" else False, **transcribe_kwargs)
+            self.logger.info(f"Transcription successful.")
+            return result #return result
+        except Exception as e:
+            self.logger.error(f"Transcription failed for {audio_path}: {e}")
+            raise
+
+    def get_time_transcription(self, audio_path: str, fp16: bool = False, **transcribe_kwargs: Any) -> List[List[Any]]:
+        """
+        Transcribes an audio file and returns a list of lists, where each inner list contains the start time, end time, and transcribed text.
+        """
+        result = self.transcribe(audio_path=audio_path, fp16=fp16 if self._device == "cuda" else False, **transcribe_kwargs)
+        segments = result.get("segments", [])
+        return [
+            [seg["start"], seg["end"], seg["text"]]
+            for seg in segments
+        ]
+    
+    def get_transcription(self, audio_path: str, fp16: bool = False, **transcribe_kwargs: Any) -> str:
+        """
+        Transcribes an audio file and returns only the full transcribed text as a single string, without any additional data.
+        """
+        text = self.transcribe(audio_path=audio_path, fp16=fp16, **transcribe_kwargs)["text"]
+        return text
+
+    def get_lang_detect(self, audio_path: str, fp16: bool = False, **transcribe_kwargs: Any) -> str:
+        """
+        Transcribes an audio file to determine the language of the audio and returns the detected language as a string.
+        """
+        language = self.transcribe(audio_path=audio_path, fp16=fp16, **transcribe_kwargs)["language"]
+        return language
+    
+    def __setattr__(self, name, value):
+        # once initialized, block these core attributes
+        if getattr(self, "_initialized", False) and name in ("_model", "_device"):
+            raise AttributeError(f"Cannot reassign '{name}' after initialization")
+        super().__setattr__(name, value)
---
+++ packages/freeai-utils/freeai_utils-0.6.3-py3-none-any.whl/freeai_utils/document_filter.py
@@ -0,0 +1,244 @@
+from collections import Counter
+import os
+os.environ["HF_HUB_OFFLINE"] = "1"
+from haystack import Document
+from haystack.components.readers import ExtractiveReader
+from haystack.utils.device import ComponentDevice
+from typing import List, Optional, Dict, Tuple
+from .pdf_docx_reader import PDF_DOCX_Reader
+from freeai_utils.log_set_up import setup_logging
+import torch
+import re
+from .utils import enforce_type
+
+#smaller model: deepset/roberta-base-squad2
+class AIDocumentSearcher:
+    """
+    AI-driven approach to document search. 
+    It uses an extractive question-answering model to understand the meaning and intent behind a query. 
+    Instead of just looking for keywords, 
+    it can find relevant answers even if they are paraphrased or semantically related to the query.
+    """
+    
+    __slots__ = ("_reader", "threshold", "max_per_doc", "top_k", "_documents", "_initialized", "logger")
+    
+    _initialized: bool
+    _reader: ExtractiveReader
+    threshold: float
+    max_per_doc: int
+    top_k: int
+    _documents: List[Document]
+    
+    def __init__(self, model_name="deepset/tinyroberta-squad2", path : Optional[str] = None, threshold : float = 0.4, max_per_doc : int = 2, top_answer : int = 4, device: str = "cuda", auto_init : bool = True) -> None:
+        #check type first
+        enforce_type(threshold, float, "threshold")
+        enforce_type(max_per_doc, int, "max_per_doc")
+        enforce_type(top_answer, int, "top_answer")
+        enforce_type(auto_init, bool, "auto_init")
+        self.logger = setup_logging(self.__class__.__name__)
+        self.logger.propagate = False  # Prevent propagation to the root logger
+        
+        # init not lock
+        super().__setattr__("_initialized", False)
+        # set core reader
+        self._reader = None
+        
+        preferred_devices = []
+        if device is not None:
+            enforce_type(device, str, "device")
+            preferred_devices.append(device)
+        if torch.cuda.is_available() and "cuda" not in preferred_devices:
+            preferred_devices.append("cuda")
+        if "cpu" not in preferred_devices:
+            preferred_devices.append("cpu")
+        
+        load_success = False 
+        for dev in preferred_devices:
+            if load_success: break
+            for offline in (True, False):
+                if offline:
+                    os.environ["HF_HUB_OFFLINE"] = "1"
+                    mode = "offline"
+                else:
+                    os.environ.pop("HF_HUB_OFFLINE", None)
+                    mode = "online"
+                try:
+                    device_obj = ComponentDevice.from_str(dev)
+                    reader = ExtractiveReader(model=model_name, device=device_obj)
+                    reader.warm_up()
+                    self._reader = reader
+                    load_success = True
+                    self.logger.info(f"Successfully runs on {dev} with mode {mode}")
+                    break
+                except Exception as e:
+                    self.logger.info(f"Failed to initialize on device='{dev}' in {mode} mode: {e}")
+        
+        if self._reader is None:
+            raise RuntimeError(f"Could not load model on any device.")
+        
+        # lock down, reader can't be modified
+        super().__setattr__("_initialized", True)
+    
+        self.threshold = threshold
+        self.max_per_doc = max_per_doc
+        self.top_k = top_answer
+        self._documents: List[Document] = [] #init var to hold document
+        if path is None:
+            path = os.getcwd()
+        if not os.path.exists(path):
+            raise FileNotFoundError(f"The path '{path}' does not exist.")
+        if auto_init:
+            self.__init_documents(path) #init documents from the path
+        self.logger.info(f"Initialize successfully at path {path}")
+    
+    @property
+    def reader(self):
+        return self._reader
+    
+    @property
+    def documents(self):
+        return self._documents
+       
+    def __setattr__(self, name, value):
+        # once initialized, prevent changing core internals
+        if getattr(self, "_initialized", False) and name in ("_reader"):
+            raise AttributeError(f"Cannot reassign '{name}' after initialization")
+        super().__setattr__(name, value)
+    
+    def search_document(self, prompt : str = None) -> List[Tuple]: #content text, score, and doc.id
+        """Searches a collection of documents for answers to a given prompt,
+        then filters the results based on a threshold score, a limit per document, and uniqueness.\n
+        Returns a list of tuples containing the text, score, and document ID.
+        """
+        enforce_type(prompt, str, "prompt") #check type before start
+        
+        result = self._reader.run(query=prompt, documents=self._documents, top_k=self.top_k)
+        
+        seen_texts = set()
+        counts = Counter()
+        filtered_answers = []
+        
+        for ans in result["answers"]:
+            if ans.score < self.threshold:
+                continue
+            if ans.document is None:
+                continue
+            text = ans.document.content
+            doc_id = ans.document.id
+        
+            if counts[doc_id] >= self.max_per_doc:
+                continue
+            if text in seen_texts:
+                continue
+                
+            filtered_answers.append((text, ans.score, doc_id))
+            seen_texts.add(text)
+            counts[doc_id] += 1
+        
+        #content text, score, and doc.id
+        return filtered_answers #return list of document with ranking score that > threshold
+
+    def __init_documents(self, directory : str = "") -> None:
+        """
+        Initializes and loads documents from a specified directory. 
+        It finds and extracts text from all PDF and DOCX files in the directory to prepare them for searching.
+        """
+        #get from here
+        self._documents.clear()
+        pdf_urls, docx_urls = collect_file_paths(directory)
+        
+        reader = PDF_DOCX_Reader() #init reader
+        
+        #extract from docx
+        for doc in docx_urls:
+            text = reader.extract_ordered_text(doc)
+            self._documents.append(Document(content=text))
+            
+        #extract from pdf
+        for pdf in pdf_urls:
+            text = reader.extract_ordered_text(pdf)
+            self._documents.append(Document(content=text))
+        
+class DocumentFilter:
+    """
+    Designed for efficient, keyword-based search across a collection of documents. 
+    It functions by indexing the raw text of PDF and DOCX files, 
+    allowing it to quickly identify and filter documents containing a specific keyword or phrase.
+    """
+    def __init__(self, path: Optional[str] = None, auto_init: bool = True) -> None:
+        enforce_type(auto_init, bool, "auto_init")
+        self.logger = setup_logging(self.__class__.__name__)
+        self.logger.propagate = False
+
+        self._documents: Dict[str, str] = {}  # Changed to a dictionary
+        if path is None:
+            path = os.getcwd()
+        if not os.path.exists(path):
+            raise FileNotFoundError(f"The path '{path}' does not exist.")
+        if auto_init:
+            self.__init_documents(path)
+        self.logger.info(f"Initialize successfully at path {path}")
+
+    @property
+    def documents(self):
+        return self._documents
+       
+    def search_keyword(self, keyword: str) -> Dict[str, List[str]]:
+        """
+        Searches a collection of documents for answers to a given keyword,
+        \n
+        Returns a Dict of str,list with str is the path, and list is the place it occurs the matched keyword
+        """
+        results = {}
+        
+        pattern = re.compile(r'\b' + re.escape(keyword) + r'\b', re.IGNORECASE)
+        
+        for path, text in self._documents.items():
+            # Split the document into sentences using '.'
+            sentences = text.split('.')
+            matches = []
+            
+            for sentence in sentences:
+                if pattern.search(sentence):
+                    # Add the matching sentence to the list for this file
+                    matches.append(sentence.strip())
+            
+            # Only add the file to the results if a match was found
+            if matches:
+                results[path] = matches
+                
+        return results
+    
+    def __init_documents(self, directory: str = "") -> None:
+        """
+        Initializes and loads documents from a specified directory.
+        It finds and extracts text from all PDF and DOCX files in the directory.
+        """
+        self._documents.clear()
+        pdf_urls, docx_urls = collect_file_paths(directory)
+
+        reader = PDF_DOCX_Reader()
+
+        for doc_path in docx_urls:
+            text = reader.extract_ordered_text(doc_path)
+            self._documents[doc_path] = text
+
+        for pdf_path in pdf_urls:
+            text = reader.extract_ordered_text(pdf_path)
+            self._documents[pdf_path] = text
+        
+def collect_file_paths(directory : str)-> Tuple[List[str], List[str]]:
+        """a helper that scans a directory for all PDF and DOCX files and returns their file paths in two separate lists."""
+        pdf_urls = []
+        docx_urls = []
+    
+        # Walk through all directories and files
+        for root, _, files in os.walk(directory):
+            for file in files:
+                file_path = os.path.join(root, file).replace('\\', '/')  # normalize path
+                if file.lower().endswith('.pdf'):
+                    pdf_urls.append(file_path)
+                elif file.lower().endswith('.docx'):
+                    docx_urls.append(file_path)
+        
+        return pdf_urls, docx_urls
