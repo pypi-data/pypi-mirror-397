8015e72de47119221376af7c60b33976d3883ff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 22:07:36 +0000

    Add package strictdoc/0.15.3/strictdoc-0.15.3-py3-none-any.whl

---
+++ packages/strictdoc/strictdoc-0.15.3-py3-none-any.whl/strictdoc/commands/about_command.py
@@ -0,0 +1,24 @@
+"""
+@relation(SDOC-SRS-163, scope=file)
+"""
+
+import strictdoc
+
+
+class AboutCommand:
+    @staticmethod
+    def execute() -> None:
+        print("=============")  # noqa: T201
+        print("= StrictDoc =")  # noqa: T201
+        print("=============")  # noqa: T201
+        print(  # noqa: T201
+            "Purpose: Software for writing technical requirements specifications."
+        )
+        print(f"Version: {strictdoc.__version__}")  # noqa: T201
+        print(  # noqa: T201
+            "Docs:    https://strictdoc.readthedocs.io/en/stable/"
+        )
+        print(  # noqa: T201
+            "Github:  https://github.com/strictdoc-project/strictdoc"
+        )
+        print("License: Apache 2")  # noqa: T201
