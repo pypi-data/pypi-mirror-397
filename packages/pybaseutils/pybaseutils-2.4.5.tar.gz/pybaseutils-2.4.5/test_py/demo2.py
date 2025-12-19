f69b2cba215d375bf81ae967f3f3397a494854c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 14:05:44 +0000

    Add package pybaseutils/2.4.5/pybaseutils-2.4.5.tar.gz

---
+++ packages/pybaseutils/pybaseutils-2.4.5.tar.gz/pybaseutils-2.4.5/test_py/demo2.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+    @Author : PKing
+    @E-mail : 390737991@qq.com
+    @Date   : 2022-12-31 11:37:30
+    @Brief  : https://blog.csdn.net/qdPython/article/details/121381363
+"""
+import os
+import cv2
+import random
+import types
+import torch
+from typing import List, Tuple, Dict
+import numpy as np
+from typing import Callable
+from PIL import Image
+from pybaseutils import image_utils, file_utils, text_utils, pandas_utils, json_utils, base64_utils
+import numpy as np
+import cv2
+import math
+
+
+
+
+
+if __name__ == "__main__":
+    fil
+
+
