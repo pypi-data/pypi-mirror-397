2d516e9596f29a6b65b6fe237602e6fcecc0880c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 18:06:57 +0000

    Add package minfx/0.3.0/minfx-0.3.0-py3-none-any.whl

---
+++ packages/minfx/minfx-0.3.0-py3-none-any.whl/minfx/__init__.py
@@ -0,0 +1,12 @@
+"""
+MinFX.
+
+This package provides core functionality for the minfx system.
+"""
+
+__version__ = "0.3.0"
+__author__ = "Minfx Technologies, s.r.o."
+__email__ = "contact@minfx.ai"
+
+# Package level imports can be added here as the package grows
+# Neptune is available as minfx.neptune
