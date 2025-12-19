a7da22bc50c13ba3ee71dbfc5185b1f7f3fcc37d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 18:07:43 +0000

    Add package fabi-demo/1.0.4/fabi_demo-1.0.4-py3-none-any.whl

---
+++ packages/fabi-demo/fabi_demo-1.0.4-py3-none-any.whl/fabi_demo/__init__.py
@@ -0,0 +1,8 @@
+from . import cli
+from .version import __version__
+from .fabi_demo import DEMO
+
+__all__ = (
+    "__version__",
+    "DEMO",
+)
---
+++ packages/fabi-demo/fabi_demo-1.0.4-py3-none-any.whl/fabi_demo/__init__.pyi
@@ -0,0 +1,4 @@
+from .fabi_demo import DEMO as DEMO
+from .version import __version__ as __version__
+
+__all__ = ['__version__', 'DEMO']
