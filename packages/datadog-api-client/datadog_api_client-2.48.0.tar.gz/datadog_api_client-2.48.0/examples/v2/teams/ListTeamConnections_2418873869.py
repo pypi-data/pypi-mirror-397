632d6115f01d35ed0a531347247ddd4b48ca5eb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 03:32:39 +0000

    Add package datadog-api-client/2.48.0/datadog_api_client-2.48.0.tar.gz

---
+++ packages/datadog-api-client/datadog_api_client-2.48.0.tar.gz/datadog_api_client-2.48.0/examples/v2/teams/ListTeamConnections_2418873869.py
@@ -0,0 +1,14 @@
+"""
+List team connections returns "OK" response with pagination
+"""
+
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v2.api.teams_api import TeamsApi
+
+configuration = Configuration()
+configuration.unstable_operations["list_team_connections"] = True
+with ApiClient(configuration) as api_client:
+    api_instance = TeamsApi(api_client)
+    items = api_instance.list_team_connections_with_pagination()
+    for item in items:
+        print(item)
---
+++ packages/datadog-api-client/datadog_api_client-2.48.0.tar.gz/datadog_api_client-2.48.0/src/datadog_api_client/v2/model/observability_pipeline_rename_fields_processor.py
@@ -0,0 +1,82 @@
+# Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2019-Present Datadog, Inc.
+from __future__ import annotations
+
+from typing import List, TYPE_CHECKING
+
+from datadog_api_client.model_utils import (
+    ModelNormal,
+    cached_property,
+)
+
+
+if TYPE_CHECKING:
+    from datadog_api_client.v2.model.observability_pipeline_rename_fields_processor_field import (
+        ObservabilityPipelineRenameFieldsProcessorField,
+    )
+    from datadog_api_client.v2.model.observability_pipeline_rename_fields_processor_type import (
+        ObservabilityPipelineRenameFieldsProcessorType,
+    )
+
+
+class ObservabilityPipelineRenameFieldsProcessor(ModelNormal):
+    @cached_property
+    def openapi_types(_):
+        from datadog_api_client.v2.model.observability_pipeline_rename_fields_processor_field import (
+            ObservabilityPipelineRenameFieldsProcessorField,
+        )
+        from datadog_api_client.v2.model.observability_pipeline_rename_fields_processor_type import (
+            ObservabilityPipelineRenameFieldsProcessorType,
+        )
+
+        return {
+            "enabled": (bool,),
+            "fields": ([ObservabilityPipelineRenameFieldsProcessorField],),
+            "id": (str,),
+            "include": (str,),
+            "type": (ObservabilityPipelineRenameFieldsProcessorType,),
+        }
+
+    attribute_map = {
+        "enabled": "enabled",
+        "fields": "fields",
+        "id": "id",
+        "include": "include",
+        "type": "type",
+    }
+
+    def __init__(
+        self_,
+        enabled: bool,
+        fields: List[ObservabilityPipelineRenameFieldsProcessorField],
+        id: str,
+        include: str,
+        type: ObservabilityPipelineRenameFieldsProcessorType,
+        **kwargs,
+    ):
+        """
+        The ``rename_fields`` processor changes field names.
+
+        :param enabled: Whether this processor is enabled.
+        :type enabled: bool
+
+        :param fields: A list of rename rules specifying which fields to rename in the event, what to rename them to, and whether to preserve the original fields.
+        :type fields: [ObservabilityPipelineRenameFieldsProcessorField]
+
+        :param id: A unique identifier for this component. Used to reference this component in other parts of the pipeline (e.g., as input to downstream components).
+        :type id: str
+
+        :param include: A Datadog search query used to determine which logs this processor targets.
+        :type include: str
+
+        :param type: The processor type. The value should always be ``rename_fields``.
+        :type type: ObservabilityPipelineRenameFieldsProcessorType
+        """
+        super().__init__(kwargs)
+
+        self_.enabled = enabled
+        self_.fields = fields
+        self_.id = id
+        self_.include = include
+        self_.type = type
