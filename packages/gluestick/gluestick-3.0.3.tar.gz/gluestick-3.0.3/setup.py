d286d6d077b1f8e332d7a7dfc78afbca2be9c4b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 04:16:47 +0000

    Add package gluestick/3.0.3/gluestick-3.0.3.tar.gz

---
+++ packages/gluestick/gluestick-3.0.3.tar.gz/gluestick-3.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="gluestick",
+    version="3.0.3",
+    description="ETL utility functions built for the hotglue iPaaS platform",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hotgluexyz/gluestick",
+    install_requires=[
+        "singer-python>=4.0.0",
+        "numpy>=1.4",
+        "pandas>=1.2.5",
+        "pyarrow>=8.0.0",
+        "pytz>=2022.6",
+        "polars==1.34.0"
+    ],
+    author="hotglue",
+    author_email="hello@hotglue.xyz",
+    license="MIT",
+    packages=find_packages(include=["gluestick", "gluestick.*"]),
+    zip_safe=False,
+)
