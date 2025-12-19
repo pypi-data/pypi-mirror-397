0825d86e444741490510cb0ab1b4d91042d999aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 04:15:26 +0000

    Add package tuk-time/0.1.0.2025121800/tuk_time-0.1.0.2025121800-py3-none-any.whl

---
+++ packages/tuk-time/tuk_time-0.1.0.2025121800-py3-none-any.whl/tuk/time/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+
+
+__author__ = "Tarcadia"
+__url__ = "https://github.com/Tarcadia/tuk-time"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Tarcadia"]
+__license__ = "MIT"
+__version__ = "0.1.0"
+
+
+from ._time import Time
+
+__all__ = [
+    "Time"
+]
+
