4fdd5a605b2efcaf4b18e440d9f229aa67106816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 18:07:41 +0000

    Add package cosmpy/0.12.0rc1/cosmpy-0.12.0rc1.tar.gz

---
+++ packages/cosmpy/cosmpy-0.12.0rc1.tar.gz/cosmpy-0.12.0rc1/cosmpy/ibc/applications/transfer/rest_client.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2018-2022 Fetch.AI Limited
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+"""Implementation of IBC Applications Transfer  interface using REST."""
+from google.protobuf.json_format import Parse
+
+from cosmpy.common.rest_client import RestClient
+from cosmpy.ibc.applications.transfer.interface import (  # type: ignore
+    IBCApplicationsTransfer,
+)
+from cosmpy.protos.ibc.applications.transfer.v1.query_pb2 import (
+    QueryDenomHashRequest,
+    QueryDenomHashResponse,
+    QueryDenomRequest,
+    QueryDenomResponse,
+    QueryDenomsRequest,
+    QueryDenomsResponse,
+    QueryParamsRequest,
+    QueryParamsResponse,
+)
+
+
+class IBCApplicationsTransferRestClient(IBCApplicationsTransfer):
+    """IBC Applications Transfer REST client."""
+
+    API_URL = "/ibc/apps/transfer/v1"
+
+    def __init__(self, rest_api: RestClient) -> None:
+        """
+        Initialize.
+
+        :param rest_api: RestClient api
+        """
+        self._rest_api = rest_api
+
+    def Denom(self, request: QueryDenomRequest) -> QueryDenomResponse:
+        """
+        Denom queries a denomination trace information.
+
+        :param request: QueryDenomRequest
+        :return: QueryDenomResponse
+        """
+        json_response = self._rest_api.get(f"{self.API_URL}/denoms/{request.hash}")
+        return Parse(json_response, QueryDenomResponse())
+
+    def Denoms(self, request: QueryDenomsRequest) -> QueryDenomsResponse:
+        """
+        Denoms queries all denomination traces.
+
+        :param request: QueryDenomsRequest
+        :return: QueryDenomsResponse
+        """
+        json_response = self._rest_api.get(f"{self.API_URL}/denoms", request)
+        return Parse(json_response, QueryDenomsResponse())
+
+    def Params(self, request: QueryParamsRequest) -> QueryParamsResponse:
+        """
+        Params queries all parameters of the ibc-transfer module.
+
+        :param request: QueryParamsRequest
+        :return: QueryParamsResponse
+        """
+        json_response = self._rest_api.get(f"{self.API_URL}/params")
+        return Parse(json_response, QueryParamsResponse())
+
+    def DenomHash(self, request: QueryDenomHashRequest) -> QueryDenomHashResponse:
+        """
+        DenomHash queries denomination hash.
+
+        :param request: QueryDenomHashRequest (with .trace set to something like 'transfer/channel-0/uatom')
+        :return: QueryDenomHashResponse
+        """
+        json_response = self._rest_api.get(
+            f"{self.API_URL}/denom_hashes/{request.trace}"
+        )
+        return Parse(json_response, QueryDenomHashResponse())
