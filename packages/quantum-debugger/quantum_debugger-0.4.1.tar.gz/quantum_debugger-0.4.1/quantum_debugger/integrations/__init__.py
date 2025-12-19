81ef1ad50a390ade9b9f94103d55a18c4271c29f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 17:07:54 +0000

    Add package quantum-debugger/0.4.1/quantum_debugger-0.4.1.tar.gz

---
+++ packages/quantum-debugger/quantum_debugger-0.4.1.tar.gz/quantum_debugger-0.4.1/quantum_debugger/integrations/__init__.py
@@ -0,0 +1,9 @@
+"""
+Qiskit integration module for QuantumDebugger
+
+Provides bidirectional conversion between Qiskit and QuantumDebugger circuits.
+"""
+
+from .qiskit_adapter import QiskitAdapter
+
+__all__ = ['QiskitAdapter']
