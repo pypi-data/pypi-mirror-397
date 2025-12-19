e935396c3bbc7b8d78ec399b675a6ee0137f21b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 22:07:18 +0000

    Add package python-prompt-library/1.0.8/python_prompt_library-1.0.8.tar.gz

---
+++ packages/python-prompt-library/python_prompt_library-1.0.8.tar.gz/python_prompt_library-1.0.8/python_prompt_library/utils/string_utils.py
@@ -0,0 +1,8 @@
+def to_uppercase(string):
+    """Converts a string to uppercase."""
+    return string.upper()
+
+
+def to_lowercase(string):
+    """Converts a string to lowercase."""
+    return string.lower()
