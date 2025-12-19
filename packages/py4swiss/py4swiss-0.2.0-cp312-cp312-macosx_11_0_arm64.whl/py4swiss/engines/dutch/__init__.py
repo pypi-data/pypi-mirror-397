dc053646c9619e99550e1a43b19dede2f9667471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 21:07:06 +0000

    Add package py4swiss/0.2.0/py4swiss-0.2.0-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/py4swiss/py4swiss-0.2.0-cp312-cp312-macosx_11_0_arm64.whl/py4swiss/engines/dutch/__init__.py
@@ -0,0 +1,4 @@
+from py4swiss.engines.dutch.dutch_engine import DutchEngine
+from py4swiss.engines.dutch.player import Player, get_player_infos_from_trf
+
+__all__ = ["DutchEngine", "Player", "get_player_infos_from_trf"]
