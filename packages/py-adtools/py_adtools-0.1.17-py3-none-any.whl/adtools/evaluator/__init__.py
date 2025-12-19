434779086377b2a47f469ed87810dec790665287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 19:08:13 +0000

    Add package py-adtools/0.1.17/py_adtools-0.1.17-py3-none-any.whl

---
+++ packages/py-adtools/py_adtools-0.1.17-py3-none-any.whl/adtools/evaluator/__init__.py
@@ -0,0 +1,7 @@
+from adtools.evaluator.py_evaluator import (
+    EvaluationResults,
+    PyEvaluator,
+    PyEvaluatorReturnInManagerDict,
+    PyEvaluatorReturnInSharedMemory,
+)
+from adtools.evaluator.py_evaluator_ray import PyEvaluatorRay
