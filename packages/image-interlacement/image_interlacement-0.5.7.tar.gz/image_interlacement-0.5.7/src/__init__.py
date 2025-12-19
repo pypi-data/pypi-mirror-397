a5d36230f0806e961b6fe7bf0dc2cf2dc401442c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 21:07:24 +0000

    Add package image-interlacement/0.5.7/image_interlacement-0.5.7.tar.gz

---
+++ packages/image-interlacement/image_interlacement-0.5.7.tar.gz/image_interlacement-0.5.7/src/__init__.py
@@ -0,0 +1,5 @@
+"""Image Interlacement Program - A Python CLI tool for image interlacing operations."""
+
+__version__ = "0.5.7"
+__author__ = "e. couture"
+__description__ = "A simple image interlacement/interleavment program created to aid in creating weave structures for the TC2 loom."
