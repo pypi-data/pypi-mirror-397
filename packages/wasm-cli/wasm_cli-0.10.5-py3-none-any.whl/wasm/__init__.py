2bb7350b3b34cbcc120234943d3025a765abba34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 23:07:21 +0000

    Add package wasm-cli/0.10.5/wasm_cli-0.10.5-py3-none-any.whl

---
+++ packages/wasm-cli/wasm_cli-0.10.5-py3-none-any.whl/wasm/__init__.py
@@ -0,0 +1,20 @@
+"""
+WASM - Web App System Management
+
+A robust CLI tool for deploying and managing web applications on Linux servers.
+"""
+
+__version__ = "0.10.5"
+__author__ = "WASM Team"
+__license__ = "MIT"
+
+from wasm.core.config import Config
+from wasm.core.logger import Logger
+from wasm.core.exceptions import WASMError
+
+__all__ = [
+    "Config",
+    "Logger",
+    "WASMError",
+    "__version__",
+]
