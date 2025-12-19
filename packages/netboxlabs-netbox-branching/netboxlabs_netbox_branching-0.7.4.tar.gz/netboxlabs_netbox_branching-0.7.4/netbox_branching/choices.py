50fc08abadd11146f6d09f8d3c432d41096592fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 04:16:11 +0000

    Add package netboxlabs-netbox-branching/0.7.4/netboxlabs_netbox_branching-0.7.4.tar.gz

---
+++ packages/netboxlabs-netbox-branching/netboxlabs_netbox_branching-0.7.4.tar.gz/netboxlabs_netbox_branching-0.7.4/netbox_branching/choices.py
@@ -0,0 +1,64 @@
+from django.utils.translation import gettext_lazy as _
+
+from utilities.choices import ChoiceSet
+
+
+class BranchStatusChoices(ChoiceSet):
+    NEW = 'new'
+    PROVISIONING = 'provisioning'
+    READY = 'ready'
+    SYNCING = 'syncing'
+    MIGRATING = 'migrating'
+    MERGING = 'merging'
+    REVERTING = 'reverting'
+    MERGED = 'merged'
+    ARCHIVED = 'archived'
+    PENDING_MIGRATIONS = 'pending-migrations'
+    FAILED = 'failed'
+
+    CHOICES = (
+        (NEW, _('New'), 'cyan'),
+        (PROVISIONING, _('Provisioning'), 'orange'),
+        (READY, _('Ready'), 'green'),
+        (SYNCING, _('Syncing'), 'orange'),
+        (MIGRATING, _('Migrating'), 'orange'),
+        (MERGING, _('Merging'), 'orange'),
+        (REVERTING, _('Reverting'), 'orange'),
+        (MERGED, _('Merged'), 'blue'),
+        (ARCHIVED, _('Archived'), 'gray'),
+        (PENDING_MIGRATIONS, _('Pending Migrations'), 'red'),
+        (FAILED, _('Failed'), 'red'),
+    )
+
+    TRANSITIONAL = (
+        PROVISIONING,
+        SYNCING,
+        MIGRATING,
+        MERGING,
+        REVERTING,
+    )
+
+    WORKING = (
+        NEW,
+        READY,
+        PENDING_MIGRATIONS,
+        *TRANSITIONAL,
+    )
+
+
+class BranchEventTypeChoices(ChoiceSet):
+    PROVISIONED = 'provisioned'
+    SYNCED = 'synced'
+    MIGRATED = 'migrated'
+    MERGED = 'merged'
+    REVERTED = 'reverted'
+    ARCHIVED = 'archived'
+
+    CHOICES = (
+        (PROVISIONED, _('Provisioned'), 'green'),
+        (SYNCED, _('Synced'), 'cyan'),
+        (MIGRATED, _('Migrated'), 'purple'),
+        (MERGED, _('Merged'), 'blue'),
+        (REVERTED, _('Reverted'), 'orange'),
+        (ARCHIVED, _('Archived'), 'gray'),
+    )
