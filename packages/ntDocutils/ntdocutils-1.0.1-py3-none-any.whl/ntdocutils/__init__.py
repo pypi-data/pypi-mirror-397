dd870a8f1e2cf42c2ba60390ff651fcf0f774fbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 04:15:27 +0000

    Add package ntDocutils/1.0.1/ntdocutils-1.0.1-py3-none-any.whl

---
+++ packages/ntDocutils/ntdocutils-1.0.1-py3-none-any.whl/ntdocutils/__init__.py
@@ -0,0 +1,12 @@
+# Copyright 2017 Miguel Angel Rivera Notararigo. All rights reserved.
+# This source code was released under the MIT license.
+"""ntDocutils."""
+
+__version__ = '1.0.1'
+__docformat__ = 'reStructuredText'
+
+COPYRIGHT = """ntDocutils v{} https://ntrrg.dev/en/projects/ntdocutils
+Copyright (c) 2017 Miguel Angel Rivera Notararigo
+Licensed under The MIT License""".format(__version__)
+
+DESCRIPTION = """ntDocutils is a theme manager for Docutils."""
