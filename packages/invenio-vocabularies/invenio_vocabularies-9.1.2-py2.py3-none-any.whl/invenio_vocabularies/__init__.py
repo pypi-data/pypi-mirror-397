7a5e5e14a456481d4663f5fc268fa540cb7c68dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 04:16:03 +0000

    Add package invenio-vocabularies/9.1.2/invenio_vocabularies-9.1.2-py2.py3-none-any.whl

---
+++ packages/invenio-vocabularies/invenio_vocabularies-9.1.2-py2.py3-none-any.whl/invenio_vocabularies/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020-2025 CERN.
+# Copyright (C) 2024-2025 Graz University of Technology.
+#
+# Invenio-Vocabularies is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Invenio module for managing vocabularies."""
+
+from .ext import InvenioVocabularies
+
+__version__ = "9.1.2"
+
+__all__ = ("__version__", "InvenioVocabularies")
