df23453c702525d5d1088a9b031e249ff08230c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 15:05:43 +0000

    Add package pxutil/0.0.49/pxutil-0.0.49-cp310-cp310-musllinux_1_2_x86_64.whl

---
+++ packages/pxutil/pxutil-0.0.49-cp310-cp310-musllinux_1_2_x86_64.whl/pxutil/__main__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Sample __main__.py to be executed as python -m <pxutil>
+
+Ref:
+https://docs.python.org/3/library/__main__.html
+pip.__main__.py
+"""
+import argparse
+import pxutil as px
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="This is a sample __main__.py to execute as `python -m pxutil.`"
+    )
+    args = parser.parse_args()
+    print("This is a sample __main__.py to execute as `python -m pxutil.`")
