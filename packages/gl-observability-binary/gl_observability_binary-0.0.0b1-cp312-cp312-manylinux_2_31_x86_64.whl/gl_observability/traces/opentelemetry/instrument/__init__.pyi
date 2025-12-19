989f3cefd550369a934f9a6bec6c6ed591f6ed3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 16:06:17 +0000

    Add package gl-observability-binary/0.0.0b1/gl_observability_binary-0.0.0b1-cp312-cp312-manylinux_2_31_x86_64.whl

---
+++ packages/gl-observability-binary/gl_observability_binary-0.0.0b1-cp312-cp312-manylinux_2_31_x86_64.whl/gl_observability/traces/opentelemetry/instrument/__init__.pyi
@@ -0,0 +1,4 @@
+from gl_observability.traces.opentelemetry.instrument.functions import BOSAFunctionsInstrumentor as BOSAFunctionsInstrumentor
+from gl_observability.traces.opentelemetry.instrument.http import HTTPClientInstrumentor as HTTPClientInstrumentor
+
+__all__ = ['BOSAFunctionsInstrumentor', 'HTTPClientInstrumentor']
