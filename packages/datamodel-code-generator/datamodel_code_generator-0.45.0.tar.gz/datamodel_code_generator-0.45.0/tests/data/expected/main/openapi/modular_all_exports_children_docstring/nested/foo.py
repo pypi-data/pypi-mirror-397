433c613717ddf6a4beab736f01e9d9004654c061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 23:07:22 +0000

    Add package datamodel-code-generator/0.45.0/datamodel_code_generator-0.45.0.tar.gz

---
+++ packages/datamodel-code-generator/datamodel_code_generator-0.45.0.tar.gz/datamodel_code_generator-0.45.0/tests/data/expected/main/openapi/modular_all_exports_children_docstring/nested/foo.py
@@ -0,0 +1,13 @@
+"""
+Custom module docstring header.
+
+This is a multi-line docstring used for testing.
+"""
+
+from __future__ import annotations
+
+from .._internal import ListModel
+from .._internal import Tea_1 as Tea
+from .._internal import TeaClone
+
+__all__ = ["ListModel", "Tea", "TeaClone"]
