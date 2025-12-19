dd8f53e6daad77de8d159a7b1930a07cb86bce7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 17:07:56 +0000

    Add package VIStk/0.3.12.23/vistk-0.3.12.23.tar.gz

---
+++ packages/VIStk/vistk-0.3.12.23.tar.gz/vistk-0.3.12.23/VIStk/Objects/_Root.py
@@ -0,0 +1,31 @@
+from tkinter import *
+from VIStk.Objects._WindowGeometry import WindowGeometry
+from VIStk.Objects._Window import Window
+
+class Root(Tk, Window):
+    """A wrapper for the Tk class with VIS attributes"""
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.WindowGeometry = WindowGeometry(self)
+        self.Active = True
+        self.protocol("WM_DELETE_WINDOW", self.unload)
+    
+    def unload(self):
+        """Closes the window neatly for VIStk"""
+        for element in self.winfo_children():
+            try:
+                element.destroy()
+            except: pass
+        
+        self.Active = False
+
+    def exitQueue(self, action, *args, **kwargs):
+        """Sets a function to call in the exit loop. Use for redirects."""
+        self.exitAction = action
+        self.exitArgs = [*args]
+        self.exitKwargs = {**kwargs}
+
+    def exitAct(self):
+        """Executes the exitAction"""
+        if not self.exitAction is None:
+            self.exitAction(tuple(self.exitArgs),self.exitKwargs)
