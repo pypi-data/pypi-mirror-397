7b3c8c8ad373405cd2eb10734e733eca5bb69755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 17:07:22 +0000

    Add package plone.base/4.0.0a4/plone_base-4.0.0a4-py3-none-any.whl

---
+++ packages/plone.base/plone_base-4.0.0a4-py3-none-any.whl/plone/base/interfaces/siteroot.py
@@ -0,0 +1,25 @@
+from Products.CMFCore.interfaces import ISiteRoot
+from zope.interface import Interface
+
+
+class INavigationRoot(Interface):
+    """A marker interface for signaling the navigation root."""
+
+
+class IPloneSiteRoot(ISiteRoot, INavigationRoot):
+    """
+    Marker interface for the object which serves as the root of a
+    Plone site.
+    """
+
+
+class IMigratingPloneSiteRoot(Interface):
+    """
+    Marker interface used for migration GenericSetup profiles.
+    """
+
+
+class ITestCasePloneSiteRoot(Interface):
+    """
+    Marker interface used for test fixture GenericSetup profiles.
+    """
