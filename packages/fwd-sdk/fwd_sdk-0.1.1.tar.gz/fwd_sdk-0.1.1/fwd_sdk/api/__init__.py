8eebaac52db2c2c6c47cfdb8000a19c447768b1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 17:07:43 +0000

    Add package fwd-sdk/0.1.1/fwd_sdk-0.1.1.tar.gz

---
+++ packages/fwd-sdk/fwd_sdk-0.1.1.tar.gz/fwd_sdk-0.1.1/fwd_sdk/api/__init__.py
@@ -0,0 +1,14 @@
+# flake8: noqa
+
+# import apis into api package
+from fwd_sdk.api.audio_api import AudioApi
+from fwd_sdk.api.chat_api import ChatApi
+from fwd_sdk.api.compute_api import ComputeApi
+from fwd_sdk.api.embeddings_api import EmbeddingsApi
+from fwd_sdk.api.images_api import ImagesApi
+from fwd_sdk.api.instances_api import InstancesApi
+from fwd_sdk.api.models_api import ModelsApi
+from fwd_sdk.api.ssh_keys_api import SSHKeysApi
+from fwd_sdk.api.support_api import SupportApi
+from fwd_sdk.api.volumes_api import VolumesApi
+
