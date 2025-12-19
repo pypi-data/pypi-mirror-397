2ff3d70eeac33b39e328b9aa416d42eb5d464055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 20:07:28 +0000

    Add package flac-detective/0.7.1/flac_detective-0.7.1-py3-none-any.whl

---
+++ packages/flac-detective/flac_detective-0.7.1-py3-none-any.whl/flac_detective/__version__.py
@@ -0,0 +1,18 @@
+"""Version information for FLAC Detective.
+
+This is the single source of truth for the version number.
+All other files should reference this file.
+"""
+
+__version__ = "0.7.1"
+__version_info__ = tuple(int(x) for x in __version__.split("."))
+
+# Release information
+__release_date__ = "2025-12-18"
+__release_name__ = "Documentation Fix - PyPI Image Display"
+
+# Metadata
+__author__ = "Guillain Méjane"
+__email__ = "guillain@poulpe.us"
+__license__ = "MIT"
+__url__ = "https://github.com/GuillainM/FLAC_Detective"
