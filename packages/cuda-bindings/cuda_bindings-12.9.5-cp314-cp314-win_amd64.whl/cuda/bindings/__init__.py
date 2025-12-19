fa7ff737da5b60198588cdfa0ba2af72ebcaf69c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 19:08:23 +0000

    Add package cuda-bindings/12.9.5/cuda_bindings-12.9.5-cp314-cp314-win_amd64.whl

---
+++ packages/cuda-bindings/cuda_bindings-12.9.5-cp314-cp314-win_amd64.whl/cuda/bindings/__init__.py
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: Copyright (c) 2021-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: LicenseRef-NVIDIA-SOFTWARE-LICENSE
+
+
+# start delvewheel patch
+def _delvewheel_patch_1_11_2():
+    import os
+    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'cuda_bindings.libs'))):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_11_2()
+del _delvewheel_patch_1_11_2
+# end delvewheel patch
+
+from cuda.bindings import utils
+from cuda.bindings._version import __version__
