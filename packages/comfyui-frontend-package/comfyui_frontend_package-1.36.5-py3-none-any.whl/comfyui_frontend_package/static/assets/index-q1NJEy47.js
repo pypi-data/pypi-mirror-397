9a4525f79e671e375dc98e958c4346bfb6051b61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 23:06:56 +0000

    Add package comfyui-frontend-package/1.36.5/comfyui_frontend_package-1.36.5-py3-none-any.whl

---
+++ packages/comfyui-frontend-package/comfyui_frontend_package-1.36.5-py3-none-any.whl/comfyui_frontend_package/static/assets/index-q1NJEy47.js
@@ -0,0 +1,2 @@
+import{c as o}from"./index-b68GB3q4.js";const e=o(["not-disabled:bg-component-node-widget-background","not-disabled:text-component-node-foreground","border-none","rounded-lg"]);export{e as W};
+//# sourceMappingURL=index-q1NJEy47.js.map
