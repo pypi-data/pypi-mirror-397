9aa2fdaaab289e77ee2735ca5aaefd14749bd9f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 21:08:03 +0000

    Add package dhxpyt/0.9.11/dhxpyt-0.9.11.tar.gz

---
+++ packages/dhxpyt/dhxpyt-0.9.11.tar.gz/dhxpyt-0.9.11/dhxpyt/__init__.py
@@ -0,0 +1,8 @@
+"""
+# DHX PyTincture WASM Based Widgetset
+"""
+
+__widgetset__ = "dhxpyt"
+__version__ = "0.9.11"
+__version_tuple__ = tuple(map(int, __version__.split('.')))
+__description__ = "Python wrapper for DHTMLX widgets"
