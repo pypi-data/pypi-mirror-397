07dd0ba8d8e4492a37c0e7cda34fdb8385c58f13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 22:07:40 +0000

    Add package oneurai/0.1.8/oneurai-0.1.8.tar.gz

---
+++ packages/oneurai/oneurai-0.1.8.tar.gz/oneurai-0.1.8/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="oneurai",
+    version="0.1.8",  # 👈 تحديث الرقم للإصدار الجديد
+    author="MTMA",
+    author_email="mtma.1@hotmail.com & mohammed7amni@gmail.com",  # ✅ تم تصحيح الإيميل
+    description="A powerful AI client library for Oneurai MLOps platform",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://oneurai.com",# to onurai
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        "requests",
+        "torch",
+        "tqdm",
+    ],
+)
