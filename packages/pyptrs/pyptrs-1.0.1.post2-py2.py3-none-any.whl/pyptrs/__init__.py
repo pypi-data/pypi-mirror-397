c43db359089765648804460db47479286c6f66a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 14:05:35 +0000

    Add package pyptrs/1.0.1.post2/pyptrs-1.0.1.post2-py2.py3-none-any.whl

---
+++ packages/pyptrs/pyptrs-1.0.1.post2-py2.py3-none-any.whl/pyptrs/__init__.py
@@ -0,0 +1,5 @@
+from .pyptrs import pointer_to_object, pointer_to_address, \
+                  dereference, address_of, \
+                  mem_read, mem_write, \
+                  mem_restore, mem_restore_last
+                  
