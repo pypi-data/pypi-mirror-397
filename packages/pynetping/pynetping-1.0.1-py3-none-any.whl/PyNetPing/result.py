d35113785dc7d59a6a095322b17571de8204b000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 16:06:19 +0000

    Add package pynetping/1.0.1/pynetping-1.0.1-py3-none-any.whl

---
+++ packages/pynetping/pynetping-1.0.1-py3-none-any.whl/PyNetPing/result.py
@@ -0,0 +1,20 @@
+# Developer: Sreeraj
+# GitHub: https://github.com/s-r-e-e-r-a-j
+
+from dataclasses import dataclass
+from datetime import datetime
+
+@dataclass(slots=True)
+class PingResult:
+    host: str
+    sent: int
+    received: int
+    loss: float
+    min_ms: float
+    avg_ms: float
+    max_ms: float
+    jitter_ms: float
+    protocol: str = "icmp"
+    ip_version: int = 4
+    timestamp: str = datetime.utcnow().isoformat()
+    error: str | None = None
