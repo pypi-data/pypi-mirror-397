e2f9f9471adca8d8c0f6e475920e4c1246d8cfd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 05:07:53 +0000

    Add package datamodel-code-generator/0.44.0/datamodel_code_generator-0.44.0.tar.gz

---
+++ packages/datamodel-code-generator/datamodel_code_generator-0.44.0.tar.gz/datamodel_code_generator-0.44.0/tests/data/expected/main/openapi/modular_all_exports_children_docstring/woo/boo.py
@@ -0,0 +1,21 @@
+"""
+Custom module docstring header.
+
+This is a multi-line docstring used for testing.
+"""
+
+from __future__ import annotations
+
+from typing import Optional
+
+from pydantic import BaseModel
+
+from .. import bar
+from .._internal import Cocoa, Source
+
+
+class Chocolate(BaseModel):
+    flavour: Optional[str] = None
+    source: Optional[Source] = None
+    cocoa: Optional[Cocoa] = None
+    field: Optional[bar.FieldModel] = None
