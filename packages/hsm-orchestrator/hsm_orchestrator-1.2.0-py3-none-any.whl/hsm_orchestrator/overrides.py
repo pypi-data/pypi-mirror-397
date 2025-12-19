0d8d54bec9f052d07ec86027591c3aff665d5def
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 03:33:05 +0000

    Add package hsm-orchestrator/1.2.0/hsm_orchestrator-1.2.0-py3-none-any.whl

---
+++ packages/hsm-orchestrator/hsm_orchestrator-1.2.0-py3-none-any.whl/hsm_orchestrator/overrides.py
@@ -0,0 +1,39 @@
+from typing import TypeVar, Text
+
+from rich.prompt import Prompt as RichPrompt
+
+DefaultType = TypeVar("DefaultType")
+
+
+class Prompt(RichPrompt):
+    def make_prompt(self, default: DefaultType) -> Text:
+        """Make prompt text.
+
+        Args:
+            default (DefaultType): Default value.
+
+        Returns:
+            Text: Text to display in prompt.
+        """
+        prompt = self.prompt.copy()
+        prompt.end = ""
+
+        if self.show_choices and self.choices:
+            # This method changes the displayed choice delimiter from "/" to " | "
+            _choices = " | ".join(self.choices)
+            choices = f"[{_choices}]"
+            prompt.append(" ")
+            prompt.append(choices, "prompt.choices")
+
+        if (
+            default != ...
+            and self.show_default
+            and isinstance(default, (str, self.response_type))
+        ):
+            prompt.append(" ")
+            _default = self.render_default(default)
+            prompt.append(_default)
+
+        prompt.append(self.prompt_suffix)
+
+        return prompt
