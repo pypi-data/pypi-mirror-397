ebe596f0a063285d9de2e66e2f73c381d769f3e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 03:32:55 +0000

    Add package mfi-mpower/1.3.4/mfi_mpower-1.3.4-py3-none-any.whl

---
+++ packages/mfi-mpower/mfi_mpower-1.3.4-py3-none-any.whl/mfi_mpower/__init__.py
@@ -0,0 +1,8 @@
+"""This is the mFi mPower module."""
+from __future__ import annotations
+
+from .device import MPowerDevice
+from .entities import MPowerSensor, MPowerSwitch
+from .exceptions import *
+
+__version__ = "1.3.4"
