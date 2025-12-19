d099618da004cac2d569f99224a287a034a72298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 19:06:56 +0000

    Add package CPILake-Utils/4.0.0/cpilake_utils-4.0.0-py3-none-any.whl

---
+++ packages/CPILake-Utils/cpilake_utils-4.0.0-py3-none-any.whl/CPILake_Utils/__init__.py
@@ -0,0 +1,4 @@
+from .CPILake_Utils import send_email_no_attachment, hash_function, send_email_no_attachment_01, QA_CheckUtil, create_lakehouse_shortcuts
+
+__all__ = ["send_email_no_attachment", "hash_function", "send_email_no_attachment_01", "QA_CheckUtil", "create_lakehouse_shortcuts"]
+
