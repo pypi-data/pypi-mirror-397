3d35ff440444bc784b866f6754fd516bbc4b1db0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 22:07:24 +0000

    Add package nominal-api/0.1045.0/nominal_api-0.1045.0-py3-none-any.whl

---
+++ packages/nominal-api/nominal_api-0.1045.0-py3-none-any.whl/nominal_api/datasource/__init__.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+from .._impl import (
+    datasource_DatasetFileId as DatasetFileId,
+    datasource_TimestampType as TimestampType,
+)
+
+__all__ = [
+    'DatasetFileId',
+    'TimestampType',
+]
+
