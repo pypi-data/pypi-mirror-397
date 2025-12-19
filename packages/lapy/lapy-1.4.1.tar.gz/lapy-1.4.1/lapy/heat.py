c575395f28e9629a915eedfddf372bf88ac5b088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 03:32:56 +0000

    Add package lapy/1.4.1/lapy-1.4.1.tar.gz

---
+++ packages/lapy/lapy-1.4.1.tar.gz/lapy-1.4.1/lapy/heat.py
@@ -0,0 +1,130 @@
+"""Functions for computing heat kernel and diffusion.
+
+Inputs are eigenvalues and eigenvectors (for heat kernel) and the
+mesh geometries (tet or tria mesh) for heat diffusion.
+"""
+
+import importlib
+from typing import Optional
+
+import numpy as np
+
+from .utils._imports import import_optional_dependency
+
+
+def diagonal(t, x, evecs, evals, n):
+    """Compute heat kernel diagonal ( K(t,x,x,) ).
+
+    For a given time t (can be a vector)
+    using only the first n smallest eigenvalues and eigenvectors.
+
+    Parameters
+    ----------
+    t : float | array
+        Time or a row vector of time values.
+    x : array
+        Vertex ids for the positions of K(t,x,x).
+    evecs : array
+        Eigenvectors (matrix: vnum x evecsnum).
+    evals : array
+        Vector of eigenvalues (col vector: evecsnum x 1).
+    n : int
+        Number of evecs and vals to use (smaller or equal length).
+
+    Returns
+    -------
+    h : array
+        Matrix, rows: vertices selected in x, cols: times in t.
+    """
+    # maybe add code to check dimensions of input and flip axis if necessary
+    h = np.matmul(evecs[x, 0:n] * evecs[x, 0:n], np.exp(-np.matmul(evals[0:n], t)))
+    return h
+
+
+def kernel(t, vfix, evecs, evals, n):
+    r"""Compute heat kernel from all points to a fixed point (vfix).
+
+    For a given time t (using only the first n smallest eigenvalues
+    and eigenvectors):
+
+    .. math::
+        K_t (p,q) = \sum_j \ exp(-eval_j \ t) \ evec_j(p) \ evec_j(q)
+
+    Parameters
+    ----------
+    t : float | array
+        Time (can also be a row vector, if passing multiple times).
+    vfix : array
+        Fixed vertex index.
+    evecs : array
+        Matrix of eigenvectors (M x N), M=#vertices, N=#eigenvectors.
+    evals : array
+        Column vector of eigenvalues (N).
+    n : int
+        Number of eigenvalues/vectors used in heat kernel (n<=N).
+
+    Returns
+    -------
+    h : array
+        Matrix m rows: all vertices, cols: times in t.
+    """
+    # h = evecs * ( exp(-evals * t) .* repmat(evecs(vfix,:)',1,length(t))  )
+    h = np.matmul(evecs[:, 0:n], (np.exp(np.matmul(-evals[0:n], t)) * evecs[vfix, 0:n]))
+    return h
+
+
+def diffusion(geometry, vids, m=1.0, aniso: Optional[int] = None, use_cholmod=False):
+    """Compute the heat diffusion from initial vertices in vids.
+
+    It uses the backward Euler solution :math:`t = m l^2`, where l describes
+    the average edge length.
+
+    Parameters
+    ----------
+    geometry : TriaMesh | TetMesh
+        Geometric object on which to run diffusion.
+    vids : array
+        Vertex index or indices where initial heat is applied.
+    m : float, default=1.0
+        Factor to compute time of heat evolution.
+    aniso : int
+        Number of smoothing iterations for curvature computation on vertices.
+    use_cholmod : bool, default=False
+        Which solver to use:
+            * True : Use Cholesky decomposition from scikit-sparse cholmod.
+            * False: Use spsolve (LU decomposition).
+
+    Returns
+    -------
+    vfunc: array of shape (n, 1)
+        Heat diffusion at vertices.
+    """
+    if use_cholmod:
+        sksparse = import_optional_dependency("sksparse", raise_error=True)
+        importlib.import_module(".cholmod", sksparse.__name__)
+    else:
+        sksparse = None
+    from . import Solver
+
+    nv = len(geometry.v)
+    fem = Solver(geometry, lump=True, aniso=aniso)
+    # time of heat evolution:
+    t = m * geometry.avg_edge_length() ** 2
+    # backward Euler matrix:
+    hmat = fem.mass + t * fem.stiffness
+    # set initial heat
+    b0 = np.zeros((nv,))
+    b0[np.array(vids)] = 1.0
+    # solve H x = b0
+    print("Matrix Format now:  " + hmat.getformat())
+    if use_cholmod:
+        print("Solver: Cholesky decomposition from scikit-sparse cholmod ...")
+        chol = sksparse.cholmod.cholesky(hmat)
+        vfunc = chol(b0)
+    else:
+        from scipy.sparse.linalg import splu
+
+        print("Solver: spsolve (LU decomposition) ...")
+        lu = splu(hmat)
+        vfunc = lu.solve(np.float32(b0))
+    return vfunc
---
+++ packages/lapy/lapy-1.4.1.tar.gz/lapy-1.4.1/lapy/shapedna.py
@@ -0,0 +1,165 @@
+"""Functions for computing and comparing Laplace spectra.
+
+Includes code for solving the anisotropic Laplace-Beltrami eigenvalue
+problem as well as functions for normalization and comparison of
+Laplace spectra.
+"""
+
+import numpy as np
+import scipy.spatial.distance as di
+
+from . import Solver
+
+
+def compute_shapedna(
+    geom, k=50, lump=False, aniso=None, aniso_smooth=10, use_cholmod=False
+):
+    """Compute the shapeDNA descriptor for triangle or tetrahedral meshes.
+
+    Parameters
+    ----------
+    geom : TriaMesh or TetMesh
+        Mesh geometry.
+    k : int, default=50
+        Number of eigenfunctions / eigenvalues.
+    lump : bool, default=False
+        If True, lump the mass matrix (diagonal).
+            (See 'lapy.Solver.Solver' class).
+    aniso : float or tuple of shape (2,)
+        Anisotropy for curvature based anisotopic Laplace.
+            (See 'lapy.Solver.Solver' class).
+    aniso_smooth : int
+        Number of smoothing iterations for curvature computation on vertices.
+            (See 'lapy.Solver.Solver' class).
+    use_cholmod : bool, default: False
+        If True, attempts to use the Cholesky decomposition for improved execution
+        speed. Requires the ``scikit-sparse`` library. If it can not be found, an error
+        will be thrown.
+        If False, will use slower LU decomposition.
+
+    Returns
+    -------
+    ev : dict
+        A dictionary, including 'Eigenvalues' and 'Eigenvectors' fields.
+    """
+    # get fem, evals, evecs
+
+    fem = Solver(
+        geom, lump=lump, aniso=aniso, aniso_smooth=aniso_smooth, use_cholmod=use_cholmod
+    )
+    evals, evecs = fem.eigs(k=k)
+
+    # write ev
+
+    evDict = dict()
+    evDict["Refine"] = 0
+    evDict["Degree"] = 1
+    if type(geom).__name__ == "TriaMesh":
+        evDict["Dimension"] = 2
+    elif type(geom).__name__ == "TetMesh":
+        evDict["Dimension"] = 3
+    evDict["Elements"] = len(geom.t)
+    evDict["DoF"] = len(geom.v)
+    evDict["NumEW"] = k
+    evDict["Eigenvalues"] = evals
+    evDict["Eigenvectors"] = evecs
+
+    return evDict
+
+
+def normalize_ev(geom, evals, method="geometry"):
+    """Normalize a surface or a volume.
+
+    Parameters
+    ----------
+    geom : TriaMesh or TetMesh
+        Mesh geometry.
+    evals : array_like
+        Set of sorted eigenvalues.
+    method : str
+        Either "surface", "volume", or "geometry";
+        "geometry" will perform surface normalization for
+        2D objects, and volume normalization for 3D objects.
+
+    Returns
+    -------
+    array_like
+        Vector of re-weighted eigenvalues.
+    """
+    if method == "surface":
+        vol = geom.area()
+
+        return evals * vol ** np.divide(2.0, 2.0)
+
+    elif method == "volume":
+        if type(geom).__name__ == "TriaMesh":
+            geom.orient_()
+
+            vol = geom.volume()
+
+        elif type(geom).__name__ == "TetMesh":
+            bnd = geom.boundary_tria()
+
+            bnd.orient_()
+
+            vol = bnd.volume()
+
+        return evals * vol ** np.divide(2.0, 3.0)
+
+    elif method == "geometry":
+        if type(geom).__name__ == "TriaMesh":
+            vol = geom.area()
+
+            return evals * vol ** np.divide(2.0, 2.0)
+
+        elif type(geom).__name__ == "TetMesh":
+            bnd = geom.boundary_tria()
+
+            bnd.orient_()
+
+            vol = bnd.volume()
+
+            return evals * vol ** np.divide(2.0, 3.0)
+
+
+def reweight_ev(evals):
+    """Apply linear re-weighting.
+
+    Parameters
+    ----------
+    evals : array_like
+        Set of sorted eigenvalues.
+
+    Returns
+    -------
+    evals: array_like
+        Vector of re-weighted eigenvalues.
+    """
+    # evals[1:] = evals[1:] / np.arange(1, len(evals))
+    evals = evals / np.arange(1, len(evals) + 1)
+
+    return evals
+
+
+def compute_distance(ev1, ev2, dist="euc"):
+    """Compute the shape dissimilarity from two shapeDNA descriptors.
+
+    Parameters
+    ----------
+    ev1 : array_like
+        First set of sorted eigenvalues.
+    ev2 : array_like
+        Second set of sorted eigenvalues.
+    dist : str
+        Distance measure; currently only 'euc' (Euclidean).
+
+    Returns
+    -------
+    * : float
+        Distance between the eigenvalue arrays.
+    """
+    if dist == "euc":
+        return di.euclidean(ev1, ev2)
+    else:
+        print("Only euclidean distance is currently implemented.")
+        return
