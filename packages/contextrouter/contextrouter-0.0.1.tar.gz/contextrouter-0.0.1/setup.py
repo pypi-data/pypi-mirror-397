9692ea3443b66b58fec54356a8d56d8a11b02926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 16:07:12 +0000

    Add package contextrouter/0.0.1/contextrouter-0.0.1.tar.gz

---
+++ packages/contextrouter/contextrouter-0.0.1.tar.gz/contextrouter-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="contextrouter",
+    version="0.0.1",
+    author="Oleksii Pylypchuk",
+    author_email="oleksiipylypchuk@gmail.com",
+    description="Context routing for RAG applications (Placeholder)",
+    long_description="This package is under active development. Official release coming soon.",
+    long_description_content_type="text/markdown",
+    url="https://contextrouter.org",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
