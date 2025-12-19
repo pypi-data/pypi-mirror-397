55ec65a317fc82f5319cdbb78f30f1c395708f9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 04:16:06 +0000

    Add package eth-utils/6.0.0b1/eth_utils-6.0.0b1.tar.gz

---
+++ packages/eth-utils/eth_utils-6.0.0b1.tar.gz/eth_utils-6.0.0b1/setup.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+MYPY_REQUIREMENT = "mypy==1.18.2"
+
+extras_require = {
+    "dev": [
+        "build>=0.9.0",
+        "bump_my_version>=0.19.0",
+        "eth-hash[pycryptodome]",
+        "ipython",
+        MYPY_REQUIREMENT,
+        "pre-commit>=3.4.0",
+        "tox>=4.0.0",
+        "twine",
+        "wheel>=0.38.1",
+    ],
+    "docs": [
+        "sphinx>=6.0.0",
+        "sphinx-autobuild>=2021.3.14",
+        "sphinx_rtd_theme>=1.0.0",
+        "towncrier>=24,<25",
+    ],
+    "test": [
+        "hypothesis>=4.43.0",
+        MYPY_REQUIREMENT,
+        "pytest>=7.0.0",
+        "pytest-xdist>=2.4.0",
+    ],
+}
+
+extras_require["dev"] = (
+    extras_require["dev"] + extras_require["docs"] + extras_require["test"]
+)
+
+
+with open("./README.md") as readme:
+    long_description = readme.read()
+
+
+setup(
+    name="eth-utils",
+    # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
+    version="6.0.0-beta.1",
+    description=(
+        """eth-utils: Common utility functions for python code that interacts with Ethereum"""
+    ),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="The Ethereum Foundation",
+    author_email="snakecharmers@ethereum.org",
+    url="https://github.com/ethereum/eth-utils",
+    include_package_data=True,
+    install_requires=[
+        "eth-hash>=0.3.1",
+        "eth-typing>=5.0.0",
+        "toolz>0.8.2;implementation_name=='pypy'",
+        "cytoolz>=0.10.1;implementation_name=='cpython'",
+        "pydantic>=2.0.0,<3",
+    ],
+    python_requires=">=3.10, <4",
+    extras_require=extras_require,
+    py_modules=["eth_utils"],
+    zip_safe=False,
+    keywords="ethereum",
+    packages=find_packages(exclude=["scripts", "scripts.*", "tests", "tests.*"]),
+    package_data={"eth_utils": ["py.typed"]},
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
+    ],
+)
