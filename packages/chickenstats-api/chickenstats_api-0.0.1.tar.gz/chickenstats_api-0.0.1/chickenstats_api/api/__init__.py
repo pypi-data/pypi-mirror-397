106547e66d74324570fd861a14415996d80a8977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 21:08:21 +0000

    Add package chickenstats-api/0.0.1/chickenstats_api-0.0.1.tar.gz

---
+++ packages/chickenstats-api/chickenstats_api-0.0.1.tar.gz/chickenstats_api-0.0.1/chickenstats_api/api/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+
+# import apis into api package
+from chickenstats_api.api.chicken_nhl_api import ChickenNhlApi
+from chickenstats_api.api.game_stats_api import GameStatsApi
+from chickenstats_api.api.lines_api import LinesApi
+from chickenstats_api.api.login_api import LoginApi
+from chickenstats_api.api.play_by_play_api import PlayByPlayApi
+from chickenstats_api.api.season_stats_api import SeasonStatsApi
+from chickenstats_api.api.stats_api import StatsApi
+from chickenstats_api.api.team_stats_api import TeamStatsApi
+from chickenstats_api.api.users_api import UsersApi
+
