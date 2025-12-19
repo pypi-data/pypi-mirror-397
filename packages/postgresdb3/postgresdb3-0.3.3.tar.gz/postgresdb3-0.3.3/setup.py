fb11db6827eb3f4439954f46d9790c646e623b40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 19:06:46 +0000

    Add package postgresdb3/0.3.3/postgresdb3-0.3.3.tar.gz

---
+++ packages/postgresdb3/postgresdb3-0.3.3.tar.gz/postgresdb3-0.3.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="postgresdb3",
+    version="0.3.3",
+    packages=find_packages(),
+    install_requires=[
+        "psycopg2>=2.9",
+        "asyncpg>=0.31.0"
+    ],
+    author="Abdulbosit Alijonov",
+    description="Python uchun oddiy PostgreSQL wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    project_urls={
+        "Source Code": "https://github.com/AlijonovUz/PostgresDB",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
