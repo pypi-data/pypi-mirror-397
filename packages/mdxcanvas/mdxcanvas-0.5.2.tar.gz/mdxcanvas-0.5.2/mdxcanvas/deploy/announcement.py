121aa8b70d99ef7dec6be159d844b215154a4db2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 03:32:54 +0000

    Add package mdxcanvas/0.5.2/mdxcanvas-0.5.2.tar.gz

---
+++ packages/mdxcanvas/mdxcanvas-0.5.2.tar.gz/mdxcanvas-0.5.2/mdxcanvas/deploy/announcement.py
@@ -0,0 +1,24 @@
+from canvasapi.course import Course
+
+from ..resources import AnnouncementInfo
+
+
+def deploy_announcement(course: Course, announcement_info: dict) -> tuple[AnnouncementInfo, None]:
+    announcement_id = announcement_info["canvas_id"]
+
+    if announcement_id:
+        canvas_announcements = course.get_discussion_topics(course_id=course.id, only_announcements=True)
+        canvas_announcement = next(
+            (a for a in canvas_announcements if a.id == announcement_id), None
+        )
+        canvas_announcement.update(**announcement_info)
+
+    else:
+        canvas_announcement = course.create_discussion_topic(**announcement_info)
+
+    announcement_object_info: AnnouncementInfo = {
+        'id':  canvas_announcement.id,
+        'url': canvas_announcement.html_url
+    }
+
+    return announcement_object_info, None
---
+++ packages/mdxcanvas/mdxcanvas-0.5.2.tar.gz/mdxcanvas-0.5.2/mdxcanvas/deploy/page.py
@@ -0,0 +1,28 @@
+from canvasapi.course import Course
+
+from ..resources import PageInfo
+
+
+def deploy_page(course: Course, page_info: dict) -> tuple[PageInfo, None]:
+    page_id = page_info["canvas_id"]
+
+    if page_id:
+        canvas_page = course.get_page(page_id)
+        canvas_page.edit(wiki_page=page_info)
+    else:
+        canvas_page = course.create_page(wiki_page=page_info)
+
+    page_object_info: PageInfo = {
+        'id': canvas_page.page_id,
+        'url': canvas_page.html_url,
+    }
+
+    return page_object_info, None
+
+
+def deploy_shell_page(course: Course, page_info: dict) -> tuple[PageInfo, None]:
+    shell_page_info = page_info.copy()
+    shell_page_info[
+        'body'] = "<p>This is a shell page created to break a dependency cycle. The full content will be deployed later.</p>"
+
+    return deploy_page(course, shell_page_info)
