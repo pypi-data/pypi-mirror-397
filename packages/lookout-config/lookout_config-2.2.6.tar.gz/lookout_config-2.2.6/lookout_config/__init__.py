2d4484c51dd7007a8b08e6e2afb8ed008c12695e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 16:06:58 +0000

    Add package lookout-config/2.2.6/lookout_config-2.2.6.tar.gz

---
+++ packages/lookout-config/lookout_config-2.2.6.tar.gz/lookout_config-2.2.6/lookout_config/__init__.py
@@ -0,0 +1,38 @@
+# IMPORTANT
+# After changing this file, run `python3 -m lookout_config.generate_schemas`
+# To re-generate the json schemas
+import os
+
+from lookout_config.config_io import ConfigIO
+from lookout_config.types import (
+    GeolocationMode,
+    LogLevel,
+    LookoutConfig,
+    Mode,
+    Network,
+    Point,
+    Polygon,
+    PositioningSystem,
+)
+
+
+def get_config_io() -> ConfigIO:
+    """Get a ConfigIO instance using the LOOKOUT_CONFIG_DIR environment variable."""
+    config_dir = os.environ.get("LOOKOUT_CONFIG_DIR")
+    if config_dir is None:
+        raise ValueError("LOOKOUT_CONFIG_DIR environment variable is not set.")
+    return ConfigIO(config_directory=config_dir)
+
+
+__all__ = [
+    "get_config_io",
+    "ConfigIO",
+    "LookoutConfig",
+    "Mode",
+    "LogLevel",
+    "Network",
+    "GeolocationMode",
+    "Point",
+    "Polygon",
+    "PositioningSystem",
+]
