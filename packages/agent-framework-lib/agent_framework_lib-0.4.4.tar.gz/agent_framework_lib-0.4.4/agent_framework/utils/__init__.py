b7dfa17e7e1ac4e88cfea853f310e83ac12bd979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 16:06:36 +0000

    Add package agent-framework-lib/0.4.4/agent_framework_lib-0.4.4.tar.gz

---
+++ packages/agent-framework-lib/agent_framework_lib-0.4.4.tar.gz/agent_framework_lib-0.4.4/agent_framework/utils/__init__.py
@@ -0,0 +1,27 @@
+"""Utility functions and helpers."""
+
+from .path_utils import PathDetector
+from .session_title_generator import (
+    SessionTitleGenerator,
+    get_title_generator,
+    DEFAULT_LABEL,
+    MAX_TITLE_LENGTH,
+)
+from .post_install import (
+    ensure_playwright_browsers,
+    ensure_deno,
+    get_deno_command,
+    post_install,
+)
+
+__all__ = [
+    'PathDetector',
+    'SessionTitleGenerator',
+    'get_title_generator',
+    'DEFAULT_LABEL',
+    'MAX_TITLE_LENGTH',
+    'ensure_playwright_browsers',
+    'ensure_deno',
+    'get_deno_command',
+    'post_install',
+]
