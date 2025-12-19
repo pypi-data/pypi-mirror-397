480c82b531c3a6c09f711ac73d235cc7daab7d63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-19 04:16:20 +0000

    Add package fastapi-slim/0.125.0/fastapi_slim-0.125.0.tar.gz

---
+++ packages/fastapi-slim/fastapi_slim-0.125.0.tar.gz/fastapi_slim-0.125.0/tests/test_validate_response_recursive/app.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+from fastapi._compat import PYDANTIC_V2
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class RecursiveItem(BaseModel):
+    sub_items: list["RecursiveItem"] = []
+    name: str
+
+
+class RecursiveSubitemInSubmodel(BaseModel):
+    sub_items2: list["RecursiveItemViaSubmodel"] = []
+    name: str
+
+
+class RecursiveItemViaSubmodel(BaseModel):
+    sub_items1: list[RecursiveSubitemInSubmodel] = []
+    name: str
+
+
+if PYDANTIC_V2:
+    RecursiveItem.model_rebuild()
+    RecursiveSubitemInSubmodel.model_rebuild()
+    RecursiveItemViaSubmodel.model_rebuild()
+else:
+    RecursiveItem.update_forward_refs()
+    RecursiveSubitemInSubmodel.update_forward_refs()
+
+
+@app.get("/items/recursive", response_model=RecursiveItem)
+def get_recursive():
+    return {"name": "item", "sub_items": [{"name": "subitem", "sub_items": []}]}
+
+
+@app.get("/items/recursive-submodel", response_model=RecursiveItemViaSubmodel)
+def get_recursive_submodel():
+    return {
+        "name": "item",
+        "sub_items1": [
+            {
+                "name": "subitem",
+                "sub_items2": [
+                    {
+                        "name": "subsubitem",
+                        "sub_items1": [{"name": "subsubsubitem", "sub_items2": []}],
+                    }
+                ],
+            }
+        ],
+    }
