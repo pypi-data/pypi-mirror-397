46a293a5286768b155dd3d4a8dac3c7ea77b9f53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-20 00:09:08 +0000

    Add package gold-dl/0.0.1/gold_dl-0.0.1.tar.gz

---
+++ packages/gold-dl/gold_dl-0.0.1.tar.gz/gold_dl-0.0.1/pyutube/__init__.py
@@ -0,0 +1,18 @@
+"""This is the initialization module for the pyutube package.
+
+It provides the main entry point for the package by importing the `app`
+object from the `cli` submodule.
+
+Example:
+    To use this package, you can import the `app` object directly:
+
+    >>> from pyutube import app
+    >>> app.run()
+
+"""
+
+
+# List the symbols you want to export from this module
+# __all__ = ['app']
+__app__ = "pyutube"
+__version__ = "1.5.1"
