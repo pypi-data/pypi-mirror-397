dd123e8d91fb629929ea1081009c6d54d68d29bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-20 00:08:57 +0000

    Add package warn-transformer/1.3.307/warn_transformer-1.3.307.tar.gz

---
+++ packages/warn-transformer/warn_transformer-1.3.307.tar.gz/warn_transformer-1.3.307/setup.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+"""Configure the package for distribution."""
+import os
+
+from setuptools import find_packages, setup
+
+
+def read(file_name):
+    """Read the provided file."""
+    this_dir = os.path.dirname(__file__)
+    file_path = os.path.join(this_dir, file_name)
+    with open(file_path) as f:
+        return f.read()
+
+
+def version_scheme(version):
+    """
+    Version scheme hack for setuptools_scm.
+
+    Appears to be necessary to due to the bug documented here: https://github.com/pypa/setuptools_scm/issues/342
+
+    If that issue is resolved, this method can be removed.
+    """
+    import time
+
+    from setuptools_scm.version import guess_next_version
+
+    if version.exact:
+        return version.format_with("{tag}")
+    else:
+        _super_value = version.format_next_version(guess_next_version)
+        now = int(time.time())
+        return _super_value + str(now)
+
+
+def local_version(version):
+    """
+    Local version scheme hack for setuptools_scm.
+
+    Appears to be necessary to due to the bug documented here: https://github.com/pypa/setuptools_scm/issues/342
+
+    If that issue is resolved, this method can be removed.
+    """
+    return ""
+
+
+setup(
+    name="warn-transformer",
+    description="Consolidate, enrich and analyze the data gathered by warn-scraper",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author="Big Local News",
+    url="https://github.com/biglocalnews/warn-transformer",
+    packages=find_packages(
+        exclude=[
+            "tests",
+        ]
+    ),
+    include_package_data=True,
+    install_requires=[
+        "bln>=2.3.11",
+        "marshmallow==3.26.1",
+        "click",
+        "jellyfish",
+    ],
+    license="Apache 2.0 license",
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    setup_requires=["setuptools_scm"],
+    use_scm_version={"version_scheme": version_scheme, "local_scheme": local_version},
+    entry_points="""
+        [console_scripts]
+        warn-transformer=warn_transformer.cli:cli
+    """,
+)
