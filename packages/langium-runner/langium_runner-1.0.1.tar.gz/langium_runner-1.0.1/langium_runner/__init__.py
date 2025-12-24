f78ea0301268b7599f05f06ed8f0c26ce2d1e025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 16:06:51 +0000

    Add package langium-runner/1.0.1/langium_runner-1.0.1.tar.gz

---
+++ packages/langium-runner/langium_runner-1.0.1.tar.gz/langium_runner-1.0.1/langium_runner/__init__.py
@@ -0,0 +1,5 @@
+# Import LangiumEvaluatorRunner from the evaluator module
+from .evaluator_runner import LangiumEvaluatorRunner
+
+# Expose LangiumEvaluatorRunner at the package level
+__all__ = ["LangiumEvaluatorRunner"]
