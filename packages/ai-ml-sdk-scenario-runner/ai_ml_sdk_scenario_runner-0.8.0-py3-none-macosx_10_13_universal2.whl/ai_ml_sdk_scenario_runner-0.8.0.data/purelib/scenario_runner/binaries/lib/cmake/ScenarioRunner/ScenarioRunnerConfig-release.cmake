de605e02f1dd6a61775a9d66999445d921e26f10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 17:07:15 +0000

    Add package ai-ml-sdk-scenario-runner/0.8.0/ai_ml_sdk_scenario_runner-0.8.0-py3-none-macosx_10_13_universal2.whl

---
+++ packages/ai-ml-sdk-scenario-runner/ai_ml_sdk_scenario_runner-0.8.0-py3-none-macosx_10_13_universal2.whl/ai_ml_sdk_scenario_runner-0.8.0.data/purelib/scenario_runner/binaries/lib/cmake/ScenarioRunner/ScenarioRunnerConfig-release.cmake
@@ -0,0 +1,36 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "ScenarioRunner::glslc" for configuration "Release"
+set_property(TARGET ScenarioRunner::glslc APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ScenarioRunner::glslc PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/glslc"
+  )
+
+list(APPEND _cmake_import_check_targets ScenarioRunner::glslc )
+list(APPEND _cmake_import_check_files_for_ScenarioRunner::glslc "${_IMPORT_PREFIX}/bin/glslc" )
+
+# Import target "ScenarioRunner::dds_utils" for configuration "Release"
+set_property(TARGET ScenarioRunner::dds_utils APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ScenarioRunner::dds_utils PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/dds_utils"
+  )
+
+list(APPEND _cmake_import_check_targets ScenarioRunner::dds_utils )
+list(APPEND _cmake_import_check_files_for_ScenarioRunner::dds_utils "${_IMPORT_PREFIX}/bin/dds_utils" )
+
+# Import target "ScenarioRunner::scenario-runner" for configuration "Release"
+set_property(TARGET ScenarioRunner::scenario-runner APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ScenarioRunner::scenario-runner PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/scenario-runner"
+  )
+
+list(APPEND _cmake_import_check_targets ScenarioRunner::scenario-runner )
+list(APPEND _cmake_import_check_files_for_ScenarioRunner::scenario-runner "${_IMPORT_PREFIX}/bin/scenario-runner" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
