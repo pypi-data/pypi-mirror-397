ebecc0a997e99cf78fd99d1bb50394f703fbabdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 14:06:57 +0000

    Add package ntnx-opsmgmt-py-client/4.0.3/ntnx_opsmgmt_py_client-4.0.3.tar.gz

---
+++ packages/ntnx-opsmgmt-py-client/ntnx_opsmgmt_py_client-4.0.3.tar.gz/ntnx_opsmgmt_py_client-4.0.3/ntnx_opsmgmt_py_client/api/__init__.py
@@ -0,0 +1,10 @@
+
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ntnx_opsmgmt_py_client.api.global_report_setting_api import GlobalReportSettingApi
+from ntnx_opsmgmt_py_client.api.report_artifacts_api import ReportArtifactsApi
+from ntnx_opsmgmt_py_client.api.report_config_api import ReportConfigApi
+from ntnx_opsmgmt_py_client.api.reports_api import ReportsApi
