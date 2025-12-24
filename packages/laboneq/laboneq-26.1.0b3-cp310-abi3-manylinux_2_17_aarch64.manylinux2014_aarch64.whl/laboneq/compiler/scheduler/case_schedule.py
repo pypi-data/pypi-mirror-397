b3dc775d21277ac677f9ad0ad84a0afa336ad654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 15:08:31 +0000

    Add package laboneq/26.1.0b3/laboneq-26.1.0b3-cp310-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/laboneq/laboneq-26.1.0b3-cp310-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/laboneq/compiler/scheduler/case_schedule.py
@@ -0,0 +1,20 @@
+# Copyright 2022 Zurich Instruments AG
+# SPDX-License-Identifier: Apache-2.0
+
+
+from attrs import asdict, define
+
+from laboneq.compiler.scheduler.section_schedule import SectionSchedule
+
+
+@define(kw_only=True, slots=True)
+class CaseSchedule(SectionSchedule):
+    state: int
+
+    @classmethod
+    def from_section_schedule(cls, schedule: SectionSchedule, state: int):
+        """Down-cast from SectionSchedule."""
+        return cls(**asdict(schedule, recurse=False), state=state)
+
+    def _calculate_timing(self, _schedule_data, start: int, *__, **___) -> int:
+        return super()._calculate_timing(_schedule_data, start, *__, **___)
