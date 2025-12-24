c2268ae309807f7e669ec0dedf79b5ebec6746c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 16:06:49 +0000

    Add package ply-AI/1.0.2/ply_ai-1.0.2.tar.gz

---
+++ packages/ply-AI/ply_ai-1.0.2.tar.gz/ply_ai-1.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ply-AI",
+    version="1.0.2",   # <--- 这里改成了 1.0.2
+    description="Hardware-aware Triton accelerator library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Zeng Jianrong",
+    author_email="zeng@ply-ai.com",
+    packages=find_packages(),
+    install_requires=["torch", "triton"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+)
