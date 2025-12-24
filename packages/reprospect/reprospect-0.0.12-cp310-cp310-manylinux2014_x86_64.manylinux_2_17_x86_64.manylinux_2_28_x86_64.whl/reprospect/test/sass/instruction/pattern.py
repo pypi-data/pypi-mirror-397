7308ffc0a617708dfe9db535d4c8d61d8c6f62a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 16:07:12 +0000

    Add package reprospect/0.0.12/reprospect-0.0.12-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/reprospect/reprospect-0.0.12-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/reprospect/test/sass/instruction/pattern.py
@@ -0,0 +1,198 @@
+import functools
+import typing
+
+from reprospect.test.sass.instruction.operand import MODIFIER_MATH
+
+
+class PatternBuilder:
+    """
+    Helper class to build patterns for instruction components.
+    """
+    HEX: typing.Final[str] = r'0x[0-9A-Fa-f]+'
+    OFFSET: typing.Final[str] = r'\+0x[0-9A-Fa-f]+'
+    PRED: typing.Final[str] = r'P[0-9]+'
+    REG: typing.Final[str] = r'R[0-9]+'
+    REG64: typing.Final[str] = r'R[0-9]+\.64'
+    UREG: typing.Final[str] = r'UR[0-9]+'
+
+    #: Match a register or ``RZ``.
+    REGZ: typing.Final[str] = r'R(?:Z|\d+)'
+
+    #: Match a predicate register or ``PT``.
+    PREDT: typing.Final[str] = r'P(?:T|\d+)'
+
+    #: Match a uniform predicate register.
+    UPRED: typing.Final[str] = r'UP[0-9]+'
+
+    #: Match a uniform predicate register or ``UPT``.
+    UPREDT: typing.Final[str] = r'UP(?:T|\d+)'
+
+    OPERAND: typing.Final[str] = r'[\w@!\.\[\]\+\-\s]+'
+
+    CONSTANT_BANK: typing.Final[str] = r'0x[0-9]+'
+    """
+    Constant memory bank.
+    """
+
+    CONSTANT_OFFSET: typing.Final[str] = r'(?:0x[0-9a-f]+|' + REG + '|' + UREG + ')'
+    """
+    Constant memory offset.
+    """
+
+    CONSTANT: typing.Final[str] = r'c\[' + CONSTANT_BANK + r'\]\[' + CONSTANT_OFFSET + r'\]'
+    """
+    Constant memory location.
+    The bank looks like ``0x3`` while the address is either compile-time (*e.g.*
+    ``0x899``) or depends on a register.
+    """
+
+    IMMEDIATE: typing.Final[str] = r'(-?\d+)(\.\d*)?((e|E)[-+]?\d+)?'
+    """
+    References:
+
+    * https://github.com/cloudcores/CuAssembler/blob/96a9f72baf00f40b9b299653fcef8d3e2b4a3d49/CuAsm/CuInsParser.py#L34
+    """
+
+    PREDICATE: typing.Final[str] = r'@!?U?P(?:T|\d+)'
+    """
+    Predicate for the whole instruction (comes before the opcode).
+    """
+
+    @staticmethod
+    def zero_or_one(s: str) -> str:
+        """
+        Build an optional non-capturing pattern that matches zero or one occurrence of the given pattern.
+        """
+        return rf'(?:{s})?'
+
+    @staticmethod
+    def zero_or_more(s: str) -> str:
+        """
+        Build an optional non-capturing pattern that matches zero or more occurrences of the given pattern.
+        """
+        return rf'(?:{s})*'
+
+    @staticmethod
+    def any(*args: str) -> str:
+        """
+        Build a pattern matching any of `args`.
+        """
+        return f'({"|".join(args)})'
+
+    @staticmethod
+    def group(s: int | str, group: str) -> str:
+        """
+        Wrap a pattern in a named capture group.
+        """
+        return rf'(?P<{group}>{s})'
+
+    @staticmethod
+    def groups(s: int | str, groups: typing.Iterable[str]) -> str:
+        """
+        Wrap a pattern in named capture groups.
+        """
+        for g in groups:
+            s = PatternBuilder.group(s, group=g)
+        return str(s)
+
+    @classmethod
+    def hex(cls) -> str:
+        """
+        :py:attr:`HEX` with `operands` group.
+        """
+        return cls.group(PatternBuilder.HEX, group='operands')
+
+    @classmethod
+    def reg(cls) -> str:
+        """
+        :py:attr:`REG` with `operands` group.
+        """
+        return cls.group(cls.REG, group='operands')
+
+    @classmethod
+    def mathmodregz(cls) -> str:
+        """
+        :py:attr:`REGZ` with `operands` group and optional math modifiers :py:const:`reprospect.test.sass.instruction.operand.MODIFIER_MATH`.
+        """
+        return cls.group(cls.zero_or_one(MODIFIER_MATH) + cls.REGZ, group='operands')
+
+    @classmethod
+    def regz(cls) -> str:
+        """
+        :py:attr:`REGZ` with `operands` group.
+        """
+        return cls.group(cls.REGZ, group='operands')
+
+    @classmethod
+    def ureg(cls) -> str:
+        """
+        :py:attr:`UREG` with `operands` group.
+        """
+        return cls.group(cls.UREG, group='operands')
+
+    @classmethod
+    def anygpreg(cls, *, reuse: bool | None = None, group: str | None = None) -> str:
+        """
+        Match any general purpose register.
+        """
+        pattern = cls.any(cls.REG, cls.UREG)
+        if reuse is None:
+            pattern += PatternBuilder.zero_or_one(r'\.reuse')
+        elif reuse is True:
+            pattern += r'\.reuse'
+        if group is not None:
+            pattern = cls.group(pattern, group=group)
+        return pattern
+
+    @classmethod
+    def predt(cls) -> str:
+        """
+        :py:attr:`PREDT` with `operands` group.
+        """
+        return cls.group(cls.PREDT, group='operands')
+
+    @classmethod
+    def immediate(cls) -> str:
+        """
+        :py:attr:`IMMEDIATE` with `operands` group.
+        """
+        return cls.group(cls.IMMEDIATE, group='operands')
+
+    @classmethod
+    def predicate(cls) -> str:
+        """
+        :py:attr:`PREDICATE` with `predicate` group.
+        """
+        return cls.group(s=cls.PREDICATE, group='predicate')
+
+    @staticmethod
+    def opcode_mods(opcode: str, modifiers: typing.Iterable[int | str | None] | None = None) -> str:
+        """
+        Append each modifier with a `.`, within a proper named capture group.
+
+        Note that the modifiers starting with a `?` are matched optionally.
+        """
+        opcode = PatternBuilder.group(opcode, group='opcode')
+        if modifiers is None:
+            return opcode
+
+        parts: list[str] = [opcode]
+
+        for modifier in modifiers:
+            if not modifier:
+                continue
+            if isinstance(modifier, str) and modifier[0] == '?':
+                parts.append(PatternBuilder.zero_or_one(r'\.' + PatternBuilder.group(modifier[1:], group='modifiers')))
+            else:
+                parts.append(r'\.' + PatternBuilder.group(modifier, group='modifiers'))
+
+        return ''.join(parts)
+
+    @classmethod
+    @functools.cache
+    def operands(cls) -> str:
+        """
+        Many operands, with the `operands` named capture group.
+        """
+        op = PatternBuilder.group(cls.OPERAND, group='operands')
+        return rf'{op}(?:\s*,\s*{op})*'
---
+++ packages/reprospect/reprospect-0.0.12-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/reprospect/test/sass/instruction/register.py
@@ -0,0 +1,126 @@
+from __future__ import annotations
+
+import dataclasses
+import typing
+
+import attrs
+import regex
+
+from reprospect.test.sass.instruction.operand import (
+    MODIFIER_MATH,
+    MathModifier,
+)
+from reprospect.test.sass.instruction.pattern import PatternBuilder
+from reprospect.tools.sass.decode import RegisterType
+
+
+@dataclasses.dataclass(frozen=True, slots=True)
+class RegisterMatch:
+    """
+    If :py:attr:`index` is :py:obj:`None`, it is a special register (*e.g.*
+    ``RZ`` if :py:attr:`rtype` is :py:const:`reprospect.tools.sass.decode.RegisterType.GPR`).
+    """
+    rtype: RegisterType
+    index: int | None = None
+    reuse: bool = False
+    math: MathModifier | None = None
+
+    @classmethod
+    def parse(cls, bits: regex.Match[str]) -> RegisterMatch:
+        captured = bits.capturesdict()
+
+        if len(rtype := captured['rtype']) != 1:
+            raise ValueError(bits)
+
+        index: int | None = None
+        if (value := captured.get('index')) is not None:
+            if len(value) == 1:
+                index = int(value[0])
+
+        math: MathModifier | None = None
+        if (value := captured.get('modifier_math')) is not None:
+            if len(value) == 1:
+                math = MathModifier(value[0])
+
+        return cls(
+            rtype=RegisterType(rtype[0]),
+            index=index,
+            reuse=bool(captured.get('reuse')),
+            math=math,
+        )
+
+REGISTER_MATCHER_MOD_PRE: typing.Final[str] = PatternBuilder.zero_or_one(PatternBuilder.group(MODIFIER_MATH, group='modifier_math'))
+
+@attrs.define(frozen=True, slots=True, kw_only=True)
+class RegisterMatcher:
+    """
+    Matcher for a register.
+    """
+    rtype: RegisterType | None = None
+    special: bool | None = None
+    index: int | None = None
+    reuse: bool | None = None
+    math: MathModifier | None = None
+
+    pattern: regex.Pattern[str] = attrs.field(init=False)
+
+    def __attrs_post_init__(self) -> None:
+        if self.special is not None and self.index is not None:
+            raise RuntimeError(self)
+
+        object.__setattr__(self, 'pattern', regex.compile(''.join(filter(None, (
+            self._build_modifier_math(),
+            PatternBuilder.group(self.rtype.value if self.rtype else 'R|UR|P|UP', group='rtype'),
+            self._build_special(),
+            self._build_index(),
+            self._build_reuse(),
+        )))))
+
+    def match(self, reg: str) -> RegisterMatch | None:
+        if (matched := self.pattern.match(reg)) is not None:
+            return RegisterMatch.parse(bits=matched)
+        return None
+
+    def __call__(self, reg: str) -> RegisterMatch | None:
+        return self.match(reg=reg)
+
+    def _build_modifier_math(self) -> str | None:
+        if self.math is None:
+            return REGISTER_MATCHER_MOD_PRE
+        return PatternBuilder.group(self.math.value, group='modifier_math')
+
+    def _build_special(self) -> str | None:
+        if self.index is not None:
+            return None
+        if self.special is False:
+            return None
+
+        idx = self.rtype.special if self.rtype is not None else r'Z|T'
+
+        special = PatternBuilder.group(idx, group='special')
+
+        if self.special is None:
+            return special + '?'
+        return special
+
+    def _build_index(self) -> str | None:
+        if self.special is True:
+            return None
+        if self.index is not None:
+            return PatternBuilder.group(self.index, group='index')
+        index = PatternBuilder.group(r'\d+', group='index')
+        if self.special is None:
+            return index + '?'
+        return index
+
+    def _build_reuse(self) -> str | None:
+        if self.reuse is False:
+            return None
+        if self.special is True:
+            return None
+        if self.rtype is not None and self.rtype.is_predicate:
+            return None
+        reuse = PatternBuilder.group(r'\.reuse', group='reuse')
+        if self.reuse is None:
+            return reuse + '?'
+        return reuse
