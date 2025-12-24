5f87b29617af1bfa801d8532dda516b609511fb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 14:07:00 +0000

    Add package ntnx-multidomain-py-client/4.2.1/ntnx_multidomain_py_client-4.2.1-py3-none-any.whl

---
+++ packages/ntnx-multidomain-py-client/ntnx_multidomain_py_client-4.2.1-py3-none-any.whl/ntnx_multidomain_py_client/api/__init__.py
@@ -0,0 +1,7 @@
+
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ntnx_multidomain_py_client.api.external_repositories_api import ExternalRepositoriesApi
