163bcf0a8f46a7258e4ecd6e186c3701c58ab72c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 16:08:27 +0000

    Add package environ-odoo-config/0.6.4/environ_odoo_config-0.6.4.tar.gz

---
+++ packages/environ-odoo-config/environ_odoo_config-0.6.4.tar.gz/environ_odoo_config-0.6.4/src/odoo/addons/environ_odoo_config/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Odoo config from env cli",
+    "description": """Odoo config from env cli""",
+    "author": "Mangono",
+    "maintainers": "Mangono",
+    "website": "https://mangono.fr",
+    "category": "Technical",
+    "version": "1.7.0",
+    "license": "AGPL-3",
+    "installable": False,
+}
