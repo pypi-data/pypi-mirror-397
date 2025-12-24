d54234661e5221f1f8da0bc3c14ceacef8adfaaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 16:06:42 +0000

    Add package gaycore/4.0/gaycore-4.0.tar.gz

---
+++ packages/gaycore/gaycore-4.0.tar.gz/gaycore-4.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+VERSION = "4.0"
+
+setup(
+    name="gaycore",
+    version=VERSION,
+    description="A tiny and smart cli player of gcore audio, based on Python package curses and mpg123 for Linux or Mac",
+    long_description="gaycore listen player using cli enjoy it",
+    keywords="python gcore gaycore cli terminal",
+    author="yihong0618",
+    author_email="zouzou0208@gmail.com",
+    url="https://github.com/yihong0618/gaycore",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=["requests", "miservice-fork"],
+    python_requires=">=3.10",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Software Development :: Libraries",
+    ],
+    entry_points={
+        "console_scripts": ["gaycore = gaycore.cli:run"],
+    },
+)
