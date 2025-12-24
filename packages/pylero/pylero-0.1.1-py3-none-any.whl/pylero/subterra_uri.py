eb91e4ddbacac58b7fb7ed628b9915cf09cb457a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 16:06:38 +0000

    Add package pylero/0.1.1/pylero-0.1.1-py3-none-any.whl

---
+++ packages/pylero/pylero-0.1.1-py3-none-any.whl/pylero/subterra_uri.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from pylero.base_polarion import BasePolarion
+
+
+# Unknown what this class object is supposed to do. Will determine during
+# more intensive testing
+class SubterraURI(BasePolarion):
+    def __init__(self):
+        pass
+
+
+class ArrayOfSubterraURI(BasePolarion):
+    def __init__(self):
+        pass
