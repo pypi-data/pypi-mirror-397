56f77e228da011ac8b6f243d1bc468645636c04c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 15:07:55 +0000

    Add package castmail2list/0.5.2/castmail2list-0.5.2-py3-none-any.whl

---
+++ packages/castmail2list/castmail2list-0.5.2-py3-none-any.whl/castmail2list/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2025 Max Mehl <https://mehl.mx>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""Castmail2List"""
+
+from importlib.metadata import version
+
+__version__ = version("castmail2list")
---
+++ packages/castmail2list/castmail2list-0.5.2-py3-none-any.whl/castmail2list/views/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2025 Max Mehl <https://mehl.mx>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""Global constants"""
+
+from importlib.metadata import version
+
+__version__ = version("castmail2list")
