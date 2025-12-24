7ea59e445fd6e71bb5e91680304950ccdb308b64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 16:06:47 +0000

    Add package pylero/0.2.0/pylero-0.2.0-py3-none-any.whl

---
+++ packages/pylero/pylero-0.2.0-py3-none-any.whl/pylero/server.py
@@ -0,0 +1,47 @@
+# -*- coding: utf8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from pylero._compatible import object
+from pylero.session import Session
+
+
+class Server(object):
+    """Server object is instantiated once per Polarion session and creates the
+    session that is used to connect to the server.
+    """
+
+    def __init__(
+        self,
+        url,
+        user,
+        password,
+        token,
+        default_project=None,
+        relogin_timeout=60,
+        timeout=120,
+        cert_path=None,
+    ):
+        """An object that defines the properties of the Polarion server to
+        connect to.
+
+        Args:
+            url: url of the Polarion server
+            user: username
+            password: password
+            default_project: default project to use to for configuarations
+            relogin_timeout: timeout after which the session will try to login
+                             again
+            timeout: http tiemout
+            cert_path: path to customize CA bundle
+        """
+        self.url = url
+        self.user = user
+        self.password = password
+        self.token = token
+        self.default_project = default_project
+        self.relogin_timeout = relogin_timeout
+        self.timeout = timeout
+        self.cert_path = cert_path
+
+    def session(self):
+        return Session(self, self.timeout)
---
+++ packages/pylero/pylero-0.2.0-py3-none-any.whl/pylero/test_step_result.py
@@ -0,0 +1,41 @@
+# -*- coding: utf8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from pylero.base_polarion import BasePolarion
+from pylero.enum_option_id import EnumOptionId
+from pylero.test_run_attachment import ArrayOfTestRunAttachment, TestRunAttachment
+from pylero.text import Text
+
+
+class TestStepResult(BasePolarion):
+    """Object to handle the Polarion WSDL tns3:TestStepResult class
+
+    Attributes:
+        attachments (ArrayOfTestRunAttachment)
+        comment (Text)
+        result (EnumOptionId)"""
+
+    _cls_suds_map = {
+        "attachments": {
+            "field_name": "attachments",
+            "is_array": True,
+            "cls": TestRunAttachment,
+            "arr_cls": ArrayOfTestRunAttachment,
+            "inner_field_name": "TestRunAttachment",
+        },
+        "comment": {"field_name": "comment", "cls": Text},
+        "result": {
+            "field_name": "result",
+            "cls": EnumOptionId,
+            "enum_id": "testing/test-result",
+        },
+        "uri": "_uri",
+        "_unresolved": "_unresolved",
+    }
+    _obj_client = "test_management_client"
+    _obj_struct = "tns3:TestStepResult"
+
+
+class ArrayOfTestStepResult(BasePolarion):
+    _obj_client = "test_management_client"
+    _obj_struct = "tns3:ArrayOfTestStepResult"
