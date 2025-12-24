ef0810bbce8062cebce81ccfbca47e266301c44b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 14:07:05 +0000

    Add package vllama/1.2.0/vllama-1.2.0-py3-none-any.whl

---
+++ packages/vllama/vllama-1.2.0-py3-none-any.whl/vllama/test.py
@@ -0,0 +1,10 @@
+from core import *
+
+if __name__ == "__main__":
+    # text_to_speech("Hello world this is Manvith building the text to speech module with in built library of python")
+    # text_to_speech()
+    # text_to_speech("exit")
+
+    # list_downloads()
+    translation = translate_fast("hello world this is your boy from bangalore india building vllama which helps in translation. I have built a framework called vllama which helps all in translation")
+    print(translation)
