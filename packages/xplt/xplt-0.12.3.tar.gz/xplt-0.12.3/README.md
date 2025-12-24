0c0dce9d58fa320a8fc5ffa144a93b0f51fadc98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 15:07:12 +0000

    Add package xplt/0.12.3/xplt-0.12.3.tar.gz

---
+++ packages/xplt/xplt-0.12.3.tar.gz/xplt-0.12.3/README.md
@@ -0,0 +1,24 @@
+# Xplt
+
+<!--[![Contributors](https://img.shields.io/github/contributors/xsuite/xplt)](https://github.com/xsuite/xplt/graphs/contributors)-->
+[![Build status](https://img.shields.io/github/actions/workflow/status/xsuite/xplt/test.yaml?branch=main)](https://github.com/xsuite/xplt/actions)
+[![Pip version](https://img.shields.io/pypi/v/xplt?label=pip)](https://pypi.org/project/xplt)
+
+<img width="40%" align="right" src="https://github.com/xsuite/xplt/raw/main/.github/sampleplot.png"/>
+
+A plotting library for [Xsuite](https://github.com/xsuite) and simmilar accelerator physics codes.
+
+
+## Usage
+
+```bash
+pip install xplt[recommended]
+```
+
+Read the docs at https://xsuite.github.io/xplt
+
+
+
+## Developers and Contributors
+
+See the wiki page at https://github.com/xsuite/xplt/wiki
---
+++ packages/xplt/xplt-0.12.3.tar.gz/xplt-0.12.3/xplt/__init__.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "Philipp Niedermayer"
+__contact__ = "eltos@outlook.de"
+
+
+__version__ = "0.12.3"
+
+
+# expose the following in global namespace
+from .colors import *
+from .line import *
+from .particles import *
+from .phasespace import *
+from .properties import *
+from .timestructure import *
+from .twiss import *
+from .util import AUTO
+
+# allow usage of xplt.mpl.* without importing matplotlib
+import matplotlib as mpl
+
+
+# Deprecated, for backwards compatibility
+class TimeVariationPlot(SpillQualityPlot):
+    """
+    .. deprecated:: 0.8
+        Use :class:`xplt.SpillQualityPlot` instead.
+    """
+
+    pass
+
+
+class TimeVariationScalePlot(SpillQualityTimescalePlot):
+    """
+    .. deprecated:: 0.8
+        Use :class:`xplt.SpillQualityTimescalePlot` instead.
+    """
+
+    pass
+
+
+from . import hooks as _hooks
+
+_hooks.try_register_hooks()
+
+import matplotlib.style as _mpl_style
+
+
+def apply_style():
+    """Apply xplt's matplotlib style sheet and update rcParams"""
+    try:
+        _mpl_style.use("xplt.xplt")
+    except IOError:
+        import matplotlib, shutil, os
+
+        # for matplotlib versions prior to 3.7: copy the style file to the config directory
+        dir = os.path.join(matplotlib.get_configdir(), "stylelib")
+        os.makedirs(dir, exist_ok=True)
+        print("Installing xplt style in", dir)
+        shutil.copy(os.path.join(os.path.dirname(__file__), "xplt.mplstyle"), dir)
+        matplotlib.style.reload_library()
+        matplotlib.style.use("xplt")
+
+    # import pint.formatting as _pint_formatting
+    # _pint_formatting.format_default = "X"  # use explicit format instead!
