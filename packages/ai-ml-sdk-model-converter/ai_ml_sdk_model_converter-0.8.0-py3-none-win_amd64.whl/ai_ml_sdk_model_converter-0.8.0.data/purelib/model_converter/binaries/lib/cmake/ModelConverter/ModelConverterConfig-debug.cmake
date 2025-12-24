a7c1cb79770556d22db94c3b9476b13f99adcca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 17:07:14 +0000

    Add package ai-ml-sdk-model-converter/0.8.0/ai_ml_sdk_model_converter-0.8.0-py3-none-win_amd64.whl

---
+++ packages/ai-ml-sdk-model-converter/ai_ml_sdk_model_converter-0.8.0-py3-none-win_amd64.whl/ai_ml_sdk_model_converter-0.8.0.data/purelib/model_converter/binaries/lib/cmake/ModelConverter/ModelConverterConfig-debug.cmake
@@ -0,0 +1,18 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "ModelConverter::model-converter" for configuration "Debug"
+set_property(TARGET ModelConverter::model-converter APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(ModelConverter::model-converter PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/model-converter.exe"
+  )
+
+list(APPEND _cmake_import_check_targets ModelConverter::model-converter )
+list(APPEND _cmake_import_check_files_for_ModelConverter::model-converter "${_IMPORT_PREFIX}/bin/model-converter.exe" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
---
+++ packages/ai-ml-sdk-model-converter/ai_ml_sdk_model_converter-0.8.0-py3-none-win_amd64.whl/ai_ml_sdk_model_converter-0.8.0.data/purelib/model_converter/cli.py
@@ -0,0 +1,16 @@
+#
+# SPDX-FileCopyrightText: Copyright 2025 Arm Limited and/or its affiliates <open-source-office@arm.com>
+# SPDX-License-Identifier: Apache-2.0
+#
+import os
+import subprocess
+import sys
+
+
+def main():
+    binary_name = (
+        "model-converter.exe" if sys.platform.startswith("win") else "model-converter"
+    )
+    binary_path = os.path.join(os.path.dirname(__file__), "binaries/bin", binary_name)
+    result = subprocess.run([binary_path] + sys.argv[1:])
+    return result.returncode
