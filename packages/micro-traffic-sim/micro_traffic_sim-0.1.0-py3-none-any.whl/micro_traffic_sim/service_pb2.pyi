86288a8f3ca007a158736740dddd99060091c89a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 16:06:50 +0000

    Add package micro-traffic-sim/0.1.0/micro_traffic_sim-0.1.0-py3-none-any.whl

---
+++ packages/micro-traffic-sim/micro_traffic_sim-0.1.0-py3-none-any.whl/micro_traffic_sim/service_pb2.pyi
@@ -0,0 +1,11 @@
+import uuid_pb2 as _uuid_pb2
+import session_pb2 as _session_pb2
+import cell_pb2 as _cell_pb2
+import trip_pb2 as _trip_pb2
+import step_pb2 as _step_pb2
+import tls_pb2 as _tls_pb2
+import conflict_zones_pb2 as _conflict_zones_pb2
+from google.protobuf import descriptor as _descriptor
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
