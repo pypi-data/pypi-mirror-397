226cb0eedc306b4331176773cfbd3416518399dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 17:07:02 +0000

    Add package powl/2.2.7/powl-2.2.7-py3-none-any.whl

---
+++ packages/powl/powl-2.2.7-py3-none-any.whl/powl/discovery/total_order_based/inductive/variants/decision_graph/cyclic_dg_cut.py
@@ -0,0 +1,44 @@
+from abc import ABC
+from itertools import combinations
+from typing import Any, Collection, Dict, List, Optional
+
+from pm4py.algo.discovery.inductive.cuts import utils as cut_util
+from pm4py.algo.discovery.inductive.cuts.abc import T
+from pm4py.algo.discovery.inductive.dtypes.im_ds import IMDataStructureUVCL
+
+from powl.discovery.total_order_based.inductive.variants.decision_graph.max_decision_graph_cut import (
+    MaximalDecisionGraphCut, MaximalDecisionGraphCutUVCL,
+)
+
+
+class CyclicDecisionGraphCut(MaximalDecisionGraphCut[T], ABC):
+    @classmethod
+    def holds(
+        cls, obj: T, parameters: Optional[Dict[str, Any]] = None
+    ) -> Optional[List[Any]]:
+
+        dfg = obj.dfg
+        alphabet = parameters["alphabet"]
+
+        groups = [frozenset([a]) for a in alphabet]
+
+        for a, b in combinations(alphabet, 2):
+            if (a, b) in dfg.graph and (b, a) in dfg.graph:
+                groups = cut_util.merge_groups_based_on_activities(a, b, groups)
+
+        if len(groups) < 2:
+            return None
+
+        return groups
+
+
+class CyclicDecisionGraphCutUVCL(CyclicDecisionGraphCut[IMDataStructureUVCL], ABC):
+    @classmethod
+    def project(
+        cls,
+        obj: IMDataStructureUVCL,
+        groups: List[Collection[Any]],
+        parameters: Optional[Dict[str, Any]] = None,
+    ) -> List[IMDataStructureUVCL]:
+
+        return MaximalDecisionGraphCutUVCL.project(obj, groups, parameters)
