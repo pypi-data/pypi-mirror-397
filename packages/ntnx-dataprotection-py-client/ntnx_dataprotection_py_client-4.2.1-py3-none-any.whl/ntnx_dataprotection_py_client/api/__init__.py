7455ce5e52e9377621e54fd72491fad3102df388
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 14:06:58 +0000

    Add package ntnx-dataprotection-py-client/4.2.1/ntnx_dataprotection_py_client-4.2.1-py3-none-any.whl

---
+++ packages/ntnx-dataprotection-py-client/ntnx_dataprotection_py_client-4.2.1-py3-none-any.whl/ntnx_dataprotection_py_client/api/__init__.py
@@ -0,0 +1,12 @@
+
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ntnx_dataprotection_py_client.api.consistency_groups_api import ConsistencyGroupsApi
+from ntnx_dataprotection_py_client.api.data_protection_cluster_capabilities_api import DataProtectionClusterCapabilitiesApi
+from ntnx_dataprotection_py_client.api.protected_resources_api import ProtectedResourcesApi
+from ntnx_dataprotection_py_client.api.recovery_plan_actions_api import RecoveryPlanActionsApi
+from ntnx_dataprotection_py_client.api.recovery_plan_jobs_api import RecoveryPlanJobsApi
+from ntnx_dataprotection_py_client.api.recovery_points_api import RecoveryPointsApi
