61790fa249c4f537a4c5c02ccc46ffb15ae7eb9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 14:07:27 +0000

    Add package iclick-auto/1.0.0/iclick_auto-1.0.0.tar.gz

---
+++ packages/iclick-auto/iclick_auto-1.0.0.tar.gz/iclick_auto-1.0.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="iclick-auto",
+    version="1.0.0",
+    author="iClick",
+    description="iClick Automation Tool Without Jailbreak Python Package.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Undefined-Token/iclick-python",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "websocket-client>=1.6.0",
+    ],
+    keywords=["IOS", "Automation", "Without Jailbreak"],
+)
+
