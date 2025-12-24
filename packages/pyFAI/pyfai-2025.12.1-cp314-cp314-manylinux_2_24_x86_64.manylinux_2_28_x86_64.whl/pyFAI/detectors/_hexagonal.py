dfd4b2be42fa84d24304c51f04db07d1c5c3f77e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 14:06:57 +0000

    Add package pyFAI/2025.12.1/pyfai-2025.12.1-cp314-cp314-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/pyFAI/pyfai-2025.12.1-cp314-cp314-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl/pyFAI/detectors/_hexagonal.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+#
+#    Project: Fast Azimuthal integration
+#             https://github.com/silx-kit/pyFAI
+#
+#    Copyright (C) 2022-2023 European Synchrotron Radiation Facility, Grenoble, France
+#
+#    Principal author:       Jérôme Kieffer (Jerome.Kieffer@ESRF.eu)
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#  .
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#  .
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#  THE SOFTWARE.
+
+"""
+Detectors with hexagonal pixel shape
+"""
+
+__author__ = "Jerome Kieffer"
+__contact__ = "Jerome.Kieffer@ESRF.eu"
+__license__ = "MIT"
+__copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
+__date__ = "04/11/2025"
+__status__ = "production"
+
+
+import numpy
+import logging
+from math import sqrt
+from ._common import Detector
+logger = logging.getLogger(__name__)
+
+
+class HexDetector(Detector):
+    """
+    Abstract class for regular hexagonal-pixel detectors
+
+    This is characterized by the pitch, distance between 2 pixels
+
+    Pixels are aligned horizontally with the provided pitch
+    Vertically, 2 raws are separated by pitch*sqrt(3)/2
+    Odd raws are offsetted horizontally by pitch/2
+    """
+    uniform_pixel = False # ensures we use the array of position !
+    IS_CONTIGUOUS = False
+    IS_FLAT = True
+    CORNERS = 6
+
+    @classmethod
+    def build_pixel_coordinates(cls, shape, pitch=1):
+        """Build the 4D array with pixel coordinates for a detector composed of hexagonal-pixels
+
+        :param shape: 2-tuple with size of the detector in number of pixels (y, x)
+        :param pitch: the distance between two pixels
+        :return: array with pixel coordinates
+        """
+        if len(shape) != 2:
+            raise RecursionError("detector shape is expected to be a 2-tuple")
+        ary = numpy.zeros(shape + (cls.CORNERS, 3), dtype=numpy.float32)
+        sqrt3 = sqrt(3.0)
+        h = 0.5*sqrt3
+        r = numpy.linspace(0, 2, 7, endpoint=True)[:-1] - 0.5
+        c = numpy.exp((0+1j)*r*numpy.pi) / sqrt3
+        c += complex(-c.real.min(), -c.imag.min())
+
+        px = numpy.atleast_3d(numpy.outer(numpy.ones(shape[0]), numpy.arange(shape[1])))
+        py = numpy.atleast_3d(numpy.outer(numpy.arange(shape[0]), numpy.ones(shape[1])))*h
+        cxy = px + complex(0, 1)*py
+        del px, py
+        cplx = cxy + c[None, None, :]
+        ary[..., 1] = cplx.imag
+        ary[..., 2] = cplx.real
+        ary[1::2, ... , 2] += 0.5
+        ary *= pitch
+        return ary
+
+    def __init__(self, pitch=None, pixel1=None, pixel2=None, max_shape=None, orientation=0):
+        if pitch:
+            pitch = float(pitch)
+            h = 0.5*pitch*sqrt(3.0)
+        else: #fallback on standard signature
+            pitch = pixel2
+            h = pixel1
+        super().__init__(pixel1=h, pixel2=pitch, max_shape=max_shape, orientation=orientation)
+        self.set_pixel_corners(self.build_pixel_coordinates(self.max_shape, self.pitch))
+
+    @property
+    def pitch(self):
+        "This is the distance between 2 pixel in the hexagonal pattern"
+        return self.pixel2
+
+    def __repr__(self):
+        return f"Hexagonal-pixel detector {self.name}\t Pitch= {self.pitch:.3e} m"
+
+
+class Pixirad1(HexDetector):
+    MAX_SHAPE = (476, 512)  # max size of the detector
+    MANUFACTURER = "Pixirad"
+    aliases = ["Pixirad-1"]
+    def __init__(self, pitch=60e-6, pixel1=None, pixel2=None, max_shape=None, orientation=0):
+        super().__init__(pitch=pitch, pixel1=pixel1, pixel2=pixel2,
+                             max_shape=max_shape, orientation=orientation)
+
+
+class Pixirad2(HexDetector):
+    MAX_SHAPE = (476, 1024)  # max size of the detector
+    MANUFACTURER = "Pixirad"
+    aliases = ["Pixirad-2"]
+    def __init__(self, pitch=60e-6, pixel1=None, pixel2=None, max_shape=None, orientation=0):
+        super().__init__(pitch=pitch, pixel1=pixel1, pixel2=pixel2,
+                             max_shape=max_shape, orientation=orientation)
+
+
+class Pixirad4(HexDetector):
+    MAX_SHAPE = (476, 2048)  # max size of the detector
+    MANUFACTURER = "Pixirad"
+    aliases = ["Pixirad-4"]
+    def __init__(self, pitch=60e-6, pixel1=None, pixel2=None, max_shape=None, orientation=0):
+        super().__init__(pitch=pitch, pixel1=pixel1, pixel2=pixel2,
+                             max_shape=max_shape, orientation=orientation)
+
+
+class Pixirad8(HexDetector):
+    MAX_SHAPE = (476, 4096)  # max size of the detector
+    MANUFACTURER = "Pixirad"
+    aliases = ["Pixirad-8"]
+    def __init__(self, pitch=60e-6, pixel1=None, pixel2=None, max_shape=None, orientation=0):
+        super().__init__(pitch=pitch, pixel1=pixel1, pixel2=pixel2,
+                             max_shape=max_shape, orientation=orientation)
---
+++ packages/pyFAI/pyfai-2025.12.1-cp314-cp314-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl/pyFAI/test/test_blob_detection.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# coding: utf-8
+#
+#    Project: Azimuthal integration
+#             https://github.com/silx-kit/pyFAI
+#
+#    Copyright (C) 2015-2025 European Synchrotron Radiation Facility, Grenoble, France
+#
+#    Principal author:       Jérôme Kieffer (Jerome.Kieffer@ESRF.eu)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+"""Test suite for blob detection cython accelerated code"""
+
+__author__ = "Jérôme Kieffer"
+__contact__ = "Jerome.Kieffer@esrf.fr"
+__license__ = "MIT"
+__copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
+__date__ = "10/10/2025"
+
+import unittest
+import numpy
+import logging
+from ..detectors import detector_factory
+from ..integrator.azimuthal import AzimuthalIntegrator
+from ..blob_detection import BlobDetection, local_max
+from ..ext import _blob
+from ..ext import morphology
+from scipy import ndimage
+logger = logging.getLogger(__name__)
+
+def image_test_rings():
+    rings = 10
+    mod = 50
+    detector = detector_factory("Titan")
+    sigma = detector.pixel1 * 4
+    shape = detector.max_shape
+    ai = AzimuthalIntegrator(detector=detector)
+    ai.setFit2D(1000, 1000, 1000)
+    r = ai.rArray(shape)
+    r_max = r.max()
+    chi = ai.chiArray(shape)
+    img = numpy.zeros(shape)
+    modulation = (1 + numpy.sin(5 * r + chi * mod))
+    for radius in numpy.linspace(0, r_max, rings):
+        img += numpy.exp(-(r - radius) ** 2 / (2 * (sigma * sigma)))
+    return img * modulation
+
+
+class TestBlobDetection(unittest.TestCase):
+    img = None
+
+    def setUp(self):
+        if self.img is None:
+            self.img = image_test_rings()
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+        self.img = None
+
+    def test_local_max(self):
+        bd = BlobDetection(self.img)
+        bd._one_octave(shrink=False, refine=False, n_5=False)
+        self.assertTrue(numpy.all(_blob.local_max(bd.dogs, bd.cur_mask, False) ==
+                                      local_max(bd.dogs, bd.cur_mask, False)), "max test, 3x3x3")
+        self.assertTrue(numpy.all(_blob.local_max(bd.dogs, bd.cur_mask, True) ==
+                                      local_max(bd.dogs, bd.cur_mask, True)), "max test, 3x5x5")
+
+class TestMorphology(unittest.TestCase):
+    shape = (101, 103)
+    img = None
+
+    @classmethod
+    def setUpClass(cls):
+        yc = cls.shape[0] // 2
+        xc = cls.shape[0] // 2
+        y, x = numpy.ogrid[:cls.shape[0],:cls.shape[1]]
+        r = 0.4 * min(cls.shape)
+        r2 = r * r
+        y -= yc
+        x -= xc
+        cls.img = (x * x + y * y < r2).astype(numpy.int8)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.img = None
+
+    def test_morphology(self):
+        ref_d = ndimage.binary_dilation(self.img.astype(bool)).astype("int8")
+        res_d = morphology.binary_dilation(self.img)
+        self.assertTrue(numpy.allclose(ref_d, res_d), "binary dilation")
+        res_e = morphology.binary_erosion(res_d)
+        self.assertTrue(numpy.allclose(self.img, res_e), "binary erosion")
+
+
+def suite():
+    loader = unittest.defaultTestLoader.loadTestsFromTestCase
+    testsuite = unittest.TestSuite()
+    testsuite.addTest(loader(TestBlobDetection))
+    testsuite.addTest(loader(TestMorphology))
+    return testsuite
+
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
