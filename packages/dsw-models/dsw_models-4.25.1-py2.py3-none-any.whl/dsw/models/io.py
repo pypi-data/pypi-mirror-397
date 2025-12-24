d3d0922a47dfe1add45c76ff4f3f366708554bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 15:08:17 +0000

    Add package dsw-models/4.25.1/dsw_models-4.25.1-py2.py3-none-any.whl

---
+++ packages/dsw-models/dsw_models-4.25.1-py2.py3-none-any.whl/dsw/models/io.py
@@ -0,0 +1,10 @@
+import json
+import typing
+
+
+class WizardJSONEncoder(json.JSONEncoder):
+
+    def default(self, o: typing.Any) -> typing.Any:
+        if hasattr(o, 'to_dict') and callable(o.to_dict):
+            return o.to_dict()
+        return super().default(o)
