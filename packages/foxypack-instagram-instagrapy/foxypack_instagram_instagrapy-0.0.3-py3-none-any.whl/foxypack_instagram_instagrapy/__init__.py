aa51d8a86c802a0d730596d22643340fb214df87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 15:07:08 +0000

    Add package foxypack-instagram-instagrapy/0.0.3/foxypack_instagram_instagrapy-0.0.3-py3-none-any.whl

---
+++ packages/foxypack-instagram-instagrapy/foxypack_instagram_instagrapy-0.0.3-py3-none-any.whl/foxypack_instagram_instagrapy/__init__.py
@@ -0,0 +1,7 @@
+from foxypack_instagram_instagrapy.engine import (
+    InstagramAccount,
+    FoxyInstagramAnalysis,
+    FoxyInstagramStat,
+)
+
+__all__ = [InstagramAccount, FoxyInstagramAnalysis, FoxyInstagramStat]
