7135dfc420234999dccbc89bd58eb8fa83c365fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 15:08:03 +0000

    Add package titiler-core/1.0.2/titiler_core-1.0.2-py3-none-any.whl

---
+++ packages/titiler-core/titiler_core-1.0.2-py3-none-any.whl/titiler/core/algorithm/index.py
@@ -0,0 +1,38 @@
+"""titiler.core.algorithm Normalized Index."""
+
+from collections.abc import Sequence
+
+import numpy
+from rio_tiler.models import ImageData
+
+from titiler.core.algorithm.base import BaseAlgorithm
+
+__all__ = ["NormalizedIndex"]
+
+
+class NormalizedIndex(BaseAlgorithm):
+    """Normalized Difference Index."""
+
+    title: str = "Normalized Difference Index"
+    description: str = "Compute normalized difference index from two bands."
+
+    # metadata
+    input_nbands: int = 2
+    output_nbands: int = 1
+    output_dtype: str = "float32"
+    output_min: Sequence[float] = [-1.0]
+    output_max: Sequence[float] = [1.0]
+
+    def __call__(self, img: ImageData) -> ImageData:
+        """Normalized difference."""
+        b1 = img.array[0].astype("float32")
+        b2 = img.array[1].astype("float32")
+        arr = numpy.ma.MaskedArray((b2 - b1) / (b2 + b1), dtype=self.output_dtype)
+        bnames = img.band_names
+        return ImageData(
+            arr,
+            assets=img.assets,
+            crs=img.crs,
+            bounds=img.bounds,
+            band_names=[f"({bnames[1]} - {bnames[0]}) / ({bnames[1]} + {bnames[0]})"],
+        )
