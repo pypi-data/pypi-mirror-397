8398111e3806f1819286b99306fd659229bc22b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 17:06:55 +0000

    Add package typer/0.20.1/typer-0.20.1.tar.gz

---
+++ packages/typer/typer-0.20.1.tar.gz/typer-0.20.1/docs_src/arguments/help/tutorial004_an.py
@@ -0,0 +1,20 @@
+import typer
+from typing_extensions import Annotated
+
+app = typer.Typer()
+
+
+@app.command()
+def main(
+    name: Annotated[
+        str, typer.Argument(help="Who to greet", show_default=False)
+    ] = "World",
+):
+    """
+    Say hi to NAME very gently, like Dirk.
+    """
+    print(f"Hello {name}")
+
+
+if __name__ == "__main__":
+    app()
---
+++ packages/typer/typer-0.20.1.tar.gz/typer-0.20.1/docs_src/arguments/help/tutorial008_an.py
@@ -0,0 +1,16 @@
+import typer
+from typing_extensions import Annotated
+
+app = typer.Typer()
+
+
+@app.command()
+def main(name: Annotated[str, typer.Argument(hidden=True)] = "World"):
+    """
+    Say hi to NAME very gently, like Dirk.
+    """
+    print(f"Hello {name}")
+
+
+if __name__ == "__main__":
+    app()
