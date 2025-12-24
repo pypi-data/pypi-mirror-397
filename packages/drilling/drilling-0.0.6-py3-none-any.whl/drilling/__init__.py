2a5b24b3c51e26f071810181b1fcaf75c3215ae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 14:06:51 +0000

    Add package drilling/0.0.6/drilling-0.0.6-py3-none-any.whl

---
+++ packages/drilling/drilling-0.0.6-py3-none-any.whl/drilling/__init__.py
@@ -0,0 +1,3 @@
+# src/minha_lib_top/__init__.py
+from drilling.libs import *
+from drilling.conn import Connect
---
+++ packages/drilling/drilling-0.0.6-py3-none-any.whl/drilling/libs.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# SQLAlchemy
+from sqlalchemy import (
+    create_engine, MetaData, Table, Column,
+    String, DateTime, Integer, Boolean, Time, Date, Numeric, inspect, insert, text
+)
