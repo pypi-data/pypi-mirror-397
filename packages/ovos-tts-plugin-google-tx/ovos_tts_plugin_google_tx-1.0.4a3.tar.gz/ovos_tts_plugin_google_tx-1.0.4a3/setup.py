6768495586939e668b5f8b6314da330d233c76d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 16:06:43 +0000

    Add package ovos-tts-plugin-google-tx/1.0.4a3/ovos_tts_plugin_google_tx-1.0.4a3.tar.gz

---
+++ packages/ovos-tts-plugin-google-tx/ovos_tts_plugin_google_tx-1.0.4a3.tar.gz/ovos_tts_plugin_google_tx-1.0.4a3/setup.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+import os
+
+from setuptools import setup
+
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_version():
+    """ Find the version of the package"""
+    version = None
+    version_file = os.path.join(BASEDIR, 'ovos_tts_plugin_google_tx', 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if alpha and int(alpha) > 0:
+        version += f"a{alpha}"
+    return version
+
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        if 'MYCROFT_LOOSE_REQUIREMENTS' in os.environ:
+            print('USING LOOSE REQUIREMENTS!')
+            requirements = [r.replace('==', '>=').replace('~=', '>=') for r in requirements]
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
+
+
+PLUGIN_ENTRY_POINT = 'ovos-tts-plugin-google-tx = ovos_tts_plugin_google_tx:GoogleTranslateTTS'
+
+
+setup(
+    name='ovos-tts-plugin-google-tx',
+    version=get_version(),
+    description='google translate tts plugin for OVOS',
+    url='https://github.com/OpenVoiceOS/ovos-tts-plugin-google-tx',
+    author='JarbasAi',
+    author_email='jarbasai@mailfence.com',
+    license='Apache-2.0',
+    packages=['ovos_tts_plugin_google_tx'],
+    install_requires=required("requirements.txt"),
+    zip_safe=True,
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Text Processing :: Linguistic",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords='mycroft OpenVoiceOS OVOS google translate plugin tts',
+    entry_points={'mycroft.plugin.tts': PLUGIN_ENTRY_POINT}
+)
