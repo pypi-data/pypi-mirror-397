ca7e63c8cb31460544f0898c85ecfebf0a36e27c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 16:07:51 +0000

    Add package wry-py/0.1.5/wry_py-0.1.5-cp311-cp311-win_amd64.whl

---
+++ packages/wry-py/wry_py-0.1.5-cp311-cp311-win_amd64.whl/wry_py/__init__.py
@@ -0,0 +1,14 @@
+from .wry_py import Element, ElementBuilder, UiWindow, div, text, button, input, image
+from .app import AppBase
+
+__all__ = [
+    "Element",
+    "ElementBuilder",
+    "UiWindow",
+    "AppBase",
+    "div",
+    "text",
+    "button",
+    "input",
+    "image",
+]
