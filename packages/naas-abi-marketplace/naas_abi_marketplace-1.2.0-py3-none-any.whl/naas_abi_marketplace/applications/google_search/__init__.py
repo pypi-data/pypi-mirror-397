e7f646685e5d312dfcc6a8432e4ce9f49b36c8a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 15:08:30 +0000

    Add package naas-abi-marketplace/1.2.0/naas_abi_marketplace-1.2.0-py3-none-any.whl

---
+++ packages/naas-abi-marketplace/naas_abi_marketplace-1.2.0-py3-none-any.whl/naas_abi_marketplace/applications/google_search/__init__.py
@@ -0,0 +1,33 @@
+from naas_abi_core.module.Module import (
+    BaseModule,
+    ModuleConfiguration,
+    ModuleDependencies,
+)
+from naas_abi_core.services.object_storage.ObjectStorageService import (
+    ObjectStorageService,
+)
+from naas_abi_core.services.triple_store.TripleStoreService import TripleStoreService
+
+
+class ABIModule(BaseModule):
+    dependencies: ModuleDependencies = ModuleDependencies(
+        modules=[
+            "naas_abi_marketplace.ai.gemini",
+        ],
+        services=[ObjectStorageService, TripleStoreService],
+    )
+
+    class Configuration(ModuleConfiguration):
+        """
+        Configuration example:
+
+        module: naas_abi_marketplace.applications.google_search
+        enabled: true
+        config:
+            datastore_path: "google_search"
+            google_custom_search_api_key: "{{ secret.GOOGLE_CUSTOM_SEARCH_API_KEY }}"
+            google_custom_search_engine_id: "{{ secret.GOOGLE_CUSTOM_SEARCH_ENGINE_ID }}"
+        """
+        datastore_path: str
+        google_custom_search_api_key: str
+        google_custom_search_engine_id: str
