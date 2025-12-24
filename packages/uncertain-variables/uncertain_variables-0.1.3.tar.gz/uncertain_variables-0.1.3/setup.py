4e765705c7015dfc588e5e97b87e27458ab626f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 16:07:18 +0000

    Add package uncertain-variables/0.1.3/uncertain_variables-0.1.3.tar.gz

---
+++ packages/uncertain-variables/uncertain_variables-0.1.3.tar.gz/uncertain_variables-0.1.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='uncertain_variables',
+    version='0.1.3',
+    author='András Urbanics, Bence Popovics, Emese Vastag, Elmar Zander, Noémi Friedman',
+    author_email='popbence@hun-ren.sztaki.hu',
+    description='Defining and handling variable sets with probability distributions for surrogate modelling and uncertainty quantification applications',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/TRACE-Structures/uncertain_variables/',
+    packages=find_packages(),
+    py_modules=['uncertain_variables'],
+    install_requires=[
+        'numpy',
+        'scikit-learn',
+        'scipy',
+        'matplotlib',
+        'SALib',
+        'pint'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+    ],
+)
