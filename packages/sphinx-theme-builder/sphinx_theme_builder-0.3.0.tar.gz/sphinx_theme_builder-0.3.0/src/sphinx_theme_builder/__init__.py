94bb732105a44a9100a4569a5cdc618f57857c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 17:06:53 +0000

    Add package sphinx-theme-builder/0.3.0/sphinx_theme_builder-0.3.0.tar.gz

---
+++ packages/sphinx-theme-builder/sphinx_theme_builder-0.3.0.tar.gz/sphinx_theme_builder-0.3.0/src/sphinx_theme_builder/__init__.py
@@ -0,0 +1,126 @@
+"""A tool for authoring Sphinx themes with a simple (opinionated) workflow.
+
+This module also serves as the build-backend API for this project.
+"""
+
+__version__ = "0.3.0"
+
+import contextlib
+import sys
+import tempfile
+from collections.abc import Iterator
+from pathlib import Path
+
+import rich
+
+from ._internal.distributions import generate_metadata as _generate_metadata
+from ._internal.distributions import generate_source_distribution as _generate_sdist
+from ._internal.distributions import generate_wheel_distribution as _generate_wheel
+from ._internal.errors import STBError
+from ._internal.project import Project
+
+
+@contextlib.contextmanager
+def _ensure_has_metadata(
+    project: Project, metadata_directory: str | None
+) -> Iterator[Path]:
+    """Ensure that the body of a with statement, is executed with metadata generated."""
+    if metadata_directory is not None:
+        yield Path(metadata_directory)
+    else:
+        # The build frontend didn't generate the metadata. Generate it in a temporary
+        # directory, and provide that. Cleans up when the context is exited.
+        with tempfile.TemporaryDirectory() as dirname:
+            metadata_dirname = Path(dirname)
+            metadata_basename = _generate_metadata(
+                project,
+                destination=Path(dirname),
+            )
+            yield metadata_dirname / metadata_basename
+
+
+@contextlib.contextmanager
+def _clean_error_presentation() -> Iterator[None]:
+    try:
+        yield
+    except STBError as error:
+        rich.get_console().print(error, soft_wrap=True)
+        sys.exit(1)
+
+
+#
+# Source Distributions
+#
+def build_sdist(
+    sdist_directory: str,
+    config_settings: dict[str, str] | None = None,
+) -> str:
+    """Generate a source distribution and place it into `sdist_directory`.
+
+    https://www.python.org/dev/peps/pep-0517/#build-sdist
+    """
+    with _clean_error_presentation():
+        project = Project.from_cwd()
+        return _generate_sdist(project, destination=Path(sdist_directory))
+
+
+#
+# Wheel Distributions
+#
+def prepare_metadata_for_build_wheel(
+    metadata_directory: str,
+    config_settings: dict[str, str] | None = None,
+) -> str:
+    """Generate the metadata (.dist-info) and place it in `metadata_directory`.
+
+    https://www.python.org/dev/peps/pep-0517/#prepare-metadata-for-build-wheel
+    """
+    with _clean_error_presentation():
+        project = Project.from_cwd()
+        return _generate_metadata(project, destination=Path(metadata_directory))
+
+
+def build_wheel(
+    wheel_directory: str,
+    config_settings: dict[str, str] | None = None,
+    metadata_directory: str | None = None,
+) -> str:
+    """Generate a wheel distribution and place it into `wheel_directory`.
+
+    https://www.python.org/dev/peps/pep-0517/#build-wheel
+    """
+    with _clean_error_presentation():
+        project = Project.from_cwd()
+        with _ensure_has_metadata(project, metadata_directory) as metadata_path:
+            return _generate_wheel(
+                project,
+                destination=Path(wheel_directory),
+                metadata_directory=metadata_path,
+                editable=False,
+            )
+
+
+#
+# Editable installs
+#
+prepare_metadata_for_build_editable = prepare_metadata_for_build_wheel
+
+
+def build_editable(
+    wheel_directory: str,
+    config_settings: dict[str, str] | None = None,
+    metadata_directory: str | None = None,
+) -> str:
+    """Generate an editable wheel distribution and place it into `wheel_directory`.
+
+    https://www.python.org/dev/peps/pep-0660/#build-editable
+    """
+    with _clean_error_presentation():
+        project = Project.from_cwd()
+        with _ensure_has_metadata(project, metadata_directory) as metadata_path:
+            return _generate_wheel(
+                project,
+                destination=Path(wheel_directory),
+                metadata_directory=metadata_path,
+                editable=True,
+            )
