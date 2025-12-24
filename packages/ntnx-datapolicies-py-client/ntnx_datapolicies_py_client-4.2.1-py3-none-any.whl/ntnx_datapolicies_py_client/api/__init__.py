a2da60e09bb9536d8bbd8cfdbe88dfeef8787ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 14:06:57 +0000

    Add package ntnx-datapolicies-py-client/4.2.1/ntnx_datapolicies_py_client-4.2.1-py3-none-any.whl

---
+++ packages/ntnx-datapolicies-py-client/ntnx_datapolicies_py_client-4.2.1-py3-none-any.whl/ntnx_datapolicies_py_client/api/__init__.py
@@ -0,0 +1,10 @@
+
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ntnx_datapolicies_py_client.api.entity_sync_policies_api import EntitySyncPoliciesApi
+from ntnx_datapolicies_py_client.api.protection_policies_api import ProtectionPoliciesApi
+from ntnx_datapolicies_py_client.api.recovery_plans_api import RecoveryPlansApi
+from ntnx_datapolicies_py_client.api.storage_policies_api import StoragePoliciesApi
