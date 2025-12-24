cc2dd71f71e0b024feaf5b46c05bdad7068fd299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 17:07:00 +0000

    Add package fluidimage/0.5.5/fluidimage-0.5.5-cp314-cp314t-macosx_11_0_arm64.whl

---
+++ packages/fluidimage/fluidimage-0.5.5-cp314-cp314t-macosx_11_0_arm64.whl/fluidimage/image2image.py
@@ -0,0 +1,108 @@
+""" "Image to image" processing
+==============================
+
+.. autofunction:: apply_im2im_filter
+
+.. autofunction:: im2im_func_example
+
+.. autoclass:: Im2ImExample
+   :members:
+
+.. autoclass:: Work
+   :members:
+   :private-members:
+
+.. autoclass:: Topology
+   :members:
+   :private-members:
+
+"""
+
+from pathlib import Path
+
+import numpy as np
+
+from fluiddyn.util.serieofarrays import SerieOfArraysFromFiles
+from fluidimage.topologies.image2image import Topology, TopologyImage2Image
+from fluidimage.works.image2image import (
+    Work,
+    WorkImage2Image,
+    init_im2im_function,
+)
+
+
+def im2im_func_example(tuple_image_path):
+    """Process one image
+
+    This is just an example to show how to write functions which can be plugged
+    to the class
+    :class:`fluidimage.topologies.image2image.TopologyImage2Image`.
+
+    """
+    path, image = tuple_image_path
+    # the processing can be adjusted depending on the value of the path.
+    print("process file:\n" + path)
+    image_out = np.round(image * (255 / image.max())).astype(np.uint8)
+    return image_out
+
+
+class Im2ImExample:
+    """Process one image
+
+    This is just an example to show how to write classes which can be plugged
+    to the class
+    :class:`fluidimage.topologies.image2image.TopologyImage2Image`.
+
+    """
+
+    def __init__(self, arg0, arg1):
+        print("init with arguments:", arg0, arg1)
+        self.arg0 = arg0
+        self.arg1 = arg1
+
+    # time consuming tasks can be done here
+
+    def calcul(self, tuple_image_path):
+        """Method processing one image"""
+        print(
+            "calcul with arguments (unused in the example):", self.arg0, self.arg1
+        )
+        return im2im_func_example(tuple_image_path)
+
+
+def apply_im2im_filter(serie, im2im=None, args_init=()):
+    """Apply an image-to-image filter to a serie of images.
+
+    Parameters
+    ----------
+
+    serie : :class:`fluiddyn.util.serieofarrays.SerieOfArraysFromFiles`
+
+    im2im : Optional[str]
+
+    args_init : tuple
+
+    """
+    if im2im is None:
+        return serie
+
+    _, im2im_func = init_im2im_function(im2im, args_init)
+
+    if not isinstance(serie, SerieOfArraysFromFiles):
+        raise NotImplementedError
+
+    result = {"names": [], "arrays": [], "paths": []}
+
+    arrays = serie.get_arrays()
+    paths = serie.get_path_arrays()
+
+    for arr, path in zip(arrays, paths):
+        new_arr = im2im_func((path, arr))
+        result["arrays"].append(new_arr)
+        result["paths"].append(path)
+        result["names"].append(Path(path).name)
+
+    return result
+
+
+__all__ = ["WorkImage2Image", "Work", "TopologyImage2Image", "Topology"]
---
+++ packages/fluidimage/fluidimage-0.5.5-cp314-cp314t-macosx_11_0_arm64.whl/fluidimage/topologies/test_example.py
@@ -0,0 +1,77 @@
+import sys
+
+import pytest
+
+from fluidimage.executors import supported_multi_executors
+from fluidimage.topologies.example import TopologyExample
+
+executors = [
+    "exec_sequential",
+    "exec_async_sequential",
+    "exec_async",
+    "exec_async_multi",
+    "exec_async_servers_threading",
+]
+
+if sys.platform != "darwin":
+    # too much ConnectionRefusedError on Github Actions
+    executors.extend(
+        [
+            "exec_async_servers",
+        ]
+    )
+
+executors.extend(supported_multi_executors)
+
+# tmp: TopologyExample doesn't have a Splitter
+executors.remove("multi_exec_subproc")
+executors.remove("multi_exec_subproc_sync")
+
+
+@pytest.mark.usefixtures("close_plt_figs")
+@pytest.mark.parametrize("executor", executors)
+def test_topo_example(tmp_path_karman, executor):
+
+    path_input = tmp_path_karman
+
+    params = TopologyExample.create_default_params()
+    params["path_input"] = path_input
+
+    path_dir_result = path_input.parent / f"Images.{executor}"
+    params["path_dir_result"] = path_dir_result
+
+    topology = TopologyExample(params, logging_level="info")
+    topology.compute(executor, nb_max_workers=2)
+
+    # there is a logging problem with this class but we don't mind.
+    if executor != "exec_async_servers_threading":
+        log = topology.read_log_data()
+        assert (
+            log.topology_name == "fluidimage.topologies.example.TopologyExample"
+        )
+
+        if executor != "exec_sequential":
+            assert log.nb_max_workers == 2
+
+        if [
+            len(log.durations[key])
+            for key in ("read_array", "cpu1", "cpu2", "save")
+        ] != [4, 3, 2, 2]:
+            print("Issue with this log file:")
+            print(log.path_log_file.read_text())
+
+        log.plot_durations()
+        log.plot_memory()
+        log.plot_nb_workers()
+
+        # does not always work for "exec_async_multi" (?)
+        if executor != "exec_async_multi":
+            assert len(log.durations["read_array"]) == 4
+            assert len(log.durations["cpu1"]) == 3
+            assert len(log.durations["cpu2"]) == 2
+            assert len(log.durations["save"]) == 2
+
+    path_files = tuple(path_dir_result.glob("Karman*"))
+
+    assert len(path_files) > 0, "No files saved"
+    assert len(path_files) == 2, "Bad number of saved files"
