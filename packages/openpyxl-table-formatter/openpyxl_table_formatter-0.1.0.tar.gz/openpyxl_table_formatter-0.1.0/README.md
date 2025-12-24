680a2c98ad15db707a9717d6ca8a857ed042e769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 15:07:31 +0000

    Add package openpyxl-table-formatter/0.1.0/openpyxl_table_formatter-0.1.0.tar.gz

---
+++ packages/openpyxl-table-formatter/openpyxl_table_formatter-0.1.0.tar.gz/openpyxl_table_formatter-0.1.0/README.md
@@ -0,0 +1,16 @@
+# openpyxl_table_formatter
+
+A simple package to help export pandas DataFrame to Excel files.
+
+Please note this package is still in early development and only 3 themes are available:
+- Medium 1 - grey accent
+- Medium 2 - blue accent
+- Medium 3 - orange accent
+
+## Instalation
+
+- with pip:
+`pip install openpyxl-table-formatter`
+
+- with uv:
+`uv add openpyxl-table-formatter`
---
+++ packages/openpyxl-table-formatter/openpyxl_table_formatter-0.1.0.tar.gz/openpyxl_table_formatter-0.1.0/src/openpyxl_table_formatter/__init__.py
@@ -0,0 +1,172 @@
+import pandas as pd
+from openpyxl.styles import Border, Font, PatternFill, Side
+from openpyxl.utils.dataframe import dataframe_to_rows
+from openpyxl.worksheet.worksheet import Worksheet
+
+
+def main() -> None:
+    pass
+
+
+def add_formatted_table_to_worksheet(
+    table: pd.DataFrame,
+    worksheet: Worksheet | None,
+    start_cell: str | None = None,
+    index: bool = True,
+    header: bool = True,
+    style: str = "Medium 2",
+    columns_formats: list[str] | None = None,
+) -> None:
+    """Appends a pandas DataFrame to file using one of default MS Office formattings.
+
+    Parameters
+    ----------
+    table : pandas.DataFrame
+        DataFrame to be added to the worksheet.
+    worksheet : openpyxl.Worksheet
+        Worksheet to use.
+    worksheet : str or None, default: None
+        Cell from which appending starts. If not provided - table will be appended starting in the first blank row of column A.
+    index: bool, default: True
+        If index of DataFrame should be included.
+    header: bool, default: True
+        If header of DataFrame should be included.
+    style: str, default: "Medium 2"
+        Style of table, from the Office 365 standard templates.
+    columns_formats: list of str or None, default: None
+        List of formats for each column, should follow Office 365 format standard.
+    """
+    match style:
+        case "Medium 1":
+            header_fill_type = "solid"
+            header_fill_color = "FF000000"
+            header_font_color = "FFFFFFFF"
+            odd_row_fill_type = "solid"
+            odd_row_fill_color = "FFD9D9D9"
+            even_row_fill_type = None
+            even_row_fill_color = "00FFFFFF"
+            border_color = "FF000000"
+        case "Medium 2":
+            header_fill_type = "solid"
+            header_fill_color = "FF156082"
+            header_font_color = "FFFFFFFF"
+            odd_row_fill_type = "solid"
+            odd_row_fill_color = "FFC0E6F5"
+            even_row_fill_type = None
+            even_row_fill_color = "00FFFFFF"
+            border_color = "FF44B3E1"
+        case "Medium 3":
+            header_fill_type = "solid"
+            header_fill_color = "FFE97123"
+            header_font_color = "FFFFFFFF"
+            odd_row_fill_type = "solid"
+            odd_row_fill_color = "FFFBE2D5"
+            even_row_fill_type = None
+            even_row_fill_color = "00FFFFFF"
+            border_color = "FFF1A983"
+        case _:
+            header_fill_type = None
+            header_fill_color = "00FFFFFF"
+            header_font_color = "FF000000"
+            odd_row_fill_type = None
+            odd_row_fill_color = "00FFFFFF"
+            even_row_fill_type = None
+            even_row_fill_color = "00FFFFFF"
+            border_color = "FF000000"
+    if worksheet is not None:
+        if start_cell is not None:
+            start_row = worksheet[start_cell].row
+            start_column = worksheet[start_cell].column
+        else:
+            max_h: int = worksheet.max_row
+            if max_h == 1 and worksheet.cell(row=1, column=1).value is None:
+                start_row = 1
+                start_column = 1
+            else:
+                start_row = max_h + 1
+                start_column = 1
+        for row_count, dataframe_row in enumerate(
+            dataframe_to_rows(table, index=index, header=header)
+        ):
+            if dataframe_row == [None]:
+                continue
+            if header and row_count == 0:
+                for column_number, cell_value in enumerate(dataframe_row):
+                    current_cell = worksheet.cell(
+                        row=start_row, column=column_number + start_column
+                    )
+                    current_cell.value = cell_value
+                    current_cell.font = Font(
+                        name="Calibri", bold=True, color=header_font_color
+                    )
+                    current_cell.fill = PatternFill(
+                        fill_type=header_fill_type,
+                        start_color=header_fill_color,
+                        end_color=header_fill_color,
+                    )
+                    if column_number == 0:
+                        current_cell.border = Border(
+                            top=Side(style="thin", color=border_color),
+                            left=Side(style="thin", color=border_color),
+                            bottom=Side(style="thin", color=border_color),
+                        )
+                    elif column_number == (len(dataframe_row) - 1):
+                        current_cell.border = Border(
+                            top=Side(style="thin", color=border_color),
+                            right=Side(style="thin", color=border_color),
+                            bottom=Side(style="thin", color=border_color),
+                        )
+                    else:
+                        current_cell.border = Border(
+                            top=Side(style="thin", color=border_color),
+                            bottom=Side(style="thin", color=border_color),
+                        )
+            else:
+                if header:
+                    is_odd_row = row_count % 2 == 1
+                    current_row = start_row + row_count - 1
+                else:
+                    is_odd_row = row_count % 2 == 0
+                    current_row = start_row + row_count
+                for column_number, cell_value in enumerate(dataframe_row):
+                    current_cell = worksheet.cell(
+                        row=current_row, column=column_number + start_column
+                    )
+                    current_cell.value = cell_value
+                    if (columns_formats is not None) and (
+                        len(columns_formats) == len(dataframe_row)
+                    ):
+                        current_cell.number_format = columns_formats[column_number]
+                    if column_number == 0:
+                        current_cell.border = Border(
+                            top=Side(style="thin", color=border_color),
+                            left=Side(style="thin", color=border_color),
+                            bottom=Side(style="thin", color=border_color),
+                        )
+                    elif column_number == (len(dataframe_row) - 1):
+                        current_cell.border = Border(
+                            top=Side(style="thin", color=border_color),
+                            right=Side(style="thin", color=border_color),
+                            bottom=Side(style="thin", color=border_color),
+                        )
+                    else:
+                        current_cell.border = Border(
+                            top=Side(style="thin", color=border_color),
+                            bottom=Side(style="thin", color=border_color),
+                        )
+                    if is_odd_row:
+                        current_cell.fill = PatternFill(
+                            fill_type=odd_row_fill_type,
+                            start_color=odd_row_fill_color,
+                            end_color=odd_row_fill_color,
+                        )
+                    else:
+                        current_cell.fill = PatternFill(
+                            fill_type=even_row_fill_type,
+                            start_color=even_row_fill_color,
+                            end_color=even_row_fill_color,
+                        )
+
+
+if __name__ == "__main__":
+    main()
