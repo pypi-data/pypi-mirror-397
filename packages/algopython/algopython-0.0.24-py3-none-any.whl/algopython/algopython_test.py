3912ad6dda0cc096407c16421c9d86eb67af6b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 14:07:01 +0000

    Add package algopython/0.0.24/algopython-0.0.24-py3-none-any.whl

---
+++ packages/algopython/algopython-0.0.24-py3-none-any.whl/algopython/algopython_test.py
@@ -0,0 +1,16 @@
+from __init__ import *
+
+algopython_init()
+
+move_smartivo_square(1,1)
+light(2,1,10,'blue')
+light(1,1,10,'red')
+light(2,1,10,'blue')
+move('A',1,10,1)
+move('B',1,10,1)
+# move('A',1,10,1)
+# hand_motion_detection()
+# light(1,1,10,'red')
+# light(2,1,10,'blue')
+
+algopython_exit()
