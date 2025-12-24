74a8b04a919f1a89c82262e5429931206e2266ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 14:07:03 +0000

    Add package healthsecure/0.2.0a1/healthsecure-0.2.0a1.tar.gz

---
+++ packages/healthsecure/healthsecure-0.2.0a1.tar.gz/healthsecure-0.2.0a1/healthsecure/__init__.py
@@ -0,0 +1,46 @@
+from .client import HealthSecureClient
+from .types import (
+    SignalPayload,
+    SignalResponse,
+    PolicyContext,
+    ExecutionContextV2,
+    ConfidenceBand,
+)
+from .exceptions import (
+    HealthSecureError,
+    AuthenticationError,
+    RateLimitError,
+    APIError
+)
+from .extract import (
+    extract_from_json,
+    extract_from_text,
+    extract_v2_json,
+    extract_v2_text,
+)
+from .helpers import build_v2_signal
+from .llm_wrapper import safe_llm_call
+
+__version__ = "0.2.0a1"
+
+__all__ = [
+    # v1 exports
+    "HealthSecureClient",
+    "SignalPayload",
+    "SignalResponse",
+    "HealthSecureError",
+    "AuthenticationError",
+    "RateLimitError",
+    "APIError",
+    "extract_from_json",
+    "extract_from_text",
+    "safe_llm_call",
+    # v2 exports
+    "extract_v2_json",
+    "extract_v2_text",
+    "build_v2_signal",
+    "PolicyContext",
+    "ExecutionContextV2",
+    "ConfidenceBand",
+]
+
