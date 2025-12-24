5553b2fb9e992ae87035f6c451c2fa71ad4047d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 16:07:29 +0000

    Add package fps_auth/0.9.6/fps_auth-0.9.6.tar.gz

---
+++ packages/fps_auth/fps_auth-0.9.6.tar.gz/fps_auth-0.9.6/src/fps_auth/config.py
@@ -0,0 +1,18 @@
+from uuid import uuid4
+
+from jupyverse_api.auth import AuthConfig
+
+
+class _AuthConfig(AuthConfig):
+    client_id: str = ""
+    client_secret: str = ""
+    redirect_uri: str = ""
+    # mode: Literal["noauth", "token", "user"] = "token"
+    mode: str = "token"
+    token: str = uuid4().hex
+    global_email: str = "guest@jupyter.com"
+    cookie_secure: bool = False  # FIXME: should default to True, and set to False for tests
+    clear_users: bool = False
+    test: bool = False
+    login_url: str | None = None
+    directory: str | None = None
---
+++ packages/fps_auth/fps_auth-0.9.6.tar.gz/fps_auth-0.9.6/src/fps_auth/main.py
@@ -0,0 +1,59 @@
+import structlog
+from fastapi_users.exceptions import UserAlreadyExists
+from fps import Module
+from jupyverse_api.app import App
+from jupyverse_api.auth import Auth, AuthConfig
+from jupyverse_api.frontend import FrontendConfig
+from jupyverse_api.main import QueryParams
+
+from .config import _AuthConfig
+from .routes import auth_factory
+
+log = structlog.get_logger()
+
+
+class AuthModule(Module):
+    def __init__(self, name: str, **kwargs):
+        super().__init__(name)
+        self.config = _AuthConfig(**kwargs)
+
+    async def prepare(self) -> None:
+        self.put(self.config, AuthConfig)
+
+        app = await self.get(App)
+        frontend_config = await self.get(FrontendConfig)
+
+        auth = auth_factory(app, self.config, frontend_config)
+        self.put(auth, Auth)
+
+        await auth.db.create_db_and_tables()
+
+        if self.config.test:
+            try:
+                await auth.create_user(
+                    username="admin@jupyter.com",
+                    email="admin@jupyter.com",
+                    password="jupyverse",
+                    permissions={"admin": ["read", "write"]},
+                )
+            except UserAlreadyExists:
+                pass
+
+        try:
+            await auth.create_user(
+                username=self.config.token,
+                email=self.config.global_email,
+                password="",
+                permissions={},
+            )
+        except UserAlreadyExists:
+            global_user = await auth.get_user_by_email(self.config.global_email)
+            await auth._update_user(
+                global_user,
+                username=self.config.token,
+                permissions={},
+            )
+
+        if self.config.mode == "token":
+            query_params = await self.get(QueryParams)
+            query_params.d["token"] = self.config.token
