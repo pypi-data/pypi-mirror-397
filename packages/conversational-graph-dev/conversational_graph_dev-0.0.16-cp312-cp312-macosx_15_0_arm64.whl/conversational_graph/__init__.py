9650c5133f37a33088a8e3a62fc38bf9c450600b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 14:06:59 +0000

    Add package conversational-graph-dev/0.0.16/conversational_graph_dev-0.0.16-cp312-cp312-macosx_15_0_arm64.whl

---
+++ packages/conversational-graph-dev/conversational_graph_dev-0.0.16-cp312-cp312-macosx_15_0_arm64.whl/conversational_graph/__init__.py
@@ -0,0 +1,3 @@
+from .factory import ConversationalGraph, PreDefinedTool,HttpToolRequest, ToolAuthDetails,ConversationalDataModel
+
+__all__ = ["ConversationalGraph", "PreDefinedTool","HttpToolRequest","ToolAuthDetails","ConversationalDataModel"]
