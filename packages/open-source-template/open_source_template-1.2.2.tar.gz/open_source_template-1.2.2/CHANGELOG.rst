c67e5f6cbd06eb1271fac99ed3b57c3b5d6701a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 14:06:51 +0000

    Add package open-source-template/1.2.2/open_source_template-1.2.2.tar.gz

---
+++ packages/open-source-template/open_source_template-1.2.2.tar.gz/open_source_template-1.2.2/CHANGELOG.rst
@@ -0,0 +1,24 @@
+=========
+Changelog
+=========
+
+Version 1.2.2
+-------------
+* Use global version switcher configuration
+
+
+Version 1.2.0
+-------------
+* Integrated multi-versioning feature in documentation pages
+
+
+Version 1.1.0
+-------------
+* Added `another_dummy_func` to `main.py` that converts an integer to a float.
+* Updated this Changelog
+
+
+Version 1.0.0
+----------------
+
+* Initial release.
---
+++ packages/open-source-template/open_source_template-1.2.2.tar.gz/open_source_template-1.2.2/open_source_template/main.py
@@ -0,0 +1,29 @@
+"""
+Copyright (c) 2025 MeteoSwiss, contributors listed in AUTHORS
+
+Distributed under the terms of the BSD 3-Clause License.
+
+SPDX-License-Identifier: BSD-3-Clause
+"""
+
+def dummy_func(input_value: float) -> int:
+    """
+    Dummy function.
+
+    :param input_value: Argument to the function.
+    :type input_value: float
+    :return: An integer.
+    :rtype: int
+    """
+    return int(input_value)
+
+def another_dummy_func(input_value: int) -> float:
+    """
+    Another dummy function.
+
+    :param input_value: Argument to the function.
+    :type input_value: int
+    :return: A float.
+    :rtype: float
+    """
+    return float(input_value)
