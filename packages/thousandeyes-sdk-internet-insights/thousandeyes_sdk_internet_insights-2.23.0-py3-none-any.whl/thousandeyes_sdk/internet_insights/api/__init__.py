d473558e868333adc7c286b3d11728efa86c25a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 16:06:58 +0000

    Add package thousandeyes-sdk-internet-insights/2.23.0/thousandeyes_sdk_internet_insights-2.23.0-py3-none-any.whl

---
+++ packages/thousandeyes-sdk-internet-insights/thousandeyes_sdk_internet_insights-2.23.0-py3-none-any.whl/thousandeyes_sdk/internet_insights/api/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+
+# import apis into api package
+from thousandeyes_sdk.internet_insights.api.internet_insights_catalog_providers_api import InternetInsightsCatalogProvidersApi
+from thousandeyes_sdk.internet_insights.api.internet_insights_outages_api import InternetInsightsOutagesApi
+
