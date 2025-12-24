cb30f7f26d7f367d968a9fe7ad6063536404b9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 15:07:09 +0000

    Add package templatest/0.12.0/templatest-0.12.0-py3-none-any.whl

---
+++ packages/templatest/templatest-0.12.0-py3-none-any.whl/templatest/__init__.py
@@ -0,0 +1,16 @@
+"""Templates for testing with strings."""
+
+from . import exceptions, templates, utils
+from ._abc import BaseTemplate
+from ._objects import Registered, Template
+from ._version import __version__
+
+__all__ = [
+    "__version__",
+    "BaseTemplate",
+    "exceptions",
+    "templates",
+    "utils",
+    "Template",
+    "Registered",
+]
---
+++ packages/templatest/templatest-0.12.0-py3-none-any.whl/templatest/_objects.py
@@ -0,0 +1,82 @@
+"""
+templatest._objects
+===================
+"""
+
+from __future__ import annotations
+
+import typing as _t
+
+from ._collections import MutableSequence as _MutableSequence
+
+
+class Template(_t.NamedTuple):
+    """Contains registered :class:`BaseTemplate` properties."""
+
+    #: The name of the inherited class, parsed for test ID.
+    name: str
+
+    #: Template to test.
+    template: str
+
+    #: Expected result.
+    expected: str
+
+
+class Registered(_MutableSequence[Template]):
+    """Mutable sequence of :class:`Template` named tuples.
+
+    :param args: Instantiate with any number of :class:`Template`
+        objects.
+    """
+
+    def __init__(self, *args: Template) -> None:
+        super().__init__()
+        self.extend(args)
+
+    def filtergroup(self, *prefix: str) -> Registered:
+        """Get new object excluding templates sharing a prefix.
+
+        :param prefix: Common prefix(s) to registered subclasses.
+        :return: New :class:`Registered` object containing
+            :class:`Template` objects that do not have the provided
+            prefix in their names.
+        """
+        return Registered(
+            *(x for x in self if not any(x.name.startswith(y) for y in prefix))
+        )
+
+    def getgroup(self, *prefix: str) -> Registered:
+        """Get new object containing templates sharing a prefix.
+
+        :param prefix: Common prefix(s) to registered subclasses.
+        :return: New :class:`Registered` object containing
+            :class:`Template` objects with common prefix to their names.
+        """
+        return Registered(
+            *(x for y in prefix for x in self if x.name.startswith(y))
+        )
+
+    def getids(self) -> _t.Tuple[str, ...]:
+        """Returns a tuple of all the names of the classes contained.
+
+        :return: A tuple of names of the classes within this sequence.
+        """
+        return tuple(i.name for i in self)
+
+    def getbyname(self, name: str) -> Template | None:
+        """Get a template by name if it exists.
+
+        :param name: Name assigned to template.
+        :return: :class:`Template` object.
+        """
+        return next((i for i in self if i.name == name), None)
+
+    def getindex(self, name: str) -> int | None:
+        """Get the index of a template by name if it exists.
+
+        :param name: Name assigned to template.
+        :return: Index of the template if it exists, else None.
+        """
+        template = self.getbyname(name)
+        return template if template is None else self.index(template)
