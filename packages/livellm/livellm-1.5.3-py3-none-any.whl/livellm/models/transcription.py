1cf293ef4cae6d2b136d183f24824f3c8c3cae91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 15:08:03 +0000

    Add package livellm/1.5.3/livellm-1.5.3-py3-none-any.whl

---
+++ packages/livellm/livellm-1.5.3-py3-none-any.whl/livellm/models/transcription.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel, Field, field_validator
+from livellm.models.audio.speak import SpeakMimeType
+from typing import Optional
+import base64
+
+class TranscriptionInitWsRequest(BaseModel):
+    provider_uid: str = Field(..., description="The provider uid")
+    model: str = Field(..., description="The model")
+    language: str = Field(default="auto", description="The language")
+    input_sample_rate: int = Field(default=24000, description="The input sample rate")
+    input_audio_format: SpeakMimeType = Field(default=SpeakMimeType.PCM, description="The input audio format (pcm, ulaw, alaw)")
+    gen_config: dict = Field(default={}, description="The generation configuration")
+
+class TranscriptionInitWsResponse(BaseModel):
+    success: bool = Field(..., description="Whether the initialization was successful")
+    error: Optional[str] = Field(default=None, description="The error message if the initialization was not successful")
+
+class TranscriptionAudioChunkWsRequest(BaseModel):
+    audio: str = Field(..., description="The audio (base64 encoded)")
+
+    @field_validator('audio', mode='before')
+    @classmethod
+    def validate_audio(cls, v: str | bytes) -> str:
+        """
+        encode audio to base64 string if needed
+        """
+        if isinstance(v, bytes):
+            return base64.b64encode(v).decode("utf-8")
+        elif isinstance(v, str):
+            return v # already base64 encoded
+        else:
+            raise ValueError(f"Invalid audio type: {type(v)}")
+
+class TranscriptionWsResponse(BaseModel):
+    transcription: str = Field(..., description="The transcription")
