658ace5f238cfbb759e06ff26d87a64959cdf045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-12-24 17:06:43 +0000

    Add package django-htmx-autocomplete/1.0.15/django_htmx_autocomplete-1.0.15-py3-none-any.whl

---
+++ packages/django-htmx-autocomplete/django_htmx_autocomplete-1.0.15-py3-none-any.whl/autocomplete/__init__.py
@@ -0,0 +1,8 @@
+"""
+Allow HTMXAutoComplete to be imported from the module directly
+"""
+
+from .core import Autocomplete, register
+from .shortcuts import ModelAutocomplete
+from .views import urls
+from .widgets import AutocompleteWidget
